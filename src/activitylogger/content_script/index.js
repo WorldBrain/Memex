6b0b2c865ce092ea4b5e99ac15830d18a278e106
Gerben <gerben@treora.com>
2017-01-17 01:27:25 +0100

    Use Mozilla's Readability for text exctraction

--- src/activitylogger/content_script/index.js
+++ src/activitylogger/content_script/index.js
@@ -1,10 +1,36 @@
 import { getMetadata, metadataRules } from 'page-metadata-parser'
+import Readability from 'readability'
+
+function extractPageText() {
+    let article
+    const loc = window.location
+    const uri = {
+        spec: loc.href,
+        host: loc.host,
+        prePath: loc.protocol + "//" + loc.host,
+        scheme: loc.protocol.substr(0, loc.protocol.indexOf(":")),
+        pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1)
+    }
+    try {
+        article = new Readability(
+            uri,
+            document.cloneNode(true),
+        ).parse()
+    }
+    catch (err) {
+        // Bummer.
+        console.error('Readability failed', err)
+    }
+    // Fall back to full body text if Readability failed us.
+    const text = article ? article.textContent : document.body.innerText
+    return text
+}
 
 // Get the basic info and text content (for search index) of this page.
 function gatherPageInfo() {
     // Extract info from all sorts of meta tags (og, twitter, etc.)
     const visitedUrl = window.location.href
-    const text = document.body.innerText
+    const text = extractPageText()
     const pageMetadata = getMetadata(document, visitedUrl, metadataRules)
     return {
         ...pageMetadata,
