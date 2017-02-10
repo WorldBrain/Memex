3ada9ca10a457a21f4b100b4c6739a5f70c591d7
Gerben <gerben@treora.com>
2017-02-10 21:51:25 +0100

    Support multiple events in event-to-promise

--- src/util/event-to-promise.js
+++ src/util/event-to-promise.js
@@ -1,46 +1,69 @@
 // Create a promise that resolves when resolve.event occurs, or rejects when
-// reject.event occurs.
+// reject.event occurs. Either argument may also be an array of multiple such
+// resolve (/reject) settings; whichever triggers first completes the promise.
+//
 // Arguments:
 // - resolve.event (/reject.event)
-// The event to listen to. Either can be omitted.
+//   The event to listen to.
 // - resolve.filter (/reject.filter)
-// Supply an event handler that returns either true or false if you wish to
-// decide whether to resolve(/reject) or ignore an event.
+//   Supply an event handler that returns either true or false if you wish to
+//   decide for each event whether to resolve(/reject) or ignore it.
 // - resolve.value (/reject.reason)
-// A function that provides the value to resolve (/reason to reject) with.
-// Will be called with the event handler's arguments.
+//   The value to resolve (/reason to reject) with; or a function that provides
+//   this value (which receives the event handler's arguments).
 export default function eventToPromise({
     resolve: resolveOpts,
     reject: rejectOpts,
 }) {
+    // We can get a single options object, or an array of them, or undefined.
+    if (typeof resolveOpts !== 'Array') {
+        resolveOpts = (resolveOpts !== undefined) ? [resolveOpts] : []
+    }
+    if (typeof rejectOpts !== 'Array') {
+        rejectOpts = (rejectOpts !== undefined) ? [rejectOpts] : []
+    }
+
     return new Promise(function (resolve, reject) {
-        function maybeResolve(...args) {
-            if (!resolveOpts.filter || resolveOpts.filter(...args)) {
-                removeListeners()
-                if (resolveOpts.value)
-                    return (typeof resolveOpts.value === 'function')
-                        ? resolve(resolveOpts.value(...args))
-                        : resolve(resolveOpts.value)
-                else
-                    resolve()
-            }
-        }
-        function maybeReject(...args) {
-            if (!rejectOpts.filter || rejectOpts.filter(...args)) {
-                removeListeners()
-                if (rejectOpts.reason)
-                    return (typeof rejectOpts.reason === 'function')
-                        ? reject(rejectOpts.reason(...args))
-                        : reject(rejectOpts.reason)
-                else
-                    reject()
-            }
-        }
+        const listeners = []
         function removeListeners() {
-            resolveOpts.event && resolveOpts.event.removeListener(maybeResolve)
-            rejectOpts.event && rejectOpts.event.removeListener(maybeReject)
+            listeners.forEach(listener => {
+                listener.event.removeListener(listener.listener)
+            })
         }
-        resolveOpts.event && resolveOpts.event.addListener(maybeResolve)
-        rejectOpts.event && rejectOpts.event.addListener(maybeReject)
+        resolveOpts.forEach(opts => {
+            listeners.push({
+                event: opts.event,
+                listener: function maybeResolve(...args) {
+                    if (!opts.filter || opts.filter(...args)) {
+                        removeListeners()
+                        if (opts.value)
+                            return (typeof opts.value === 'function')
+                                ? resolve(opts.value(...args))
+                                : resolve(opts.value)
+                        else
+                            resolve()
+                    }
+                }
+            })
+        })
+        rejectOpts.forEach(opts => {
+            listeners.push({
+                event: opts.event,
+                listener: function maybeReject(...args) {
+                    if (!opts.filter || opts.filter(...args)) {
+                        removeListeners()
+                        if (opts.reason)
+                            return (typeof opts.reason === 'function')
+                                ? reject(opts.reason(...args))
+                                : reject(opts.reason)
+                        else
+                            reject()
+                    }
+                }
+            })
+        })
+        listeners.forEach(listener => {
+            listener.event.addListener(listener.listener)
+        })
     })
 }
