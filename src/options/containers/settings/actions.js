5b2e7e36d7f9d4989907c3bd92103843185cb512
Aquib Master <aquibmaster@gmail.com>
2017-03-04 16:35:41 +1300

    First pass implementation of the blacklist functionality

---
+++ src/options/containers/settings/actions.js
@@ -0,0 +1,4 @@
+import { createAction } from 'redux-act'
+
+export const addNewBlacklistedSite = createAction('settings/addNewBlacklistedSite')
+export const deleteBlacklistedSite = createAction('settings/deleteBlacklistedSite')
