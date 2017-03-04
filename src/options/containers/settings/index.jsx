f2c769db6a388379b15eee0b9f6f5fb765edeb1c
Aquib Master <aquibmaster@gmail.com>
2017-03-04 16:35:41 +1300

    First pass implementation of the blacklist functionality

--- src/options/containers/settings/index.jsx
+++ src/options/containers/settings/index.jsx
@@ -1,17 +1,88 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-import { routeTitle } from '../../base.css'
+import * as actions from './actions'
+import Blacklist from '../../components/blacklist'
+
+import { routeTitle, sectionTitle } from '../../base.css'
+import styles from './style.css'
 
 class SettingsContainer extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isAddingEnabled: false
+        }
+
+        this.onAddClicked = this.onAddClicked.bind(this)
+        this.onCancelAdding = this.onCancelAdding.bind(this)
+        this.onNewBlacklistItemAdded = this.onNewBlacklistItemAdded.bind(this)
+        this.onDeleteClicked = this.onDeleteClicked.bind(this)
+    }
+
+    onAddClicked() {
+        this.setState({ isAddingEnabled: true })
+    }
+
+    onCancelAdding() {
+        this.setState({ isAddingEnabled: false })
+    }
+
+    onNewBlacklistItemAdded(value) {
+        // TODO(AM): Validation
+        const { actions } = this.props
+
+        actions.addNewBlacklistedSite({
+            expression: value,
+            dateAdded: new Date()
+        })
+    }
+
+    onDeleteClicked(itemIndex) {
+        // TODO(AM): Confirmation or undo
+        const { actions } = this.props
+
+        actions.deleteBlacklistedSite({
+            index: itemIndex
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1 className={routeTitle}>Settings</h1>
-                <p>Settings specific components to come in here.</p>
+                
+                <section className={styles.section}>
+                    <h2 className={sectionTitle}>Ignored Sites</h2>
+
+                    <Blacklist blacklist={this.props.blacklist}
+                               isAddingEnabled={this.state.isAddingEnabled}
+                               onNewBlacklistItemAdded={this.onNewBlacklistItemAdded}
+                               onAddClicked={this.onAddClicked}
+                               onCancelAdding={this.onCancelAdding}
+                               onDeleteClicked={this.onDeleteClicked} />
+                </section>
             </div>
         )
     }
 }
 
-export default SettingsContainer
+SettingsContainer.propTypes = {
+    blacklist: PropTypes.array.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        blacklist: state.settings.blacklist
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SettingsContainer)
--- src/options/main.jsx
+++ src/options/main.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, IndexRedirect, hashHistory } from 'react-router'
+import { Provider } from 'react-redux'
 
+import configureStore from './store'
 import Layout from './layout'
 import Routes from './routes'
 
-// Render the UI to the screen
+// Include development tools if we are not building for production
+let ReduxDevTools = undefined
+if (process.env.NODE_ENV !== 'production') {
+    ReduxDevTools = require('../dev/redux-devtools').default
+}
+
+const store = configureStore({ReduxDevTools})
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Layout}>
-            <IndexRedirect to="/settings" />
-            { Routes.map(route =>
-                <Route
-                    key={route.pathname}
-                    path={route.pathname}
-                    component={route.component}
-                />
-            )}
-        </Route>
-    </Router>,
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route path="/" component={Layout}>
+                <IndexRedirect to="/settings" />
+                { Routes.map(route =>
+                    <Route
+                        key={route.pathname}
+                        path={route.pathname}
+                        component={route.component}
+                    />
+                )}
+            </Route>
+        </Router>
+    </Provider>,
     document.getElementById('app')
 )
