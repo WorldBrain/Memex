4972739b418eeb78d2028ac43e51a8050a7d188a
Oliver Sauter <oli@worldbrain.io>
2017-04-02 12:25:14 +0200

    file path from current master

--- src/overview/components/VisitAsListItem.js
+++ src/overview/components/VisitAsListItem.js
@@ -3,7 +3,7 @@ import classNames from 'classnames'
 import niceTime from '../../util/nice-time'
 import styles from './VisitAsListItem.css'
 
-import {localVersionAvailable, LinkToLocalVersion } from '../../page-viewer'
+import {localVersionAvailable, LinkToLocalVersion } from 'src/page-viewer'
 
 const VisitAsListItem = ({doc, compact}) => {
     const visitClasses = classNames({
