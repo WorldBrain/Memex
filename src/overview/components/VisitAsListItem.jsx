fe1e19d79c79931b8e314925dae6eb474f32bee9
Gerben <gerben@treora.com>
2017-04-05 18:47:14 +0200

    Reorder&rename css classes.

--- src/overview/components/VisitAsListItem.jsx
+++ src/overview/components/VisitAsListItem.jsx
@@ -20,18 +20,18 @@ const VisitAsListItem = ({doc, compact}) => {
             // DEBUG Show document props on ctrl+meta+click
             onClick={e => { if (e.metaKey && e.ctrlKey) { console.log(doc); e.preventDefault() } }}
         >
-            <span className={styles.logo} title={doc.page.title}>
+            <span className={styles.pageIconContainer} title={doc.page.title}>
                 {doc.page.favIcon
-                    ? <img className={styles.favicon} src={doc.page.favIcon} />
-                    : <img className={styles.favicon} src='img/null-icon.png' />
+                    ? <img src={doc.page.favIcon} />
+                    : <img src='img/null-icon.png' />
                 }
             </span>
-            <div className={styles.resultsContent}>
+            <div className={styles.descriptionContainer}>
                 <div className={styles.title}><strong>{doc.page.title}</strong></div>
                 <div className={styles.url}>{doc.page.url}</div>
                 <div className={styles.time}>{niceTime(doc.visitStart)}</div>
             </div>
-            <div className={styles.iconsContainer}>
+            <div className={styles.buttonsContainer}>
                 {localVersionAvailable({page: doc.page})
                     ? (
                         <LinkToLocalVersion page={doc.page}>
