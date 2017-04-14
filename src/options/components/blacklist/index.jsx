d84fb53bed752e8249a5e5daa5ce6e5a50d5c4d7
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-14 18:27:59 +0700

    Remove fn braces from view comps
    - refactoring commit
    - now they're passed all needed controller logic and just dumb views, so might as well remove the explicit returns and braces

--- src/options/components/blacklist/index.jsx
+++ src/options/components/blacklist/index.jsx
@@ -7,41 +7,39 @@ import styles from './style.css'
 const Blacklist = ({
     blacklist, siteInputValue, isAddingEnabled, onNewBlacklistItemAdded,
     onAddClicked, onDeleteClicked, onInputChange, handleInputKeyPress,
-}) => {
-    return (
-        <div>
-            <div className={styles.toolbar}>
-                <button onClick={onAddClicked} className={styles.addButton}>Add</button>
-            </div>
-
-            <div className={styles.tableContainer}>
-                <table className={styles.table}>
-                    <thead>
-                        <tr>
-                            <th className={styles.domainCell}>Domain / Expression</th>
-                            <th>Added</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <BlacklistNewSite isEnabled={isAddingEnabled}
-                                        onAdd={onNewBlacklistItemAdded}
-                                        value={siteInputValue}
-                                        handleKeyPress={handleInputKeyPress}
-                                        onInputChange={onInputChange} />
-
-                        { blacklist.map((item, idx) => (
-                            <BlacklistRow blacklistItem={item}
-                                        key={idx}
-                                        itemId={idx}
-                                        onDeleteClicked={onDeleteClicked} />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+}) => (
+    <div>
+        <div className={styles.toolbar}>
+            <button onClick={onAddClicked} className={styles.addButton}>Add</button>
         </div>
-    );
-}
+
+        <div className={styles.tableContainer}>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th className={styles.domainCell}>Domain / Expression</th>
+                        <th>Added</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <BlacklistNewSite isEnabled={isAddingEnabled}
+                                    onAdd={onNewBlacklistItemAdded}
+                                    value={siteInputValue}
+                                    handleKeyPress={handleInputKeyPress}
+                                    onInputChange={onInputChange} />
+
+                    { blacklist.map((item, idx) => (
+                        <BlacklistRow blacklistItem={item}
+                                    key={idx}
+                                    itemId={idx}
+                                    onDeleteClicked={onDeleteClicked} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </div>
+)
 
 Blacklist.propTypes = {
     blacklist: PropTypes.array.isRequired,
@@ -51,7 +49,7 @@ Blacklist.propTypes = {
     handleInputKeyPress: PropTypes.func.isRequired,
     onNewBlacklistItemAdded: PropTypes.func.isRequired,
     onAddClicked: PropTypes.func.isRequired,
-    onDeleteClicked: PropTypes.func.isRequired
+    onDeleteClicked: PropTypes.func.isRequired,
 }
 
 export default Blacklist
