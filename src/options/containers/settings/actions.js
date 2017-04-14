e38a883bf26c8b9803f7fc821b11b86453d7ed06
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-14 18:02:27 +0700

    Add input val state resetting logic
    - another common action with the input state
    - now explicitly called as part of the onNewBlacklistItemAdded handler method, but also could be triggered by addSiteToBlacklist action

--- src/options/containers/settings/actions.js
+++ src/options/containers/settings/actions.js
@@ -1,6 +1,7 @@
 import { createAction } from 'redux-act'
 
 export const setSiteInputValue = createAction('settings/setSiteInputValue')
+export const resetSiteInputValue = createAction('settings/resetSiteInputValue')
 export const setBlacklist = createAction('settings/setBlacklist')
 export const addSiteToBlacklist = createAction('settings/addSiteToBlacklist')
 export const removeSiteFromBlacklist = createAction('settings/removeSiteFromBlacklist')
