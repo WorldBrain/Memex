612f59c8f1ba5cb19d79b380bfefd918e0941b0d
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-14 18:27:59 +0700

    Remove fn braces from view comps
    - refactoring commit
    - now they're passed all needed controller logic and just dumb views, so might as well remove the explicit returns and braces

--- src/options/components/blacklistNewSite/index.jsx
+++ src/options/components/blacklistNewSite/index.jsx
@@ -2,31 +2,29 @@ import React, { PropTypes } from 'react'
 
 import styles from './style.css'
 
-const BlacklistNewSite = ({ isEnabled, value, onAdd, handleKeyPress, onInputChange }) => {
-    return (
-        <tr>
-            { isEnabled && (
-                <td colSpan={3} className={styles.cell}>
-                    <div className={styles.newSite}>
+const BlacklistNewSite = ({ isEnabled, value, onAdd, handleKeyPress, onInputChange }) => (
+    <tr>
+        { isEnabled && (
+            <td colSpan={3} className={styles.cell}>
+                <div className={styles.newSite}>
 
-                        <input ref={input => input && input.focus()}
-                               value={value}
-                               className={styles.input}
-                               type="text"
-                               placeholder="Enter any text, domain or regular expression"
-                               onChange={onInputChange}
-                               onKeyUp={handleKeyPress} />
+                    <input ref={input => input && input.focus()}
+                            value={value}
+                            className={styles.input}
+                            type="text"
+                            placeholder="Enter any text, domain or regular expression"
+                            onChange={onInputChange}
+                            onKeyUp={handleKeyPress} />
 
-                        <button onClick={onAdd} className={styles.saveButton}>
-                            <span className="material-icons">save</span>
-                        </button>
+                    <button onClick={onAdd} className={styles.saveButton}>
+                        <span className="material-icons">save</span>
+                    </button>
 
-                    </div>
-                </td>
-            )}
-        </tr>
-    )
-}
+                </div>
+            </td>
+        )}
+    </tr>
+)
 
 BlacklistNewSite.propTypes = {
     isEnabled: PropTypes.bool.isRequired,
