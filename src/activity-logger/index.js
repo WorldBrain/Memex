d6e1240073957f59eb993844429f115b669c4009
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-19 12:53:37 +0700

    Main checking of URL against blacklist logic
    - for a given URL the fn will load the blacklist from local storage and check if any matches are found
    - the main matching logic is done in the `doesExpressionMatchURL` fn, which is applied to each blacklist item in `blacklistMatchesExist`
    - now the main logic consists of a `String.prototype.includes()` call; obviously this is very naiive, so next step is to improve this

--- src/activity-logger/index.js
+++ src/activity-logger/index.js
@@ -4,6 +4,7 @@ import docuri from 'docuri'
 import randomString from 'src/util/random-string'
 
 export const visitKeyPrefix = 'visit/'
+export const blacklistStorageKey = 'blacklist'
 
 // Creates an _id string given the variables, or vice versa parses such strings
 // We simply use the creation time for the id, for easy chronological sorting.
@@ -20,6 +21,33 @@ export function generateVisitDocId({timestamp, nonce} = {}) {
         timestamp: date.getTime(),
         nonce: nonce || randomString(),
     })
+}
+
+/**
+ * Given a URL, checks the URL against the user's stored blacklist expressions to see if any
+ * rule matches it.
+ *
+ * @param {string} url The URL to check against the blacklist.
+ * @return {boolean} Denotes whether or not the given URL matches any blacklist expressions.
+ */
+async function isURLBlacklisted(url = '') {
+    // Main checking logic between a given blacklist expression and current URL
+    const doesExpressionMatchURL = (expression = '') => url.includes(expression)
+
+    // Reduces blacklist to a bool by running main checking logic against each blacklist expression
+    // (returns true if a single match is found in entire blacklist)
+    const blacklistMatchesExist = (blacklist = []) => blacklist.reduce(
+        (prev, curr) => doesExpressionMatchURL(curr.expression) || prev, false)
+
+    // Do the check against blacklist stored in local storage
+    const { blacklist } = await browser.storage.local.get(blacklistStorageKey)
+    if (!blacklist) {
+        return false // If blacklist isn't found in storage, then URL can't be considered blacklisted
+    }
+
+    // Sync parse of serialized blacklist data for programmatic use
+    const blacklistArr = JSON.parse(blacklist)
+    return blacklistMatchesExist(blacklistArr)
 }
 
 // Decide whether to remember or ignore a visited page.
