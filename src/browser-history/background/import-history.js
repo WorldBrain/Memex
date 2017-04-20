4865b47034dcfefe227933605da78e7bd7488312
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-20 10:40:01 +0700

    Move asyncFilter to 'async-wrappers' util module
    - refactor commit
    - didn't belong in import-history module; could be used in other places later

--- src/browser-history/background/import-history.js
+++ src/browser-history/background/import-history.js
@@ -6,27 +6,7 @@ import db from 'src/pouchdb'
 import { isWorthRemembering, generateVisitDocId,
          visitKeyPrefix, convertVisitDocId } from 'src/activity-logger'
 import { generatePageDocId } from 'src/page-storage'
-
-
-/**
- * Wrapper around the sync `Array.prototype.filter` to allow using async logic
- * as the filter predicate.
- * @param {Array<any>} arr The data to filter.
- * @param {(any) => boolean} pred The async filter predicate that returns a bool/promise that resolves to a bool.
- * @returns {Array<any>} A filtered array containing data from the original array.
- */
-async function asyncFilter(arr, pred) {
-    // Make a copy of the array contents; could theoretically change while resolving the async logic
-    const arrCopy = Array.from(arr)
-
-    // Runs all async logic (the predicate) against the array,
-    //   leaving us with an array of bools that maps to the original data
-    const results = await Promise.all(arrCopy.map(el => pred(el)))
-
-    // Now that async logic is resolved, do a sync filter on the original data
-    //   using the results at each array index
-    return arrCopy.filter((el, i) => results[i])
-}
+import { asyncFilter } from 'src/util/async-wrappers'
 
 // Get the historyItems (visited places/pages; not each visit to them)
 async function getHistoryItems({
