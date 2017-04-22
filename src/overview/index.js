4e0070e5e3a6c8e19881e038b63bbbda86bbc20d
Gerben <gerben@treora.com>
2017-04-23 00:39:24 +0200

    Expose search query in window location query.
    The UI logic should not have to bother about which pieces of state are
    exposed in the location query (in my opinion). Therefore, this commit
    introduces a listener that bidirectionally syncs variables between the
    state and the window location.

--- src/overview/index.js
+++ src/overview/index.js
@@ -1,6 +1,7 @@
 import reducer from './reducer'
 import * as actions from './actions'
 import * as epics from './epics'
+import enhancer from './enhancer'
 import Overview from './components/Overview'
 
-export default {reducer, actions, epics, components: { Overview }}
+export default {reducer, actions, epics, enhancer, components: { Overview }}
