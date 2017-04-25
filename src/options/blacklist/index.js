49c5d83b15b6d5f8231be1c7ea0d681688cf739e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-25 09:27:10 +0700

    Move blacklist container outside of index entrypoint
    - put into container.jsx
    - reverted index.jsx extension to simply JS (no need for JSX code; it's just an entrypoint now)

--- src/options/blacklist/index.js
+++ src/options/blacklist/index.js
@@ -1,6 +1,5 @@
 import * as actions from './actions'
-import * as constants from './constants'
 export { default as reducer } from './reducers'
-export { default as enhancer } from './enhancer'
 export { default } from './container'
-export { actions, constants }
+export { actions }
+
