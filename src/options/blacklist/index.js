7cecac2e6651d05c02f3aad7bc2c30588359306c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-25 09:59:45 +0700

    Add constants file to blacklist module
    - can contain any feature module-specific constants (now it contains the local storage key 'blacklist')
    - reexported from index.js entrypoint
    - refactor persistence module logic to use the constant

--- src/options/blacklist/index.js
+++ src/options/blacklist/index.js
@@ -1,5 +1,6 @@
 import * as actions from './actions'
+import * as constants from './constants'
 export { default as reducer } from './reducers'
 export { default as configurePersistence } from './persistence'
 export { default } from './container'
-export { actions }
+export { actions, constants }
