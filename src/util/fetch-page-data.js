b1519e940fa8fe1e8e382e5a8e22f4717fb5af49
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-25 12:47:15 +0700

    Initial fetch-page-data implementation
    Add skeleton fetch-page-data stuff
    
    - currently contains a working DOM/Document async fetching function which uses the old XMLHttpRequest API
    - ideas in the JSDoc; to be expanded on
    - just a building block of the fetchAndAnalyse function
    
    Write Location-interface from URL logic
    
    - this one is weird; better way is through the URL API but it's experimental
    - discus more later
    
    Replace weird dummy anchor el with experimental URL API
    
    - this is gonna be called for large inputs, hence we don't want to keep adding dummy els to the DOM
    - URL is supported in Chrome and FF; may change API however, but seems safe so far
    
    Add main logic to fetchPageData fn
    
    - now it seems to work (without error handling!)
    
    Update fetchPageData for page data interface changes
    
    - upstream changed this after merging in PDF stuff; now extract-page-content is the entrypoint for all page metadata, text + PDF text/metadata logic

---
+++ src/util/fetch-page-data.js
@@ -0,0 +1,49 @@
+import extractPageContent from 'src/page-analysis/content_script/extract-page-content'
+
+/**
+ * Given a URL will attempt an async fetch of the text and metadata from the page
+ * which the URL points to.
+ *
+ * @param {string} url The URL which points to the page to fetch text + meta-data for.
+ * @return {any} Object containing `text` and `metadata` objects containing given data
+ *  for the found page pointed at by the URL.
+ */
+export default async function fetchPageData({
+    url = '',
+} = {}) {
+    const doc = await fetchDOMFromUrl(url)
+    const loc = getLocationFromURL(url)
+
+    const { text, metadata } = await extractPageContent({ doc, loc, url })
+    return { text, metadata }
+}
+
+/**
+ * Given a URL string, converts it to a Location-interface compatible object.
+ * Uses experimental URL API: https://developer.mozilla.org/en/docs/Web/API/URL
+ *
+ * @param {string} url The URL to get Location-interface object for.
+ * @return {Location} A Location-interface compatible object.
+ */
+const getLocationFromURL = url => new URL(url)
+
+/**
+ * Async function that given a URL will attempt to grab the current DOM which it points to.
+ * Uses native XMLHttpRequest API, as newer Fetch API doesn't seem to support fetching of
+ * the DOM; the Response object must be parsed.
+ *
+ * @param {string} url The URL to fetch the DOM for.
+ * @return {Document} The DOM which the URL points to.
+ */
+const fetchDOMFromUrl = async url => new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest()
+
+    req.onload = () => resolve(req.responseXML)
+    req.onerror = () => reject(req.statusText)
+
+    req.open('GET', url)
+
+    // Sets the responseXML to be of Document/DOM type
+    req.responseType = 'document'
+    req.send()
+})
