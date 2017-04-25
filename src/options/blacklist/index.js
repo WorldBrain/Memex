a83c3ba8422392f8add2f37c271fa9182ee63011
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-04-25 09:50:21 +0700

    Move configurePersistence code to blacklist module
    - exported from entrypoint as named export 'configurePersistence'
    - each module can add an optional 'persistence.js' file containing the redux store->local storage/pouchDB persistence logic

--- src/options/blacklist/index.js
+++ src/options/blacklist/index.js
@@ -1,5 +1,5 @@
 import * as actions from './actions'
 export { default as reducer } from './reducers'
+export { default as configurePersistence } from './persistence'
 export { default } from './container'
 export { actions }
-
