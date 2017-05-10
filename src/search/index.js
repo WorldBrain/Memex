63750d06026a6aabe99da2d15616a30f80c890a9
Urban Cvek <urban@cvek.me>
2017-05-10 12:51:07 +0200

    Set new time filters before waiting for search to start.

--- src/search/index.js
+++ src/search/index.js
@@ -1,6 +1,5 @@
 import { findVisits, addVisitsContext } from './find-visits'
 import { searchPages } from './find-pages'
-import extractTimeFiltersFromQuery from './../util/nlp-time-filter'
 
 // Search by keyword query, returning all docs if no query is given
 export async function filterVisitsByQuery({
@@ -13,10 +12,8 @@ export async function filterVisitsByQuery({
     if (query === '') {
         return await findVisits({startDate, endDate, limit})
     } else {
-        const { startDate, endDate, extractedQuery } = extractTimeFiltersFromQuery(query)
-
         const pagesResult = await searchPages({
-            extractedQuery,
+            query,
             limit,
             followRedirects: true,
         })
