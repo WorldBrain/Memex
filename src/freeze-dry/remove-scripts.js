0bf60bf7f92bfb8ccb56f9ef015dad11bc0aab5e
Gerben <gerben@treora.com>
2017-05-11 19:41:56 -0700

    Remove all on* attributes in freeze-dry.

--- src/freeze-dry/remove-scripts.js
+++ src/freeze-dry/remove-scripts.js
@@ -3,8 +3,18 @@ function removeNode(node) {
 }
 
 export default async function removeScripts({rootElement}) {
+    // Remove all <script> elements.
     const scripts = Array.from(rootElement.querySelectorAll('script'))
     scripts.forEach(removeNode)
 
-    // TODO Remove event handlers
+    // Remove event handlers (onclick, onload, etcetera).
+    const elements = Array.from(rootElement.querySelectorAll('*'))
+    elements.forEach(element => {
+        // A crude approach: any attribute starting with 'on' is removed.
+        Array.from(element.attributes)
+            .filter(attribute => attribute.name.startsWith('on'))
+            .forEach(attribute => {
+                element.removeAttribute(attribute.name)
+            })
+    })
 }
