WorldBrain/Memex

commit 2bb4f6ad4f868ca6d89740b975ab0d0f8e4b40be

Store images as attachments & load them asyncly.
Rendering the image is the tricky part. We want to load it asynchronously,
which is the main point of using attachments. However, we cannot just give
the images their own URL and put that in the img src attribute, as we
cannot host files, and for a data/blob URI we have to have the attachment
loaded already.

(sidenote: in Firefox we could work around the hosting problem: we could
use made-up URLs, block requests to those URLs while returning a promise
that resolves with a {redirectUrl} containing the blob/data URI. But then
again, redirection to blob/data URIs is said not to work in e10s..)

The solution chosen here is to make a React component that fetches the
attachment, stores the data URI in its state, and passes it to its child
when it is available.
Using component state here, rather than Redux state, to hide this
irrelevant workaround from the rest of the application.

Possible improvements: generalise to not be specific to <img>, make it a
higher order component, perhaps using async-update-props or similar.

src/overview/components/VisitAsListItem.jsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 
 import {localVersionAvailable, LinkToLocalVersion} from 'src/page-viewer'
 import niceTime from 'src/util/nice-time'
+import ImgFromPouch from './ImgFromPouch'
 import styles from './VisitAsListItem.css'
 
 
@@ -11,12 +12,15 @@ const VisitAsListItem = ({doc, compact}) => {
         [styles.root]: true,
         [styles.compact]: compact,
     })
-    const favIcon = (
-        <img
-            className={styles.favIcon}
-            src={doc.page.favIcon || 'img/null-icon.png'}
-        />
-    )
+    const favIcon = doc.page._attachments && doc.page._attachments.favIcon
+        ? (
+            <ImgFromPouch
+                className={styles.favIcon}
+                doc={doc.page}
+                attachmentId='favIcon'
+            />
+        )
+        : <img className={styles.favIcon} src='img/null-icon.png' />
     return (
         <a
             className={visitClasses}
@@ -25,8 +29,14 @@ const VisitAsListItem = ({doc, compact}) => {
             onClick={e => { if (e.metaKey && e.ctrlKey) { console.log(doc); e.preventDefault() } }}
         >
             <div className={styles.screenshotContainer}>
-                {doc.page.screenshot
-                    ? <img className={styles.screenshot} src={doc.page.screenshot} />
+                {doc.page._attachments && doc.page._attachments.screenshot
+                    ? (
+                        <ImgFromPouch
+                            className={styles.screenshot}
+                            doc={doc.page}
+                            attachmentId='screenshot'
+                        />
+                    )
                     : favIcon
                 }
             </div>

src/overview/components/ImgFromPouch.jsx
@@ -1,0 +1,64 @@
+import get from 'lodash/fp/get'
+import omit from 'lodash/fp/omit'
+import { blobToBase64String } from 'blob-util'
+import React, { PropTypes } from 'react'
+
+import db from 'src/pouchdb'
+
+
+const readHash = ({doc, attachmentId}) =>
+    doc && attachmentId && get(['_attachments', attachmentId, 'digest'])(doc)
+
+async function getAttachment({doc, attachmentId}) {
+    if (!doc
+        || !attachmentId
+        || readHash({doc, attachmentId}) === undefined
+    ) {
+        return undefined
+    }
+    const blob = await db.getAttachment(doc._id, attachmentId)
+    const base64 = await blobToBase64String(blob)
+    const dataUri = `data:${blob.type};base64,${base64}`
+    return dataUri
+}
+
+export default class ImgFromPouch extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataUri: undefined,
+        }
+    }
+
+    componentWillMount() {
+        // Fetch the attachment. Note we will not have it on our first mount.
+        this.updateFile(this.props)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // If the attachment has changed, rerun the update
+        if (readHash(nextProps) !== readHash(this.props)) {
+            this.updateFile(nextProps)
+        }
+    }
+
+    async updateFile({doc, attachmentId}) {
+        const dataUri = await getAttachment({doc, attachmentId})
+        this.setState({dataUri})
+    }
+
+    render() {
+        const childProps = omit(Object.keys(this.constructor.propTypes))(this.props)
+        return (
+            <img
+                {...childProps}
+                src={this.state.dataUri}
+            />
+        )
+    }
+}
+
+ImgFromPouch.propTypes = {
+    doc: PropTypes.object,
+    attachmentId: PropTypes.string,
+}

src/page-analysis/background/index.js
@@ -1,4 +1,5 @@
 import assocPath from 'lodash/fp/assocPath'
+import { dataURLToBlob } from 'blob-util'
 
 import { whenPageDOMLoaded } from 'src/util/tab-events'
 import { remoteFunction } from 'src/util/webextensionRPC'
@@ -19,15 +20,26 @@ async function performPageAnalysis({pageId, tabId}) {
     const setDocField = (db, docId, key) =>
         value => db.upsert(docId, doc => assocPath(key, value)(doc))
 
+    // A shorthand for adding an attachment to a doc.
+    const setDocAttachment = (db, docId, attachmentId) => blob =>
+        db.upsert(docId, doc =>
+            assocPath(
+                ['_attachments', attachmentId],
+                {content_type: blob.type, data: blob}
+            )(doc)
+        )
+
     // Get and store the fav-icon
-    const storeFavIcon = getFavIcon({tabId}).then(
-        setDocField(db, pageId, 'favIcon')
-    )
+    const storeFavIcon = getFavIcon({tabId}).then(async dataUri => {
+        const blob = await dataURLToBlob(dataUri)
+        await setDocAttachment(db, pageId, 'favIcon')(blob)
+    })
 
     // Capture a screenshot.
-    const storeScreenshot = makeScreenshot({tabId}).then(
-        setDocField(db, pageId, 'screenshot')
-    )
+    const storeScreenshot = makeScreenshot({tabId}).then(async dataUri => {
+        const blob = await dataURLToBlob(dataUri)
+        await setDocAttachment(db, pageId, 'screenshot')(blob)
+    })
 
     // Extract the text and metadata
     const storePageContent = extractPageContent().then(

commit a814d846efe22648062a1d18732ca31a36781455

Fix favicon besides title.
Was broken in 2bb4f6a (store images as attachments).

src/overview/components/VisitAsListItem.jsx
@@ -12,7 +12,8 @@ const VisitAsListItem = ({doc, compact}) => {
         [styles.root]: true,
         [styles.compact]: compact,
     })
-    const favIcon = doc.page._attachments && doc.page._attachments.favIcon
+    const hasFavIcon = !!(doc.page._attachments && doc.page._attachments.favIcon)
+    const favIcon = hasFavIcon
         ? (
             <ImgFromPouch
                 className={styles.favIcon}
@@ -45,7 +46,7 @@ const VisitAsListItem = ({doc, compact}) => {
                     className={styles.title}
                     title={doc.page.title}
                 >
-                    {doc.page.favIcon && favIcon}
+                    {hasFavIcon && favIcon}
                     {doc.page.title}
                 </div>
                 <div className={styles.url}>