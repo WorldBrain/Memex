0d8778b86babd421d921b79d78ede0af43ebc0a0
Gerben <gerben@treora.com>
2017-05-21 16:07:12 -0700

    Change page data model.
    Store less unused data, flatten object structure, harmonise fields for
    pdf and html.

--- src/page-analysis/index.js
+++ src/page-analysis/index.js
@@ -2,22 +2,39 @@
 
 // The properties of a page that should be indexed for full-text search.
 export const searchableTextFields = [
+    'fullText',
     'title',
-    'extractedMetadata.title',
-    'extractedMetadata.Title',
-    'extractedMetadata.Author',
-    'extractedMetadata.Subject',
-    'extractedMetadata.Keywords',
-    'extractedText.bodyInnerText',
+    'author',
+    'description',
+    'keywords',
 ]
 
-// Revise and augment a page doc using available fields from the extracted data.
-// (done at retrieval rather than at storage time for the sake of flexibility)
+// Revise and augment a page doc, used for two possible reasons:
+//   1: Rename old fields to the current scheme, without needing data migration.
+//   2: Choose the best fields from the available data (doing this at retrieval
+//      rather than at storage time gives flexibility).
 export const revisePageFields = doc => ({
     ...doc,
-    // Choose something presentable as a title.
-    title: (doc.extractedMetadata && doc.extractedMetadata.title)
-        || (doc.extractedMetadata && doc.extractedMetadata.Title)
-        || doc.title
-        || doc.url,
+    fullText: (
+        doc.fullText
+        || (doc.extractedText && doc.extractedText.bodyInnerText) // old format
+        || undefined
+    ),
+    title: (
+        doc.title
+        || (doc.extractedMetadata && doc.extractedMetadata.title) // old format
+        || (doc.extractedMetadata && doc.extractedMetadata.Title) // old format for PDFs
+        || undefined
+    ),
+    author: (
+        doc.author
+        || (doc.extractedMetadata && doc.extractedMetadata.author) // old format
+        || (doc.extractedMetadata && doc.extractedMetadata.Author) // old format for PDFs
+        || undefined
+    ),
+    keywords: (
+        doc.keywords
+        || (doc.extractedMetadata && doc.extractedMetadata.Keywords) // old format for PDFs
+        || undefined
+    ),
 })
