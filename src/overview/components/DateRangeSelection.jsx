209ce5f1cfc74c8ae73da5b5b40a16942ad6fcfd
Urban Cvek <urban@cvek.me>
2017-05-24 19:49:56 +0200

    Adds NLP for date pickers.

--- src/overview/components/DateRangeSelection.jsx
+++ src/overview/components/DateRangeSelection.jsx
@@ -1,46 +1,102 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import moment from 'moment'
+import chrono from 'chrono-node'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker-cssmodules.css'
 
 import styles from './DateRangeSelection.css'
 
 
-const DateRangeSelection = ({
-    startDate, endDate,
-    onStartDateChange, onEndDateChange,
-}) => (
-    <div className={styles.dateRangeSelection}>
-        <DatePicker
-            className={styles.datePicker}
-            dateFormat='DD-MM-YYYY'
-            placeholderText='after..'
-            isClearable
-            selected={startDate && moment(startDate)}
-            selectsStart
-            startDate={moment(startDate || 0)}
-            endDate={moment(endDate)}
-            maxDate={moment()}
-            onChange={date => onStartDateChange(
-                date ? date.valueOf() : undefined,
-            )}
-        />
-        <DatePicker
-            className={styles.datePicker}
-            dateFormat='DD-MM-YYYY'
-            placeholderText='before..'
-            isClearable
-            selected={endDate && moment(endDate)}
-            selectsEnd
-            startDate={moment(startDate || 0)}
-            endDate={moment(endDate)}
-            maxDate={moment()}
-            onChange={date => onEndDateChange(
-                date ? date.endOf('day').valueOf() : undefined,
-            )}
-        />
-    </div>
-)
+class DateRangeSelection extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            startDateText: null,
+            endDateText: null,
+        }
+    }
+
+    render() {
+        const {
+            startDate, endDate,
+            onStartDateChange, onEndDateChange,
+        } = this.props
+
+        return (
+            <div className={styles.dateRangeSelection}>
+                <DatePicker
+                    className={styles.datePicker}
+                    dateFormat='DD-MM-YYYY'
+                    placeholderText='after..'
+                    isClearable
+                    selected={startDate && moment(startDate)}
+                    selectsStart
+                    startDate={moment(startDate || 0)}
+                    endDate={moment(endDate)}
+                    maxDate={moment()}
+                    onChange={date => {
+                        // Syncs the change of date to state
+                        this.setState({
+                            startDateText: date ? date.format('DD-MM-YYYY') : null,
+                        })
+
+                        onStartDateChange(
+                            date ? date.valueOf() : undefined,
+                        )
+                    }}
+                    onChangeRaw={e => {
+                        this.setState({ startDateText: e.target.value })
+                    }}
+                    onBlur={() => {
+                        const date = moment(this.state.startDateText, 'DD-MM-YYYY')
+                        const nlpDate = chrono.parseDate(this.state.startDateText)
+
+                        const dateToChange = date.isValid()
+                            ? date.valueOf()
+                            : nlpDate && nlpDate.getTime()
+
+                        if (dateToChange !== startDate) onStartDateChange(dateToChange)
+                    }}
+                />
+                <DatePicker
+                    className={styles.datePicker}
+                    dateFormat='DD-MM-YYYY'
+                    placeholderText='before..'
+                    isClearable
+                    selected={endDate && moment(endDate)}
+                    selectsEnd
+                    startDate={moment(startDate || 0)}
+                    endDate={moment(endDate)}
+                    maxDate={moment()}
+                    onChange={date => {
+                        // Syncs the change of end date to state
+                        this.setState({
+                            endDateText: date ? date.format('DD-MM-YYYY') : null,
+                        })
+
+                        onEndDateChange(
+                            date ? date.valueOf() : undefined,
+                        )
+                    }}
+                    onChangeRaw={e => {
+                        this.setState({ endDateText: e.target.value })
+                    }}
+                    onBlur={() => {
+                        const date = moment(this.state.endDateText, 'DD-MM-YYYY')
+                        const nlpDate = chrono.parseDate(this.state.endDateText)
+
+                        const dateToChange = date.isValid()
+                            ? date.valueOf()
+                            : nlpDate && nlpDate.getTime()
+
+                        if (dateToChange !== endDate) onEndDateChange(dateToChange)
+                    }}
+                />
+            </div>
+        )
+    }
+}
 
 DateRangeSelection.propTypes = {
     startDate: PropTypes.number,
