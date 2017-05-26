0a77481753a0dabd485a6e85ee7f91c13b2be631
Urban Cvek <urban@cvek.me>
2017-05-26 03:15:41 +0200

    Enable strict parsing for moment.
    Previously moment would try everything just to parse something out of the string. This would lead to "10 days ago" being parsed into 10th of the current month.

--- src/overview/components/DateRangeSelection.jsx
+++ src/overview/components/DateRangeSelection.jsx
@@ -34,7 +34,7 @@ class DateRangeSelection extends Component {
 
     submitDateChange(type) {
         if (type === 'startDate') {
-            const date = moment(this.state.startDateText, 'DD-MM-YYYY')
+            const date = moment(this.state.startDateText, 'DD-MM-YYYY', true)
             const nlpDate = chrono.parseDate(this.state.startDateText)
 
             const dateToChange = date.isValid()
@@ -43,7 +43,7 @@ class DateRangeSelection extends Component {
 
             if (dateToChange !== this.props.startDate) this.props.onStartDateChange(dateToChange)
         } else if (type === 'endDate') {
-            const date = moment(this.state.endDateText, 'DD-MM-YYYY')
+            const date = moment(this.state.endDateText, 'DD-MM-YYYY', true)
             const nlpDate = chrono.parseDate(this.state.endDateText)
 
             const dateToChange = date.isValid()
