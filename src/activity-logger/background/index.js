7fa73e36687e63155e55a0e5890d5b37a0615b6d
Urban Cvek <urban@cvek.me>
2017-05-30 12:58:09 +0200

    Adds debouncing based on tabs.

--- src/activity-logger/background/index.js
+++ src/activity-logger/background/index.js
@@ -1,8 +1,17 @@
 import maybeLogPageVisit from './log-page-visit'
+import _ from 'lodash'
+
+// Debounced function for each tab are stored here
+const tabs = {}
 
 // Listens for url changes of the page
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url && tab.url) {
-        maybeLogPageVisit({url: tab.url, tabId: tabId})
+        // Check if we already have a debounced function for this tab and cancel it
+        if (tabs[tabId]) tabs[tabId].cancel()
+
+        // Create debounced function and call it
+        tabs[tabId] = _.debounce(() => maybeLogPageVisit({url: tab.url, tabId: tabId}), 10000)
+        tabs[tabId]()
     }
 })
