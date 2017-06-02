0acb67cd2c6e74480719b6a3894f25d8ba966c7c
Gerben <gerben@treora.com>
2017-06-02 23:05:56 +0200

    Use iframe srcdoc to view pages.
    Less hacky than rewriting the document's innerHTML, and enables adding
    some 'custom browser chrome'.

--- src/page-viewer/localpage.js
+++ src/page-viewer/localpage.js
@@ -3,34 +3,49 @@ import { blobToArrayBuffer } from 'blob-util'
 import db from 'src/pouchdb'
 
 
+function fixChromiumInjectedStylesheet(document) {
+    // Pragmatic workaround for Chromium, which appears to inject two style
+    // rules into extension pages (with font-size: 75%, for some reason?).
+    const styleEl = document.createElement('style')
+    styleEl.innerHTML = `body {
+        font-size: inherit;
+        font-family: inherit;
+    }`
+    document.head.insertAdjacentElement('afterbegin', styleEl)
+}
+
 async function showPage(pageId) {
     // Read the html file from the database.
     const blob = await db.getAttachment(pageId, 'frozen-page.html')
+    // We assume utf-8 encoding. TODO: read encoding from document.
+    const html = new TextDecoder('utf-8').decode(await blobToArrayBuffer(blob))
 
-    // Replace the document contents.
-    try {
-        // We assume utf-8 encoding. TODO: read encoding from document.
-        const html = new TextDecoder('utf-8').decode(await blobToArrayBuffer(blob))
-        // Strip the <html>...</html> tags because we set innerHTML.
-        const innerHTML = html.match(/[^]*?<html[^]*?>([^]*)<\/html[^]*?>/i)[1]
-        document.documentElement.innerHTML = innerHTML
+    // Show the page in the iframe.
+    const iframe = document.getElementById('page')
+    iframe.srcdoc = html
+    // XXX The DOMContentLoaded event would be better, but how to listen to that?
+    iframe.onload = () => {
+        const doc = iframe.contentDocument
 
-        // Pragmatic workaround for Chromium, which appears to inject values for
-        // these two properties (with font-family: 75%, for some reason?)
-        if (document.head !== undefined) {
-            const styleEl = document.createElement('style')
-            styleEl.innerHTML = `body {
-                font-size: inherit;
-                font-family: inherit;
-            }`
-            document.head.insertAdjacentElement('afterbegin', styleEl)
+        // Ensure a head element exists.
+        if (!doc.head) {
+            const head = doc.createElement('head')
+            doc.documentElement.insertAdjacentElement('afterbegin', head)
         }
-    } catch (err) {
-        // Alternatively, use a blob URL (downside: the url is very temporary).
-        const blobUrl = URL.createObjectURL(blob)
-        window.location = blobUrl
+
+        // Make links open in the whole tab, not inside the iframe
+        const baseEl = doc.createElement('base')
+        baseEl.setAttribute('target', '_parent')
+        doc.head.insertAdjacentElement('afterbegin', baseEl)
+
+        // Workaround required for Chromium.
+        fixChromiumInjectedStylesheet(doc)
+
+        // Focus on the page so it receives e.g. keyboard input
+        iframe.contentWindow.focus()
     }
 }
 
+// Read pageId from location: ?page=pageId
 const pageId = new URL(window.location).searchParams.get('page')
 showPage(pageId)
