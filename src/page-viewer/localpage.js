a31a20283c2512ad3abf197d1bec43b8c039d766
Gerben <gerben@treora.com>
2017-06-05 19:26:51 +0200

    Add some text to page viewer bar.

--- src/page-viewer/localpage.js
+++ src/page-viewer/localpage.js
@@ -1,6 +1,10 @@
 import { blobToArrayBuffer } from 'blob-util'
 
 import db from 'src/pouchdb'
+import { getPage } from 'src/search/find-pages'
+import { getTimestamp } from 'src/activity-logger'
+import shortUrl from 'src/util/short-url'
+import niceTime from 'src/util/nice-time'
 
 
 function fixChromiumInjectedStylesheet(document) {
@@ -15,6 +19,15 @@ function fixChromiumInjectedStylesheet(document) {
 }
 
 async function showPage(pageId) {
+    const page = await getPage({pageId})
+    const timestamp = getTimestamp(page)
+
+    document.title = `💾 ${page.title}`
+    const bar = document.getElementById('bar')
+    const barContent = `<span>Snapshot of <a href="${page.url}">${shortUrl(page.url)}</a></span>`
+        + `<span><time datetime="${new Date(timestamp)}">⌚ ${niceTime(timestamp)}</time></span>`
+    bar.innerHTML = barContent
+
     // Read the html file from the database.
     const blob = await db.getAttachment(pageId, 'frozen-page.html')
     // We assume utf-8 encoding. TODO: read encoding from document.
