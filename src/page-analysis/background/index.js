WorldBrain/Memex

commit 5ab3fb881a6f7d1689a4ac8c344680307a248beb

Update instead of upsert.
So things simply fail if the page was deleted, rather than recreating it.
Might need more error catching on the deduplication side.
Might need upsert-style retrying for theoretically possible conflicts.

src/page-analysis/background/index.js
@@ -20,17 +20,21 @@ async function performPageAnalysis({pageId, tabId}) {
     const freezeDry = remoteFunction('freezeDry', {tabId})
 
     // A shorthand for updating a single field in a doc.
-    const setDocField = (db, docId, key) =>
-        value => db.upsert(docId, doc => assocPath(key, value)(doc))
+    const setDocField = (db, docId, key) => async value => {
+        const doc = await db.get(docId)
+        const updatedDoc = assocPath(key, value)(doc)
+        await db.put(updatedDoc)
+    }
 
     // A shorthand for adding an attachment to a doc.
-    const setDocAttachment = (db, docId, attachmentId) => blob =>
-        db.upsert(docId, doc =>
-            assocPath(
-                ['_attachments', attachmentId],
-                {content_type: blob.type, data: blob}
-            )(doc)
-        )
+    const setDocAttachment = (db, docId, attachmentId) => async blob => {
+        const doc = await db.get(docId)
+        const updatedDoc = assocPath(
+            ['_attachments', attachmentId],
+            {content_type: blob.type, data: blob}
+        )(doc)
+        await db.put(updatedDoc)
+    }
 
     // Get and store the fav-icon
     const storeFavIcon = getFavIcon({tabId}).then(async dataUri => {

src/page-storage/deduplication.js
@@ -19,22 +19,26 @@ function samenessLinkType({sameness}) {
     return types[sameness]
 }
 
-function forgetPageContents({page}) {
+async function forgetPageContents({page}) {
     // Remove analysed bulky stuff
-    return db.upsert(page._id, doc => ({
+    const doc = await db.get(page._id)
+    const updatedDoc = {
         ...doc,
         extractedText: undefined,
         extractedMetadata: undefined,
         screenshot: undefined,
         favIcon: undefined,
-    }))
+    }
+    await db.put(updatedDoc)
 }
 
 async function addLink({targetId, sourceId, linkType}) {
-    await db.upsert(sourceId, doc => ({
+    const doc = await db.get(sourceId)
+    const updatedDoc = {
         ...doc,
         [linkType]: {_id: targetId},
-    }))
+    }
+    await db.put(updatedDoc)
 }
 
 async function replaceWithRedirect({oldPage, newPage, sameness}) {

src/pouchdb.js
@@ -1,13 +1,11 @@
 import fromPairs from 'lodash/fp/fromPairs'
-
 import PouchDB from 'pouchdb-browser'
 import PouchDBQuickSearch from 'pouchdb-quick-search'
 import PouchDBFind from 'pouchdb-find'
-import PouchDBUpsert from 'pouchdb-upsert'
+
 
 PouchDB.plugin(PouchDBQuickSearch)
 PouchDB.plugin(PouchDBFind)
-PouchDB.plugin(PouchDBUpsert)
 
 const db = new PouchDB({
     name: 'webmemex',

commit 2135978c3be55545a254713bbc4f0f6b40de1885

Retry on conflicts when updating docs.
Problem introduced in 5ab3fb88; conflicts appear not just theoretical.

src/page-analysis/background/index.js
@@ -6,6 +6,7 @@ import { remoteFunction } from 'src/util/webextensionRPC'
 import whenAllSettled from 'src/util/when-all-settled'
 import delay from 'src/util/delay'
 import db from 'src/pouchdb'
+import updateDoc from 'src/util/pouchdb-update-doc'
 import { updatePageSearchIndex } from 'src/search/find-pages'
 
 import { revisePageFields } from '..'
@@ -21,19 +22,17 @@ async function performPageAnalysis({pageId, tabId}) {
 
     // A shorthand for updating a single field in a doc.
     const setDocField = (db, docId, key) => async value => {
-        const doc = await db.get(docId)
-        const updatedDoc = assocPath(key, value)(doc)
-        await db.put(updatedDoc)
+        await updateDoc(db, docId, doc => assocPath(key, value)(doc))
     }
 
     // A shorthand for adding an attachment to a doc.
     const setDocAttachment = (db, docId, attachmentId) => async blob => {
-        const doc = await db.get(docId)
-        const updatedDoc = assocPath(
-            ['_attachments', attachmentId],
-            {content_type: blob.type, data: blob}
-        )(doc)
-        await db.put(updatedDoc)
+        await updateDoc(db, docId,
+            doc => assocPath(
+                ['_attachments', attachmentId],
+                {content_type: blob.type, data: blob}
+            )(doc)
+        )
     }
 
     // Get and store the fav-icon

src/page-storage/deduplication.js
@@ -3,6 +3,7 @@
 import maxBy from 'lodash/fp/maxBy'
 
 import db from 'src/pouchdb'
+import updateDoc from 'src/util/pouchdb-update-doc'
 import { getTimestamp } from 'src/activity-logger'
 import determinePageSameness, { Sameness } from './sameness'
 
@@ -21,24 +22,20 @@ function samenessLinkType({sameness}) {
 
 async function forgetPageContents({page}) {
     // Remove analysed bulky stuff
-    const doc = await db.get(page._id)
-    const updatedDoc = {
+    await updateDoc(db, page._id, doc => ({
         ...doc,
         extractedText: undefined,
         extractedMetadata: undefined,
         screenshot: undefined,
         favIcon: undefined,
-    }
-    await db.put(updatedDoc)
+    }))
 }
 
 async function addLink({targetId, sourceId, linkType}) {
-    const doc = await db.get(sourceId)
-    const updatedDoc = {
+    await updateDoc(db, sourceId, doc => ({
         ...doc,
         [linkType]: {_id: targetId},
-    }
-    await db.put(updatedDoc)
+    }))
 }
 
 async function replaceWithRedirect({oldPage, newPage, sameness}) {

src/util/pouchdb-update-doc.js
@@ -1,0 +1,22 @@
+// Set a limit to not hang forever if something is more fundamentally broken.
+const MAX_ATTEMPTS = 9999
+
+export default async function updateDoc(db, docId, updateFunc) {
+    let conflict
+    let attemptCount = 0
+    do {
+        const doc = await db.get(docId)
+        const updatedDoc = await updateFunc(doc)
+        try {
+            await db.put(updatedDoc)
+            conflict = false
+        } catch (err) {
+            if (err.name === 'conflict' && ++attemptCount < MAX_ATTEMPTS) {
+                // Try again.
+                conflict = true
+            } else {
+                throw err
+            }
+        }
+    } while (conflict)
+}