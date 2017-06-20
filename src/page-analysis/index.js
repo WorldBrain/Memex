2e8128d98fa54d200b310215bbaa6d01828ad18d
Gerben <gerben@treora.com>
2017-06-20 11:34:21 +0200

    Remove page field revisions.
    No need to support a data model that was never published.

--- src/page-analysis/index.js
+++ src/page-analysis/index.js
@@ -13,28 +13,7 @@ export const searchableTextFields = [
 //   1: Rename old fields to the current scheme, without needing data migration.
 //   2: Choose the best fields from the available data (doing this at retrieval
 //      rather than at storage time gives flexibility).
-export const revisePageFields = doc => ({
-    ...doc,
-    fullText: (
-        doc.fullText
-        || (doc.extractedText && doc.extractedText.bodyInnerText) // old format
-        || undefined
-    ),
-    title: (
-        doc.title
-        || (doc.extractedMetadata && doc.extractedMetadata.title) // old format
-        || (doc.extractedMetadata && doc.extractedMetadata.Title) // old format for PDFs
-        || undefined
-    ),
-    author: (
-        doc.author
-        || (doc.extractedMetadata && doc.extractedMetadata.author) // old format
-        || (doc.extractedMetadata && doc.extractedMetadata.Author) // old format for PDFs
-        || undefined
-    ),
-    keywords: (
-        doc.keywords
-        || (doc.extractedMetadata && doc.extractedMetadata.Keywords) // old format for PDFs
-        || undefined
-    ),
-})
+export const revisePageFields = doc => {
+    // No revision needed. Keeping this function still for expected future use.
+    return doc
+}
