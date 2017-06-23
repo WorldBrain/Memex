dc36d693a8372923e075535091eaded71aecf67c
Gerben <gerben@treora.com>
2017-06-23 11:35:44 +0200

    Show size of stored page on hover.
    And hush the too crude eslint operator spacing rule; omitting spaces
    around e.g. ** operators helps understanding the formula.

--- src/page-viewer/index.jsx
+++ src/page-viewer/index.jsx
@@ -1,3 +1,4 @@
+import get from 'lodash/fp/get'
 import React from 'react'
 import PropTypes from 'prop-types'
 
@@ -5,15 +6,19 @@ export const localVersionAvailable = ({page}) => (
     !!(page._attachments && page._attachments['frozen-page.html'])
 )
 
-export const LinkToLocalVersion = ({page, children, ...props}) => (
-    <a
-        href={`/page-viewer/localpage.html?page=${page._id}`}
-        title='Stored version available'
-        {...props}
-    >
-        {children}
-    </a>
-)
+export const LinkToLocalVersion = ({page, children, ...props}) => {
+    const size = get(['_attachments', 'frozen-page.html', 'length'])(page)
+    const sizeInMB = Math.round(size / 1024**2 * 10) / 10
+    return (
+        <a
+            href={`/page-viewer/localpage.html?page=${page._id}`}
+            title={`Stored version available (${sizeInMB} MB)`}
+            {...props}
+        >
+            {children}
+        </a>
+    )
+}
 LinkToLocalVersion.propTypes = {
     page: PropTypes.object.isRequired,
     children: PropTypes.node,
