aa6e8fe748491fd99211077e43bb05fab136c254
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-06-28 17:11:25 +0700

    Write old->new ext page conversion logic
    - still need the entry point handler

--- src/util/old-data-converter.js
+++ src/util/old-data-converter.js
@@ -1,5 +1,7 @@
 import uniqBy from 'lodash/fp/uniqBy'
 
+import { generatePageDocId } from 'src/page-storage'
+
 /**
  * @typedef IBlacklistOldExt
  * @type {Object}
@@ -7,6 +9,30 @@ import uniqBy from 'lodash/fp/uniqBy'
  * @property {Array<String>} SITE An array of blocked sites (UNUSED).
  * @property {Array<String>} REGEX An array of strings to use to match sites.
  */
+
+/**
+ * @typedef IPageOldExt
+ * @type {Object}
+ * @property {String} text The extracted page text.
+ * @property {String} time The time the page data was stored.
+ * @property {String} title The page title.
+ * @property {String} url The URL pointing to the page.
+ */
+
+/**
+ * @param {IPageOldExt} oldPage
+ * @return {IPageDoc} The converted minimal page doc.
+ */
+const transformPageEntry = isStub => ({ text, time, title, url }) => ({
+    _id: generatePageDocId({ timestamp: time }),
+    title,
+    url,
+    isStub,
+    content: {
+        fullText: text,
+        title,
+    },
+})
 
 const transformBlacklistEntry = dateAdded => expression => ({ expression, dateAdded })
 
@@ -26,4 +52,14 @@ export function convertBlacklist(blacklist) {
     ])
 
     return JSON.stringify(blacklistArr) // Serialize it, as stored in new model
+}
+
+/**
+ * @param {Array<IPageOldExt>} oldPage Array of old model pages to convert.
+ * @param {boolean} [setAsStubs=false] Flag denoting whether or not to note the converted docs as stubs
+ *  (to schedule for imports).
+ * @return {Array<IPageDoc} Array of converted minimal page docs.
+ */
+export function convertPages(oldPages, setAsStubs = false) {
+    return oldPages.map(transformPageEntry(setAsStubs))
 }
