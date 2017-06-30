f31d2e4318cc58a847a06e30985f3b838fe6b14d
Gerben <gerben@treora.com>
2017-06-30 02:34:34 +0200

    Improve omnibox behaviour.
    - Lead to stored pages, not original URLs.
    - Lead to search page when not choosing a suggestion.
    - Hide markup in Firefox, it is not interpreted there.
    - Change keyword/letter from 'w' to 'm', for 'memory', or 'memex', seems
    more fitting. 'w' for 'web' or 'where was it?!' is nice too though.

--- src/omnibar.js
+++ src/omnibar.js
@@ -1,23 +1,34 @@
 import debounce from 'lodash/fp/debounce'
+import fromPairs from 'lodash/fp/fromPairs'
 import escapeHtml from 'lodash/fp/escape'
 
 import { filterVisitsByQuery } from 'src/search'
 import niceTime from 'src/util/nice-time'
-import shortUrl from 'src/util/short-url'
 
+// Read which browser we are running in.
+let browserName
+;(async () => {
+    browserName = await browser.runtime.getBrowserInfo()
+})()
 
 const visitToSuggestion = doc => {
     const visitDate = escapeHtml(niceTime(doc.visitStart))
-    const url = escapeHtml(shortUrl(doc.url))
+    const url = escapeHtml(doc.url)
     const title = escapeHtml(doc.page.title)
-    const description
-        = `<url>${url}</url> — ${title} <dim>(visited ${visitDate})</dim>`
+    const plainDescription = `⌚ ${visitDate}  —  ${title}  —  ${url}`
+    const markedUpDescription
+        = `⌚ <dim>${visitDate}</dim>    —    ${title}  —  <url>${url}</url>`
+    // Firefox interprets the description as plain text, so we distinguish here.
+    const description = (browserName === 'Firefox')
+        ? plainDescription
+        : markedUpDescription
     return ({
-        content: doc.url,
-        description: description.toString(),
+        content: plainDescription,
+        description,
     })
 }
 
+let suggestionToPageId = {}
 let currentQuery
 let latestResolvedQuery
 async function makeSuggestion(query, suggest) {
@@ -34,7 +45,7 @@ async function makeSuggestion(query, suggest) {
     }
 
     browser.omnibox.setDefaultSuggestion({
-        description: 'Searching your memory..',
+        description: 'Searching your memory.. (press enter to search deeper)',
     })
 
     const queryForOldSuggestions = latestResolvedQuery
@@ -48,30 +59,44 @@ async function makeSuggestion(query, suggest) {
 
     if (visitDocs.length === 0) {
         browser.omnibox.setDefaultSuggestion({
-            description: 'No results found in your memory.',
+            description: 'No results found in your memory. (press enter to search deeper)',
         })
     } else {
         browser.omnibox.setDefaultSuggestion({
-            description: 'Found these pages in your memory:',
+            description: `Found these ${visitDocs.length} pages in your memory: (press enter to search deeper)`,
         })
     }
     const suggestions = visitDocs.map(visitToSuggestion)
 
     suggest(suggestions)
+
+    suggestionToPageId = fromPairs(suggestions.map(
+        (suggestion, i) => [suggestion.content, visitDocs[i].page._id]
+    ))
     latestResolvedQuery = query
 }
 
 const acceptInput = (text, disposition) => {
-    // TODO if text is not a suggested URL, open the overview with this query.
+    // The user has clicked a suggestion, or pressed enter.
+    let url
+    if (text in suggestionToPageId) {
+        // Open the page chosen from the suggestions
+        const pageId = suggestionToPageId[text]
+        url = `/page-viewer/localpage.html?page=${encodeURIComponent(pageId)}`
+    } else {
+        // Treat input as search query, open the search
+        url = `/overview/overview.html?q=${encodeURIComponent(text)}`
+    }
+    // Open it in the place the browser requests us to.
     switch (disposition) {
         case 'currentTab':
-            browser.tabs.update({url: text})
+            browser.tabs.update({url})
             break
         case 'newForegroundTab':
-            browser.tabs.create({url: text})
+            browser.tabs.create({url})
             break
         case 'newBackgroundTab':
-            browser.tabs.create({url: text, active: false})
+            browser.tabs.create({url, active: false})
             break
     }
 }
