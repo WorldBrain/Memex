8aa6dabfc1d5a5ba6c9990e87640571c7163bf9e
Gerben <gerben@treora.com>
2017-07-03 15:37:24 +0200

    Add browser action context menu, tweak actions.

--- src/background.js
+++ src/background.js
@@ -1,4 +1,6 @@
-import { logActivePageVisit } from 'src/activity-logger/background/log-page-visit'
+import get from 'lodash/fp/get'
+
+import manifest from '../extension/manifest.json'
 
 
 function openOverview() {
@@ -7,16 +9,49 @@ function openOverview() {
     })
 }
 
-// Listen to keyboard shortcut commands declared in the manifest.
+// Functions to call for commands declared in the manifest.
+const commandActions = {
+    openOverview,
+}
+
+const commands = manifest.commands
+
+async function updateOrCreateContextMenuItem(id, options) {
+    try {
+        await browser.contextMenus.update(id, options)
+    } catch (err) {
+        await browser.contextMenus.create({id, ...options})
+    }
+}
+
+// Add all commands to the browser action button's context menu
+for (let commandId in commands) {
+    const options = commands[commandId]
+    // Ignore _execute_browser_action, etcetera.
+    if (commandId.startsWith('_')) continue
+
+    // XXX This always shows the default key, no OS-specific variants nor user's settings.
+    const shortcutKey = get('suggested_key.default')(options)
+    const shortcutInfo = shortcutKey ? ` (${shortcutKey})` : ''
+    const title = options.description + shortcutInfo
+    updateOrCreateContextMenuItem(commandId, {
+        title,
+        contexts: ['browser_action'],
+    })
+}
+
+// Listen to context menu actions.
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    const commandId = info.menuItemId
+    commandActions[commandId]()
+})
+
+// Listen to keyboard shortcut commands.
 browser.commands.onCommand.addListener(command => {
-    if (command === 'openOverview') {
-        openOverview()
-    }
-    if (command === 'storeActivePage') {
-        logActivePageVisit()
-    }
+    commandActions[command]()
 })
 
 // Run scripts that set their own event listeners.
+/* eslint-disable import/first */
 import 'src/activity-logger/background'
 import 'src/omnibar'
