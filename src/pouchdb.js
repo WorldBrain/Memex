13bdb0766a15d30fa1a93de1d34be3295cd68eaf
Gerben <gerben@treora.com>
2017-07-05 12:44:08 +0200

    Remove pouchdb test variant again.
    It still bundles a huge bulk of pouchdb-node dependencies into prod.
    We can fall back to mocking pouchdb in the test setup (TODO).
    We should perhaps also find a smarter bundler.

--- src/pouchdb.js
+++ src/pouchdb.js
@@ -10,23 +10,9 @@ const pouchdbOptions = {
     auto_compaction: true,
 }
 
-let db_
-if (process.env.NODE_ENV !== 'test') {
-    db_ = PouchDB(pouchdbOptions)
-} else {
-    // Export a non-persistent version of PouchDB for running tests.
-    const PouchDB = require('pouchdb-node')
-    const PouchDBMemory = require('pouchdb-adapter-memory')
-    PouchDB.plugin(PouchDBMemory)
-
-    db_ = PouchDB({
-        ...pouchdbOptions,
-        name: 'testdb',
-        adapter: 'memory',
-    })
-}
-const db = db_
+const db = PouchDB(pouchdbOptions)
 export default db
+
 // Expose db for debugging
 if (process.env.NODE_ENV !== 'production') {
     window.db = db
