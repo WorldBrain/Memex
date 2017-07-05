adaee463dbb7d51b0002f61dadd0f3f1ec5b2560
Jessica Leach <jessicamaryleach@gmail.com>
2017-07-05 11:19:15 +0200

    added src/popup.js to gulp build process

---
+++ src/popup.js
@@ -0,0 +1,11 @@
+import extractTimeFiltersFromQuery from 'src/util/nlp-time-filter.js'
+
+console.log('hello')
+
+console.log('hello')
+
+var search = document.getElementById('input')
+
+extractTimeFiltersFromQuery(search)
+
+console.log(search)
