429475cb99e4088aec5ac11c6b036101e5dbae39
Jessica Leach <jessicamaryleach@gmail.com>
2017-07-06 17:11:55 +0200

    testing extractTimeFiltersFromQuery()

--- src/popup.js
+++ src/popup.js
@@ -2,9 +2,6 @@ import extractTimeFiltersFromQuery from 'src/util/nlp-time-filter.js'
 
 console.log('hello')
 
-console.log(document.getElementById('search'))
-
 console.log(document.getElementById('search').value)
 
 extractTimeFiltersFromQuery(document.getElementById('search').value)
-
