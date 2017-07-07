5f712e8913b44b815d6a96461437224c7a336a4f
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-07-07 11:38:32 +0700

    Create tab in popup search event handler
    - cleaned up a little bit of code as well
    - decided just use query-string package to make the query params to avoid messy conditions

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,18 @@
+import qs from 'query-string'
+
 import extractTimeFiltersFromQuery from 'src/util/nlp-time-filter.js'
 
-console.log('hello')
+const overviewURL = 'overview/overview.html'
+const input = document.getElementById('search')
 
-console.log(document.getElementById('search').value)
+input.addEventListener('keydown', event => {
+    if (event.keyCode === 13) { // If 'Enter' pressed
+        event.preventDefault() // So the form doesn't submit
 
-extractTimeFiltersFromQuery(document.getElementById('search').value)
+        const { extractedQuery: query, startDate, endDate } = extractTimeFiltersFromQuery(input.value)
+        const queryParams = qs.stringify({ query, startDate, endDate })
+
+        browser.tabs.create({ url: `${overviewURL}?${queryParams}` }) // New tab with query
+        window.close() // Close the popup
+    }
+})
