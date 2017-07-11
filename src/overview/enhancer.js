0108c5d83f251e3aab12a0a5be613aac0e76a8ec
Gerben <gerben@treora.com>
2017-07-11 19:05:20 +0200

    Expose endDate in location query.

--- src/overview/enhancer.js
+++ src/overview/enhancer.js
@@ -1,8 +1,12 @@
 import { compose } from 'redux'
 import ReduxQuerySync from 'redux-query-sync'
+import moment from 'moment'
 
 import { ourState } from './selectors'
-import { setQuery } from './actions'
+import { setQuery, setEndDate } from './actions'
+
+
+const dateFormat = 'DD-MM-YYYY'
 
 // Keep search query in sync with the query parameter in the window location.
 const locationSync = ReduxQuerySync.enhancer({
@@ -13,6 +17,13 @@ const locationSync = ReduxQuerySync.enhancer({
             selector: state => ourState(state).currentQueryParams.query,
             action: query => setQuery(query),
             defaultValue: '',
+        },
+        t: {
+            selector: state => ourState(state).currentQueryParams.endDate,
+            action: timestamp => setEndDate(timestamp),
+            defaultValue: undefined,
+            valueToString: timestamp => moment(timestamp).format(dateFormat),
+            stringToValue: string => moment(string, dateFormat).endOf('day').valueOf() || undefined,
         },
     },
 })
