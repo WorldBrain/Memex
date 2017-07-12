709be2532d8c7edd148795fbf9fea943fe482fe9
Jessica Leach <jessicamaryleach@gmail.com>
2017-07-12 10:40:16 +0200

    src/options/containers notifications page added

---
+++ src/options/containers/notifications/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Blacklist from '../../blacklist'
+import { routeTitle, sectionTitle } from '../../base.css'
+import styles from './style.css'
+
+const NotificationsContainer = () => (
+    <div>
+        <h1 className={routeTitle}>Notifications</h1>
+
+        <section className={styles.section}>
+            <h2 className={sectionTitle}>Click to mark as unread</h2>
+
+            <Blacklist />
+        </section>
+    </div>
+)
+
+export default NotificationsContainer
