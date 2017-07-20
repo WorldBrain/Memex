111e5337e030491f4752d78c08c417708809af65
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-07-20 11:42:39 +0700

    Set up browser/system notifs for pause recording events
    - one normal notif when user pauses
    - one notif when pause finishes to indicate history recording, which requires user to close
    - maybe should add option to preferences to disalbe notifs

--- src/activity-logger/background/pause-logging.js
+++ src/activity-logger/background/pause-logging.js
@@ -1,5 +1,13 @@
 import { PAUSE_STORAGE_KEY } from '..'
 
+const getNotifOptions = (message, requireInteraction = false) => ({
+    type: 'basic',
+    iconUrl: '/overview/img/worldbrain-logo-narrow.png',
+    title: 'WorldBrain Activity Logger',
+    message,
+    requireInteraction,
+})
+
 const getState = async () => (await browser.storage.local.get(PAUSE_STORAGE_KEY))[PAUSE_STORAGE_KEY] || false
 const setState = state => browser.storage.local.set({ [PAUSE_STORAGE_KEY]: state })
 
@@ -11,17 +19,25 @@ const setState = state => browser.storage.local.set({ [PAUSE_STORAGE_KEY]: state
  *  specifying of a number of minutes for timeout.
  */
 export default function initPauser() {
-    const unpause = () => setState(false)
     let timeoutId
     setState(false) // Set default unpaused state in local storage flag
+
+    const unpause = () => {
+        browser.notifications.create(getNotifOptions('Activity logger now running in background again', true))
+        setState(false)
+    }
 
     // Either clears or sets the pause timeout depending on its running state as denoted by local storage flag
     return async timeoutMins => {
         const isPaused = await getState()
         setState(!isPaused) // Immediately toggle the local storage flag and continue on
 
-        timeoutId = isPaused
-            ? clearTimeout(timeoutId)
-            : setTimeout(unpause, timeoutMins * 60000)
+        if (isPaused) { // Interrupt
+            clearTimeout(timeoutId)
+            unpause()
+        } else { // New pause timeout request
+            browser.notifications.create(getNotifOptions(`Activity logger now paused for ${timeoutMins} mins`))
+            timeoutId = setTimeout(unpause, timeoutMins * 60000)
+        }
     }
 }
