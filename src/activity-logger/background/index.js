ac1e4e6c3fe857bec4d45aefe50a7eb9d2d6733e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-07-20 12:03:35 +0700

    Write check to not log pages when paused
    - happens in activity-logger module within the debounce page logging function
    - seems fine here now; may want to move somewhere else later

--- src/activity-logger/background/index.js
+++ src/activity-logger/background/index.js
@@ -1,7 +1,12 @@
+import debounce from 'lodash/debounce'
+
 import { makeRemotelyCallable } from 'src/util/webextensionRPC'
 import { maybeLogPageVisit } from './log-page-visit'
 import initPauser from './pause-logging'
-import debounce from 'lodash/debounce'
+import { PAUSE_STORAGE_KEY } from '..'
+
+const isPaused = async () =>
+    (await browser.storage.local.get(PAUSE_STORAGE_KEY))[PAUSE_STORAGE_KEY] || false
 
 // Allow logging pause state toggle to be called from other scripts
 const toggleLoggingPause = initPauser()
@@ -11,13 +16,18 @@ makeRemotelyCallable({ toggleLoggingPause })
 const tabs = {}
 
 // Listens for url changes of the page
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if (changeInfo.url && tab.url) {
         // Check if we already have a debounced function for this tab and cancel it
         if (tabs[tabId]) tabs[tabId].cancel()
 
         // Create debounced function and call it
-        tabs[tabId] = debounce(() => maybeLogPageVisit({url: tab.url, tabId: tabId}), 10000)
+        tabs[tabId] = debounce(async () => {
+            // Bail-out if logging paused
+            if (await isPaused()) return
+
+            return maybeLogPageVisit({url: tab.url, tabId: tabId})
+        }, 10000)
         tabs[tabId]()
     }
 })
