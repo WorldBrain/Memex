a40bbc7becbb0343e859d37d33fc242984ce8cc9
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-03 19:47:18 +0200

    badge responds to number of unread notifs, not read notifs

--- src/util/setUnreadCount.js
+++ src/util/setUnreadCount.js
@@ -2,17 +2,29 @@ import db from "../options/containers/notifications/index-pouch.js"
 import PouchDB from "pouchdb"
 
 export default function setUnreadCount(itemToCount) {
-    // console.log(itemToCount);
-   
+    console.log(itemToCount)
     return db
         .allDocs({
             include_docs: true,
             attachments: true,
             startkey: "notif",
-            endkey: "notif\ufff0"
+            endkey: "notif\ufff0",
         })
-        .then(({ rows }) => rows.filter(({ doc }) => doc.viewed).length)
+        .then(function(response) {
+            // handle response
+            console.log('resp', response)
+            let viewedNotifs = response.rows
+            // loop through them, if viewed is set to true, add to unread notifs counter
+            viewedNotifs.forEach(function(element) {
+                // console.log(element.doc.viewed);
+                if (element.doc.viewed === false) {
+                    itemToCount = itemToCount + 1
+                }
+                return itemToCount
+            })
+            return itemToCount
+        })
         .catch(function(err) {
             console.log("err", err)
-        });
+        })
 }
