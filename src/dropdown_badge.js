2f502649a47fd98685078cf6734ed026de2d79af
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-05 10:48:49 +0200

    if there are no unread notifs, dropdown material icon on li does not display badge

--- src/dropdown_badge.js
+++ src/dropdown_badge.js
@@ -2,10 +2,13 @@ import setUnreadCount from 'src/util/setUnreadCount.js'
 
 setUnreadCount(0).then(function(res) {
     let badge = document.getElementById('notifs')
-    // badge.innerHTML = 'feedback'
-    badge.setAttribute("data-badge", res)
-    console.log('does this run?')
-    console.log(badge.innerHTML)
+    if (res === 0) {
+        badge.removeAttribute("data-badge")
+    } else {
+        badge.setAttribute("data-badge", res)
+        console.log('does this run?')
+        console.log(badge.innerHTML)
+    }
 })
     .catch(function(err) {
         console.log("err")
