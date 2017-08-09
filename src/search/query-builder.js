70692ca41a1a3a4bed389d70ce5524e58deea915
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-08-09 10:17:17 +0700

    Update index QueryBuilder field handling
    Update QueryBuilder to use new timestamp fields
    
    - now it's split between bookmarkTimestamps/visitTimestamps in the index structure, but our queries don't take advtanage of this yet
    - hence any time-based queries should create filters on both fields
    - also added in the needed pouch _id prefixes
    
    Allow QueryBuilder's time filters to be unset if no data
    
    - before was pushiung the responsibility to the caller to handle this
    - this allows the caller to call these methods without specifying anything without the query being compromised

--- src/search/query-builder.js
+++ src/search/query-builder.js
@@ -2,34 +2,48 @@
 class QueryBuilder {
     constructor() {
         this.content = []
-        this.timestamp = []
-        this.offset = null;
-        this.pageSize = null;
+        this.bookmarkTimestamps = []
+        this.visitTimestamps = []
+        this.offset = null
+        this.pageSize = null
     }
 
-    _setDate = field => time => {
-        const current = this.timestamp.length ? this.timestamp[0] : {}
-        const updated = [{ ...current, [field]: String(time) }]
+    /**
+     * Sets up the query on time-based fields. Currently mirrors for both, as
+     * our current search does not differentiate between visits and bookmarks.
+     */
+    _setDate = op => time => {
+        // Don't add anything if the time isn't given
+        if (!time) { return this }
+        const currentBookmarks = this.bookmarkTimestamps.length ? this.bookmarkTimestamps[0] : {}
+        const currentVisits = this.visitTimestamps.length ? this.visitTimestamps[0] : {}
+        const updatedBookmarks = [{ ...currentBookmarks, [op]: `bookmark/${time}` }]
+        const updatedVisits = [{ ...currentVisits, [op]: `visit/${time}` }]
 
-        // Search index needs defaults if they're missing
-        if (!updated[0].gte) updated[0].gte = '0'
-        if (!updated[0].lte) updated[0].lte = String(Date.now())
+        // search-index needs defaults if they're missing
+        if (!updatedBookmarks[0].gte) updatedBookmarks[0].gte = 'bookmark/'
+        if (!updatedBookmarks[0].lte) updatedBookmarks[0].lte = `bookmark/${Date.now()}`
+        if (!updatedVisits[0].gte) updatedVisits[0].gte = 'visit/'
+        if (!updatedVisits[0].lte) updatedVisits[0].lte = `visit/${Date.now()}`
 
-        this.timestamp = updated
+        // Set updated query fields
+        this.bookmarkTimestamps = updatedBookmarks
+        this.visitTimestamps = updatedVisits
         return this
     }
 
     get = () => {
-        const query = {
-            query: {
-                AND: { content: this.content, timestamp: this.timestamp },
-            },
+        const q = {
+            query: [
+                { AND: { content: this.content, bookmarkTimestamps: this.bookmarkTimestamps } },
+                { AND: { content: this.content, visitTimestamps: this.visitTimestamps } },
+            ],
         }
 
-        if (this.offset) query.offset = this.offset
-        if (this.pageSize) query.pageSize = this.pageSize
+        if (this.offset) q.offset = this.offset
+        if (this.pageSize) q.pageSize = this.pageSize
 
-        return query
+        return q
     }
 
     skipUntil = skip => {
