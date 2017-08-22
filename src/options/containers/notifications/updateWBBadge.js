d708d3762bb9d6f26d78a4d1096cc8449cc051c2
Jessica Leach <jessicamaryleach@gmail.com>
2017-08-22 10:03:39 +0200

    updateWBBadge module rewritten as async/await

--- src/options/containers/notifications/updateWBBadge.js
+++ src/options/containers/notifications/updateWBBadge.js
@@ -1,37 +1,16 @@
 import setUnreadCount from "../../../util/setUnreadCount.js"
 
-// export default function updateWBBadge() {
-//     setUnreadCount(0).then(function(res) {
-//         var ba = chrome.browserAction
-//         ba.setBadgeBackgroundColor({ color: [62, 185, 149, 128] })
-//         if (res > 0) {
-//             ba.setBadgeText({ text: "" + res })
-//         } else {
-//             ba.setBadgeText({ text: "" })
-//         }
-//     })
-//         .catch(function(err) {
-//             console.log("err")
-//         })
-// }
-
 export default async function updateWBBadge() {
     let ba = chrome.browserAction
+    ba.setBadgeBackgroundColor({ color: [62, 185, 149, 128] })
     try {
         let res = await setUnreadCount(0)
-        await ba.setBadgeBackgroundColor({ color: [62, 185, 149, 128] })
-        await ba.setBadgeText({ text: "" + res })
+        if (res > 0) {
+            await ba.setBadgeText({ text: "" + res })
+        } else {
+            await ba.setBadgeText({ text: "" })
+        }
     } catch (err) {
         console.err("err", err)
     }
-};
-
-// async function doAsyncOp () {
-//     try {
-//     var val = await asynchronousOperation();
-//     val = await asynchronousOperation(val);
-//     return await asynchronousOperation(val);
-//   } catch (err) {
-//     console.err(err);
-//   }
-// };
+}
