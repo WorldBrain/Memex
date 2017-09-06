f4afd5d163ff99b54dfaa9d45265b50a542e57ba
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-09-06 12:31:29 +0700

    Write basic fathom ruleset for extracting DOM content
    - this is kind of meant to be like Readability; takes a DOM and then tries to determine and return the "main content" via a set of rules
    - this one proved to be fairly innaccurate for the pages I tried (articles from various news sites), but it actually works after a few hours of messing around with it
    - treat this as more of an example

---
+++ src/util/fathom-extractor.js
@@ -0,0 +1,50 @@
+import { rule, ruleset, dom, type, props, score, out, domSort } from 'fathom-web'
+import { inlineTextLength, linkDensity } from 'fathom-web/utils'
+
+/**
+ * @param {fnode} fnode
+ * @return {any} Object containing a `score` key derived from the element's text length
+ */
+const scoreByLength = ({ element }) => ({
+    score: inlineTextLength(element),
+})
+
+
+// Based on: https://hacks.mozilla.org/2017/04/fathom-a-framework-for-understanding-web-pages/
+// Meant to be similar to Readability-like extraction of a page's main-content
+// Initial tests of this are pretty innaccurate; lots to learn to be able to tweak the rules and use it well
+const rules = ruleset(
+    rule(
+        dom('p,div,li,blockquote,h1,h2,h3,h4,h5,h6'),
+        props(scoreByLength)
+            .type('paragraphish'),
+    ),
+    rule(
+        type('paragraphish'),
+        score(fnode => {
+            const paragraphishNote = fnode.noteFor('paragraphish')
+            return paragraphishNote
+                ? (1 - linkDensity(fnode, paragraphishNote.inlineLength)) * 1.5
+                : (1 - linkDensity(fnode)) * 1.5
+        }),
+    ),
+    rule(
+        dom('p'),
+        score(4.5)
+            .type('paragraphish'),
+    ),
+    rule(
+        type('paragraphish')
+            .bestCluster({
+                splittingDistance: 3,
+                differentDepthCost: 6.5,
+                differentTagCost: 2,
+                sameTagCost: 0.5,
+                strideCost: 0,
+            }),
+        out('content')
+            .allThrough(domSort),
+    ),
+)
+
+export default rules
