a55eee4434f6c9339dc3c078e5ee026f6f575ff6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-09-10 16:54:29 +0700

    Update search results->pouch mapping stage for new model
    - a lot got to be simplified here with new model, which is great
    - sadly the results are slightly weird with this data model; need to look into it tomorrow. Probably I'm missing something obvious somewhere in the query-builder (hopefully)

--- src/search/map-search-to-pouch.js
+++ src/search/map-search-to-pouch.js
@@ -1,64 +1,38 @@
-import map from 'lodash/fp/map'
 import reduce from 'lodash/fp/reduce'
-import flatten from 'lodash/fp/flatten'
-import compose from 'lodash/fp/compose'
-import partition from 'lodash/fp/partition'
 
 import db, { bulkGetResultsToArray } from 'src/pouchdb'
-import { pageKeyPrefix } from 'src/page-storage'
-import { convertMetaDocId, visitKeyPrefix } from 'src/activity-logger'
+import { RESULT_TYPES } from 'src/overview/constants'
 
 /**
- * @param {string} id ID of the doc to extract timestamp from.
- * @returns {number} Extracted timestamp as a number.
+ * Given an augmented page doc determine its "type" for use to display in the views.
+ * Either `visit` or `bookmark` will be chosen determined by whichever is latest (if available).
+ * @param {any} augmentedPageDoc The augmented page doc from search results.
+ * @returns {string} Display type string matching one of `constants.RESULT_TYPES`.
+ *  Unknown type given in case of bad data (no assoc. meta timestamp)
  */
-const getTimestampFromId = id => +convertMetaDocId(id).timestamp
+function checkPageDocType({ bookmark, visit }) {
+    let type = RESULT_TYPES.UNKNOWN
 
-/**
-* Given an augmented page doc (returned from `mapResultsToPouchDocs`), returns the timestamp of the
-* latest associated meta (either bookmark or visit doc).
-* @param {AugmentedPageDoc} pageDoc
-* @returns {number} Timestamp of the latest associated meta doc, or MAX_INT if none (bad data).
-*/
-function grabLatestAssocMetaTimestamp({ assoc }) {
-    let latest = Number.MAX_SAFE_INTEGER
-
-    // If both visit and bookmark exist, return the latest of both
-    if (assoc.visit && assoc.bookmark) {
-        const visit = getTimestampFromId(assoc.visit._id)
-        const bookmark = getTimestampFromId(assoc.bookmark._id)
-        latest = visit > bookmark ? visit : bookmark
-    } else if (assoc.visit) {
-        latest = getTimestampFromId(assoc.visit._id)
-    } else if (assoc.bookmark) {
-        latest = getTimestampFromId(assoc.bookmark._id)
+    // If both exist, find the time of the latest oneO
+    if (visit && bookmark) {
+        type = visit > bookmark
+            ? RESULT_TYPES.VISIT
+            : RESULT_TYPES.BOOKMARK
+    } else if (visit) {
+        type = RESULT_TYPES.VISIT
+    } else if (bookmark) {
+        type = RESULT_TYPES.BOOKMARK
     }
 
-    return latest
+    return type
 }
 
 /**
-* Returns the latest bookmark or visit doc from index results + IDs of the later metas.
-* @param {Array<string>} metaIds Array of IDs to sort and extract latest from.
-* @param {any} timeFilters Time filters of the search query, if any.
-* @returns {any} Object containing `latest` meta ID and ordered `rest` array.
-*  `latest` should be `undefined` if input array length is 0.
+* NOTE: Assumes order for O(1) "lookup" of latest time
+* @param {Array<string>} timestamps ORDERED array of timestamp strings to get latest (last) one.
+* @returns {string} The latest timestamp
 */
-function getLatestMeta(metaIds = [], { startDate, endDate }) {
-    const now = Date.now()
-    // Filter out out-of-range meta IDs then sort them by time
-    const sorted = metaIds
-        .filter(id => getTimestampFromId(id) >= (startDate || 0) && getTimestampFromId(id) <= (endDate || now))
-        .sort((idA, idB) => getTimestampFromId(idB) - getTimestampFromId(idA))
-
-    if (!sorted.length) {
-        return { latest: undefined, rest: [] }
-    }
-
-    // Split the latest result from the rest
-    const [latest, ...rest] = sorted
-    return { latest, rest }
-}
+const getLatestTime = (timestamps = []) => timestamps[timestamps.length - 1]
 
 /**
 * Creates a quick-lookup dictionary of page doc IDs to the associated meta doc IDs
@@ -66,95 +40,50 @@ function getLatestMeta(metaIds = [], { startDate, endDate }) {
 * @param {Array<any>} results Array of search-idnex results.
 * @returns {any} Object with page ID keys and values containing either bookmark or visit or both docs.
 */
-const createResultIdsDict = timeFilters => reduce((acc, result) => ({
+const createResultIdsDict = reduce((acc, result) => ({
     ...acc,
     [result.document.id]: {
         pageId: result.document.id,
-        visitIds: getLatestMeta(result.document.visits, timeFilters),
-        bookmarkIds: getLatestMeta(result.document.bookmarks, timeFilters),
+        visit: getLatestTime(result.document.visits),
+        bookmark: getLatestTime(result.document.bookmarks),
         score: result.score,
     },
 }), {})
 
-/**
-* @param {any} dict Lookup dictionary returned from `createResultIdsDict`.
-* @returns {Array<any>} Array of `{ id: string }`, ready for injestion into `PouchDB.bulkGet`.
-*/
-const formatBulkGetInput = compose(
-    flatten,
-    map(idBatch => [
-        { id: idBatch.pageId },
-        { id: idBatch.visitIds.latest },
-        { id: idBatch.bookmarkIds.latest },
-    ].filter(doc => doc.id != null)) // Filter out missing metas
-)
-
-/**
-* Creates a quick-lookup dictionary of page doc IDs to the associated meta doc contents
-* from an array of meta doc inputs.
-* @param {Array<any>} metaDocs Array of meta docs to map page IDs to.
-* @returns {any} Object with page ID keys and values containing either bookmark or visit or both docs.
-*/
-const createMetaDocsDict = reduce((acc, metaDoc) => {
-    const key = metaDoc._id.startsWith(visitKeyPrefix) ? 'visit' : 'bookmark'
-
-    // Merge with existing bookmark/visit if present, else create new
-    const newEntry = acc[metaDoc.page._id]
-        ? { ...acc[metaDoc.page._id], [key]: metaDoc }
-        : { [key]: metaDoc }
-
-    return {
-        ...acc,
-        [metaDoc.page._id]: newEntry,
-    }
-}, {})
-
 // Sort predicates based on time and search relevance
 const sortByTime = () => (docA, docB) =>
-    grabLatestAssocMetaTimestamp(docB) - grabLatestAssocMetaTimestamp(docA)
+    docB[docB.displayType] - docA[docA.displayType]
 
 const sortByRelevance = resultIdsDict => (docA, docB) =>
     resultIdsDict[docA._id].score - resultIdsDict[docB._id].score
 
 /**
 * Performs all the messy logic needed to resolve search-index results against our PouchDB model.
-* Given n results, should produce n page docs with associated meta docs available under `assoc` key.
-* For the sake of differentiation, call these "augmented page docs".
-* The latest visit/bookmark doc should be available on the page doc (assuming they exist), while
-* all later metas will only be as _ids to avoid large fetches in the cases of pages with large
-* number of assoc. meta docs (they can be lazy fetched later on user actions).
-*
-* TODO: All this resolution is messy as hell; is there a better way?
+* Given n results, should produce n page docs with the latest associated meta docs timestamps
+* available under either `bookmark` or `visit` key, or both. For the sake of differentiation,
+* call these "augmented page docs", and are generally only used by the overview.
 *
 * @param {Array<any>} results Array of results gotten from our search-index query.
-* @param {any} timeFilters Time filters of the search query, if any.
 * @param {boolean} [shouldSortByTime=false] Whether or not to order results by time.
-* @returns {Array<any>} Array of augmented page docs containing linked meta docs.
+* @returns {Array<any>} Array of augmented page docs containing linked meta doc timestamps.
 */
-export default async function mapResultsToPouchDocs(results, timeFilters, shouldSortByTime = false) {
+export default async function mapResultsToPouchDocs(results, shouldSortByTime = false) {
     // Convert results to dictionary of page IDs to related meta IDs
-    const resultIdsDict = createResultIdsDict(timeFilters)(results)
+    const resultIdsDict = createResultIdsDict(results)
 
     // Format IDs of docs needed to be immediately fetched from Pouch
-    const bulkGetInput = formatBulkGetInput(resultIdsDict)
+    const bulkGetInput = results.map(result => ({ id: result.document.id }))
 
     // Perform bulk fetch for needed docs from pouch
     const bulkRes = await db.bulkGet({ docs: bulkGetInput })
-    const docs = bulkGetResultsToArray(bulkRes)
+    const pageDocs = bulkGetResultsToArray(bulkRes)
 
-    // Parition into meta and page docs
-    const [pageDocs, metaDocs] = partition(doc => doc._id.startsWith(pageKeyPrefix))(docs)
-
-    // Insert meta docs into the relevant page docs,
-    //  including later meta doc IDs, within the `pageDoc.assoc` key
-    const metaDocsDict = createMetaDocsDict(metaDocs)
+    // Augment the page docs with latest meta timestamp/s and type denoting the latest time for display
     const augmentedPageDocs = pageDocs.map(doc => ({
         ...doc,
-        assoc: {
-            ...metaDocsDict[doc._id],
-            laterVisits: resultIdsDict[doc._id].visitIds.rest,
-            laterBookmarks: resultIdsDict[doc._id].bookmarkIds.rest,
-        },
+        visit: +resultIdsDict[doc._id].visit,
+        bookmark: +resultIdsDict[doc._id].bookmark,
+        displayType: checkPageDocType(resultIdsDict[doc._id]),
     }))
 
     // Perform time-based sort if default query (based on greatest meta doc time)
