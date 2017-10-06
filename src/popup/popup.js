d8a1b9212e999b7d4c70ff941a3ebc6354107227
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-10-06 10:50:37 +0700

    Put React 16 ErrorBoundary on Popup UI

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import { ErrorBoundary, RuntimeError } from 'src/common-ui/components'
 import Popup from './container'
 
-render(<Popup />, document.getElementById('popup'))
+render(
+    <ErrorBoundary component={RuntimeError}>
+        <Popup />
+    </ErrorBoundary>,
+    document.getElementById('popup'))
