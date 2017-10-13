b705c73351a625ad1957275bff15147420c3fd73
Mukesh Kharita <mukeshnithcse@gmail.com>
2017-10-13 11:44:42 +0530

    Rewrite navigation component

--- src/options/components/navigation/index.jsx
+++ src/options/components/navigation/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import classNames from 'classnames'
-
+import * as selectors from '../../imports/selectors'
+import * as actions from '../../imports/actions'
 import styles from './styles.css'
+import { bindActionCreators } from 'redux'
 
-const Navigation = ({ currentLocation, routes }) => {
-    function buildRoutes() {
+class Navigation extends Component {
+    constructor(props) {
+        super(props)
+        this.isActive = this.isActive.bind(this)
+        this.buildRoutes = this.buildRoutes.bind(this)
+    }
+
+    isActive(route) {
+        return this.props.currentLocation.pathname === route.pathname
+    }
+
+    buildRoutes() {
+        const { routes, isIdle, isRunning, isStopped } = this.props
+
         return routes.map((route, idx) => {
             const navClasses = classNames({
                 [styles.navLink]: true,
-                [styles.isActive]: isActive(route),
+                [styles.isActive]: this.isActive(route),
             })
 
             const navIcon = classNames({
@@ -38,11 +53,33 @@ const Navigation = ({ currentLocation, routes }) => {
                         )}
                     </li>
                     {route.name === 'Import' &&
-                        isActive(route) && (
+                        this.isActive(route) && (
                             <div className={styles.importSubItems}>
-                                <div className={styles.active}>1. Analysis</div>
-                                <div>2. Download Progress</div>
-                                <div>3. Status Report</div>
+                                <div
+                                    className={
+                                        isIdle
+                                            ? styles.active
+                                            : isRunning || isStopped
+                                              ? styles.done
+                                              : null
+                                    }
+                                >
+                                    1. Analysis
+                                </div>
+                                <div
+                                    className={
+                                        isRunning
+                                            ? styles.active
+                                            : isStopped ? styles.done : null
+                                    }
+                                >
+                                    2. Download Progress
+                                </div>
+                                <div
+                                    className={isStopped ? styles.active : null}
+                                >
+                                    3. Status Report
+                                </div>
                             </div>
                         )}
                 </li>
@@ -50,23 +87,41 @@ const Navigation = ({ currentLocation, routes }) => {
         })
     }
 
-    function isActive(route) {
-        return currentLocation.pathname === route.pathname
+    render() {
+        console.log(this.props)
+        return (
+            <nav className={styles.root}>
+                <div className={styles.icon_div}>
+                    <img
+                        src="/img/worldbrain-logo.png"
+                        className={styles.icon}
+                    />
+                </div>
+                <ul className={styles.nav}>{this.buildRoutes()}</ul>
+            </nav>
+        )
     }
-
-    return (
-        <nav className={styles.root}>
-            <div className={styles.icon_div}>
-                <img src="/img/worldbrain-logo.png" className={styles.icon} />
-            </div>
-            <ul className={styles.nav}>{buildRoutes()}</ul>
-        </nav>
-    )
 }
 
 Navigation.propTypes = {
+    isRunning: PropTypes.bool.isRequired,
+    isStopped: PropTypes.bool.isRequired,
+    isIdle: PropTypes.bool.isRequired,
     currentLocation: PropTypes.object.isRequired,
     routes: PropTypes.array.isRequired,
 }
 
-export default Navigation
+const mapStateToProps = state => ({
+    isRunning: selectors.isRunning(state),
+    isPaused: selectors.isPaused(state),
+    isStopped: selectors.isStopped(state),
+    isLoading: selectors.isLoading(state),
+    isIdle: selectors.isIdle(state),
+    isStartBtnDisabled: selectors.isStartBtnDisabled(state),
+})
+
+const mapDispatchToProps = dispatch => ({
+    boundActions: bindActionCreators(actions, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
