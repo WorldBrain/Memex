d102250f03ce8d95b6c5fe4e0eb824181bbb96d8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-11-07 01:44:53 +0700

    Add lots of missing entries to URL normalization rules
    - mostly google sites
    - all happen to just care about `q` param for now; this is subject to change

--- src/util/filter-params.js
+++
@@ -1,7 +0,0 @@
-const filterParams = {
-    'twitter.com': ['q'],
-    'google.com': ['q'],
-    'google.co.in': ['q'],
-    'facebook.com': ['q'],
-}
-export default filterParams
---
+++ src/util/query-string-normalization-rules.js
@@ -0,0 +1,37 @@
+/**
+ * Hostname keys to arrays of wanted query params values.
+ * These will be used to remove all but specific query params from URLs with given
+ * hostnames.
+ */
+export default {
+    'twitter.com': ['q'],
+    'google.com': ['q'],
+    'google.com.vn': ['q'],
+    'google.com.au': ['q'],
+    'google.com.tw': ['q'],
+    'google.com.br': ['q'],
+    'google.com.tr': ['q'],
+    'google.com.gr': ['q'],
+    'google.com.mx': ['q'],
+    'google.com.ar': ['q'],
+    'google.com.co': ['q'],
+    'google.com.pk': ['q'],
+    'google.co.jp': ['q'],
+    'google.co.uk': ['q'],
+    'google.co.in': ['q'],
+    'google.co.kr': ['q'],
+    'google.es': ['q'],
+    'google.pl': ['q'],
+    'google.pt': ['q'],
+    'google.ie': ['q'],
+    'google.cl': ['q'],
+    'google.at': ['q'],
+    'google.dk': ['q'],
+    'google.be': ['q'],
+    'google.ca': ['q'],
+    'google.de': ['q'],
+    'google.it': ['q'],
+    'google.fr': ['q'],
+    'google.nl': ['q'],
+    'facebook.com': ['q'],
+}
