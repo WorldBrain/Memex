52296ed0fbb24ce932ac0e360e089bdbb6d2c9ff
Arpit Gogia <arpitgogia@protonmail.com>
2017-11-06 13:55:41 +0530

    Added filter params file

--- src/util/url-filter.js
+++ src/util/url-filter.js
@@ -1,6 +1,6 @@
 // Remove unnecessary query params from the URL to avoid redundancy
 import normalizeUrl from 'normalize-url'
-
+import filterParams from './filter-params'
 const NORMALIZE_OPTS = {
     stripWWW: true,
     removeDirectoryIndex: false,
@@ -8,12 +8,13 @@ const NORMALIZE_OPTS = {
     stripFragment: true,
     normalizeProtocol: false,
 }
-
-const RULES = new Set(['q'])
+const filterParamsDict = new Map(Object.entries(filterParams))
 export default function urlFilter(url) {
     const parsedUrl = new URL(normalizeUrl(url, NORMALIZE_OPTS))
+    const rules = filterParamsDict.get(parsedUrl.hostname)
+    if (typeof rules === 'undefined') return parsedUrl.href
     for (const param of parsedUrl.searchParams.keys()) {
-        if (!RULES.has(param)) {
+        if (!rules.has(param)) {
             parsedUrl.searchParams.delete(param)
         }
     }
