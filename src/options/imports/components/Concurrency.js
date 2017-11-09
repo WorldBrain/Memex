7c30de247544a05820413b9e67ea6a92c3055f09
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-11-09 11:28:02 +0700

    Offset concurrency input

--- src/options/imports/components/Concurrency.js
+++ src/options/imports/components/Concurrency.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types'
 
 import { Wrapper } from 'src/common-ui/components'
 
+import styles from './AdvSettings.css'
+
 const Concurrency = ({ concurrency, onConcurrencyChange }) => (
     <Wrapper>
         <label htmlFor="concurrency">Import item concurrency level</label>
         <input
+            className={styles.concurrencyInput}
             id="concurrency"
             onChange={onConcurrencyChange}
             value={concurrency}
