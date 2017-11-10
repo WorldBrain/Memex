2764e182d74fe3d400560c4e75bc29fc969aa154
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-11-10 10:03:51 +0700

    Improve import item creation filtering
    - a lot of the existing logic could be optimised by changing the order of operations (ensure all filtering done before encoding, easier filters first)
    - encoding being done twice before, now at an earlier stage to use later

--- src/imports/background/import-item-creation.js
+++ src/imports/background/import-item-creation.js
@@ -2,7 +2,7 @@ import chunk from 'lodash/fp/chunk'
 
 import db from 'src/pouchdb'
 import encodeUrl from 'src/util/encode-url-for-id'
-import { pageKeyPrefix, generatePageDocId } from 'src/page-storage'
+import { pageKeyPrefix } from 'src/page-storage'
 import { checkWithBlacklist } from 'src/blacklist'
 import { isLoggable } from 'src/activity-logger'
 import {
@@ -12,11 +12,11 @@ import {
 } from 'src/options/imports/constants'
 
 // Binds old ext bookmark urls to a function that transforms old ext data to an import item
-const transformOldExtDataToImportItem = bookmarkUrls => (data, index) => ({
+const transformOldExtDataToImportItem = bookmarkUrls => (item, index) => ({
     type: IMPORT_TYPE.OLD,
-    url: data.url,
-    timestamp: data.time,
-    hasBookmark: bookmarkUrls.has(data.url),
+    url: item.url,
+    timestamp: item.time,
+    hasBookmark: bookmarkUrls.has(item.url),
     index,
 })
 
@@ -24,6 +24,7 @@ const transformOldExtDataToImportItem = bookmarkUrls => (data, index) => ({
 const transformBrowserItemToImportItem = type => item => ({
     browserId: item.id,
     url: item.url,
+    encodedUrl: item.encodedUrl,
     // HistoryItems contain lastVisitTime while BookmarkTreeNodes contain dateAdded
     timestamp: item.lastVisitTime || item.dateAdded,
     type,
@@ -40,36 +41,47 @@ async function checkWithExistingDocs() {
     })
 
     const existingIds = new Set(existingPageDocs.map(row => row.id))
-    return item => {
-        try {
-            return !existingIds.has(generatePageDocId(item))
-        } catch (err) {
-            return false // Malformed URI
-        }
-    }
+    return item => existingIds.has(`${pageKeyPrefix}${item.encodedUrl}`)
 }
 
+/**
+ * @param {Array<any>} items Items with `encodedUrl` prop
+ * @returns {Map<string, any>} Map of `encodedUrl` to the remaining import item data.
+ */
 const transformToImportItemMap = items =>
     items.reduce(
-        (acc, currItem) => acc.set(encodeUrl(currItem.url), currItem),
+        (acc, { encodedUrl, ...currItem }) => acc.set(encodedUrl, currItem),
         new Map(),
     )
 
-/**
- * Handles performing blacklist and pending import filtering logic on any type
- * of items containing a `url` field.
- *
- * @param {Array<any>} browserItems Some Iterable of items of any shape, as long as they contain `url` field.
- * @returns {Array<any>} Filtered version of the items arg.
- */
 async function initFilterItemsByUrl() {
     const isNotBlacklisted = await checkWithBlacklist()
-    const doesNotExist = await checkWithExistingDocs()
+    const alreadyExists = await checkWithExistingDocs()
 
-    const isWorthRemembering = item =>
-        isLoggable(item) && isNotBlacklisted(item) && doesNotExist(item)
+    /**
+     * @param {Array<any>} items Array of items with `url` to filter on.
+     * @return {Array<any>} Filtered version of `items` array, also augmented with `encodedUrl`.
+     */
+    return items =>
+        items.reduce((accItems, currItem) => {
+            if (!isLoggable(currItem) || !isNotBlacklisted(currItem)) {
+                return accItems
+            }
 
-    return browserItems => browserItems.filter(isWorthRemembering)
+            try {
+                // Augment the item with encoded URL for DB check and item map
+                const encodedUrl = encodeUrl(currItem.url)
+                const augItem = { ...currItem, encodedUrl }
+
+                // Filter out items already existing in DB
+                return alreadyExists(augItem)
+                    ? accItems
+                    : [...accItems, augItem]
+            } catch (error) {
+                // URI malformed; don't add to list
+                return accItems
+            }
+        }, [])
 }
 
 /**
@@ -132,36 +144,35 @@ export async function getOldExtItems() {
     return importItems
 }
 
+/**
+ * @param {any} allowTypes
+ * @returns {any} Object containing three Maps of encoded URL keys to import item values.
+ *   Used to create the imports list and estimate counts.
+ */
 export default async function createImportItems(allowTypes = DEF_ALLOW) {
     const filterItemsByUrl = await initFilterItemsByUrl()
 
-    console.time('hist')
-    const historyItems = allowTypes[IMPORT_TYPE.HISTORY]
-        ? await getHistoryItems()
-        : []
-    console.timeEnd('hist')
-    console.time('bm')
-    const bookmarkItems = allowTypes[IMPORT_TYPE.BOOKMARK]
-        ? await getBookmarkItems()
-        : []
-    console.timeEnd('bm')
-    console.time('old')
-    const oldExtItems = allowTypes[IMPORT_TYPE.OLD]
-        ? await getOldExtItems()
-        : []
-    console.timeEnd('old')
+    const historyItems = filterItemsByUrl(
+        allowTypes[IMPORT_TYPE.HISTORY] ? await getHistoryItems() : [],
+    )
+    const bookmarkItems = filterItemsByUrl(
+        allowTypes[IMPORT_TYPE.BOOKMARK] ? await getBookmarkItems() : [],
+    )
+    const oldExtItems = filterItemsByUrl(
+        allowTypes[IMPORT_TYPE.OLD] ? await getOldExtItems() : [],
+    )
 
     return {
         historyItemsMap: transformToImportItemMap(
-            filterItemsByUrl(historyItems).map(
+            historyItems.map(
                 transformBrowserItemToImportItem(IMPORT_TYPE.HISTORY),
             ),
         ),
         bookmarkItemsMap: transformToImportItemMap(
-            filterItemsByUrl(bookmarkItems).map(
+            bookmarkItems.map(
                 transformBrowserItemToImportItem(IMPORT_TYPE.BOOKMARK),
             ),
         ),
-        oldExtItemsMap: transformToImportItemMap(filterItemsByUrl(oldExtItems)),
+        oldExtItemsMap: transformToImportItemMap(oldExtItems),
     }
 }
