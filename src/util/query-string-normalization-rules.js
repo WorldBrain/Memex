8fd4e2620f96a7ef8d93bbe811f10d5484185617
Arpit Gogia <arpitgogia@protonmail.com>
2017-11-12 23:52:52 +0530

    Modified structure of rule object

--- src/util/query-string-normalization-rules.js
+++ src/util/query-string-normalization-rules.js
@@ -6,37 +6,129 @@
  * May move this to external source at later stage.
  */
 export const rules = {
-	'facebook.com': ['fbid']
-	'twitter.com': ['q'],
-    'google.com': ['q'],
-    'google.com.vn': ['q'],
-    'google.com.au': ['q'],
-    'google.com.tw': ['q'],
-    'google.com.br': ['q'],
-    'google.com.tr': ['q'],
-    'google.com.gr': ['q'],
-    'google.com.mx': ['q'],
-    'google.com.ar': ['q'],
-    'google.com.co': ['q'],
-    'google.com.pk': ['q'],
-    'google.co.jp': ['q'],
-    'google.co.uk': ['q'],
-    'google.co.in': ['q'],
-    'google.co.kr': ['q'],
-    'google.es': ['q'],
-    'google.pl': ['q'],
-    'google.pt': ['q'],
-    'google.ie': ['q'],
-    'google.cl': ['q'],
-    'google.at': ['q'],
-    'google.dk': ['q'],
-    'google.be': ['q'],
-    'google.ca': ['q'],
-    'google.de': ['q'],
-    'google.it': ['q'],
-    'google.fr': ['q'],
-    'google.nl': ['q'],
-    'facebook.com': ['q'],
+    'facebook.com': {
+        rules: ['fbid', 'q'],
+        type: 'keep',
+    },
+    'twitter.com': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.vn': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.au': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.tw': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.br': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.tr': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.gr': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.mx': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.ar': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.co': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.com.pk': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.co.jp': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.co.uk': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.co.in': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.co.kr': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.es': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.pl': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.pt': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.ie': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.cl': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.at': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.dk': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.be': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.ca': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.de': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.it': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.fr': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'google.nl': {
+        rules: ['q'],
+        type: 'keep',
+    },
+    'instagram.com': {
+        rules: ['taken-by'],
+        type: 'remove',
+    }
 }
-
 export default new Map(Object.entries(rules))
