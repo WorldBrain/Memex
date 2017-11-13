6ac29977cf378d27e1e537495c6b6f78aadf82fe
nilesh <nileshgulia@gmail.com>
2017-11-14 01:14:55 +0530

    added icons

--- src/overview/shareButtons.js
+++ src/overview/shareButtons.js
@@ -9,17 +9,17 @@ class ShareButtons extends Component {
 
     render() {
         return (
-            <div
-                className="a2a_kit a2a_kit_size_32 a2a_floating_style a2a_vertical_style"
-                style="left:0px; top:150px;"
-            >
-                <a className="a2a_button_facebook" />
-                <a className="a2a_button_twitter" />
-                <a className="a2a_button_google_plus" />
-                <a className="a2a_button_pinterest" />
-                <a className="a2a_dd" href="https://www.addtoany.com/share" />
+            <div>
+                <img
+                    style={{
+                        right: 45,
+                        top: 150,
+                    }}
+                    src="/img/face.png"
+                />
             </div>
         )
     }
 }
+ReactDOM.render(<ShareButtons />, document.getElementById('dab'))
 export default ShareButtons
