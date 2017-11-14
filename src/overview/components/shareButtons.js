e0f855b442602bb507e5095dc3d9bf8c5fe06a6e
nilesh <nileshgulia@gmail.com>
2017-11-15 00:40:44 +0530

    improve icons & set breakpoints

--- src/overview/components/shareButtons.js
+++ src/overview/components/shareButtons.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react'
-import styles from './ShareIcons.css'
+import styles from './ShareButtons.css'
 
 class ShareButtons extends Component {
     render() {
         return (
             <div className={styles.shareIcons}>
-                <p className={styles.shareText}>Spread the love</p>
+                <p className={styles.shareText}> Spread the love </p>{' '}
                 <a href="#">
-                    <img width={25} height={25} src="/img/face.png" />
-                </a>
+                    <img
+                        width={25}
+                        height={25}
+                        vspace={2}
+                        src="/img/face.png"
+                    />
+                </a>{' '}
                 <a href="#">
-                    <img width={25} height={25} src="/img/twitt.png" />
-                </a>
+                    <img
+                        width={25}
+                        height={25}
+                        vspace={2}
+                        src="/img/twitt.png"
+                    />
+                </a>{' '}
                 <a href="#">
-                    <img width={25} height={25} src="/img/@.png" />
-                </a>
+                    <img width={25} height={25} vspace={2} src="/img/@.png" />
+                </a>{' '}
                 <a href="#">
-                    <img width={25} height={25} src="/img/ic.png" />
-                </a>
+                    <img width={25} height={25} vspace={2} src="/img/ic.png" />
+                </a>{' '}
                 <div id={styles.fedIcon}>
                     <a href="#" className={styles.FedLink}>
-                        FEEDBACK
-                    </a>
-                </div>
+                        Feedback{' '}
+                    </a>{' '}
+                </div>{' '}
             </div>
         )
     }
