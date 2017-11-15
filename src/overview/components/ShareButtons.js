a846a3ee8d6c902d5aa0a606860e03821116bd60
Oliver Sauter <oli@worldbrain.io>
2017-11-15 23:59:58 +0100

    Updated Styling and Links

--- src/overview/components/ShareButtons.js
+++ src/overview/components/ShareButtons.js
@@ -5,19 +5,23 @@ import { Link } from 'react-router-dom'
 const ShareButtons = props => (
     <div className={styles.shareIcons}>
         <p className={styles.shareText}>Spread the love</p>{' '}
-        <a href="#">
+        <a target="_blank" href="https://worldbrain.io/share/facebook">
             <img className={styles.shareImg} vspace={2} src="/img/face.png" />
         </a>{' '}
-        <a Link to="/">
+        <a target="_blank" href="https://worldbrain.io/share/twitter">
             <img className={styles.shareImg} vspace={2} src="/img/twitt.png" />
         </a>{' '}
-        <a Link to="#">
+        <a target="_blank" href="https://worldbrain.io/share/reddit">
             <img className={styles.shareImg} vspace={2} src="/img/ic.png" />
         </a>{' '}
-        <a Link to="#">
+        <a target="_blank" href="https://worldbrain.io/share/email">
             <img className={styles.shareImg} vspace={2} src="/img/@.png" />
         </a>{' '}
-        <a Link to="#" className={styles.fedLink}>
+        <a
+            target="_blank"
+            href="https://worldbrain.io/share/feedback"
+            className={styles.fedLink}
+        >
             Feedback{' '}
         </a>{' '}
     </div>
