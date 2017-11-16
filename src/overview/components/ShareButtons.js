509a6df62fadcaeff67fcab40774c98ad0a76ba8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-11-16 10:20:15 +0700

    Fix up accidental package inclusion
    - also minor linting errors
    - removed unused import

--- src/overview/components/ShareButtons.js
+++ src/overview/components/ShareButtons.js
@@ -1,6 +1,6 @@
 import React from 'react'
+
 import styles from './ShareButtons.css'
-import { Link } from 'react-router-dom'
 
 const ShareButtons = props => (
     <div className={styles.shareIcons}>
