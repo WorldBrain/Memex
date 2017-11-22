b74fbaacea24b2646237a5b1c3c3817b49a15359
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-11-22 10:12:02 +0700

    Set up runtime JS error tracking
    - this client lib supports it with `trackErrors` option. Seems to work alright giving the line the error occurred at + message

--- src/util/analytics.js
+++ src/util/analytics.js
@@ -46,6 +46,7 @@ class Analytics {
 const analytics = new Analytics({
     url: process.env.PIWIK_HOST,
     siteId: process.env.PIWIK_SITE_ID,
+    trackErrors: true,
 })
 
 export default analytics
