1b94c3b3f1bd7fb9fcc87df3f188738dc64b8210
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-12-11 12:14:07 +0700

    Clean up bookmark deletion logic
    - main flow remains the same
    - added missing JSDoc
    - ensure Error thrown in bad case rather than just `console.warn` log
    - ensure if entire page removal happens (orphan page case) Pouch removal logic still happens

--- src/bookmarks/background/deletion.js
+++ src/bookmarks/background/deletion.js
@@ -4,31 +4,37 @@ import * as index from 'src/search'
 import deleteDocsByUrl, { deleteDocs } from 'src/page-storage/deletion'
 import { bookmarkKeyPrefix } from '..'
 
+/**
+ * TODO: Decided if we actually need these bookmark docs in Pouch; I don't think they're being used for anything.
+ *
+ * @param {string} url URL to remove _all_ bookmarks for.
+ * @throws {Error} Error thrown when `pageId` param does not correspond to existing document (or any other
+ *  standard indexing-related Error encountered during updates).
+ * @returns {Promise<void>}
+ */
 async function removeBookmarkByUrl(url) {
     const pageId = generatePageDocId({ url })
     const reverseIndexDoc = await index.initSingleLookup()(pageId)
 
     if (reverseIndexDoc == null) {
-        console.warn(
-            'Cannot remove bookmark with no corresponding reverse index entry',
-            pageId,
+        throw new Error(
+            `No document exists in reverse page index for the supplied page ID: ${pageId}`,
         )
-        return
     }
 
     // If no visits, we don't want an orphaned page, so remove everything for given URL
     if (!reverseIndexDoc.visits.size) {
-        return deleteDocsByUrl(url)
+        await deleteDocsByUrl(url)
+    } else {
+        // Standard case where only bookmarks removed from index
+
+        // Deindex from bookmarks index
+        await index.del([...reverseIndexDoc.bookmarks.values()])
+
+        // Update reverse index bookmarks for current doc
+        reverseIndexDoc.bookmarks.clear()
+        await index.put(pageId, reverseIndexDoc)
     }
-
-    // Deindex from bookmarks index
-    await index.del([...reverseIndexDoc.bookmarks.values()])
-
-    // Update reverse index bookmarks for current doc
-    await index.put(pageId, {
-        ...reverseIndexDoc,
-        bookmarks: new Set(),
-    })
 
     // Remove corresponding bookmark docs from pouch
     const fetchDocsByType = fetchDocTypesByUrl(url)
