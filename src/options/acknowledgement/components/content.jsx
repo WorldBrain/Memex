bf4b07c1b7e949b852b4916172d8f4a72e5f7e33
Oliver Sauter <oli@worldbrain.io>
2017-12-14 21:18:00 +0100

    change name in omnibar

--- src/options/acknowledgement/components/content.jsx
+++ src/options/acknowledgement/components/content.jsx
@@ -6,7 +6,8 @@ import localStyles from './styles.css'
 const AcknowledgementContainer = () => (
     <div className={localStyles.acknowledgement}>
         <span className={localStyles.title}>
-            WorldBrain can only happen thanks to our talented collaborators.
+            WorldBrain's Memex can only happen thanks to our talented
+            collaborators.
         </span>
         <div className={localStyles.content}>
             Many thanks goes out to our{' '}
@@ -19,7 +20,7 @@ const AcknowledgementContainer = () => (
             who helped building our software.
             <br />We also have immense grattitude for the decades-long pioneer
             work done by technologists and philosophers that enabled the
-            WorldBrain project with their pieces of code and thought.
+            WorldBrain's Memex project with their pieces of code and thought.
             <br />
             <br />This software is{' '}
             <a
