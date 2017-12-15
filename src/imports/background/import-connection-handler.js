7de6fc53c4df3f9904204629b2c1f6389b6ca4d0
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-12-15 14:03:41 +0700

    Clean up import-connection-handler module
    - no real code changes, just refactoring to a class
    - now no need to constantly pass around the port and importer instances around as we have been for a while and it's really confusing
    - some of the side-effects, like local storage accesses, should be looked into to see if we can encapsulate in here

--- src/imports/background/import-connection-handler.js
+++ src/imports/background/import-connection-handler.js
@@ -16,85 +16,68 @@ import {
 } from '../'
 import ProgressManager from './import-progress'
 
-/**
- * Handles building the collection of import items in local storage.
- *
- * @param {any} allowTypes Object containings bools for each valid type of import, denoting whether
- *   or not import and page docs should be created for that import type.
- */
-async function prepareImportItems(allowTypes = {}) {
-    let bookmarkItems
+class ImportConnectionHandler {
+    /**
+     * @property {runtime.Port} Runtime connection port to afford message communication with UI script
+     */
+    port
 
-    for await (let { data, type } of createImportItems()) {
-        if (type === IMPORT_TYPE.BOOKMARK) {
-            // Bookmarks should always yield before history
-            bookmarkItems = data
-        } else if (type === IMPORT_TYPE.HISTORY) {
-            // Don't include pages in history that exist as bookmarks as well
-            data = differMaps(bookmarkItems)(data)
-        }
+    /**
+     * @property {ImportProgressManager} Importer instance
+     */
+    importer
 
-        if (allowTypes[type]) {
-            await stateManager.addItems(data)
-        }
-    }
-}
+    constructor(port) {
+        this.port = port
 
-/**
- * Essentially the logic which happens when the user presses the "Start Import" button.
- *
- * @param {Port} port The open connection port to send messages over.
- * @param {ImportProgressManager} importer The progress maanger instance to start.
- * @param {any} allowTypes Object with keys as valid import types pointing to bool values denoting whether
- * or not to process that given type of imports.
- */
-async function startImport(port, importer, allowTypes) {
-    // Perform history-stubs, vists, and history import state creation, if import not in progress
-    const importInProgress = await getImportInProgressFlag()
-    if (!importInProgress) {
-        await prepareImportItems(allowTypes)
+        this.importer = new ProgressManager(this.processChunk, DEF_CONCURRENCY)
+
+        // Handle any incoming messages to control theimporter
+        port.onMessage.addListener(this.messageListener)
+
+        this.attemptRehydrate()
     }
 
-    port.postMessage({ cmd: CMDS.START }) // Tell UI to finish loading state and move into progress view
-    setImportInProgressFlag()
-    importer.start()
-}
+    async attemptRehydrate() {
+        // If import isn't started earlier, get estimates and set view state to init
+        const importInProgress = await getImportInProgressFlag()
+        if (!importInProgress) {
+            // Make sure estimates view init'd with count data
+            const estimateCounts = await getEstimateCounts({
+                forceRecalc: false,
+            })
+            this.port.postMessage({ cmd: CMDS.INIT, ...estimateCounts })
+        } else {
+            // Make sure to start the view in paused state
+            this.port.postMessage({ cmd: CMDS.PAUSE })
+        }
+    }
 
-/**
- * The cleanup logic that happens when user chooses to finish an import
- * (either after completion or cancellation).
- */
-async function finishImport(port) {
-    clearImportInProgressFlag()
+    messageListener = ({ cmd, payload }) => {
+        switch (cmd) {
+            case CMDS.START:
+                return this.startImport(payload)
+            case CMDS.RESUME:
+                return this.importer.start()
+            case CMDS.PAUSE:
+                return this.importer.stop()
+            case CMDS.CANCEL:
+                return this.cancelImport()
+            case CMDS.FINISH:
+                return this.finishImport()
+            case CMDS.SET_CONCURRENCY:
+                return (this.importer.concurrency = payload)
+            default:
+                return console.error(`unknown command: ${cmd}`)
+        }
+    }
 
-    // Re-init the estimates view with updated estimates data
-    const estimateCounts = await getEstimateCounts({ forceRecalc: true })
-    port.postMessage({ cmd: CMDS.INIT, ...estimateCounts })
-}
-
-async function cancelImport(port, importer) {
-    importer.stop()
-
-    // Clean up any import-related stubs or state
-    await stateManager.clearItems()
-
-    // Resume UI at complete state
-    port.postMessage({ cmd: CMDS.COMPLETE })
-    clearImportInProgressFlag()
-}
-
-/**
- * Binds the chunk processor logic to the UI connection port, to allow progress to be reported from within.
- *
- * @param {Port} port
- */
-const processChunk = port =>
     /**
      * @param {any} chunkData The data of currently-being-processed chunk (contains `chunk` and `chunkKey`).
      * @param {number} concurrency Externally-specified concurrency level to use to implement concurrency (TODO).
      * @param {any} token Token object to allow attaching of rejection callback, affording caller Promise cancellation.
      */
-    async function({ chunk, chunkKey }, concurrency, token) {
+    processChunk = async ({ chunk, chunkKey }, concurrency, token) => {
         for (const [encodedUrl, importItem] of Object.entries(chunk)) {
             let status, url, error
             try {
@@ -111,7 +94,7 @@ const processChunk = port =>
                 error = err.message
             } finally {
                 // Send item data + outcome status down to UI (and error if present)
-                port.postMessage({
+                this.port.postMessage({
                     cmd: CMDS.NEXT,
                     url: url,
                     type: importItem.type,
@@ -123,46 +106,82 @@ const processChunk = port =>
         }
     }
 
+    /**
+     * Handles building the collection of import items in local storage.
+     *
+     * @param {any} allowTypes Object containings bools for each valid type of import, denoting whether
+     *   or not import and page docs should be created for that import type.
+     */
+    async prepareImportItems(allowTypes = {}) {
+        let bookmarkItems
+
+        for await (let { data, type } of createImportItems()) {
+            if (type === IMPORT_TYPE.BOOKMARK) {
+                // Bookmarks should always yield before history
+                bookmarkItems = data
+            } else if (type === IMPORT_TYPE.HISTORY) {
+                // Don't include pages in history that exist as bookmarks as well
+                data = differMaps(bookmarkItems)(data)
+            }
+
+            if (allowTypes[type]) {
+                await stateManager.addItems(data)
+            }
+        }
+    }
+
+    /**
+     * @param {any} allowTypes Object with keys as valid import types pointing to bool values denoting whether
+     * or not to process that given type of imports.
+     */
+    async startImport(allowTypes) {
+        // Perform history-stubs, vists, and history import state creation, if import not in progress
+        const importInProgress = await getImportInProgressFlag()
+        if (!importInProgress) {
+            await this.prepareImportItems(allowTypes)
+        }
+
+        this.port.postMessage({ cmd: CMDS.START }) // Tell UI to finish loading state and move into progress view
+        setImportInProgressFlag()
+        this.importer.start()
+    }
+
+    /**
+     * The cleanup logic that happens when user chooses to finish an import
+     * (either after completion or cancellation).
+     */
+    async finishImport() {
+        clearImportInProgressFlag()
+
+        // Re-init the estimates view with updated estimates data
+        const estimateCounts = await getEstimateCounts({ forceRecalc: true })
+        this.port.postMessage({ cmd: CMDS.INIT, ...estimateCounts })
+    }
+
+    async cancelImport() {
+        this.importer.stop()
+
+        // Clean up any import-related stubs or state
+        await stateManager.clearItems()
+
+        // Resume UI at complete state
+        this.port.postMessage({ cmd: CMDS.COMPLETE })
+        clearImportInProgressFlag()
+    }
+}
+
 /**
  * Main connection handler to handle background importing and fetch&analysis batching
  * logic via commands issued from the UI.
+ *
+ * @param {runtime.Port} port Object passed from API event.
+ * @returns {ImportConnectionHandler}
  */
-export default async function importsConnectionHandler(port) {
+export default function importsConnectionHandler(port) {
     // Make sure to only handle connection logic for imports (allows other use of runtime.connect)
     if (port.name !== IMPORT_CONN_NAME) return
 
     console.log('importer connected')
 
-    const importer = new ProgressManager(processChunk(port), DEF_CONCURRENCY)
-
-    // If import isn't started earlier, get estimates and set view state to init
-    const importInProgress = await getImportInProgressFlag()
-    if (!importInProgress) {
-        // Make sure estimates view init'd with count data
-        const estimateCounts = await getEstimateCounts({ forceRecalc: false })
-        port.postMessage({ cmd: CMDS.INIT, ...estimateCounts })
-    } else {
-        // Make sure to start the view in paused state
-        port.postMessage({ cmd: CMDS.PAUSE })
-    }
-
-    // Handle any incoming messages to control theimporter
-    port.onMessage.addListener(async ({ cmd, payload }) => {
-        switch (cmd) {
-            case CMDS.START:
-                return await startImport(port, importer, payload)
-            case CMDS.RESUME:
-                return importer.start()
-            case CMDS.PAUSE:
-                return importer.stop()
-            case CMDS.CANCEL:
-                return await cancelImport(port, importer)
-            case CMDS.FINISH:
-                return await finishImport(port)
-            case CMDS.SET_CONCURRENCY:
-                return (importer.concurrency = payload)
-            default:
-                return console.error(`unknown command: ${cmd}`)
-        }
-    })
+    return new ImportConnectionHandler(port)
 }
