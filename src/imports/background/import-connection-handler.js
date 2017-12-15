20b984a5ee15c4e35f82eb857f7b31508260fa33
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-12-15 12:31:13 +0700

    Hook up imports connection handler to new state + runner
    - no more need for RxJS in imports! all pure JS now
    - TODO: implement concurrency on the batch-data level
    - TODO: implement concurrent Promise cancelling via array of tokens
    - TODO: page refresh pause not working
    
    Remove old state interface logic

--- src/imports/background/import-connection-handler.js
+++ src/imports/background/import-connection-handler.js
@@ -1,4 +1,3 @@
-import PromiseBatcher from 'src/util/promise-batcher'
 import {
     CMDS,
     IMPORT_CONN_NAME,
@@ -7,18 +6,14 @@ import {
 } from 'src/options/imports/constants'
 import getEstimateCounts from './import-estimates'
 import processImportItem from './import-item-processor'
-import {
-    getImportItems,
-    setImportItems,
-    removeImportItem,
-    clearImportItems,
-} from './'
+import stateManager from './import-state'
 import createImportItems from './import-item-creation'
 import {
     getImportInProgressFlag,
     setImportInProgressFlag,
     clearImportInProgressFlag,
 } from '../'
+import ProgressManager from './import-progress'
 
 /**
  * Handles building the collection of import items in local storage.
@@ -31,7 +26,7 @@ async function prepareImportItems(allowTypes = {}) {
 
     // Union all import item maps, allowing old ext items precedence over bookmarks which have
     //  precedence over history
-    await setImportItems(
+    await stateManager.setItems(
         new Map([
             ...(allowTypes[IMPORT_TYPE.HISTORY] ? items.historyItemsMap : []),
             ...(allowTypes[IMPORT_TYPE.BOOKMARK] ? items.bookmarkItemsMap : []),
@@ -41,53 +36,14 @@ async function prepareImportItems(allowTypes = {}) {
 }
 
 /**
- * Creates observer functions to afford sending of messages over connection port
- * to UI on certain batcher events. Note that next (success) and error handlers are pretty
- * much the same; they either contain error or output/status keys, which UI handles if present.
- *
- * @param {Port} port The open connection port to send messages over.
- */
-const getBatchObserver = port => {
-    const handleFinishedItem = ({
-        input: [encodedUrl, { type, url }],
-        output: { status } = {},
-        error,
-    }) => {
-        // Send item data + outcome status down to UI (and error if present)
-        port.postMessage({
-            cmd: CMDS.NEXT,
-            url: url,
-            type,
-            status,
-            error,
-        })
-
-        removeImportItem(encodedUrl)
-    }
-
-    return {
-        // Triggers on any error thrown during the processing of each input
-        error: handleFinishedItem,
-        // Triggers on the successful finish of each batch input
-        next: handleFinishedItem,
-        // Triggers when ALL batch inputs are finished
-        async complete() {
-            // Tell UI that it's finished
-            port.postMessage({ cmd: CMDS.COMPLETE })
-            clearImportInProgressFlag()
-        },
-    }
-}
-
-/**
  * Essentially the logic which happens when the user presses the "Start Import" button.
  *
  * @param {Port} port The open connection port to send messages over.
- * @param {any} batch The init'd batcher to init with input and then start.
+ * @param {ImportProgressManager} importer The progress maanger instance to start.
  * @param {any} allowTypes Object with keys as valid import types pointing to bool values denoting whether
  * or not to process that given type of imports.
  */
-async function startImport(port, batch, allowTypes) {
+async function startImport(port, importer, allowTypes) {
     // Perform history-stubs, vists, and history import state creation, if import not in progress
     const importInProgress = await getImportInProgressFlag()
     if (!importInProgress) {
@@ -96,7 +52,7 @@ async function startImport(port, batch, allowTypes) {
 
     port.postMessage({ cmd: CMDS.START }) // Tell UI to finish loading state and move into progress view
     setImportInProgressFlag()
-    batch.start()
+    importer.start()
 }
 
 /**
@@ -111,16 +67,56 @@ async function finishImport(port) {
     port.postMessage({ cmd: CMDS.INIT, ...estimateCounts })
 }
 
-async function cancelImport(port, batch) {
-    batch.stop()
+async function cancelImport(port, importer) {
+    importer.stop()
 
     // Clean up any import-related stubs or state
-    await clearImportItems()
+    await stateManager.clearItems()
 
     // Resume UI at complete state
     port.postMessage({ cmd: CMDS.COMPLETE })
     clearImportInProgressFlag()
 }
+
+/**
+ * Binds the chunk processor logic to the UI connection port, to allow progress to be reported from within.
+ *
+ * @param {Port} port
+ */
+const processChunk = port =>
+    /**
+     * @param {any} chunkData The data of currently-being-processed chunk (contains `chunk` and `chunkKey`).
+     * @param {number} concurrency Externally-specified concurrency level to use to implement concurrency (TODO).
+     * @param {any} token Token object to allow attaching of rejection callback, affording caller Promise cancellation.
+     */
+    async function({ chunk, chunkKey }, concurrency, token) {
+        for (const [encodedUrl, importItem] of Object.entries(chunk)) {
+            let status, url, error
+            try {
+                const processingResult = await processImportItem(
+                    importItem,
+                    token,
+                )
+                status = processingResult.status
+            } catch (err) {
+                // Throw execution was cancelled, throw error up the stack
+                if (err.cancelled) {
+                    throw err
+                }
+                error = err.message
+            } finally {
+                // Send item data + outcome status down to UI (and error if present)
+                port.postMessage({
+                    cmd: CMDS.NEXT,
+                    url: url,
+                    type: importItem.type,
+                    status,
+                    error,
+                })
+                await stateManager.removeItem(chunkKey, encodedUrl)
+            }
+        }
+    }
 
 /**
  * Main connection handler to handle background importing and fetch&analysis batching
@@ -132,12 +128,7 @@ export default async function importsConnectionHandler(port) {
 
     console.log('importer connected')
 
-    const batch = new PromiseBatcher({
-        nextInputBatchCb: getImportItems,
-        processingCb: processImportItem,
-        concurrency: DEF_CONCURRENCY,
-        observer: getBatchObserver(port),
-    })
+    const importer = new ProgressManager(processChunk(port), DEF_CONCURRENCY)
 
     // If import isn't started earlier, get estimates and set view state to init
     const importInProgress = await getImportInProgressFlag()
@@ -150,21 +141,21 @@ export default async function importsConnectionHandler(port) {
         port.postMessage({ cmd: CMDS.PAUSE })
     }
 
-    // Handle any incoming messages to control the batch
+    // Handle any incoming messages to control theimporter
     port.onMessage.addListener(async ({ cmd, payload }) => {
         switch (cmd) {
             case CMDS.START:
-                return await startImport(port, batch, payload)
+                return await startImport(port, importer, payload)
             case CMDS.RESUME:
-                return batch.start()
+                return importer.start()
             case CMDS.PAUSE:
-                return batch.stop()
+                return importer.stop()
             case CMDS.CANCEL:
-                return await cancelImport(port, batch)
+                return await cancelImport(port, importer)
             case CMDS.FINISH:
                 return await finishImport(port)
             case CMDS.SET_CONCURRENCY:
-                return (batch.concurrency = payload)
+                return (importer.concurrency = payload)
             default:
                 return console.error(`unknown command: ${cmd}`)
         }
