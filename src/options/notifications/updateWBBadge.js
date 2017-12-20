03f42e3065620a12ca32b3948ca89b31f4124a39
Mukesh Kharita <mukeshnithcse@gmail.com>
2017-12-20 09:38:39 +0530

    Remove lint errors

--- src/options/notifications/updateWBBadge.js
+++ src/options/notifications/updateWBBadge.js
@@ -1,10 +1,10 @@
 import setUnreadCount from '../../util/setUnreadCount.js'
 
 export default async function updateWBBadge() {
-    let ba = chrome.browserAction
+    const ba = chrome.browserAction
     ba.setBadgeBackgroundColor({ color: [62, 185, 149, 128] })
     try {
-        let res = await setUnreadCount(0)
+        const res = await setUnreadCount(0)
         if (res > 0) {
             await ba.setBadgeText({ text: '' + res })
         } else {
