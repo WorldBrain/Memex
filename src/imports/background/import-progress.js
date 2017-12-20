3c661d1ab92ab97aa5019036fd6df52af4edc4e9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-12-20 10:17:40 +0700

    Write misc. fixes for fetch-page-data + import cancellation
    Update fetch-page-data to return Promise + XHR abort cb
    
    - allows outside users of module to abort the XHR
    
    Implement cancellation on import-item-processor module
    
    - this makes import item processor a class that encapsulates some state affording cancellation of execution
    - ImportsProgress instance keeps track of all the ItemProcessor instances and cancels them if ConnectionHandler receives message instructing it
    - ImportsProgress now also wipes the index queue to remove any queued-up index reqs from other ItemProcessors
    - one may get through as it is currently processing on the index queue, but we can look into rollbacks later
    
    Fix Promise execution order in fetch-page-data
    
    - main interface doesn't need to be async function; it returns Promise + abortion callback
    - also stop the inner XHR async logic from running until async returned `promise` function called in caller

--- src/imports/background/import-progress.js
+++ src/imports/background/import-progress.js
@@ -1,23 +1,17 @@
 import promiseLimit from 'promise-limit'
 
+import { indexQueue } from 'src/search/search-index'
 import stateManager from './import-state'
-import processImportItem from './import-item-processor'
+import ItemProcessor from './import-item-processor'
 
 class ImportProgressManager {
     static IMPORTS_PROGRESS_KEY = 'is-imports-in-progress'
     static CONCURR_LIMIT = 10
 
     /**
-     * @property {any[]} Token objects with `cancel` method to afford cancellation of currently
-     *  running Promises.
+     * @property {ItemProcessor[]} Currently scheduled processor instances, affording control over execution.
      */
-    tokens = []
-
-    /**
-     * @property {number} Number between 0 and `this._concurrency` representing the latest scheduled
-     *  job's index in `this.tokens`.
-     */
-    _tokenIndex = 0
+    processors = []
 
     /**
      * @property {number} Currently set level of concurrency.
@@ -30,11 +24,14 @@ class ImportProgressManager {
      */
     observer
 
+    /**
+     * @property {boolean} Flag denoting whether or not current state is stopped or not.
+     */
+    stopped = false
+
     constructor(initConcurrency, initObserver) {
         this.concurrency = initConcurrency
         this.observer = initObserver
-
-        this.processImportItem = this.makeCancellable(processImportItem)
     }
 
     set concurrency(value) {
@@ -62,32 +59,28 @@ class ImportProgressManager {
     }
 
     /**
-     * Allow token index state to loop back around from 0 to `this._concurrency`.
+     * Get next available processor index.
+     *
+     * @returns {number} Index between 0 and `this._concurrency`.
      */
-    nextTokenIndex() {
-        if (this._tokenIndex === this._concurrency - 1) {
-            this._tokenIndex = 0
-        } else {
-            this._tokenIndex += 1
+    nextProcIndex() {
+        for (let i = 0; i < this._concurrency; i++) {
+            const proc = this.processors[i]
+
+            // Take current spot if empty or saved processor is in finished state
+            if (proc == null || proc.finished) {
+                return i
+            }
         }
 
-        return this._tokenIndex
-    }
-
-    /**
-     * Updates token state to add another cancellable token for a running job.
-     *
-     * @param {Function} cancelCb Callback to run when all jobs are cancelled.
-     */
-    bindNextCancellableToken(cancelCb) {
-        this.tokens[this.nextTokenIndex()] = { cancel: cancelCb }
+        return 0 // Base case; should not be reached
     }
 
     /**
      * Start execution
      */
     async start() {
-        let cancelled = false
+        this.stopped = false
 
         // Iterate through data chunks from the state manager
         for await (const { chunk, chunkKey } of stateManager.getItems()) {
@@ -100,60 +93,39 @@ class ImportProgressManager {
             } catch (err) {
                 // If execution cancelled break Iterator processing
                 if (err.cancelled) {
-                    cancelled = true
                     break
                 }
                 console.error(err)
             }
         }
 
-        if (!cancelled) {
+        if (!this.stopped) {
             // Notify observer that we're done!
             this.observer.complete()
         }
     }
 
     /**
-     * Goes through each Promise token and runs the `cancel()` callback, passing in Error
-     * with `cancel` bool property. Token state is cleared afterwards.
+     * Goes through each processor and runs the `cancel` method.
+     * Processor state is cleared afterwards.
      */
     stop() {
-        // Create error with `cancelled` bool property to distinguish in callers
-        const err = new Error('Stopping progress')
-        err.cancelled = true
+        this.stopped = true
 
-        let token
-        while (this.tokens.length) {
-            token = this.tokens.pop()
-            if (token != null && typeof token.cancel === 'function') {
-                // Run token's cancal callback to stop running async logic
-                token.cancel(err)
-            }
-        }
+        // Run processors' cancal methods to stop running async logic, then wipe references
+        this.processors.forEach(proc => proc != null && proc.cancel())
+        this.processors = []
+
+        // Ensure index queue is cleared so queued up item's indexing doesn't happen
+        indexQueue.clear()
     }
-
-    /**
-     * @param {(a: any) => Promise<any>} asyncFn Async function to make cancellable
-     * @returns {(a: any) => Promise<any>} asyncFn Same function which will be interrupted upon `stop()` method call.
-     */
-    makeCancellable = asyncFn => (...args) =>
-        new Promise((resolve, reject) => {
-            // Bind the reject callback to token to allow outside cancellation of `this` Promise
-            this.bindNextCancellableToken(reject)
-
-            // Run orig async function
-            asyncFn(...args)
-                .then(resolve)
-                .catch(reject)
-        })
 
     /**
      * @param {string} chunkKey The key of the chunk currently being processed.
      * @returns {(chunkEntry) => Promise<void>} Async function affording processing of single entry in chunk.
      */
     processItem = chunkKey => async ([encodedUrl, importItem]) => {
-        // Used to flag if `this` async logic gets cancelled (don't send message)
-        let cancelled = false
+        const processor = new ItemProcessor()
 
         // Used to build the message to send to observer
         const msg = {
@@ -162,19 +134,25 @@ class ImportProgressManager {
         }
 
         try {
-            const res = await this.processImportItem(importItem)
+            if (this.stopped) {
+                throw ItemProcessor.makeInterruptedErr()
+            }
+
+            // Save reference to processor for cancelling later
+            this.processors[this.nextProcIndex()] = processor
+            const res = await processor.process(importItem)
             msg.status = res.status
         } catch (err) {
             // Throw execution was cancelled, throw error up the stack
             if (err.cancelled) {
-                cancelled = true
                 throw err
-            } else {
-                msg.error = err.message
             }
+            msg.error = err.message
         } finally {
+            processor.finished = true
+
             // Send item data + outcome status down to UI (and error if present)
-            if (!cancelled) {
+            if (!processor.cancelled) {
                 this.observer.next(msg)
 
                 // Either flag as error or remove from state depending on processing error status
