b6fbfe68a6ff3e11dd16495e9652cf78c6f88e03
Mukesh Kharita <mukeshnithcse@gmail.com>
2017-12-22 15:41:28 +0530

    Now only new notifications receive after installing the extension, the previos notification directly goes to the archive

--- src/options/notifications/polling/fetchNewNotifs.js
+++ src/options/notifications/polling/fetchNewNotifs.js
@@ -9,15 +9,20 @@ export default async function fetchNewNotifs() {
         const res = await fetch(
             'https://salty-fjord-43561.herokuapp.com/api/notifications',
         )
+        const installTimestamp = (await browser.storage.local.get(
+            'extension_install_time',
+        )).extension_install_time
+
         const newNotes = await res.json()
         newNotes.forEach(function(element) {
+            const dateTimestamp = new Date(element.date).getTime()
             db.put({
                 _id: 'notifs_' + element._id,
                 MongoId: element._id,
                 title: element.title,
                 body: element.body,
                 date: element.date,
-                viewed: false,
+                viewed: installTimestamp > dateTimestamp,
             })
         })
         await setUnreadCount()
