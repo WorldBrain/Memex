15229c93d3231652ae9fe7e0aff54bbfc4ca21f6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-12-29 10:35:02 +0700

    Set up bg script interfaces for tag functions
    - index mutation fns for add + delete - concurrency safe
    - tags suggestion/search fn
    - all remotely callbable from other scripts

--- src/search/background/index.js
+++ src/search/background/index.js
@@ -1,4 +1,14 @@
+import { makeRemotelyCallable } from 'src/util/webextensionRPC'
 import searchConnectionHandler from './search-connection-handler'
+import { addTagsConcurrent } from '../search-index/add'
+import { delTagsConcurrent } from '../search-index/del'
+import suggestTags from '../search-index/tag-suggestions'
+
+makeRemotelyCallable({
+    addTags: addTagsConcurrent,
+    delTags: delTagsConcurrent,
+    suggestTags,
+})
 
 // Allow other scripts to connect to background index and send queries
 browser.runtime.onConnect.addListener(searchConnectionHandler)
