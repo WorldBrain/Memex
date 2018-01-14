03330fc989058b1f45f5249d410720045ba14630
Mukesh Kharita <mukeshnithcse@gmail.com>
2018-01-15 00:28:57 +0530

    Added pause and play icon

--- src/popup/components/HistoryPauser.js
+++ src/popup/components/HistoryPauser.js
@@ -6,12 +6,10 @@ import styles from './Button.css'
 
 const getIconStyles = isPaused =>
     classNames({
-        'material-icons': true,
-        [styles.clickableIcon]: true,
-        [styles.clickableIconNeg]: !isPaused,
+        [styles.customIcon]: true,
+        [styles.playIcon]: isPaused,
+        [styles.pauseIcon]: !isPaused,
     })
-const getIcon = isPaused =>
-    isPaused ? 'play_circle_outline' : 'pause_circle_outline'
 
 const HistoryPauser = ({
     onConfirm,
@@ -22,9 +20,10 @@ const HistoryPauser = ({
     ...selectProps
 }) => (
     <div className={classNames(styles.item, styles.itemDropdown)}>
-        <i className={getIconStyles(isPaused)} onClick={onConfirm}>
-            {getIcon(isPaused)}
-        </i>
+        <div
+            className={classNames(getIconStyles(isPaused))}
+            onClick={onConfirm}
+        />
         {isPaused ? (
             'Indexing paused'
         ) : (
