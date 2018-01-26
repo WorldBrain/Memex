40e6daa3081b279127d2af52062dff8782fdea54
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-01-26 11:50:39 +0700

    Set analytics to be opt-in (disabled by default)
    - TODO: some kind of option on install to enable it:
    https://developer.mozilla.org/en-US/Add-ons/Source_Code_Submission
    - also bit of cleanup of background script entry point (install logic)

--- src/background.js
+++ src/background.js
@@ -44,49 +44,53 @@ async function openOverview() {
     }
 }
 
+async function onInstall() {
+    // Ensure default blacklist entries are stored (before doing anything else)
+    await blacklist.addToBlacklist(blacklistConsts.DEF_ENTRIES)
+    analytics.trackEvent({ category: 'Global', action: 'Install' })
+    // Open onboarding page
+    browser.tabs.create({ url: '/options/options.html#/new_install' })
+    // Store the timestamp of when the extension was installed + default blacklist
+    browser.storage.local.set({ [installTimeStorageKey]: Date.now() })
+}
+
+async function onUpdate() {
+    // If no prior conversion, convert old ext blacklist + show static notif page
+    const {
+        [blacklistConsts.CONVERT_TIME_KEY]: blacklistConverted,
+        [OLD_EXT_UPDATE_KEY]: alreadyUpdated,
+        [OLD_EXT_KEYS.INDEX]: oldExtIndex,
+    } = await browser.storage.local.get([
+        blacklistConsts.CONVERT_TIME_KEY,
+        OLD_EXT_UPDATE_KEY,
+        OLD_EXT_KEYS.INDEX,
+    ])
+
+    if (!blacklistConverted) {
+        convertOldExtBlacklist()
+    }
+
+    // Over complicated check of old ext data + already upgraded flag to only show update page once
+    if (!alreadyUpdated && oldExtIndex && oldExtIndex.index instanceof Array) {
+        browser.tabs.create({ url: UPDATE_URL })
+        browser.storage.local.set({ [OLD_EXT_UPDATE_KEY]: Date.now() })
+    }
+}
+
 browser.commands.onCommand.addListener(command => {
-    if (command === 'openOverview') {
-        openOverview()
+    switch (command) {
+        case 'openOverview':
+            return openOverview()
+        default:
     }
 })
 
-browser.runtime.onInstalled.addListener(async details => {
+browser.runtime.onInstalled.addListener(details => {
     switch (details.reason) {
         case 'install':
-            // Ensure default blacklist entries are stored (before doing anything else)
-            await blacklist.addToBlacklist(blacklistConsts.DEF_ENTRIES)
-            analytics.trackEvent({ category: 'Global', action: 'Install' })
-            // Open onboarding page
-            browser.tabs.create({ url: '/options/options.html#/new_install' })
-            // Store the timestamp of when the extension was installed + default blacklist
-            browser.storage.local.set({ [installTimeStorageKey]: Date.now() })
-            break
+            return onInstall()
         case 'update':
-            // If no prior conversion, convert old ext blacklist + show static notif page
-            const {
-                [blacklistConsts.CONVERT_TIME_KEY]: blacklistConverted,
-                [OLD_EXT_UPDATE_KEY]: alreadyUpdated,
-                [OLD_EXT_KEYS.INDEX]: oldExtIndex,
-            } = await browser.storage.local.get([
-                blacklistConsts.CONVERT_TIME_KEY,
-                OLD_EXT_UPDATE_KEY,
-                OLD_EXT_KEYS.INDEX,
-            ])
-
-            if (!blacklistConverted) {
-                convertOldExtBlacklist()
-            }
-
-            // Over complicated check of old ext data + already upgraded flag to only show update page once
-            if (
-                !alreadyUpdated &&
-                oldExtIndex &&
-                oldExtIndex.index instanceof Array
-            ) {
-                browser.tabs.create({ url: UPDATE_URL })
-                browser.storage.local.set({ [OLD_EXT_UPDATE_KEY]: Date.now() })
-            }
-            break
+            return onUpdate()
         default:
     }
 })
