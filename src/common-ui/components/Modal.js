08bc2473e73c201980d83f6522185e22856002f9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-01-30 10:50:21 +0700

    Separate react-portal based Modal comp into Overlay
    - Overlay simply acts as a container around the React Portal logic, binding the children to a new DOM root element
    - Modal is now just the view part of what it was before

--- src/common-ui/components/Modal.js
+++ src/common-ui/components/Modal.js
@@ -1,56 +1,33 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
+import Overlay from './Overlay'
 import styles from './Modal.css'
 
-class Modal extends Component {
+class Modal extends PureComponent {
     static propTypes = {
         onClose: PropTypes.func,
-        children: PropTypes.oneOfType([
-            PropTypes.node,
-            PropTypes.arrayOf(PropTypes.node),
-        ]).isRequired,
-    }
-
-    constructor(props) {
-        super(props)
-
-        this.modalRoot = document.createElement('div')
-    }
-
-    componentDidMount() {
-        document.body.appendChild(this.modalRoot)
-    }
-
-    componentWillUnmount() {
-        document.body.removeChild(this.modalRoot)
+        children: Overlay.propTypes.children,
     }
 
     render() {
-        return ReactDOM.createPortal(
-            <div
+        return (
+            <Overlay
                 className={styles.overlay}
-                disabled={this.props.onClose == null}
-                onClick={this.props.onClose}
+                innerClassName={styles.popup}
+                onClose={this.props.onClose}
             >
-                <div
-                    className={styles.popup}
-                    onClick={event => event.stopPropagation()}
-                >
-                    {this.props.onClose && (
-                        <button
-                            className={cx(styles.close, styles.button)}
-                            onClick={this.props.onClose}
-                        >
-                            ×
-                        </button>
-                    )}
-                    <div className={styles.content}>{this.props.children}</div>
-                </div>
-            </div>,
-            this.modalRoot,
+                {this.props.onClose && (
+                    <button
+                        className={cx(styles.close, styles.button)}
+                        onClick={this.props.onClose}
+                    >
+                        ×
+                    </button>
+                )}
+                <div className={styles.content}>{this.props.children}</div>
+            </Overlay>
         )
     }
 }
