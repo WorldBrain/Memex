b394588abd061319cd51f492b5e404936dca70cf
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-02-12 11:47:56 +0700

    Implement current tab bookmarking via shortcut cmd
    - also big cleanup of the create bookmark by URL code
    - seems to work well so far

--- src/bookmarks/background/addition.js
+++ src/bookmarks/background/addition.js
@@ -5,6 +5,7 @@ import * as index from 'src/search'
 import db from 'src/pouchdb'
 import { transformToBookmarkDoc } from 'src/imports'
 import { generatePageDocId } from 'src/page-storage'
+import { generateBookmarkDocId } from 'src/bookmarks'
 import storePage from 'src/page-storage/store-page'
 
 async function getAttachments(pageData) {
@@ -54,62 +55,29 @@ export async function createNewPageForBookmark(id, bookmarkInfo) {
 }
 
 /**
- * Adds a bookmarkDoc entry into specified index doc.
- * NOTE: Assumes the existence of indexed `pageId`.
- *
- * @param {bool} pageExist PageExist or not.
- * @param {string} pageId ID of page doc to associate with.
- * @param {object} pageDoc page doc associated with pageId.
- * @param {object} bookmarkDoc bookmark doc associated with pageId.
-
- */
-async function addBookmarkDoc(pageExist, pageId, pageDoc, bookmarkDoc) {
-    if (pageExist) {
-        return await Promise.all([
-            index.addBookmarkConcurrent(pageId),
-            db.put(bookmarkDoc),
-        ])
-    } else {
-        return await Promise.all([
-            await index.addPageConcurrent({
-                pageDoc: pageDoc,
-                bookmarkDocs: [bookmarkDoc],
-            }),
-            await db.put(bookmarkDoc),
-        ])
-    }
-}
-
-/**
- * TODO: Decided if we actually need these bookmark docs in Pouch; I don't think they're being used for anything.
- *
  * @param {string} url URL to generate page ID from to associate bookmark with.
  * @throws {Error} Error thrown if page for supplied URL not indexed.
  * @returns {Promise<void>}
  */
-export async function createBookmarkByUrl(url, tabId = null) {
+export async function createBookmarkByUrl(url, tabId) {
     const pageId = generatePageDocId({ url })
-    let pageDoc
 
-    // This is used for page is exist when we are bookmarking the page and default value is 1 that represents page exist in db.
-    let pageExist = 1
+    const pageDoc = await index.initSingleLookup()(pageId)
 
-    const timestamp = Date.now()
-
-    try {
-        pageDoc = await db.get(pageId)
-    } catch (err) {
-        if (err.status === 404) {
-            pageExist = 0
-            pageDoc = await storePage({ tabId, url })
-        }
+    // Already bookmarked
+    if (pageDoc != null && pageDoc.bookmarks.size > 0) {
+        throw new Error(`Bookmark already exists for page with URL: ${url}`)
     }
 
-    const bookmarkDoc = transformToBookmarkDoc({ _id: pageId })({
-        dateAdded: timestamp,
-        title: pageDoc.content.title,
-        url: pageDoc.url,
-    })
+    // Generally the page will always exist, but this is to catch that if not
+    if (pageDoc == null) {
+        return await index.addPageConcurrent({
+            pageDoc: await storePage({ url, tabId }),
+            bookmarkDocs: [
+                { _id: generateBookmarkDocId({ url, timestamp: Date.now() }) },
+            ],
+        })
+    }
 
-    await addBookmarkDoc(pageExist, pageId, pageDoc, bookmarkDoc)
+    return await index.addBookmarkConcurrent(pageId)
 }
--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,8 @@
 import urlRegex from 'url-regex'
 
 import { OVERVIEW_URL } from 'src/background'
+import { isLoggable } from 'src/activity-logger'
+import { createBookmarkByUrl } from 'src/bookmarks/background/addition'
 
 export async function openOverview() {
     const [currentTab] = await browser.tabs.query({ active: true })
@@ -16,13 +18,21 @@ export async function openOverview() {
 export async function bookmarkPage() {
     const [currentTab] = await browser.tabs.query({ active: true })
 
-    // TODO: actually bookmark
-    browser.notifications.create({
-        type: 'basic',
-        title: 'Memex Page Bookmarked',
-        iconUrl: '/img/worldbrain-logo-narrow.png',
-        message: currentTab.url,
-    })
+    // Don't do anything on non-loggable pages
+    if (!isLoggable({ url: currentTab.url })) {
+        return
+    }
+
+    try {
+        await createBookmarkByUrl(currentTab.url, currentTab.id)
+
+        browser.notifications.create({
+            type: 'basic',
+            title: 'Memex Page Bookmarked',
+            iconUrl: '/img/worldbrain-logo-narrow.png',
+            message: currentTab.url,
+        })
+    } catch (error) {}
 }
 
 // Set up commands listener
