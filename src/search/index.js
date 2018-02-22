977330d86295d691f3904865f4db15fcbedc2a8a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-02-22 11:29:01 +0700

    Hook up new search to UI
    - bit easier to play around with newly indexed data
    - note it outputs to the background script stdout, actual UI should say "no results" as it will query the old index
    - output is only the keys (URL, latest visit time) - no mapping back to docs yet

--- src/search/index.js
+++ src/search/index.js
@@ -1,3 +1,6 @@
+import newIndex from 'src/search-index'
+import QueryBuilder from './query-builder'
+import mapResultsToPouchDocs from './map-search-to-pouch'
 import * as oldBackend from './search-index-old/api'
 import * as newBackend from './search-index-new'
 
@@ -97,8 +100,89 @@ export async function grabExistingKeys(...args) {
 // Searching & suggesting
 //
 
-export async function search(...args) {
-    return await (await getBackend()).search(...args)
+export async function search({
+    query,
+    startDate,
+    endDate,
+    tags = [],
+    domains = [],
+    skip = 0,
+    limit = 10,
+    getTotalCount = false,
+    showOnlyBookmarks = false,
+    mapResultsFunc = mapResultsToPouchDocs,
+}) {
+    query = query.trim() // Don't count whitespace searches
+
+    const res = await newIndex.search({
+        query,
+        startTime: startDate,
+        endTime: endDate,
+        bookmarks: showOnlyBookmarks,
+        skip,
+        limit,
+    })
+    console.log('new index res:', res)
+
+    // Create SI query
+    const indexQuery = new QueryBuilder()
+        .searchTerm(query)
+        .filterTime({ startDate, endDate }, 'bookmark/')
+        .filterTime({ startDate, endDate }, 'visit/')
+        .filterTags(tags)
+        .filterDomains(domains)
+        .skipUntil(skip)
+        .limitUntil(limit)
+        .bookmarksFilter(showOnlyBookmarks)
+        .get()
+
+    // If there is only Bad Terms don't continue
+    if (indexQuery.isBadTerm) {
+        return {
+            docs: [],
+            resultsExhausted: true,
+            totalCount: getTotalCount ? 0 : undefined,
+            isBadTerm: true,
+        }
+    }
+
+    console.log('DEBUG: query', indexQuery)
+
+    // Get index results, filtering out any unexpectedly structured results
+    const {
+        results,
+        totalCount,
+    } = await (await getBackend()).search(indexQuery, {
+        count: getTotalCount,
+    })
+
+    // console.log('got results', results)
+
+    // Short-circuit if no results
+    if (!results.length) {
+        return {
+            docs: [],
+            resultsExhausted: true,
+            totalCount,
+            isBadTerm: false,
+        }
+    }
+
+    // Match the index results to data docs available in Pouch, consolidating meta docs
+    const docs = await mapResultsFunc(results, {
+        startDate,
+        endDate,
+        showOnlyBookmarks,
+    })
+
+    console.log('DEBUG: final UI results', docs)
+
+    return {
+        docs,
+        resultsExhausted: docs.length < limit,
+        totalCount,
+        isBadTerm: false,
+    }
 }
 
 export async function suggest(...args) {
