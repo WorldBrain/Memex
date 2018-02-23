712fd0a67a93465b4b8c42441aa95a1294804f95
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-02-23 11:00:05 +0700

    Move old-implementation specific search code
    - query-builder maybe can be re-used, but may be unnecessary; we'll see

--- src/search/search-index-old/map-search-to-pouch.js
+++ src/search/search-index-old/map-search-to-pouch.js
@@ -1,6 +1,6 @@
 import reduce from 'lodash/fp/reduce'
 
-import db, { bulkGetResultsToArray } from '../pouchdb'
+import db, { bulkGetResultsToArray } from '../../pouchdb'
 import { removeKeyType } from './util'
 
 /**
