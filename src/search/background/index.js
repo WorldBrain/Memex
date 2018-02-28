c6c69de61f876dd6ce1f000825ba71ab150d3d0c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-02-28 12:37:09 +0700

    Update popup container's coupling to old page structure
    - hide behind `getPage` and `pageLookup` functions
    - also fix up old index's `getPage` return type

--- src/search/background/index.js
+++ src/search/background/index.js
@@ -3,58 +3,43 @@ import searchConnectionHandler from './search-connection-handler'
 import {
     addTag,
     delTag,
-    initSingleLookup,
-    removeKeyType,
     suggest,
     delBookmark,
     addBookmark,
     handleBookmarkCreation,
+    getPage,
     delPages,
     delPagesByDomain,
     delPagesByPattern,
     getMatchingPageCount,
 } from '../'
 
-const singleLookup = initSingleLookup()
-
 makeRemotelyCallable({
     addTag,
     delTag,
     suggest,
-    pageLookup: (id, projectOpts = {}) =>
-        singleLookup(id).then(
-            page =>
-                page != null
-                    ? transformPageForSending(page, projectOpts)
-                    : page,
-        ),
     addBookmark,
     delBookmark,
     delPages,
     delPagesByDomain,
     delPagesByPattern,
     getMatchingPageCount,
+    pageLookup: url => getPage(url).then(transformPageForSending),
 })
 
-const destructPageAtt = (att = []) => [...att]
+async function transformPageForSending(page) {
+    if (page == null) {
+        return null
+    }
 
-/**
- * Objects like `Set` and `Map` won't send nicely over the script connection APIs. Destructure them into
- * Arrays for sending, if defined in `projectOpts`.
- *
- * @param {any} page
- * @param {any} projectOpts Object containing attribute for each `page` Set att. If att is defined,
- *  destructure it into array.
- */
-const transformPageForSending = (page, projectOpts) => ({
-    ...page,
-    urlTerms: projectOpts.urlTerms ? destructPageAtt(page.urlTerms) : [],
-    titleTerms: projectOpts.titleTerms ? destructPageAtt(page.titleTerms) : [],
-    terms: projectOpts.terms ? destructPageAtt(page.terms) : [],
-    visits: projectOpts.visits ? destructPageAtt(page.visits) : [],
-    bookmarks: projectOpts.bookmarks ? destructPageAtt(page.bookmarks) : [],
-    tags: projectOpts.tags ? destructPageAtt(page.tags).map(removeKeyType) : [],
-})
+    await page.loadRels()
+
+    return {
+        hasBookmark: page.hasBookmark,
+        tags: page.tags,
+        latest: page.latest,
+    }
+}
 
 // Allow other scripts to connect to background index and send queries
 browser.runtime.onConnect.addListener(searchConnectionHandler)
