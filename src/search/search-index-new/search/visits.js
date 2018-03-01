a4db6cfc57034ea546e2876c9330e77bfe0cf192
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-03-01 13:02:01 +0700

    Move tags filtering to start of search algo
    - they should reduce the search size considerably, so do early

--- src/search/search-index-new/search/visits.js
+++ src/search/search-index-new/search/visits.js
@@ -11,13 +11,19 @@ import db, { Storage } from '..'
  * @param {number} [args.endTime=Date.now()] Upper-bound for visit time.
  * @param {number} [args.skip=0]
  * @param {number} [args.limit=10]
+ * @param {Set<string>} urlScopeSet Set of URLs to only include, if there are any defined.
  * @return {Map<string, number>} Map of URL keys to latest visit time numbers. Should be size <= skip + limit.
  */
 export async function getLatestVisitsByUrl(
     { startTime = 0, endTime = Date.now(), skip = 0, limit = 10 },
+    urlScopeSet,
     shallowLookback = false,
 ) {
     const latestVisitsByUrl = new Map()
+    const shouldRecord = url =>
+        !latestVisitsByUrl.has(url) && urlScopeSet.size > 0
+            ? urlScopeSet.has(url)
+            : true
 
     let visitColl = db.visits
         .where('[time+url]')
@@ -31,7 +37,7 @@ export async function getLatestVisitsByUrl(
 
     await visitColl.eachPrimaryKey(([time, url]) => {
         // Only ever record the first (latest) visit for each URL
-        if (!latestVisitsByUrl.get(url)) {
+        if (shouldRecord(url)) {
             latestVisitsByUrl.set(url, time)
         }
     })
