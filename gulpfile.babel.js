fea5858f55f87637ab3645d4f39b0fb08bc1c99f
Vincent den Boer <vincent@shishkabab.net>
2018-03-14 14:37:19 +0100

    Fix TypeScript errors preventing build
    Fix missing Symbol.asyncIterator
    
    Fix last TypeScript-related error
    
    Explictly import es7.symbol polyfill
    
    - Symbol.asyncIterator is a readonly prop so it throws a TypeError on being set
    - looked into babel-preset-env and the polyfills it replaces `babel-polyfill` import with; it doesn't include `es7.symbol`

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -90,7 +90,8 @@ function createBundle(
               browserify({ ...watchify.args, ...browserifySettings, entries }),
           ).on('update', bundle)
         : browserify({ ...browserifySettings, entries })
-    b.transform(babelify)
+    b.plugin(tsify)
+    b.transform(babelify, { extensions: ['.js', '.jsx', '.ts', '.tsx'] })
     b.transform(
         envify({
             NODE_ENV: production ? 'production' : 'development',
@@ -104,7 +105,6 @@ function createBundle(
         }),
         { global: true },
     )
-    b.plugin(tsify)
 
     if (cssOutput) {
         b.plugin(cssModulesify, {
@@ -120,6 +120,7 @@ function createBundle(
             b
                 .bundle()
                 .on('error', err => {
+                    console.error('ERROR creating bundle', err)
                     console.error(err.stack)
                     // Fail entire gulp build if browserify emits error, but not in dev/watch mode
                     if (!watch) {
