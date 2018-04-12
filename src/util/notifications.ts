ba17b144e7ea1eb64010a3b5de1cd8b6b914f057
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-04-12 09:34:31 +0700

    Set up notif module to filter out unknown opts on FF
    - FF only supports a small subset of web ext API notif options
    - filter others out if FF, as it will throw Errors on unexpected options... annoying

--- src/util/notifications.ts
+++ src/util/notifications.ts
@@ -4,6 +4,11 @@ import * as noop from 'lodash/fp/noop'
 export const DEF_ICON_URL = '/img/worldbrain-logo-narrow.png'
 export const DEF_TYPE = 'basic'
 
+// Chrome allows some extra notif opts that the standard web ext API doesn't support
+export interface NotifOpts extends Notifications.CreateNotificationOptions {
+    [chromeKeys: string]: any
+}
+
 const onClickListeners = new Map<string, Function>()
 
 browser.notifications.onClicked.addListener(id => {
@@ -14,15 +19,26 @@ browser.notifications.onClicked.addListener(id => {
     onClickListeners.delete(id) // Manually clean up ref
 })
 
+/**
+ * Firefox supports only a subset of notif options. If you pass unknowns, it throws Errors.
+ * So filter them down if browser is FF, else nah.
+ */
+function filterOpts({ type, iconUrl, title, message, ...rest }: NotifOpts): NotifOpts {
+    const opts = { type, iconUrl, title, message }
+    return browser.runtime.getBrowserInfo != null
+        ? opts
+        : { ...opts, ...rest }
+}
+
 async function createNotification(
-    notifOptions: Notifications.CreateNotificationOptions,
+    notifOptions: NotifOpts,
     onClick = noop as Function,
 ) {
-    const id = await browser.notifications.create({
+    const id = await browser.notifications.create(filterOpts({
         type: DEF_TYPE,
         iconUrl: DEF_ICON_URL,
         ...notifOptions,
-    })
+    }))
 
     onClickListeners.set(id, onClick)
 }
