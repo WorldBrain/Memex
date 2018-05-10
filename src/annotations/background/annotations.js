1bc552cc84af006a949ccca77af5b0ff7a7ef744
digi0ps <sriru1998@gmail.com>
2018-05-10 22:09:37 +0530

    Add new annotations module.

---
+++ src/annotations/background/annotations.js
@@ -0,0 +1,12 @@
+// Functions to return dummy data for now
+
+const random = () => Math.random() > 5
+
+export const fetchAnnotation = doc => {
+    if (random())
+        return {
+            body: `Random  comment #${doc.id}`,
+            timestamp: new Date().getTime(),
+        }
+    else return undefined
+}
---
+++ src/annotations/background/index.js
@@ -0,0 +1,3 @@
+import * as annotations from './annotations'
+
+export { annotations }
