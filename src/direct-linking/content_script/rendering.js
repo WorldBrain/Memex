d8c5a61684b5ea22cfd94ddbcafcb20bab963dc8
Vincent den Boer <vincent@shishkabab.net>
2018-05-17 12:31:05 +0200

    Support dynamic content

--- src/direct-linking/content_script/rendering.js
+++ src/direct-linking/content_script/rendering.js
@@ -1,10 +1,18 @@
+import { retryUntil } from '../utils'
 import { descriptorToRange, markRange } from './annotations'
 
 export async function highlightAnnotation({ annotation }) {
-    console.log('highlighting')
+    // console.log('highlighting')
     const descriptor = annotation.anchors[0].descriptor
-    const range = await descriptorToRange({ descriptor })
-    console.log('found range', range)
+    const range = await retryUntil(
+        () => descriptorToRange({ descriptor }),
+        range => range !== null,
+        {
+            intervalMiliseconds: 200,
+            timeoutMiliseconds: 5000,
+        },
+    )
+    // console.log('found range', range)
     markRange({ range, cssClass: 'memex-highlight' })
     document.querySelector('.memex-highlight').style.background = '#3eb995'
 }
