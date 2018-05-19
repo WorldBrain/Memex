006d4f912bccc15454d0124f9284af4ffad62a64
Subrat Sahu <ssubrat098@gmail.com>
2018-05-19 23:05:47 +0530

    Added temporary dummy data

---
+++ src/custom-lists/dummy-data/index.js
@@ -0,0 +1,274 @@
+const list = [
+    {
+        _id: '5afaed178c4d884ca6329788',
+        index: 3,
+        name: 'Esse ut veniam ullamco excepteur.',
+        isDeletable: false,
+        pages: [
+            {
+                url: 'http://Woodlands.name/Ladera',
+            },
+            {
+                url: 'http://Waiohinu.name/Gilmore',
+            },
+            {
+                url: 'http://Sedley.name/Moquino',
+            },
+            {
+                url: 'http://Nile.name/Montura',
+            },
+            {
+                url: 'http://Steinhatchee.name/Oretta',
+            },
+            {
+                url: 'http://Gasquet.name/Haring',
+            },
+            {
+                url: 'http://Datil.name/Blairstown',
+            },
+            {
+                url: 'http://Kiskimere.name/Rehrersburg',
+            },
+            {
+                url: 'http://Falconaire.name/Jacksonwald',
+            },
+            {
+                url: 'http://Boling.name/Levant',
+            },
+        ],
+    },
+    {
+        _id: '5afaed176f1e0550250b0742',
+        index: 4,
+        name: 'Adipisicing Lorem incididunt dolore duis dolore.',
+        isDeletable: true,
+        pages: [
+            {
+                url: 'http://Columbus.name/Gerber',
+            },
+            {
+                url: 'http://Basye.name/Bloomington',
+            },
+            {
+                url: 'http://Cazadero.name/Fairhaven',
+            },
+            {
+                url: 'http://Chalfant.name/Stevens',
+            },
+            {
+                url: 'http://Oasis.name/Tuttle',
+            },
+            {
+                url: 'http://Castleton.name/Osage',
+            },
+            {
+                url: 'http://Stockwell.name/Gracey',
+            },
+            {
+                url: 'http://Clarence.name/Beason',
+            },
+            {
+                url: 'http://Noblestown.name/Gorst',
+            },
+            {
+                url: 'http://Crown.name/Jackpot',
+            },
+            {
+                url: 'http://Wright.name/Allison',
+            },
+            {
+                url: 'http://Caroline.name/Slovan',
+            },
+            {
+                url: 'http://Heil.name/Stagecoach',
+            },
+            {
+                url: 'http://Roberts.name/Rosewood',
+            },
+            {
+                url: 'http://Elrama.name/Clayville',
+            },
+            {
+                url: 'http://Keyport.name/Weogufka',
+            },
+            {
+                url: 'http://Gila.name/Hondah',
+            },
+            {
+                url: 'http://Bend.name/Veyo',
+            },
+            {
+                url: 'http://Chase.name/Knowlton',
+            },
+            {
+                url: 'http://Kersey.name/Roosevelt',
+            },
+            {
+                url: 'http://Grazierville.name/Marshall',
+            },
+            {
+                url: 'http://Hachita.name/Bordelonville',
+            },
+            {
+                url: 'http://Stonybrook.name/Glendale',
+            },
+            {
+                url: 'http://Wheaton.name/Dixie',
+            },
+        ],
+    },
+    {
+        _id: '5afaed172881724fe9a2730a',
+        index: 5,
+        name: 'Sint velit et proident irure ex enim ad id consectetur velit.',
+        isEditing: true,
+        isDeletable: true,
+        pages: [
+            {
+                url: 'http://Olney.name/Vandiver',
+            },
+            {
+                url: 'http://Rockhill.name/Boyd',
+            },
+            {
+                url: 'http://Eagletown.name/Naomi',
+            },
+            {
+                url: 'http://Whitestone.name/Macdona',
+            },
+            {
+                url: 'http://Volta.name/Reno',
+            },
+            {
+                url: 'http://Nash.name/Cutter',
+            },
+            {
+                url: 'http://Groveville.name/Thornport',
+            },
+            {
+                url: 'http://Groton.name/Klagetoh',
+            },
+            {
+                url: 'http://Brutus.name/Wiscon',
+            },
+            {
+                url: 'http://Garfield.name/Dante',
+            },
+            {
+                url: 'http://Thomasville.name/Westerville',
+            },
+            {
+                url: 'http://Hobucken.name/Wadsworth',
+            },
+            {
+                url: 'http://Accoville.name/Lydia',
+            },
+            {
+                url: 'http://Teasdale.name/Thynedale',
+            },
+            {
+                url: 'http://Alleghenyville.name/Wanship',
+            },
+            {
+                url: 'http://Lisco.name/Fulford',
+            },
+        ],
+    },
+    {
+        _id: '5afaed17f08c3f5ab031f5fe',
+        index: 6,
+        name:
+            'Officia ut proident esse nulla reprehenderit ullamco elit mollit.',
+        isDeletable: true,
+        pages: [
+            {
+                url: 'http://Marion.name/Wacissa',
+            },
+            {
+                url: 'http://Shindler.name/Robinette',
+            },
+            {
+                url: 'http://Urbana.name/Cavalero',
+            },
+            {
+                url: 'http://Grantville.name/Avoca',
+            },
+            {
+                url: 'http://Coinjock.name/Camptown',
+            },
+            {
+                url: 'http://Keller.name/Ellerslie',
+            },
+            {
+                url: 'http://Chelsea.name/Temperanceville',
+            },
+            {
+                url: 'http://Remington.name/Cawood',
+            },
+        ],
+    },
+    {
+        _id: '5afaed17d1d58cdb1962bfb5',
+        index: 7,
+        name: 'Sint irure nostrud dolore quis ea occaecat.',
+        isEditing: true,
+        isDeletable: true,
+        pages: [
+            {
+                url: 'http://Croom.name/Eastmont',
+            },
+            {
+                url: 'http://Hatteras.name/Allentown',
+            },
+            {
+                url: 'http://Crayne.name/Rose',
+            },
+            {
+                url: 'http://Farmers.name/Ernstville',
+            },
+            {
+                url: 'http://Rosedale.name/Efland',
+            },
+        ],
+    },
+    {
+        _id: '5afaed17739fc30bd77eb17c',
+        index: 8,
+        name: 'Ad reprehenderit ea amet tempor enim magna laboris.',
+        isDeletable: true,
+        pages: [
+            {
+                url: 'http://Summerset.name/Ribera',
+            },
+            {
+                url: 'http://Saddlebrooke.name/Walton',
+            },
+            {
+                url: 'http://Kula.name/Silkworth',
+            },
+            {
+                url: 'http://Machias.name/Sperryville',
+            },
+            {
+                url: 'http://Greenfields.name/Clara',
+            },
+            {
+                url: 'http://Inkerman.name/Sultana',
+            },
+            {
+                url: 'http://Allendale.name/Faywood',
+            },
+            {
+                url: 'http://Dunbar.name/Mahtowa',
+            },
+            {
+                url: 'http://Guilford.name/Cliffside',
+            },
+            {
+                url: 'http://Dale.name/Osmond',
+            },
+        ],
+    },
+]
+
+export default list
