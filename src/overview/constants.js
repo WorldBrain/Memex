e4e33211b5d98feec31b7cd28a78e66b3a8c12ff
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-05-23 12:24:22 +0700

    Change excluded domain `site:` syntax
    - now `-site:github.com` should exclude `github.com`

--- src/overview/constants.js
+++ src/overview/constants.js
@@ -29,20 +29,20 @@ export const SHOWN_FILTER_LIMIT = 3
  * Pattern to match entire string to match `domain.tld`-like format + optional subdomain
  * prefix, ccTLD postfix, `site:` prefix, and excluded `-` prefix.
  */
-export const DOMAIN_TLD_PATTERN = /^(site:)?-?(\w+\.)?[\w-]{2,}\.\w{2,3}(\.\w{2})?$/
+export const DOMAIN_TLD_PATTERN = /^-?(site:)?(\w+\.)?[\w-]{2,}\.\w{2,3}(\.\w{2})?$/
 
 /**
  * Pattern to match against individual terms to determine whether or not they are excluded
  * (prefixed with hyphen: -). Also supports site exclusion, which may be prefixed with
  * `site:` syntax.
  */
-export const EXCLUDE_PATTERN = /^(site:)?-(?=\w+)/
+export const EXCLUDE_PATTERN = /^-(site:)?(?=\w+)/
 
 /**
  * Pattern used to remove excluded syntax, and leading "site:" syntax from a term.
  * This is used after the type of term is determined (excluded, site/normal term, etc.).
  */
-export const TERM_CLEAN_PATTERN = /^(site:)?-?(?=\w+)/
+export const TERM_CLEAN_PATTERN = /^-?(site:)?-?(?=\w+)/
 
 /**
  * Pattern to match hashtag prefix syntax for tags.
