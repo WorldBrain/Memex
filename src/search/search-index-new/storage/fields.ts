96e5b1e206030700b69656cd52fea826c9e6acd0
Vincent den Boer <vincent@shishkabab.net>
2018-05-24 09:49:25 +0200

    Slight tooltip refactor

---
+++ src/search/search-index-new/storage/fields.ts
@@ -0,0 +1,38 @@
+import normalize from '../../../util/encode-url-for-id'
+
+export class Field {
+    prepareForStorage(input) {
+        return input
+    }
+
+    prepareFromStorage(stored) {
+        return stored
+    }
+}
+
+export interface UrlFieldConfig {
+    normalize?: string
+}
+
+export class UrlField extends Field {
+    constructor(public config: UrlFieldConfig = {}) {
+        super()
+
+        this.config = config
+        this.config.normalize = _default(config.normalize, true)
+    }
+
+    prepareForStorage(input) {
+        if (this.config.normalize) {
+            input = normalize(input)
+        }
+
+        return input
+    }
+}
+
+function _default(option, def) {
+    if (typeof option === 'undefined') {
+        return def
+    }
+}
