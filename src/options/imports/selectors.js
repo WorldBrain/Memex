8d3057619159c4c2881a9afca7d1c483c68dd0a2
Arpit Gogia <aarpitgogia@gmail.com>
2018-05-30 22:31:27 +0530

    Refactor time estimate method

--- src/options/imports/selectors.js
+++ src/options/imports/selectors.js
@@ -158,7 +158,7 @@ export const progressPercent = createSelector(
             (allowTypes[TYPE.HISTORY] ? progress[TYPE.HISTORY].complete : 0) +
             (allowTypes[TYPE.BOOKMARK] ? progress[TYPE.BOOKMARK].complete : 0)
 
-        return complete / total * 100
+        return (complete / total) * 100
     },
 )
 
@@ -168,10 +168,12 @@ const getMins = time =>
     Math.floor((time - getHours(time) * 3600) / 60).toFixed(0)
 const getPaddedMins = time =>
     getMins(time) < 10 ? `0${getMins(time)}` : getMins(time)
-const getTimeEstStr = time =>
-    `${getHours(time)}:${getPaddedMins(time)}` === '0:00'
+const getTimeEstStr = time => {
+    const timeEstimate = `${getHours(time)}:${getPaddedMins(time)}`
+    return timeEstimate === '0:00'
         ? '0:00'
-        : moment.duration(`${getHours(time)}:${getPaddedMins(time)}`).humanize()
+        : moment.duration(timeEstimate).humanize()
+}
 const getEstimate = (complete, remaining) => ({
     complete,
     remaining,
