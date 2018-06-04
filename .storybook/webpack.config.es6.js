aefdd5a240c47e486f57b6f15c5e85b8614eacb6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-06-04 12:45:30 +0700

    Get storybook working with dev webpack conf
    - updated storybook to latest alpha (uses webpack 4 rather than 3)
    - replaced the config with our current dev build config
    - added option to switch between injecting styles and extracting them

--- .storybook/webpack.config.es6.js
+++ .storybook/webpack.config.es6.js
@@ -1,30 +1,8 @@
 import path from 'path'
-import * as loaders from '../build/loaders'
+import initConf from '../build'
 
-module.exports = (baseConfig, env, defaultConfig) => {
-    defaultConfig.module.rules.push({
-        test: /\.(j|t)sx?$/,
-        include: path.resolve(__dirname, '../src'),
-        use: [loaders.babelLoader, loaders.tsLoader],
-    })
-
-    defaultConfig.module.rules.push({
-        test: /\.css$/,
-        include: path.resolve(__dirname, '../src'),
-        use: [
-            loaders.styleLoader,
-            loaders.cssModulesLoader,
-            loaders.postcssLoader,
-        ],
-    })
-
-    defaultConfig.module.rules.push({
-        test: /\.css$/,
-        include: path.resolve(__dirname, '../node_modules'),
-        use: [loaders.styleLoader, loaders.cssVanillaLoader],
-    })
-
-    defaultConfig.resolve.alias.src = path.resolve(__dirname, '../src')
-
-    return defaultConfig
-}
+module.exports = initConf({
+    context: path.resolve(__dirname, '..'),
+    mode: 'development',
+    injectStyles: true,
+})
