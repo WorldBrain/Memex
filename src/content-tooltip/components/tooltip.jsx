a9d41baeb9cb15e9632d0237e08d96613eaace48
digi0ps <sriru1998@gmail.com>
2018-06-07 21:13:47 +0530

    Show description for tooltip icons.

--- src/content-tooltip/components/tooltip.jsx
+++ src/content-tooltip/components/tooltip.jsx
@@ -6,13 +6,13 @@ import { getExtURL } from '../utils'
 import { INFO_URL } from '../constants'
 import styles from './tooltip.css'
 
-const cssClasses = {
-    pristine: 'stateInitial',
-    running: 'stateCreatingLink',
-    done: 'stateCreatedLink',
-    error: 'stateLinkError',
-    copied: 'stateLinkCopied',
-}
+// const cssClasses = {
+//     pristine: 'stateInitial',
+//     running: 'stateCreatingLink',
+//     done: 'stateCreatedLink',
+//     error: 'stateLinkError',
+//     copied: 'stateLinkCopied',
+// }
 
 const images = {
     logo: getExtURL('/img/worldbrain-logo-narrow.png'),
@@ -22,6 +22,11 @@ const images = {
     settings: getExtURL('/img/settings.svg'),
 }
 
+const deriveTooltipClass = description =>
+    classNames(styles.tooltip, {
+        [styles.expanded]: description.length,
+    })
+
 const Tooltip = ({
     x,
     y,
@@ -30,19 +35,31 @@ const Tooltip = ({
     closeTooltip,
     openSettings,
     description,
+    setDescription,
+    removeDescription,
 }) => (
     <div
-        className={classNames(styles.tooltip, styles[cssClasses[state]])}
+        className={deriveTooltipClass(description)}
         style={{ left: x, top: y }}
         id="memex-tooltip"
     >
         {tooltipComponent}
 
         <span className={styles.buttons}>
-            <a onClick={closeTooltip} className={styles.smallButton}>
+            <a
+                onClick={closeTooltip}
+                className={styles.smallButton}
+                onMouseEnter={setDescription('Close tooltip for now')}
+                onMouseLeave={removeDescription}
+            >
                 <img className={styles.imgCross} src={images.cross} />
             </a>
-            <a onClick={openSettings} className={styles.smallButton}>
+            <a
+                onClick={openSettings}
+                className={styles.smallButton}
+                onMouseEnter={setDescription('Memex settings')}
+                onMouseLeave={removeDescription}
+            >
                 <img className={styles.imgSettings} src={images.settings} />
             </a>
             <a href={INFO_URL} className={styles.smallButton}>
@@ -50,7 +67,9 @@ const Tooltip = ({
             </a>
         </span>
 
-        <div className={styles.descriptionContainer}>{description}</div>
+        {description && (
+            <div className={styles.descriptionContainer}>{description}</div>
+        )}
     </div>
 )
 
@@ -62,6 +81,8 @@ Tooltip.propTypes = {
     closeTooltip: PropTypes.func.isRequired,
     openSettings: PropTypes.func.isRequired,
     description: PropTypes.string.isRequired,
+    setDescription: PropTypes.func.isRequired,
+    removeDescription: PropTypes.func.isRequired,
 }
 
 export default Tooltip
