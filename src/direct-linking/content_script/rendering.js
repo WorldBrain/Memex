c30d406e330c1c69877cd80e9f3b9ba3cdddc983
Vincent den Boer <vincent@shishkabab.net>
2018-06-13 15:23:23 +0200

    Add annotation selector breacrumb for Direct Linking

--- src/direct-linking/content_script/rendering.js
+++ src/direct-linking/content_script/rendering.js
@@ -1,19 +1,31 @@
+import * as AllRaven from 'raven-js'
 import { retryUntil } from '../utils'
 import { descriptorToRange, markRange } from './annotations'
 
 import styles from './styles.css'
 
+const Raven = AllRaven['default']
+
 export async function highlightAnnotation({ annotation }) {
     // console.log('highlighting')
-    const descriptor = annotation.anchors[0].descriptor
-    const range = await retryUntil(
-        () => descriptorToRange({ descriptor }),
-        range => range !== null,
-        {
-            intervalMiliseconds: 200,
-            timeoutMiliseconds: 5000,
-        },
-    )
-    console.log('Memex - found range:', range, range.toString())
-    markRange({ range, cssClass: styles['memex-highlight'] })
+    await Raven.context(async () => {
+        const descriptor = annotation.anchors[0].descriptor
+        Raven.captureBreadcrumb({
+            message: 'annotation-selector-received',
+            category: 'annotations',
+            data: annotation,
+        })
+
+        const range = await retryUntil(
+            () => descriptorToRange({ descriptor }),
+            range => range !== null,
+            {
+                intervalMiliseconds: 200,
+                timeoutMiliseconds: 5000,
+            },
+        )
+        console.log('Memex - found range:', range, range.toString())
+
+        markRange({ range, cssClass: styles['memex-highlight'] })
+    })
 }
