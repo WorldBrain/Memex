3888cfc6ab182fdfefd633341a9709a3e92901b6
digi0ps <sriru1998@gmail.com>
2018-06-14 20:26:16 +0530

    Clean up css and minor tweaks.

--- src/content-tooltip/components/AnimationWrapper.js
+++ src/content-tooltip/components/AnimationWrapper.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types'
 
 import styles from './tooltip.css'
 
+/*
+TODO:
+- Animate icons leaving out.
+- Streamline animation flow using enter and exit animations
+*/
 class AnimationWrapper extends React.Component {
     static propTypes = {
         children: PropTypes.node.isRequired,
