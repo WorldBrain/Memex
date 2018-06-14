f6a688f42f4c4cb218cb83a74f79effa26ba979a
Vincent den Boer <vincent@shishkabab.net>
2018-06-14 14:14:19 +0200

    Detect when anchoring has failed in graceful way to allow for rendering of fallback overlay

--- src/direct-linking/content_script/rendering.js
+++ src/direct-linking/content_script/rendering.js
@@ -7,25 +7,32 @@ import styles from './styles.css'
 const Raven = AllRaven['default']
 
 export async function highlightAnnotation({ annotation }) {
-    // console.log('highlighting')
-    await Raven.context(async () => {
-        const descriptor = annotation.anchors[0].descriptor
-        Raven.captureBreadcrumb({
-            message: 'annotation-selector-received',
-            category: 'annotations',
-            data: annotation,
+    try {
+        await Raven.context(async () => {
+            const descriptor = annotation.anchors[0].descriptor
+            Raven.captureBreadcrumb({
+                message: 'annotation-selector-received',
+                category: 'annotations',
+                data: annotation,
+            })
+
+            const range = await retryUntil(
+                () => descriptorToRange({ descriptor }),
+                range => range !== null,
+                {
+                    intervalMiliseconds: 200,
+                    timeoutMiliseconds: 5000,
+                },
+            )
+            console.log('Memex - found range:', range, range.toString())
+
+            markRange({ range, cssClass: styles['memex-highlight'] })
         })
+    } catch (e) {
+        console.error('Error during annotation anchoring/highlighting:', e)
+        console.error(e.stack)
+        return false
+    }
 
-        const range = await retryUntil(
-            () => descriptorToRange({ descriptor }),
-            range => range !== null,
-            {
-                intervalMiliseconds: 200,
-                timeoutMiliseconds: 5000,
-            },
-        )
-        console.log('Memex - found range:', range, range.toString())
-
-        markRange({ range, cssClass: styles['memex-highlight'] })
-    })
+    return true
 }
