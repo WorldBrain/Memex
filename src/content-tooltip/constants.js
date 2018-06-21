bb726c15edd1cc97d2a3eec688bd4dceeb7b7aa8
Oliver Sauter <oli@worldbrain.io>
2018-06-21 14:28:44 +0200

    Adding info url Small style changes

--- src/content-tooltip/constants.js
+++ src/content-tooltip/constants.js
@@ -2,4 +2,5 @@ export const TOOLTIP_DEFAULT_OPTION = true
 
 export const TOOLTIP_STORAGE_NAME = 'memex_link_tooltip'
 
-export const INFO_URL = 'https://worldbrain.io'
+export const INFO_URL =
+    'https://worldbrain.helprace.com/i62-feature-memex-links-highlight-any-text-and-create-a-link-to-it'
