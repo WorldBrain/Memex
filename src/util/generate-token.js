427003a6cf3b907725775a18e0df62dfd7c58252
Mukesh <mukeshnithcse@gmail.com>
2018-06-22 17:57:07 +0530

    Update long url of server
    Code improvement

--- src/util/generate-token.js
+++ src/util/generate-token.js
@@ -1,11 +1,19 @@
 import { installTimeStorageKey } from 'src/imports/background'
+import { API_HOST } from 'src/analytics/internal/constants'
 
-const API_PATH = '/api/v1/returnToken'
+const API_PATH = '/user-token'
 
 export const USER_ID = 'user-id'
 
-export default async function generateTokenIfNot(installTime) {
+export default async function generateTokenIfNot({
+    installTime,
+    hostname = API_HOST,
+    pathname = API_PATH,
+}) {
     const userId = (await browser.storage.local.get(USER_ID))[USER_ID]
+    if (userId) {
+        return userId
+    }
 
     // if install time is not present then current time stores as install time and send to server same
     if (!installTime) {
@@ -13,19 +21,17 @@ export default async function generateTokenIfNot(installTime) {
         browser.storage.local.set({ [installTimeStorageKey]: installTime })
     }
 
-    if (!userId) {
-        const generateToken = await fetch(process.env.REDASH_API + API_PATH, {
-            method: 'POST',
-            headers: {
-                'Content-type':
-                    'application/x-www-form-urlencoded; charset=UTF-8',
-            },
-            body: `install_time=${installTime}`,
-        })
-        const token = await generateToken.json()
+    const generateToken = await fetch(hostname + pathname, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        body: `installTime=${installTime}`,
+    })
+    const token = await generateToken.json()
 
-        if (token.status === 200) {
-            browser.storage.local.set({ [USER_ID]: token.id })
-        }
+    if (token.success) {
+        browser.storage.local.set({ [USER_ID]: token.id })
+        return token.id
     }
 }
