0f1c45929f7360a5f2b149c7bd1ddc142fea1f51
digi0ps <sriru1998@gmail.com>
2018-06-26 16:27:52 +0530

    Close overview sidebar when clicked outside.

--- src/overview/sidebar/container.js
+++ src/overview/sidebar/container.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-
+import onClickOutside from 'react-onclickoutside'
 import Sidebar from 'src/sidebar-overlay/'
 import * as selectors from './selectors'
 import * as actions from './actions'
 
-const SidebarContainer = props => (
-    <div>
-        {props.showSidebar ? <Sidebar {...props} env={'overview'} /> : null}
-    </div>
-)
+class SidebarContainer extends React.Component {
+    static propTypes = {
+        showSidebar: PropTypes.bool.isRequired,
+        setShowSidebar: PropTypes.func.isRequired,
+        toggleMouseOnSidebar: PropTypes.func.isRequired,
+        pageUrl: PropTypes.string.isRequired,
+        closeSidebar: PropTypes.func.isRequired,
+    }
 
-SidebarContainer.propTypes = {
-    showSidebar: PropTypes.bool.isRequired,
-    setShowSidebar: PropTypes.func.isRequired,
-    toggleMouseOnSidebar: PropTypes.func.isRequired,
-    pageUrl: PropTypes.string.isRequired,
+    handleClickOutside = () => this.props.closeSidebar()
+
+    render() {
+        return (
+            <div>
+                {this.props.showSidebar ? (
+                    <Sidebar {...this.props} env={'overview'} />
+                ) : null}
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
@@ -28,9 +37,12 @@ const mapDispatchToProps = dispatch => ({
     setShowSidebar: showSidebar =>
         dispatch(actions.setShowSidebar(showSidebar)),
     toggleMouseOnSidebar: event => dispatch(actions.toggleMouseOnSidebar()),
+    closeSidebar: () => dispatch(actions.closeSidebar()),
 })
+
+const enhancedSidebar = onClickOutside(SidebarContainer)
 
 export default connect(
     mapStateToProps,
     mapDispatchToProps,
-)(SidebarContainer)
+)(enhancedSidebar)
