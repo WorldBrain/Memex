430978a136fc4323762b90ceeca8656468a0b97c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-07-04 13:40:11 +0700

    Write direct-linking patch for incorrect Dexie schemas
    - after looking into this more, there doesn't seem to be a need for an actual explicit migration; Dexie should be able to handle dropping the incorrect indexes and adding the correct ones
    - this adds a step to `dexie-schema` module to take the generated schemas and insert in a "patched" schema containing the incorrect indexes (ordered before the first schema version containing `directLinks` table)
    - more info: http://dexie.org/docs/Tutorial/Design#database-versioning

--- src/search/search-index-new/storage/dexie-schema.ts
+++ src/search/search-index-new/storage/dexie-schema.ts
@@ -21,7 +21,7 @@ export function getDexieHistory(storageRegistry: StorageRegistry) {
             })
         })
 
-    return versions
+    return patchDirectLinksSchema(versions)
 }
 
 function getDexieSchema(collections: RegistryCollections) {
@@ -69,3 +69,42 @@ const convertIndexToDexieExps = ({ fields, indices }: CollectionDefinition) =>
             return `${listPrefix}${indexDef.field}`
         })
         .join(', ')
+
+/**
+ * Takes the generated schema versions, based on the registed collections, and finds the
+ * first one in which `directLinks` schema was added, then generates a "patch" schema.
+ * This "patch" schema should contain the incorrect indexes that was accidently rolled out
+ * to users at the release of our Direct Links feature. This should ensure Dexie knows about
+ * both the incorrect indexes and how to drop those to migrate to the correct indexes.
+ */
+function patchDirectLinksSchema(schemaVersions: DexieSchema[]): DexieSchema[] {
+    const firstAppears = schemaVersions.findIndex(
+        ({ schema }) => schema.directLinks != null,
+    )
+
+    // Return schemas as-is if direct links schema not found (tests)
+    if (firstAppears === -1) {
+        return schemaVersions
+    }
+
+    const preceding = schemaVersions[firstAppears - 1]
+
+    const patchedSchema = {
+        schema: {
+            ...preceding.schema,
+            directLinks: 'url, *pageTitle, *body, createdWhen',
+        },
+        migrations: [],
+        version: preceding.version + 1,
+    }
+
+    return [
+        ...schemaVersions.slice(0, firstAppears),
+        // Shim the schema with the incorrect indexes, so Dexie knows about its existence
+        patchedSchema,
+        // All subsequent schemas need to be 1 version higher to take the incorrect index schema into account
+        ...schemaVersions
+            .slice(firstAppears)
+            .map(schema => ({ ...schema, version: schema.version + 1 })),
+    ]
+}
