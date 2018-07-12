da7fe60e973f7653a3ff761d6b1ef5f0f4dc0179
Mukesh <mukeshnithcse@gmail.com>
2018-07-12 10:56:46 +0530

    Delete notif util file

--- src/util/unread-notifications.js
+++
@@ -1,7 +0,0 @@
-import { remoteFunction } from 'src/util/webextensionRPC'
-
-const getUnreadCount = remoteFunction('getUnreadCount')
-
-export default async function unreadNotifNumber() {
-    return await getUnreadCount()
-}
