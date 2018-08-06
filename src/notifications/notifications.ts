f4787a3c040907fefafc447876d3c55dcb1285d0
Oliver Sauter <oli@worldbrain.io>
2018-08-06 03:08:05 +0200

    update first notifications, and fixes in styling

--- src/notifications/notifications.ts
+++ src/notifications/notifications.ts
@@ -5,7 +5,7 @@ import * as actionTypes from './action-types'
 import { NotifDefinition } from './types'
 
 /** Time when create the notif, get the current unix time (Date.now()) - Important, the notif insertation in db depends on it */
-export const releaseTime: number = 1532947573751
+export const releaseTime: number = 1533513202569
 
 /* Example Notification:
 {
@@ -59,16 +59,16 @@ export const releaseTime: number = 1532947573751
 */
 export const NOTIFS: NotifDefinition[] = [
     {
-        id: 'direct_links_inital_notification',
+        id: 'Announce Collections',
         search: {
-            title: 'New Feature: Comments & Annotation',
-            message:
-                'Highlight any text on the web and add comments to it. Also attach comments to every websites you find.',
+            title: 'New Feature: Collections',
+            message: 'Sort your favorite websites in useful lists',
             buttons: [
                 {
                     action: {
                         type: actionTypes.OPEN_URL,
-                        url: 'https://worldbrain.io',
+                        url:
+                            'https://worldbrain.helprace.com/i67-feature-collections',
                         context: 'new-tab',
                     },
                     label: 'Learn More',
@@ -76,14 +76,49 @@ export const NOTIFS: NotifDefinition[] = [
             ],
         },
         system: {
-            title: 'New Feature: Comments & Annotation',
+            title: 'New Feature: Collections',
+            message: 'Sort your favorite websites in useful lists',
+            buttons: [
+                {
+                    action: {
+                        type: actionTypes.OPEN_URL,
+                        url:
+                            'https://worldbrain.helprace.com/i67-feature-collections',
+                        context: 'new-tab',
+                    },
+                    label: 'Learn More',
+                },
+            ],
+        },
+        overview: {
+            title: 'New Feature: Collections',
+            message: 'Sort your favorite websites in useful lists',
+            buttons: [
+                {
+                    action: {
+                        type: actionTypes.OPEN_URL,
+                        url:
+                            'https://worldbrain.helprace.com/i67-feature-collections',
+                        context: 'new-tab',
+                    },
+                    label: 'Learn More',
+                },
+            ],
+        },
+    },
+
+    {
+        id: 'Announce Annotations',
+        search: {
+            title: 'New Feature: Annotation',
             message:
                 'Highlight any text on the web and add comments to it. Also attach comments to every websites you find.',
             buttons: [
                 {
                     action: {
                         type: actionTypes.OPEN_URL,
-                        url: 'https://worldbrain.io',
+                        url:
+                            'https://worldbrain.helprace.com/i66-feature-annotations-comments',
                         context: 'new-tab',
                     },
                     label: 'Learn More',
@@ -97,10 +132,12 @@ export const NOTIFS: NotifDefinition[] = [
             buttons: [
                 {
                     action: {
-                        type: actionTypes.TOGGLE_SETTING,
-                        key: storageKeys.SHOULD_TRACK_STORAGE_KEY,
+                        type: actionTypes.OPEN_URL,
+                        url:
+                            'https://worldbrain.helprace.com/i66-feature-annotations-comments',
+                        context: 'new-tab',
                     },
-                    label: 'Enable Tracking',
+                    label: 'Learn More',
                 },
             ],
         },
