ee0f672b76061b551e8b7f9b224f32b57fb224a5
digi0ps <sriru1998@gmail.com>
2018-08-07 16:13:15 +0530

    Add CRUD tests for Annotation storage
    Add tests for annotation tag related operations.
    
    Also, modified the way tag is stored and deleted.
    
    Made changes requested by @poltak.
    
    Ensure stored annotation and tag content is compared

---
+++ src/direct-linking/background/storage.test.data.ts
@@ -0,0 +1,39 @@
+export const directLink = {
+    pageTitle: 'Some random page',
+    pageUrl: 'https://somerandomurl.com',
+    url: 'https://memex.link/3234235/',
+    body: 'Some random quote in some random page',
+    selector: {},
+}
+
+export const pageUrl = 'https://annotation.url'
+
+export const tag1 = 'tag1'
+export const tag2 = 'tagwithahugename'
+
+export const highlight = {
+    pageTitle: 'Annotation title',
+    pageUrl,
+    body: 'Whooo this is a highlight',
+    url: 'https://annotation.url/#12124124124',
+    comment: '',
+    selector: {},
+}
+
+export const annotation = {
+    pageTitle: 'Annotation title',
+    pageUrl,
+    body: 'Whooo this is an annotation',
+    url: 'https://annotation.url/#12124124134',
+    comment: 'Hmm this is the annotation comment',
+    selector: {},
+}
+
+export const comment = {
+    pageTitle: 'Annotation title',
+    pageUrl,
+    body: '',
+    url: 'https://annotation.url/#12124124159',
+    comment: 'Hmm this is just a commenttuuu',
+    selector: {},
+}
