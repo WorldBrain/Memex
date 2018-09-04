31eaf2f0ea16772d5b57230788bc92ca3f84ea3a
digi0ps <sriru1998@gmail.com>
2018-09-05 01:44:38 +0530

    Resolve issue with css filename.

--- src/options/settings/components/SearchInjection.js
+++ src/options/settings/components/SearchInjection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './Settings.css'
+import styles from './settings.css'
 
 const SearchInjection = ({ children }) => {
     return (
