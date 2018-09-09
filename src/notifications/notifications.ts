2b5543dae745a9550472c4b1b4d41b1eaba3a2c4
Oliver Sauter <oli@worldbrain.io>
2018-09-09 13:50:34 +0200

    changed notificiations

--- src/notifications/notifications.ts
+++ src/notifications/notifications.ts
@@ -63,7 +63,7 @@ export const NOTIFS: NotifDefinition[] = [
         search: {
             title: 'NEW: Crowdfunding Memex',
             message:
-                'Back your favorite features with 10-20 bucks and earn rewards to get free premium upgrades.',
+                'Support the development of your favorite features with and get free premium upgrades.',
             buttons: [
                 {
                     action: {
@@ -78,7 +78,7 @@ export const NOTIFS: NotifDefinition[] = [
         overview: {
             title: 'NEW: Crowdfunding Memex',
             message:
-                'Back your favorite features with 10-20 bucks and earn rewards to get free premium upgrades.',
+                'Support the development of your favorite features with and get free premium upgrades.',
             buttons: [
                 {
                     action: {
