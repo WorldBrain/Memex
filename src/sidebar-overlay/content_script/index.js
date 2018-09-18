30b715440b82363d0474572477b51ca964592b9c
digi0ps <sriru1998@gmail.com>
2018-09-19 01:52:10 +0530

    Insert Ribbon using shortcut before page load.
    Adds an event listener after document.readyState == 'interactive',
    removes it when document.readyState becomes complete.

--- src/sidebar-overlay/content_script/index.js
+++ src/sidebar-overlay/content_script/index.js
@@ -1,20 +1,36 @@
-import { bodyLoader } from 'src/util/loader'
+import { bodyLoader, interactiveLoader } from 'src/util/loader'
 
 import * as interactions from './interactions'
 import { getLocalStorage } from 'src/util/storage'
 
 import { TOOLTIP_STORAGE_NAME } from 'src/content-tooltip/constants'
 
+const onKeydown = e => {
+    if (e.key !== 'm') {
+        return
+    }
+
+    interactions.insertRibbon()
+}
+
 const init = async () => {
     interactions.setupRPC()
 
     const isTooltipEnabled = await getLocalStorage(TOOLTIP_STORAGE_NAME, true)
-    if (!isTooltipEnabled) return
+    if (!isTooltipEnabled) {
+        return
+    }
+
+    await interactiveLoader()
+    document.addEventListener('keydown', onKeydown, false)
 
     await bodyLoader()
+    document.removeEventListener('keydown', onKeydown, false)
     const passwordInputs = document.querySelectorAll('input[type=password]')
     const hasAPasswordInput = passwordInputs.length > 0
-    if (hasAPasswordInput) return
+    if (hasAPasswordInput) {
+        return
+    }
 
     interactions.insertRibbon()
 }
