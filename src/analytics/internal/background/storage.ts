7790052f413e25148de3ad398422f0d9dcb5fdb9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-09-28 13:15:34 +0700

    Update schemas and FeatureStorage classes for storex
    Port original data model schema to storex
    
    Replace StorageManager instance with storex
    
    Update all FeatureStorage classes for storex API changes
    
    - `countAll` and `suggest` methods do not yet exist
    
    Update all `FeatureStorage` with newer storex APIs

--- src/analytics/internal/background/storage.ts
+++ src/analytics/internal/background/storage.ts
@@ -2,32 +2,38 @@ import { FeatureStorage } from '../../../search/storage'
 import { NOTIF_TYPE_EVENT_IDS, EVENT_TYPES } from '../constants'
 
 export default class EventLogStorage extends FeatureStorage {
+    static EVENT_LOG_COLL = 'eventLog'
     constructor(storageManager) {
         super(storageManager)
 
-        this.storageManager.registerCollection('eventLog', {
-            version: new Date(2018, 6, 14),
-            fields: {
-                time: { type: 'datetime' },
-                type: { type: 'string' },
-                details: { type: 'json' },
+        this.storageManager.registry.registerCollection(
+            EventLogStorage.EVENT_LOG_COLL,
+            {
+                version: new Date(2018, 6, 14),
+                fields: {
+                    time: { type: 'datetime' },
+                    type: { type: 'string' },
+                    details: { type: 'json' },
+                },
+                indices: [
+                    { field: ['time', 'type'], pk: true },
+                    { field: 'time' },
+                    { field: 'type' },
+                ],
+                watch: false,
+                backup: false,
             },
-            indices: [
-                { field: ['time', 'type'], pk: true },
-                { field: 'time' },
-                { field: 'type' },
-            ],
-            watch: false,
-            backup: false,
-        })
+        )
     }
 
     async storeEvent({ time, details, type }) {
-        await this.storageManager.putObject('eventLog', {
-            time,
-            type: EVENT_TYPES[type].id,
-            details,
-        })
+        await this.storageManager
+            .collection(EventLogStorage.EVENT_LOG_COLL)
+            .createObject({
+                time,
+                type: EVENT_TYPES[type].id,
+                details,
+            })
     }
 
     async getLatestTimeWithCount({ notifType }) {
@@ -40,20 +46,19 @@ export default class EventLogStorage extends FeatureStorage {
         }
 
         for (const type of NOTIF_TYPE_EVENT_IDS[notifType]) {
-            const latest = await this.storageManager.findObject(
-                'eventLog',
-                { type },
-                opts,
-            )
+            const latest = await this.storageManager
+                .collection(EventLogStorage.EVENT_LOG_COLL)
+                .findOneObject({ type }, opts)
+
             if (latest) {
                 latestEvent = Math.max(latest['time'], latestEvent)
             }
 
-            const eventCountNotif = await this.storageManager.countAll(
-                'eventLog',
-                { type },
-            )
-            eventLogCount += Number(eventCountNotif)
+            const eventCountNotif = await this.storageManager
+                .collection(EventLogStorage.EVENT_LOG_COLL)
+                .countObjects({ type })
+
+            eventLogCount += eventCountNotif
         }
 
         if (eventLogCount === 0) {
