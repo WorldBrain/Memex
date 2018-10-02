5a57bfcd16cfdd84f54dbb6dc1150fbca1bd10ba
Mukesh <mukeshnithcse@gmail.com>
2018-10-02 22:09:49 +0530

    Re inject content script

--- src/background-script/on-install-hooks.ts
+++ src/background-script/on-install-hooks.ts
@@ -44,6 +44,11 @@ export async function onUpdate() {
 
         if (tab.url) {
             await tabChangeListener.handleUrl(tab.id, tab, tab)
+            if (tab.url !== 'chrome://extensions/') {
+                await browser.tabs.executeScript(tab.id, {
+                    file: 'content_script.js',
+                })
+            }
         }
     }
 
