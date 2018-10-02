ea26538d4f1e83ca24eee3ada8427645d961093e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-10-02 12:53:11 +0700

    Pass in Dexie schema patching fn to Dexie storex backend
    Extend storex instance with memex suggest functionality
    
    - bringing code over from branch on `storex-backend-dexie` implementing the new suggest method
    
    Fix bad storex imports
    
    - some were from `storex/ts` which won't work properly (should be `storex/lib` to import from compiled source)
    - other types were imported from the wrong areas and leading to compile time errors
    
    Remove `as any` asserts from storex backends instantiations
    
    - apparently the tsc issue is gone now that I updated `storex-backend-dexie` to stop importing from `storex/ts`

--- src/search/storage/dexie-schema.ts
+++ src/search/storage/dexie-schema.ts
@@ -1,70 +1,4 @@
-import StorageRegistry, { RegistryCollections } from './registry'
-import { DexieSchema, CollectionDefinition, MigrationRunner } from './types'
-
-export function getDexieHistory(storageRegistry: StorageRegistry) {
-    const collections = {}
-    const versions: DexieSchema[] = []
-    let version = 0
-
-    Object.entries(storageRegistry.collectionsByVersion)
-        .sort((left, right) => (left[0] < right[0] ? -1 : 1))
-        .forEach(([versionTimestamp, defs]) => {
-            defs.forEach(def => (collections[def.name] = def))
-            versions.push({
-                ...getDexieSchema(collections),
-                version: ++version,
-            })
-        })
-
-    return patchDirectLinksSchema(versions)
-}
-
-function getDexieSchema(collections: RegistryCollections) {
-    const schema = {}
-    const migrations: MigrationRunner[] = []
-
-    Object.entries(collections).forEach(([collectionName, collectionDef]) => {
-        schema[collectionName] = convertIndexToDexieExps(collectionDef)
-
-        if (collectionDef.migrate && !collectionDef.migrate._seen) {
-            collectionDef.migrate._seen = true // TODO: Clean this up, should have no side-effects
-            migrations.push(collectionDef.migrate)
-        }
-    })
-
-    return { schema, migrations }
-}
-
-/**
- * Handles converting from StorageManager index definitions to Dexie index expressions.
- */
-function convertIndexToDexieExps({ fields, indices }: CollectionDefinition) {
-    return indices
-        .sort(({ pk }) => (pk ? -1 : 1)) // PK indexes always come first in Dexie
-        .map(indexDef => {
-            // Convert from StorageManager compound index to Dexie compound index
-            // Note that all other `IndexDefinition` opts are ignored for compound indexes
-            if (indexDef.field instanceof Array) {
-                return `[${indexDef.field[0]}+${indexDef.field[1]}]`
-            }
-
-            // Create Dexie MultiEntry index for indexed text fields: http://dexie.org/docs/MultiEntry-Index
-            // TODO: throw error if text field + PK index
-            if (fields[indexDef.field].type === 'text') {
-                const fullTextField =
-                    indexDef.fullTextIndexName ||
-                    StorageRegistry.createTermsIndex(indexDef.field)
-                return `*${fullTextField}`
-            }
-
-            // Note that order of these statements matters
-            let listPrefix = indexDef.unique ? '&' : ''
-            listPrefix = indexDef.pk && indexDef.autoInc ? '++' : listPrefix
-
-            return `${listPrefix}${indexDef.field}`
-        })
-        .join(', ')
-}
+import { DexieSchema } from './types'
 
 /**
  * Takes the generated schema versions, based on the registed collections, and finds the
@@ -73,7 +7,9 @@ function convertIndexToDexieExps({ fields, indices }: CollectionDefinition) {
  * to users at the release of our Direct Links feature. This should ensure Dexie knows about
  * both the incorrect indexes and how to drop those to migrate to the correct indexes.
  */
-function patchDirectLinksSchema(schemaVersions: DexieSchema[]): DexieSchema[] {
+export default function patchDirectLinksSchema(
+    schemaVersions: DexieSchema[],
+): DexieSchema[] {
     const firstAppears = schemaVersions.findIndex(
         ({ schema }) => schema.directLinks != null,
     )
