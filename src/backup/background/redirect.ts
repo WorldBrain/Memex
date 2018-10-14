169bd8afe272ed8f4e838f2f7966607f28571811
Vincent den Boer <vincent@shishkabab.net>
2018-10-14 22:27:43 +0200

    Fix redirect back to backup settings page

--- src/backup/background/redirect.ts
+++ src/backup/background/redirect.ts
@@ -1,19 +1,24 @@
-export function setupRequestInterceptor({ webRequest, handleLoginRedirectedBack, memexCloudOrigin }) {
+export function setupRequestInterceptor({
+    webRequest,
+    handleLoginRedirectedBack,
+    memexCloudOrigin,
+}) {
     webRequest.onBeforeRequest.addListener(
         makeRequestHandler({ handleLoginRedirectedBack }),
         { urls: [`${memexCloudOrigin}/auth/google/callback*`] },
-        ['blocking']
+        ['blocking'],
     )
 }
 
 export function makeRequestHandler({ handleLoginRedirectedBack }) {
     return ({ url, tabId }) => {
-        if (tabId === -1) { // Came from bg script, prevent infinite loop of DOOM!
+        if (tabId === -1) {
+            // Came from bg script, prevent infinite loop of DOOM!
             return {}
         }
 
         handleLoginRedirectedBack(url)
-        const targetUrl = 'chrome-extension://hideohpekofebnpgkolicememphnnidf/options.html#/backup'
+        const targetUrl = `${chrome.extension.getURL('/options.html')}#/backup`
         return { redirectUrl: targetUrl }
     }
 }
