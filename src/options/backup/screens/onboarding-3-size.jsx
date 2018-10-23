1afdba698b33a8b87f7acfb316a71ac71f5ea237
Vincent den Boer <vincent@shishkabab.net>
2018-10-23 16:03:39 +0200

    Work on backup UI

--- src/options/backup/screens/onboarding-3-size.jsx
+++ src/options/backup/screens/onboarding-3-size.jsx
@@ -1,7 +1,131 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { remoteFunction } from 'src/util/webextensionRPC'
+import styles from './onboarding-3-size.css'
+import { PrimaryButton } from '../components/primary-button'
 
 export default class OnboardingSizeContainer extends React.Component {
-    state = {}
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        onBlobPreferenceChange: PropTypes.func.isRequired,
+        onLoginRequested: PropTypes.func.isRequired,
+        onBackupRequested: PropTypes.func.isRequired,
+    }
 
-    render() {}
+    state = { estimation: null }
+
+    async componentDidMount() {
+        try {
+            this.setState({
+                estimation: await remoteFunction('estimateInitialBackupSize')(),
+            })
+        } catch (e) {
+            this.setState({ estimation: 'error' })
+            console.log(e)
+            console.trace()
+        }
+    }
+
+    renderLoadingIndicator() {
+        return null // TOOD: See if it actually takes long enough to be worth the effort
+    }
+
+    renderEstimationFailure() {
+        return 'Something went horribly wrong' // TODO: Render something nice and meaningful here
+    }
+
+    render() {
+        if (!this.state.estimation) {
+            return this.renderLoadingIndicator()
+        }
+        if (this.state.estimation === 'error') {
+            return this.renderEstimationFailure()
+        }
+
+        const sizes = {
+            withBlobs: this.state.estimation.bytesWithBlobs,
+            withoutBlobs: this.state.estimation.bytesWithoutBlobs,
+            blobs: null,
+        }
+        sizes.blobs = sizes.withBlobs - sizes.withoutBlobs
+
+        return (
+            <div>
+                <h2>
+                    <span className={styles.headerEmphasis}>
+                        Estimated size
+                    </span>
+                    of your backup
+                </h2>
+                <div>What do you want to include in the backup?</div>
+                <table>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>
+                            Searchable History, Annotations, Comments,
+                            Highlights, Collections
+                        </td>
+                        <td className={styles.estimationSize}>
+                            {_bytesToMega(sizes.withoutBlobs).toFixed(0)}
+                            MB
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input
+                                type="checkbox"
+                                onChange={event =>
+                                    this.props.onBlobPreferenceChange(
+                                        event.target.checked,
+                                    )
+                                }
+                            />
+                        </td>
+                        <td>Screenshots</td>
+                        <td className={styles.estimationSize}>
+                            {_bytesToMega(sizes.blobs).toFixed(0)}
+                            MB
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td
+                            className={classNames(
+                                styles.estimationSize,
+                                styles.sumCell,
+                            )}
+                        >
+                            {_bytesToMega(sizes.blobs).toFixed(0)}
+                            MB
+                        </td>
+                    </tr>
+                </table>
+
+                {!this.props.isAuthenticated && (
+                    <PrimaryButton
+                        onClick={() => {
+                            this.props.onLoginRequested()
+                        }}
+                    >
+                        Connect with Google Drive
+                    </PrimaryButton>
+                )}
+                {this.props.isAuthenticated && (
+                    <PrimaryButton
+                        onClick={() => {
+                            this.props.onBackupRequested()
+                        }}
+                    >
+                        Backup
+                    </PrimaryButton>
+                )}
+            </div>
+        )
+    }
+}
+
+export function _bytesToMega(bytes) {
+    return bytes / 1024 / 1024
 }
--- src/options/backup/screens/overview.jsx
+++ src/options/backup/screens/overview.jsx
@@ -1,7 +1,115 @@
+import moment from 'moment'
 import React from 'react'
+import classNames from 'classnames'
+import { remoteFunction } from 'src/util/webextensionRPC'
+import styles from './overview.css'
 
 export default class OverviewContainer extends React.Component {
-    state = {}
+    state = { automaticBackupEnabled: null, backupTimes: null }
 
-    render() {}
+    async componentDidMount() {
+        this.setState({
+            automaticBackupEnabled: await remoteFunction(
+                'isAutomaticBackupEnabled',
+            )(),
+            backupTimes: await remoteFunction('getBackupTimes')(),
+        })
+    }
+
+    render() {
+        if (!this.state.backupTimes) {
+            return null
+        }
+
+        return (
+            <div>
+                <h2>Settings</h2>
+                <div className={styles.settingsSection}>
+                    <div>
+                        <div className={styles.settingsLabel}>
+                            Automatic backup
+                        </div>
+                        <div className={styles.settingsHelp}>
+                            Worry-free. Automatically backs up your data every
+                            15 minutes.
+                        </div>
+                    </div>
+                    <div>
+                        {this.state.automaticBackupEnabled !== null && (
+                            <div
+                                className={classNames(
+                                    styles.smallButton,
+                                    this.state.automaticBackupEnabled &&
+                                        styles.dangerButton,
+                                )}
+                            >
+                                {this.state.automaticBackupEnabled && (
+                                    <span>Cancel</span>
+                                )}
+                                {!this.state.automaticBackupEnabled && (
+                                    <span>Enable</span>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className={styles.settingsSection}>
+                    <div>
+                        <div className={styles.settingsLabel}>
+                            Delete backup
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.smallButton}>Tutorial</div>
+                    </div>
+                </div>
+                <h2>Status</h2>
+                {this.state.backupTimes.lastBackup && (
+                    <div className={styles.statusLine}>
+                        <span className={styles.statusLabel}>
+                            Last backup:{' '}
+                            {moment(
+                                this.state.backupTimes.lastBackup,
+                            ).fromNow()}
+                        </span>
+                    </div>
+                )}
+                {this.state.backupTimes.nextBackup && (
+                    <div className={styles.statusLine}>
+                        <span className={styles.statusLabel}>
+                            Next backup:{' '}
+                        </span>
+                        {moment(this.state.backupTimes.nextBackup).fromNow()}
+                    </div>
+                )}
+                <h2>Restore</h2>
+                <div className={styles.settingsSection}>
+                    <div>
+                        <div
+                            className={classNames(
+                                styles.settingsLabel,
+                                styles.comingSoon,
+                            )}
+                        >
+                            Coming Very Soon
+                        </div>
+                        <div className={styles.settingsHelp}>
+                            You can back our development and get up to 4.5x of
+                            your contribution back in Premium Credits
+                        </div>
+                    </div>
+                    <div>
+                        <div
+                            className={classNames(
+                                styles.smallButton,
+                                styles.supportUs,
+                            )}
+                        >
+                            Support
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
