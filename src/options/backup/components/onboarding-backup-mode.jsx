1e4ad062cac3d4f5b66992652a4ccb09b53be691
Vincent den Boer <vincent@shishkabab.net>
2018-11-07 09:36:40 +0100

    Billing period choice on backup upgrade from overview

--- src/options/backup/components/onboarding-backup-mode.jsx
+++ src/options/backup/components/onboarding-backup-mode.jsx
@@ -3,7 +3,48 @@ import PropTypes from 'prop-types'
 import Styles from './backup-mode.css'
 import classNames from 'classnames'
 
+export default class OnboardingBackupModeContainer extends React.Component {
+    static propTypes = {
+        disableModeSelection: PropTypes.bool,
+        billingPeriod: PropTypes.string,
+        onModeChange: PropTypes.func,
+        onBillingPeriodChange: PropTypes.func.isRequired,
+    }
+
+    state = {
+        mode: null,
+        billingPeriod: null,
+    }
+
+    componentDidMount() {
+        if (this.props.disableModeSelection) {
+            this.setState({ mode: 'automatic' })
+        }
+    }
+
+    render() {
+        return (
+            <OnboardingBackupMode
+                {...this.props}
+                disableModeSelection={this.props.disableModeSelection}
+                mode={this.state.mode}
+                billingPeriod={this.state.billingPeriod}
+                showPrice={this.state.mode === 'automatic'}
+                onModeChange={mode => {
+                    this.setState({ mode })
+                    this.props.onModeChange && this.props.onModeChange(mode)
+                }}
+                onBillingPeriodChange={billingPeriod => {
+                    this.setState({ billingPeriod })
+                    this.props.onBillingPeriodChange(billingPeriod)
+                }}
+            />
+        )
+    }
+}
+
 export function OnboardingBackupMode({
+    disableModeSelection,
     mode,
     billingPeriod,
     onModeChange,
@@ -12,61 +53,68 @@ export function OnboardingBackupMode({
 }) {
     return (
         <div>
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        checked={mode === 'manual'}
-                        onChange={() => onModeChange('manual')}
-                    />{' '}
-                    <span className={Styles.option}>
-                        <span className={Styles.name}>Manual Backup</span>
-                        <span
-                            className={classNames(
-                                Styles.label,
-                                Styles.labelFree,
-                            )}
-                        >
-                            Free
-                        </span>
-                        <br />
-                        <span className={Styles.subname}>
-                            You need to regularly remember to back up yourself.
-                        </span>
-                    </span>
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        checked={mode === 'automatic'}
-                        onChange={() => onModeChange('automatic')}
-                    />{' '}
-                    <span className={Styles.option}>
-                        <span className={Styles.name}>Automatic Backup</span>
-                        <span
-                            className={classNames(
-                                Styles.label,
-                                Styles.labelPremium,
-                            )}
-                        >
-                            <i
+            {!disableModeSelection && (
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            checked={mode === 'manual'}
+                            onChange={() => onModeChange('manual')}
+                        />{' '}
+                        <span className={Styles.option}>
+                            <span className={Styles.name}>Manual Backup</span>
+                            <span
                                 className={classNames(
-                                    'material-icons',
-                                    Styles.star,
+                                    Styles.label,
+                                    Styles.labelFree,
                                 )}
                             >
-                                star
-                            </i>
-                            Premium
+                                Free
+                            </span>
+                            <br />
+                            <span className={Styles.subname}>
+                                You need to regularly remember to back up
+                                yourself.
+                            </span>
                         </span>
-                        <br />
-                        <span className={Styles.subname}>
-                            Worry-free backups every 15 minutes.
+                    </label>
+                </div>
+            )}
+            <div>
+                {!disableModeSelection && (
+                    <label>
+                        <input
+                            type="radio"
+                            checked={mode === 'automatic'}
+                            onChange={() => onModeChange('automatic')}
+                        />{' '}
+                        <span className={Styles.option}>
+                            <span className={Styles.name}>
+                                Automatic Backup
+                            </span>
+                            <span
+                                className={classNames(
+                                    Styles.label,
+                                    Styles.labelPremium,
+                                )}
+                            >
+                                <i
+                                    className={classNames(
+                                        'material-icons',
+                                        Styles.star,
+                                    )}
+                                >
+                                    star
+                                </i>
+                                Premium
+                            </span>
+                            <br />
+                            <span className={Styles.subname}>
+                                Worry-free backups every 15 minutes.
+                            </span>
                         </span>
-                    </span>
-                </label>
+                    </label>
+                )}
                 {showPrice && (
                     <div style={{ marginLeft: '40px' }}>
                         <div>
@@ -105,6 +153,7 @@ export function OnboardingBackupMode({
 }
 
 OnboardingBackupMode.propTypes = {
+    disableModeSelection: PropTypes.bool,
     mode: PropTypes.string,
     billingPeriod: PropTypes.string,
     onModeChange: PropTypes.func.isRequired,
