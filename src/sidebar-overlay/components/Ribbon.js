WorldBrain/Memex

commit e6693cfdc86309b7bd1c1e5249eee1f656cec861

Ensure sidebar iframe doesn't render by default
- stops iframe rendering when tab is created/registered
- now should register when needed (sidebar opens

src/sidebar-overlay/components/Ribbon.js
@@ -29,23 +29,16 @@ class Ribbon extends React.Component {
         // For preventing the page scroll, when sidebar is open
         isInsideFrame: false,
         top: null,
-        shouldRenderIFrame: true,
+        shouldRenderIFrame: false,
     }
 
     async componentDidMount() {
         this.setupRPCfunctions()
 
-        this.setupFrameFunctions()
-
         this.setupScrollListeners()
 
-        const annotations = await remoteFunction('getAllAnnotations')(
-            window.location.href,
-        )
+        await this.fetchAnnotations()
 
-        this.setState({
-            annotations,
-        })
         // For hiding the ribbion when fullScreen event is fired
         // For chrome
         document.addEventListener(
@@ -147,7 +140,7 @@ class Ribbon extends React.Component {
                 const selection = document.getSelection()
                 selection.removeAllRanges()
                 this.props.removeHighlights()
-                await this.openSidebarOps()
+                await this.openSidebar()
             },
         })
 
@@ -215,30 +208,6 @@ class Ribbon extends React.Component {
         this.frameFC.remoteExecute('setHoveredAnnotation')(annotationUrl)
     }
 
-    openSidebarOps = async () => {
-        if (!this.frameFC) {
-            this.setupFrameFunctions()
-        }
-        await this.frameFC.remoteExecute('setLoaderActive')()
-        await this.fetchAnnotations()
-        const highlightables = this.state.annotations.filter(
-            annotation => annotation.selector,
-        )
-        await this.props.highlightAll(
-            highlightables,
-            this.focusAnnotationContainer,
-            this.hoverAnnotationContainer,
-        )
-        this.frameFC.remoteExecute('focusCommentBox')(true)
-
-        setTimeout(() => {
-            const sorted = this.props.sortAnnotationByPosition(
-                this.state.annotations,
-            )
-            this.frameFC.remoteExecute('setAnnotations')(sorted)
-        }, 400)
-    }
-
     closeSidebarOps = async () => {
         this.props.removeHighlights()
         this.frameFC.remoteExecute('focusAnnotation')('')
@@ -257,7 +226,7 @@ class Ribbon extends React.Component {
         }
 
         if (isSidebarActive) {
-            await this.openSidebarOps()
+            await this.openSidebar()
         } else {
             this.closeSidebarOps()
         }
@@ -267,16 +236,45 @@ class Ribbon extends React.Component {
         })
     }
 
-    openSidebar = async () => {
-        await this.openSidebarOps()
-        this.setState({
-            isSidebarActive: true,
-        })
+    openSidebar = () => {
+        return this.setState(
+            {
+                isSidebarActive: true,
+                shouldRenderIFrame: true,
+            },
+            async () => {
+                if (!this.frameFC) {
+                    this.setupFrameFunctions()
+                }
+                await this.frameFC.remoteExecute('setLoaderActive')()
+                await this.fetchAnnotations()
+                const highlightables = this.state.annotations.filter(
+                    annotation => annotation.selector,
+                )
+                await this.props.highlightAll(
+                    highlightables,
+                    this.focusAnnotationContainer,
+                    this.hoverAnnotationContainer,
+                )
+                this.frameFC.remoteExecute('focusCommentBox')(true)
+
+                setTimeout(() => {
+                    const sorted = this.props.sortAnnotationByPosition(
+                        this.state.annotations,
+                    )
+                    this.frameFC.remoteExecute('setAnnotations')(sorted)
+                }, 400)
+            },
+        )
     }
 
     openSidebarAndSendAnchor = async anchor => {
-        await this.frameFC.remoteExecute('sendAnchorToSidebar')(anchor)
         await this.openSidebar()
+
+        setTimeout(
+            () => this.frameFC.remoteExecute('sendAnchorToSidebar')(anchor),
+            400,
+        )
     }
 
     handleClickOutside = e => {

src/sidebar-overlay/components/AnnotationContainer.js
@@ -16,7 +16,7 @@ import { IndexDropdown } from '../../common-ui/containers'
 class AnnotationContainer extends React.Component {
     static propTypes = {
         annotation: PropTypes.object.isRequired,
-        tags: PropTypes.array.isRequired,
+        tags: PropTypes.array,
         deleteAnnotation: PropTypes.func.isRequired,
         editAnnotation: PropTypes.func.isRequired,
         goToAnnotation: PropTypes.func.isRequired,

commit 6bbf5dff3b663a21bfd486d404473079ded320a3

Wait for sidebar to setup before attempting annot scroll
- the `openSidebar` method wasn't actually returning a `Promise`, although all invoking code treated it as if it did
- should fix #618
- regressed in e6693cfdc86309b7bd1c1e5249eee1f656cec861

src/sidebar-overlay/components/Ribbon.js
@@ -236,37 +236,41 @@ class Ribbon extends React.Component {
         })
     }
 
-    openSidebar = () => {
-        return this.setState(
-            {
-                isSidebarActive: true,
-                shouldRenderIFrame: true,
-            },
-            async () => {
-                if (!this.frameFC) {
-                    this.setupFrameFunctions()
-                }
-                await this.frameFC.remoteExecute('setLoaderActive')()
-                await this.fetchAnnotations()
-                const highlightables = this.state.annotations.filter(
-                    annotation => annotation.selector,
-                )
-                await this.props.highlightAll(
-                    highlightables,
-                    this.focusAnnotationContainer,
-                    this.hoverAnnotationContainer,
-                )
-                this.frameFC.remoteExecute('focusCommentBox')(true)
-
-                setTimeout(() => {
-                    const sorted = this.props.sortAnnotationByPosition(
-                        this.state.annotations,
+    openSidebar = () =>
+        new Promise(resolve => {
+            return this.setState(
+                {
+                    isSidebarActive: true,
+                    shouldRenderIFrame: true,
+                },
+                async () => {
+                    if (!this.frameFC) {
+                        this.setupFrameFunctions()
+                    }
+                    await this.frameFC.remoteExecute('setLoaderActive')()
+                    await this.fetchAnnotations()
+                    const highlightables = this.state.annotations.filter(
+                        annotation => annotation.selector,
                     )
-                    this.frameFC.remoteExecute('setAnnotations')(sorted)
-                }, 400)
-            },
-        )
-    }
+                    await this.props.highlightAll(
+                        highlightables,
+                        this.focusAnnotationContainer,
+                        this.hoverAnnotationContainer,
+                    )
+                    this.frameFC.remoteExecute('focusCommentBox')(true)
+
+                    setTimeout(async () => {
+                        const sorted = this.props.sortAnnotationByPosition(
+                            this.state.annotations,
+                        )
+                        await this.frameFC.remoteExecute('setAnnotations')(
+                            sorted,
+                        )
+                        resolve()
+                    }, 400)
+                },
+            )
+        })
 
     openSidebarAndSendAnchor = async anchor => {
         await this.openSidebar()