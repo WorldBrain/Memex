1d3911114fe999dded554162cd51ee64388c4836
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-11-15 14:42:25 +1100

    Bring back backups change tracking
    - no longer event-based, now just sets up a callback to be called on each table hook

---
+++ src/backup/background/change-hooks.ts
@@ -0,0 +1,42 @@
+import getDb from '../../search'
+import { StorageManager } from '../../search/types'
+
+export type ChangeTracker = (
+    args: { collection: string; pk: string; operation: string },
+) => void
+
+export default async function setupChangeTracking(
+    { registry }: StorageManager,
+    track: ChangeTracker,
+) {
+    const db = await getDb
+
+    for (const collection in registry.collections) {
+        if (registry.collections[collection].watch === false) {
+            continue
+        }
+
+        const table = db[collection]
+        table.hook('creating', (pk, obj, transaction) => {
+            track({
+                operation: 'create',
+                collection,
+                pk,
+            })
+        })
+        table.hook('updating', (mods, pk, obj, transaction) => {
+            track({
+                operation: 'update',
+                collection,
+                pk,
+            })
+        })
+        table.hook('deleting', (pk, obj, transaction) => {
+            track({
+                operation: 'delete',
+                collection,
+                pk,
+            })
+        })
+    }
+}
