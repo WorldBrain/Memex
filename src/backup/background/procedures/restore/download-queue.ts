97ea6a4f15315e83a8c7f4d9ba5b8680211dc8a5
Vincent den Boer <vincent@shishkabab.net>
2018-11-16 13:48:55 +0100

    Implement top-level restore procedure

--- src/backup/background/procedures/restore/download-queue.ts
+++ src/backup/background/procedures/restore/download-queue.ts
@@ -2,39 +2,44 @@ export type Fetcher = (url) => Promise<any>
 
 // Give in a list of URLs, and it will download the next item, while the program is processing the current one
 export class DownloadQueue {
-    urls: string[] // What's left to download
+    items: any[] // What's left to download
     fetched: any[] // What we've downloaded already and are ready to give
     private fetching: Promise<any> // When we're downloading something, listen here for that request to finish
     private fetcher: Fetcher
 
     constructor({
-        urls,
+        items,
         fetcher = fetch,
     }: {
-        urls: string[]
+        items: any[]
         fetcher: Fetcher
     }) {
-        this.urls = urls
+        this.items = items
         this.fetched = []
         this.fetcher = fetcher
-        this.downloadNext()
+        this.fetching = this.downloadNext()
     }
 
     async downloadNext() {
         if (this.fetching) {
             return this.fetching
         }
+        if (!this.items.length) {
+            return null
+        }
 
-        const url = this.urls.shift()
-        this.fetching = this.fetcher(url).then(response => {
-            this.fetched.push(response)
-            this.fetching = null
-            if (this.fetched.length <= 1) {
-                this.downloadNext()
-            }
-            return response
-        })
-        return this.fetching
+        // Make sure things are appended in-order by pre-allocating in array
+        const index = this.fetched.length
+        this.fetched.push(null)
+        const item = this.items.shift()
+
+        const response = await this.fetcher(item)
+        this.fetched[index] = response
+        this.fetching = null
+        if (this.fetched.length <= 1) {
+            this.fetching = this.downloadNext()
+        }
+        return response
     }
 
     async getNext() {
@@ -43,17 +48,20 @@ export class DownloadQueue {
         }
 
         let next
-        if (this.fetched.length) {
+        if (this.fetched.length && this.fetched[0]) {
             next = this.fetched.shift()
-            this.downloadNext()
+            this.fetching = this.downloadNext()
         } else {
-            next = this.downloadNext()
+            this.fetching = next = this.downloadNext().then(response => {
+                this.fetched.shift()
+                return response
+            })
         }
 
         return next
     }
 
     hasNext() {
-        return !!this.urls.length || this.fetched.length
+        return !!this.items.length || this.fetched.length
     }
 }
