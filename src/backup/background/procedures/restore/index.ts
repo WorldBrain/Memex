c54ae4433f56183570faec44b4a14ac264aa451d
Vincent den Boer <vincent@shishkabab.net>
2018-11-20 14:41:36 +0100

    Implement restore procedure status reporting

--- src/backup/background/procedures/restore/index.ts
+++ src/backup/background/procedures/restore/index.ts
@@ -5,10 +5,17 @@ import { BackupBackend } from '../../backend'
 import Interruptable from '../interruptable'
 import { DownloadQueue } from './download-queue'
 
+export interface BackupRestoreInfo {
+    status: 'preparing' | 'synching'
+    totalObjects?: number
+    processedObjects: number
+}
+
 export class BackupRestoreProcedure {
     storageManager: StorageManager
     backend: BackupBackend
 
+    info?: BackupRestoreInfo
     events?: EventEmitter
     interruptable?: Interruptable
 
@@ -32,19 +39,36 @@ export class BackupRestoreProcedure {
         this.interruptable = new Interruptable()
 
         const procedure = async () => {
+            this._updateInfo({ status: 'preparing', processedObjects: 0 })
+
             try {
                 await this._clearDatabase()
                 await this._blockDatabase()
 
+                const [
+                    changeSetTimestamps,
+                    imageTimestamps,
+                ] = await Promise.all([
+                    this._listBackupCollection('change-sets'),
+                    this._listBackupCollection('images'),
+                ])
+                this._updateInfo({
+                    status: 'synching',
+                    totalObjects:
+                        changeSetTimestamps.length + imageTimestamps.length,
+                })
+
                 await this.interruptable.execute(() =>
                     this._restoreCollection(
                         'change-sets',
+                        changeSetTimestamps,
                         this._writeChange.bind(this),
                     ),
                 )
                 await this.interruptable.execute(() =>
                     this._restoreCollection(
                         'images',
+                        imageTimestamps,
                         this._writeImage.bind(this),
                     ),
                 )
@@ -64,6 +88,7 @@ export class BackupRestoreProcedure {
             } finally {
                 this.interruptable = null
                 this.events = null
+                this.info = null
             }
         }
 
@@ -75,9 +100,9 @@ export class BackupRestoreProcedure {
 
     async _restoreCollection(
         collection: string,
+        timestamps: string[],
         writeObject: (object: any) => Promise<any>,
     ) {
-        const timestamps = await this._listBackupCollection(collection)
         const changeSetDownloadQueue = this._createDownloadQueue(
             collection,
             timestamps,
@@ -98,6 +123,9 @@ export class BackupRestoreProcedure {
                 const batch = await queue.getNext()
                 await this.interruptable.forOfLoop(batch, async change => {
                     await writeObject(change)
+                })
+                this._updateInfo({
+                    processedObjects: this.info.processedObjects + 1,
                 })
             },
         )
@@ -127,5 +155,10 @@ export class BackupRestoreProcedure {
     _createBackupObjectFetcher() {
         return ([collection, object]) =>
             this.backend.retrieveObject(collection, object)
+    }
+
+    _updateInfo(changes) {
+        this.info = Object.assign({}, this.info, changes)
+        this.events.emit('info', this.info)
     }
 }
