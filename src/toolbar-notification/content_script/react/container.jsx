56008d4f4902e69ed58df6a13195072935090e56
Oliver Sauter <oli@worldbrain.io>
2018-12-05 15:18:19 +0100

    remove unused import

--- src/toolbar-notification/content_script/react/container.jsx
+++ src/toolbar-notification/content_script/react/container.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import TooltipFirstCloseNotification from './notifications/tooltip-first-close'
 import styles from './styles.css'
