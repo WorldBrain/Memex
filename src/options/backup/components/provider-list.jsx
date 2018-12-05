2f1608e866b99eb8726c609039d81aa2b1c156f6
Oliver Sauter <oli@worldbrain.io>
2018-12-05 14:48:45 +0100

    Added new cloud provider options and survey

--- src/options/backup/components/provider-list.jsx
+++ src/options/backup/components/provider-list.jsx
@@ -5,6 +5,7 @@ import Styles from './provider-list.css'
 export function ProviderList({ onChange }) {
     return (
         <div>
+            <p className={Styles.subTitle}>Currently Available</p>
             <label>
                 <input type="radio" onChange={() => onChange('google-drive')} />
                 <span style={{ cursor: 'pointer' }}>
@@ -13,11 +14,35 @@ export function ProviderList({ onChange }) {
                 </span>
             </label>
             <br />
+            <p className={Styles.subTitle}>Future Options</p>
+            <div className={Styles.voteContainer}>
+                <a
+                    className={Styles.vote}
+                    target="blank"
+                    href="https://goo.gl/forms/Lh5pPWc75r7ds2m63"
+                >
+                    Vote
+                </a>
+                <span>on your favorite provider</span>
+            </div>
+            <input type="radio" disabled />
+            <span className={Styles.disabled}>Local Hard Drive</span>
+            <br />
+            <input type="radio" disabled />
+            <span className={Styles.disabled}>Own Cloud</span>
+            <br />
+            <input type="radio" disabled />
+            <span className={Styles.disabled}>Self hosting</span>
+            <br />
+            <input type="radio" disabled />
+            <span className={Styles.disabled}>Own Cloud</span>
+            <br />
             <input type="radio" disabled />
             <span className={Styles.disabled}>Dropbox</span>
             <br />
             <input type="radio" disabled />
-            <span className={Styles.disabled}>Self hosting</span>
+            <span className={Styles.disabled}>AWS</span>
+            <br />
         </div>
     )
 }
