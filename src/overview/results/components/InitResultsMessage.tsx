7b1e57e21a3c95c70148da9431f95ab704d9b968
digi0ps <sriru1998@gmail.com>
2018-12-06 17:43:06 +0530

    Replace previous InitResultsMessage content with new Onboarding box.

--- src/overview/results/components/InitResultsMessage.tsx
+++ src/overview/results/components/InitResultsMessage.tsx
@@ -3,24 +3,36 @@ import { Link } from 'react-router'
 
 import ResultsMessage from './ResultsMessage'
 
-const styles = require('./ResultsMessage.css')
+const styles = require('./InitResultsMessage.css')
 
 class InitResultsMessage extends PureComponent {
     render() {
         return (
             <ResultsMessage>
-                <div className={styles.title}>
-                    Nothing to search yet. <br />
-                    <div className={styles.subTitle}>
-                        <a
-                            target="_blank"
-                            href="http://memex.link/S1zVMgKzX/en.wikipedia.org/wiki/Memex"
-                        >
-                            Visit
-                        </a>{' '}
-                        a website, <a href="/options.html#/import">import</a>{' '}
-                        your existing history or{' '}
-                        <a href="/options.html#/tutorial">read</a> the tutorial.
+                <div className={styles.container}>
+                    <div className={styles.leftParent}>
+                        <p className={styles.welcome}>Welcome to your Memex</p>
+                        <p className={styles.text}>
+                            The all-in-one tool to get
+                            <br /> on top of your online chaos.
+                        </p>
+                        {/* <img></img> */}
+                    </div>
+                    <div className={styles.rightParent}>
+                        <p className={styles.title}>Let's get started</p>
+                        <p className={styles.subtext}>
+                            Complete the steps & get 1 month of free
+                            auto-backups
+                        </p>
+                        <div className={styles.checklist}>
+                            Make your first web annotation
+                        </div>
+                        <div className={styles.checklist}>
+                            Do your first power search
+                        </div>
+                        <div className={styles.checklist}>
+                            Import from your existing bookmarks & history
+                        </div>
                     </div>
                 </div>
             </ResultsMessage>
