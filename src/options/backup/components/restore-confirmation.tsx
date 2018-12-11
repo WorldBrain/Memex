1e701271730967eeaae128c7e7052ce1b31122c9
Vincent den Boer <vincent@shishkabab.net>
2018-12-11 14:29:43 +0100

    Work on restore UI

--- src/options/backup/components/restore-confirmation.tsx
+++ src/options/backup/components/restore-confirmation.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import * as logic from './restore-confirmation.logic'
+const styles = require('./restore-confirmation.css')
 
-export default class OverviewContainer extends React.Component {
+export default class RestoreConfirmation extends React.Component {
+    static propTypes = {
+        onConfirm: PropTypes.func.isRequired,
+        onClose: PropTypes.func.isRequired,
+    }
+
     state = logic.INITIAL_STATE
     handleEvent = null
 
@@ -13,18 +19,20 @@ export default class OverviewContainer extends React.Component {
 
     render() {
         return (
-            <div>
-                Are you sure?
-                <input
-                    type="text"
-                    value={this.state.confirmation}
-                    onChange={event =>
-                        this.handleEvent({
-                            type: 'onConfirmationChange',
-                            value: event.target.value,
-                        })
-                    }
-                />
+            <div className={styles.container}>
+                <div className={styles.box}>
+                    Are you sure?
+                    <input
+                        type="text"
+                        value={this.state.confirmation}
+                        onChange={event =>
+                            this.handleEvent({
+                                type: 'onConfirmationChange',
+                                value: event.target.value,
+                            })
+                        }
+                    />
+                </div>
             </div>
         )
     }
