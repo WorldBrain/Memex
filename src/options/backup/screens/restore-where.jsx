4ffa08d537b4a7f674c7fd5c47feb28c4f682df1
Vincent den Boer <vincent@shishkabab.net>
2018-12-13 10:42:53 +0100

    Restore where screen logic

---
+++ src/options/backup/screens/restore-where.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+// import classNames from 'classnames'
+import * as logic from './restore-where.logic'
+// const styles = require('./restore-confirmation.css')
+
+export default class RestoreConfirmation extends React.Component {
+    // static propTypes = {
+    //     onConfirm: PropTypes.func.isRequired,
+    //     onClose: PropTypes.func.isRequired,
+    // }
+
+    state = logic.INITIAL_STATE
+    handleEvent = null
+
+    componentWillMount() {
+        this.handleEvent = logic.reactEventHandler(this, logic.processEvent)
+    }
+
+    render() {
+        return <div className={styles.container}>Placeholder</div>
+    }
+}
