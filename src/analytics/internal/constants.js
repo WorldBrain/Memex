c5ea6ec1f5f4409a020b5af3abe52a3e16e7f638
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2018-12-13 14:02:39 +0530

    #492 Move away from string literals for internal analytics events

--- src/analytics/internal/constants.js
+++ src/analytics/internal/constants.js
@@ -1,7 +1,7 @@
 export const API_HOST =
-    process.env.NODE_ENV === 'production'
-        ? 'https://203bqy2f93.execute-api.eu-central-1.amazonaws.com/production'
-        : 'https://a8495szyaa.execute-api.eu-central-1.amazonaws.com/staging'
+    process.env.NODE_ENV === 'production' ?
+    'https://203bqy2f93.execute-api.eu-central-1.amazonaws.com/production' :
+    'https://a8495szyaa.execute-api.eu-central-1.amazonaws.com/staging'
 
 // This is used to change the event type into integer to optimize the space
 
@@ -266,6 +266,61 @@ export const EVENT_TYPES = {
     learnMoreCrowdFunding: {
         id: 74,
     },
+}
+
+export const EVENT_NAMES = {
+    SUCCESFUL_SEARCH: 'succesfulSearch',
+    PAGINATE_SEARCH: 'paginateSearch',
+    UNSUCCESFUL_SEARCH: 'unsuccessfulSearch',
+    DATEPICKER_DROPDOWN_START: 'datepickerByDropdownStartDate',
+    DATEPICKER_CLEAR_START: 'datepickerClearStartDate',
+    DATEPICKER_DROPDOWN_END: 'datepickerByDropdownEndDate',
+    DATEPICKER_CLEAR_END: 'datepickerClearEndDate',
+    BOOKMARK_FILTER: 'bookmarkFilter',
+    TAG_FILTER: 'tagFilter',
+    DOMAIN_FILTER: 'domainFilter',
+    ADD_TAG: 'addTag',
+    DELETE_TAG: 'deleteTag',
+    DELETE_RESULT: 'deleteResult',
+    CREATE_RESULT_BOOKMARK: 'createResultBookmark',
+    REMOVE_RESULT_BOOKMARK: 'removeResultBookmark',
+    CREATE_BROWSER_BOOKMARK: 'createBrowserBookmark',
+    REMOVE_BROWSER_BOOKMARK: 'removeBrowserBookmark',
+    CREATE_POPUP_BOOKMARK: 'createPopupBookmark',
+    REMOVE_POPUP_BOOKMARK: 'removePopupBookmark',
+    ADD_POPUP_TAG: 'addPopupTag',
+    DELETE_POPUP_TAG: 'deletePopupTag',
+    BLACKLIST_SITE: 'blacklistSite',
+    BLACKLIST_DOMAIN: 'blacklistDomain',
+    REMOVE_BLACKLIST_ENTRY: 'removeBlacklistEntry',
+    CHANGE_TRACKING_PREF_OPTOUT: 'changeTrackingPrefOptOut',
+    CHANGE_TRACKING_PREF_OPTIN: 'changeTrackingPrefOptIn',
+    START_IMPORT: 'startImport',
+    PAUSE_IMPORT: 'pauseImport',
+    RESUME_IMPORT: 'resumeImport',
+    CANCEL_IMPORT: 'cancelImport',
+    FINISH_IMPORT: 'finishImport',
+    SEARCH_POPUP: 'serachPopup',
+    SUCCESSFUL_OMNIBAR_SEARCH: 'successfulOmnibarSearch',
+    UNSUCCESSFUL_OMNIBAR_SEARCH: 'unsuccessfulOmnibarSearch',
+    DATEPICKER_NLP_START_DATE: 'datepickerByNlpStartDate',
+    DATEPICKER_NLP_END_DATE: 'datepickerByNlpEndDate',
+    NLP_SEARCH: 'nlpSearch',
+    ONBOARDING_CANCEL_IMPORT: 'onboardingCancelImport',
+    ONBOARDING_FINISH_IMPORT: 'onboardingFinishImport',
+    ADD_BLACKLIST_ENTRY: 'addBlacklistEntry',
+    READ_NOTIFICATION_SEARCH_ENGINE: 'redNotificationSearchEngine',
+    READ_NOTIFICATION_OVERVIEW: 'readNotificationOverview',
+    READ_NOTIFICATION_PAGINATION: 'readNotificationPagination',
+    OPEN_INBOX_OVERVIEW: 'openInboxOveview',
+    CLICK_STORAGE_CHANGE_NOTIF_BUTTON: 'clickStorageChangeNotifButton',
+    CLICK_OPEN_NEW_LINK_BUTTON: 'clickOpenNewLinkButton',
+    CLOSE_INBOX_OVERVIEW: 'closeInboxOverview',
+    CLICK_ON_SYSTEM_NOTIFICATION: 'clickOnSystemNotification',
+    TOGGLE_STORAGE_SEARCH_ENGINE: 'toggleStorageSearchEngine',
+    CLICK_OPEN_NEW_LINK_BUTTON_SEARCH: 'clickOpenNewLinkButtonSearch',
+    RESUME_INDEXING: 'resumeIndexing',
+    PAUSE_INDEXING: 'pauseIndexing',
 }
 
 export const NOTIF_TYPE_EVENT_IDS = {
