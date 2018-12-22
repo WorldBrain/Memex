8820f38bf399b8a14162193161817682d6d325e4
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-12-22 22:52:35 +0530

    Reorganize stuff into proper directories

---
+++ src/sidebar-overlay/ribbon-sidebar-controller/components/ribbon-sidebar-controller.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import RibbonContainer from '../../ribbon'
+
+/* tslint:disable-next-line variable-name */
+const RibbonSidebarController = () => (
+    <React.Fragment>
+        <RibbonContainer handleRemoveRibbon={() => null} />
+    </React.Fragment>
+)
+
+export default RibbonSidebarController
