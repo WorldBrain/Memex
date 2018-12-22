37a123ea204e26a3abfd1bf5ea2a2e553d09d4b8
digi0ps <sriru1998@gmail.com>
2018-12-22 13:57:44 +0530

    Added different onboarding tooltips.

--- src/overview/tooltips/components/tooltip.tsx
+++ src/overview/tooltips/components/tooltip.tsx
@@ -2,12 +2,19 @@ import React, { SFC } from 'react'
 
 const styles = require('./tooltip.css')
 
+export interface Position {
+    top: number | string
+    left: number | string
+}
 export interface Props {
     children: React.ReactChild
+    position: Position
 }
 
-const Tooltip: SFC<Props> = ({ children }) => (
-    <div className={styles.container}>{children}</div>
+const tooltip: SFC<Props> = ({ children, position }) => (
+    <div className={styles.container} style={position}>
+        {children}
+    </div>
 )
 
-export default Tooltip
+export default tooltip
