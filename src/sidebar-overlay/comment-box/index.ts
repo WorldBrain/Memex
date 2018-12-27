44e30aff683440903d38aeac583c0125bf9a02be
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-12-28 00:39:44 +0530

    Refactor parts of comment box, top bar

--- src/sidebar-overlay/comment-box/index.ts
+++ src/sidebar-overlay/comment-box/index.ts
@@ -1,7 +1,7 @@
-import CommentBoxContainer from './components'
+import CommentBox from './components'
 import * as actions from './actions'
 import reducer from './reducer'
 import * as selectors from './selectors'
 
-export default CommentBoxContainer
+export default CommentBox
 export { actions, selectors, reducer }
--- src/sidebar-overlay/sidebar/selectors.ts
+++ src/sidebar-overlay/sidebar/selectors.ts
@@ -5,3 +5,8 @@ import RootState from '../ribbon-sidebar-controller/types'
 export const sidebar = (state: RootState) => state.sidebar
 
 export const isOpen = createSelector(sidebar, state => state.isOpen)
+
+export const isUserCommenting = createSelector(
+    sidebar,
+    state => state.isUserCommenting,
+)
