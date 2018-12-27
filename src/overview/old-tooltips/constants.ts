7e947a5757b2c6bd238bde53adab9acd012ac311
digi0ps <sriru1998@gmail.com>
2018-12-27 19:35:51 +0530

    Removed the old tooltips module.

--- src/overview/old-tooltips/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SHOW_TOOL_TIP = 'is-overview-tooltips-shown'
-
-export const TOOL_TIP = 'overview-tooltips'
--- src/overview/old-tooltips/index.ts
+++
@@ -1,7 +0,0 @@
-import Tooltip from './components/TooltipContainer'
-import * as selectors from './selectors'
-import * as acts from './actions'
-import * as constants from './constants'
-import reducer from './reducer'
-
-export { selectors, reducer, acts, constants, Tooltip }
