5d7275ca71af99ecd0860c38b8f5cf9a3dc5c8a3
digi0ps <sriru1998@gmail.com>
2018-12-28 11:45:34 +0530

    Remove previous onboarding.
    - Removed previous onboarding state, actions and selectors.
    - Remove imports of previous onboarding selectors and actions.
    - Removed enhancers storing previous onboarding state.

--- src/overview/onboarding/index.js
+++ src/overview/onboarding/index.js
@@ -1,9 +1,6 @@
-import container from './container'
 import * as selectors from './selectors'
 import * as actions from './actions'
 import * as constants from './constants'
-
-export default container
 
 export { default as reducer } from './reducer'
 export { selectors, actions, constants }
