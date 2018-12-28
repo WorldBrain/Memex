dab4bcf86fe0803e60b01ffcba6f7a96578e71be
digi0ps <sriru1998@gmail.com>
2018-12-28 11:55:21 +0530

    Moved tutorial components from onboarding module.
    Moves the components and constants from overview/onboarding to
    options/tutorial.

---
+++ src/options/tutorial/constants.ts
@@ -0,0 +1,24 @@
+import { FeatureInfo } from './types'
+
+export const FEATURES_INFO: FeatureInfo[] = [
+    {
+        heading: 'Add notes to websites',
+        subheading: 'Comment on, highlight and annotate your web',
+        url: 'https://worldbrain.io/onboarding-annotations',
+    },
+    {
+        heading: 'Full-Text Search your History ',
+        subheading: 'And filter by time, domain, tags or bookmarks',
+        url: 'https://worldbrain.io/onboarding-search',
+    },
+    {
+        heading: 'Share URLs to highlighted text',
+        subheading: 'Show others specific pieces of text in any website',
+        url: 'https://worldbrain.io/onboarding-memex-link',
+    },
+    {
+        heading: 'Flexibly organise',
+        subheading: 'Bookmark, tag & and sort websites into lists',
+        url: 'https://worldbrain.io/onboarding-collections',
+    },
+]
