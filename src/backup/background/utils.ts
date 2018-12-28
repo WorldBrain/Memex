8a2790d2c1329baed7951794506c1185a28e5476
Vincent den Boer <vincent@shishkabab.net>
2018-12-28 14:35:19 +0100

    Fix bugs stemming from Storex rebase

--- src/backup/background/utils.ts
+++ src/backup/background/utils.ts
@@ -1,4 +1,4 @@
-import { CollectionDefinition } from '../../search/storage'
+import { CollectionDefinition } from 'storex/lib/types'
 
 export function isExcludedFromBackup(collection: CollectionDefinition) {
     return collection.backup === false
