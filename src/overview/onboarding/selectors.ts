38efeed58c547b8657a6fb445fbadd46137cb56c
digi0ps <sriru1998@gmail.com>
2018-12-28 17:30:20 +0530

    Refactor onboarding files to typescript.
    - Refactored reducer, actions, selectors to typescript.
    - Simplfied redux state.

---
+++ src/overview/onboarding/selectors.ts
@@ -0,0 +1,13 @@
+import { createSelector } from 'reselect'
+import { RootState } from '../../options/types'
+
+export const onboarding = (state: RootState) => state.onboarding
+
+export const annotationStage = createSelector(
+    onboarding,
+    state => state.annotationStage,
+)
+export const powerSearchStage = createSelector(
+    onboarding,
+    state => state.powerSearchStage,
+)
