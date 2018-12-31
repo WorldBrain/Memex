ed45df82094219d494b2a790a2d127e4f1d03e6b
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-12-31 21:35:00 +0530

    Add ability to save comments

--- src/sidebar-overlay/ribbon-sidebar-controller/actions.ts
+++ src/sidebar-overlay/ribbon-sidebar-controller/actions.ts
@@ -1,15 +1,1 @@
-import { getSidebarState } from '../utils'
-import { getTooltipState } from '../../content-tooltip/utils'
-import { Thunk } from '../types'
-import { actions as ribbonActions } from '../ribbon'
-
-/**
- * Hydrates the Redux store on initial startup.
- */
-export const initState: () => Thunk = () => async dispatch => {
-    const isRibbonEnabled = await getSidebarState()
-    const isTooltipEnabled = await getTooltipState()
-
-    dispatch(ribbonActions.setRibbonEnabled(isRibbonEnabled))
-    dispatch(ribbonActions.setTooltipEnabled(isTooltipEnabled))
-}
+export const dummyAction = () => null
