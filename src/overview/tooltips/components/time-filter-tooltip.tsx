14591a1715c9600fdcc40a3c0ee9e0aa5a0a2146
Oliver Sauter <oli@worldbrain.io>
2019-01-02 18:10:25 +0100

    add pro tip to datepicker

--- src/overview/tooltips/components/time-filter-tooltip.tsx
+++ src/overview/tooltips/components/time-filter-tooltip.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 
-const styles = require('./tooltip.css')
-
 const timeFilterTooltip: React.SFC = () => (
-    <React.Fragment>
-        Filter by time
-        <br />
-        <br />
-        <span className={styles.emoji}>🤓</span>
-        <span className={styles.proTip}>PRO Tip: </span>
-        <span className={styles.noBold}>try </span>
-        <span className={styles.example}>e.g. "1 hour ago"</span>
-    </React.Fragment>
+    <React.Fragment>Filter by time</React.Fragment>
 )
 
 export default timeFilterTooltip
