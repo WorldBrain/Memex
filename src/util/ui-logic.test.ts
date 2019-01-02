751e4386fd8950606fa3d3fb5ebab0f5806aab28
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-01-02 18:56:29 +0700

    Add noop test to fix broken CI

--- src/util/ui-logic.test.ts
+++ src/util/ui-logic.test.ts
@@ -1,3 +1,4 @@
 describe('Tools to connect framework independent UI logic with React', () => {
     // TODO: Write these, but not urgently since relying components test these fully
+    test.skip('noop', () => undefined)
 })
