19dd3ff60e335c65aeb4207b106234b8baf0eecb
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-01-11 13:16:53 +0700

    Ensure "Include Screenshots?" UI checkbox is adhered to
    - also set `change.object.screenshot` to `undefined` rather than `null`
    post-image-serialization
    - this ensures the final JSON omits that field completely

--- src/backup/background/backend/types.ts
+++ src/backup/background/backend/types.ts
@@ -22,6 +22,10 @@ export interface ObjectChangeBatch {
     forget: () => Promise<void>
 }
 
+export interface ObjectChangeImages {
+    screenshot: string
+}
+
 export abstract class BackupBackend {
     async getLoginUrl(params: any): Promise<string | null> {
         return null
