d13162c38f455d43566e9f372acdc69d927155ca
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2019-01-13 21:59:13 +0530

    Display 'Go to annotation' button in 'overview'.

--- src/sidebar-common/components/annotation-box/annotation-box-default-footer.tsx
+++ src/sidebar-common/components/annotation-box/annotation-box-default-footer.tsx
@@ -4,6 +4,8 @@ import cx from 'classnames'
 const styles = require('./annotation-box-default-footer.css')
 
 interface Props {
+    displayGoToAnnotation: boolean
+    goToAnnotationHandler: (e: React.MouseEvent<HTMLElement>) => void
     editIconClickHandler: () => void
     trashIconClickHandler: () => void
     shareIconClickHandler: () => void
@@ -12,45 +14,58 @@ interface Props {
 
 /* tslint:disable-next-line variable-name */
 const AnnotationBoxDefaultFooter = ({
+    displayGoToAnnotation,
+    goToAnnotationHandler,
     editIconClickHandler,
     trashIconClickHandler,
     shareIconClickHandler,
     replyIconClickHandler,
 }: Props) => (
-    <React.Fragment>
-        <span
-            className={cx(styles.commonIcon, styles.editIcon)}
-            title="Edit note"
-            onClick={e => {
-                e.stopPropagation()
-                editIconClickHandler()
-            }}
-        />
-        <span
-            className={cx(styles.commonIcon, styles.trashIcon)}
-            title="Delete note"
-            onClick={e => {
-                e.stopPropagation()
-                trashIconClickHandler()
-            }}
-        />
-        <span
-            className={cx(styles.commonIcon, styles.shareIcon)}
-            title="Share this note"
-            onClick={e => {
-                e.stopPropagation()
-                shareIconClickHandler()
-            }}
-        />
-        <span
-            className={cx(styles.commonIcon, styles.replyIcon)}
-            title="Reply to this note"
-            onClick={e => {
-                e.stopPropagation()
-                replyIconClickHandler()
-            }}
-        />
-    </React.Fragment>
+    <div className={styles.annotationBoxDefaultFooter}>
+        <div>
+            <span
+                className={cx(styles.commonIcon, styles.editIcon)}
+                title="Edit note"
+                onClick={e => {
+                    e.stopPropagation()
+                    editIconClickHandler()
+                }}
+            />
+            <span
+                className={cx(styles.commonIcon, styles.trashIcon)}
+                title="Delete note"
+                onClick={e => {
+                    e.stopPropagation()
+                    trashIconClickHandler()
+                }}
+            />
+            <span
+                className={cx(styles.commonIcon, styles.shareIcon)}
+                title="Share this note"
+                onClick={e => {
+                    e.stopPropagation()
+                    shareIconClickHandler()
+                }}
+            />
+            <span
+                className={cx(styles.commonIcon, styles.replyIcon)}
+                title="Reply to this note"
+                onClick={e => {
+                    e.stopPropagation()
+                    replyIconClickHandler()
+                }}
+            />
+        </div>
+        <div>
+            {displayGoToAnnotation && (
+                <span
+                    className={cx(styles.commonIcon, styles.goToPageIcon)}
+                    title="Go to annotation"
+                    onClick={goToAnnotationHandler}
+                />
+            )}
+        </div>
+    </div>
 )
 
 export default AnnotationBoxDefaultFooter
