ba64cece085216f3d62d0a53b0b29b1457f67224
Eik-S <eikem@posteo.de>
2019-01-21 17:44:22 +0100

    first onboarding pages finished

--- src/options/backup/components/provider-list.jsx
+++ src/options/backup/components/provider-list.jsx
@@ -6,14 +6,40 @@ export function ProviderList({ onChange }) {
     return (
         <div>
             <p className={Styles.subTitle}>Currently Available</p>
-            <label>
-                <input type="radio" onChange={() => onChange('google-drive')} />
-                <span style={{ cursor: 'pointer' }}>
-                    <img className={Styles.logo} src={'img/google-drive.png'} />
-                    <span className={Styles.name}>Google Drive</span>
-                </span>
-            </label>
-            <br />
+            <form>
+                <label>
+                    <input
+                        type="radio"
+                        name="backend-select"
+                        onChange={() => onChange('google-drive')}
+                    />
+                    <span style={{ cursor: 'pointer' }}>
+                        <img
+                            className={Styles.logo}
+                            src={'img/google-drive.png'}
+                        />
+                        <span className={Styles.name}>Google Drive</span>
+                    </span>
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="backend-select"
+                        onChange={() => onChange('local')}
+                    />
+                    <span style={{ cursor: 'pointer' }}>
+                        <span className={Styles.title}>Local Backup</span>
+                        <p className={Styles.description}>
+                            Backup your data locally, or to any cloud provider
+                            that has a syncing folder on your
+                            <br />
+                            computer (e.g. Dropbox, Spideroak, GDrive)
+                        </p>
+                    </span>
+                </label>
+                <br />
+            </form>
             <p className={Styles.subTitle}>Future Options</p>
             <div className={Styles.voteContainer}>
                 <a
@@ -25,9 +51,6 @@ export function ProviderList({ onChange }) {
                 </a>
                 <span>on your favorite provider</span>
             </div>
-            <input type="radio" disabled />
-            <span className={Styles.disabled}>Local Hard Drive</span>
-            <br />
             <input type="radio" disabled />
             <span className={Styles.disabled}>Own Cloud</span>
             <br />
