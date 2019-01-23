3ffb8cecee81938fa526e7534e924889b2a5654e
digi0ps <sriru1998@gmail.com>
2019-01-24 01:46:49 +0530

    Display annotations along with Results.
    Use dummy data to display annotations now.

--- src/overview/results/components/annotation-list.tsx
+++ src/overview/results/components/annotation-list.tsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 import cx from 'classnames'
 
+import AnnotationBox from 'src/sidebar-common/annotation-box'
+import { Annotation } from 'src/sidebar-common/sidebar/types'
+import { DUMMY_ANNOTATION } from '../constants'
+
 const styles = require('./annotation-list.css')
 
-export interface Props {}
+export interface Props {
+    annotations: Annotation[]
+}
 
 export interface State {
+    /* Boolean to denote whether the list is expanded or not */
     isExpanded: boolean
 }
 
 class AnnotationList extends Component<Props, State> {
+    static defaultProps = {
+        annotations: [
+            { ...DUMMY_ANNOTATION },
+            { ...DUMMY_ANNOTATION },
+            { ...DUMMY_ANNOTATION },
+        ],
+    }
+
     state = {
         isExpanded: false,
     }
@@ -17,21 +32,54 @@ class AnnotationList extends Component<Props, State> {
     handleArrowClick = () => {
         this.setState(
             (prevState: State): State => ({
+                ...prevState,
                 isExpanded: !prevState.isExpanded,
             }),
         )
+    }
+
+    renderAnnotations = () => {
+        return this.props.annotations.map(annot => (
+            /* Wrapper element to restrict the width of the annotation box */
+            <div className={styles.annotation}>
+                <AnnotationBox
+                    key={annot.url}
+                    env="overview"
+                    isActive={false}
+                    isHovered={false}
+                    url={annot.url}
+                    body={annot.body}
+                    comment={annot.body}
+                    tags={annot.tags}
+                    createdWhen={annot.createdWhen}
+                    handleGoToAnnotation={() => null}
+                    handleDeleteAnnotation={() => null}
+                    handleEditAnnotation={() => null}
+                    handleMouseEnter={() => null}
+                    handleMouseLeave={() => null}
+                />
+            </div>
+        ))
     }
 
     render() {
         const { isExpanded } = this.state
         return (
             <div className={styles.container}>
-                <span
-                    className={cx(styles.icon, {
-                        [styles.inverted]: isExpanded,
-                    })}
-                    onClick={this.handleArrowClick}
-                />
+                {/* Chevron up/down toggle button */}
+                <div className={styles.iconContainer}>
+                    <span
+                        className={cx(styles.icon, {
+                            [styles.inverted]: isExpanded,
+                        })}
+                        onClick={this.handleArrowClick}
+                    />
+                </div>
+
+                {/* Container for displaying AnnotationBox */}
+                <div className={styles.annotationList}>
+                    {isExpanded ? this.renderAnnotations() : null}
+                </div>
             </div>
         )
     }
--- src/overview/results/constants.ts
+++ src/overview/results/constants.ts
@@ -3,3 +3,12 @@ export const EGG_URL = '/options.html#/acknowledgements'
 export const EGG_IMG = '/../../../../img/thanks.gif'
 export const SHOWN_TAGS_LIMIT = 3
 export const SHOWN_FILTER_LIMIT = 3
+
+export const DUMMY_ANNOTATION = {
+    url: 'https://google.com#900001332',
+    pageUrl: 'https://google.com',
+    body:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make",
+    createdWhen: new Date(),
+    tags: ['memex'],
+}
