6ce89fb05f9dece9986570fbfcd187b5a4114d08
Eik-S <eikem@posteo.de>
2019-01-23 18:57:17 +0100

    backup backend selection changed from compile time to users choice

---
+++ src/backup/background/backend-select.ts
@@ -0,0 +1,48 @@
+import * as driveBackup from './backend/google-drive'
+import * as localBackup from './backend/memex-local-server'
+import * as backup from '.'
+import { browser } from 'webextension-polyfill-ts'
+import { BackupBackend } from './backend/types'
+
+export class BackendSelect {
+    async restoreBackend(): Promise<BackupBackend> {
+        const backendLocation = await this.restoreBackendLocation()
+        if (backendLocation === 'local') {
+            return this.initLocalBackend()
+        } else if (backendLocation === 'gdrive') {
+            return this.initGDriveBackend()
+        } else {
+            return undefined
+        }
+    }
+
+    async initGDriveBackend(): Promise<BackupBackend> {
+        this.saveBackendLocation('gdrive')
+        return new driveBackup.DriveBackupBackend({
+            tokenStore: new driveBackup.LocalStorageDriveTokenStore({
+                prefix: 'drive-token-',
+            }),
+            memexCloudOrigin: backup._getMemexCloudOrigin(),
+        })
+    }
+
+    async initLocalBackend(): Promise<BackupBackend> {
+        this.saveBackendLocation('local')
+        return new localBackup.MemexLocalBackend({
+            url: 'http://localhost:11922',
+        })
+    }
+
+    async restoreBackendLocation(): Promise<string> {
+        const backendInfo = await browser.storage.local.get('backendInfo')
+        if (backendInfo) {
+            return backendInfo.location
+        } else {
+            return undefined
+        }
+    }
+
+    async saveBackendLocation(location: string): Promise<void> {
+        return browser.storage.local.set({ backendInfo: { location } })
+    }
+}
