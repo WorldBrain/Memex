eaa39341d2ac5248150364b165fbe5960ad8f5ed
Oliver Sauter <oli@worldbrain.io>
2019-02-25 00:31:45 +0000

    first style changes to backup & restore

--- src/options/backup/components/overlays/download-overlay.jsx
+++ src/options/backup/components/overlays/download-overlay.jsx
@@ -1,37 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Styles from './overlay.css'
+import cx from 'classnames'
 
 export default function DownloadOverlay(props) {
     return !props.disabled ? (
         <div>
             <div className={Styles.box}>
                 <h3 className={Styles.header}>
-                    Download the Memex Backup App to continue
+                    Download Memex Backup App to enable local backups.
                 </h3>
                 <p className={Styles.description}>
-                    To enable smooth local backups, you need to install Memex
-                    <br />
-                    Backup App. Come back when you started it.
+                        Download & start it, pick a backup folder and then return here to continue your backup. <br/><a className={Styles.link} target="_blank" href="https://www.notion.so/worldbrain/Backup-Restore-locally-and-to-any-cloud-provider-7b7e470247c548eeb3e9601a03e246a7">Learn More ▸</a>
                 </p>
                 <div className={Styles.linkbox}>
                     <a
                         href="https://github.com/WorldBrain/local-backup-server/releases/download/v0.2-alpha/local-backup-server-win.exe"
                         target="_blank"
                     >
-                        <img className={Styles.logo} src={'img/github.png'} />
+                        <img className={Styles.logo} src={'img/windows_logo.svg'} />
                     </a>
                     <a
                         href="https://github.com/WorldBrain/local-backup-server/releases/download/v0.2-alpha/local-backup-server-macos"
                         target="_blank"
                     >
-                        <img className={Styles.logo} src={'img/github.png'} />
+                        <img className={Styles.logo} src={'img/apple_logo.svg'} />
                     </a>
                     <a
                         href="https://github.com/WorldBrain/local-backup-server/releases/download/v0.2-alpha/local-backup-server-linux"
                         target="_blank"
                     >
-                        <img className={Styles.logo} src={'img/github.png'} />
+                        <img className={cx(Styles.logo, Styles.linux)} src={'img/linux_logo.svg'} />
                     </a>
                 </div>
                 <div className={Styles.buttonbox}>
@@ -41,7 +40,7 @@ export default function DownloadOverlay(props) {
                             props.onClick('continue')
                         }}
                     >
-                        Continue
+                        I'm ready!
                     </div>
                     <div
                         className={Styles.cancelButton}
