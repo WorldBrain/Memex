e99277c64f7924d11f347685a1b18d68cd9c393a
dpffxhad <dpffxhad@users.noreply.github.com>
2019-03-10 00:02:34 -0800

    make buttons clickable when sidebar is open

--- src/sidebar-common/sidebar/components/menu-styles.ts
+++ src/sidebar-common/sidebar/components/menu-styles.ts
@@ -4,17 +4,19 @@ const baseStyles = {
     bmMenuWrap: {
         top: 0,
         right: '-60px',
+        zIndex: 1000,
         transition: 'all 0.3s cubic-bezier(0.65, 0.05, 0.36, 1) 0s',
     },
     bmMenu: {
         position: 'fixed',
         right: '50px',
         top: 0,
-        zIndex: 1100,
+        overflow: 'hidden',
         width: '350px',
         height: '100%',
         transition: 'all 0.3s cubic-bezier(0.65, 0.05, 0.36, 1) 0s',
-        boxShadow: 'rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px',
+        boxShadow:
+            'rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px',
     },
     bmBurgerButton: {
         display: 'none',
