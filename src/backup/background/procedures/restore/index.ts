8cf1388ab7faf677efba7fb91f9f894e3be5d447
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-03-26 10:55:53 +0700

    Add basic migration on restore logic
    - added a sample migration for the annotations `lastEdit` field
    - all past backups may have that field missing

--- src/backup/background/procedures/restore/index.ts
+++ src/backup/background/procedures/restore/index.ts
@@ -4,6 +4,7 @@ import BackupStorage from '../../storage'
 import { BackupBackend, ObjectChange } from '../../backend'
 import Interruptable from '../interruptable'
 import { DownloadQueue } from './download-queue'
+import { collections } from 'src/popup/collections-button/selectors'
 const dataURLtoBlob = require('dataurl-to-blob')
 const sorted = require('lodash/sortBy')
 const zipObject = require('lodash/zipObject')
@@ -180,6 +181,7 @@ export class BackupRestoreProcedure {
     async _writeChange(change: ObjectChange) {
         change = _filterBadChange(change)
         _deserializeChangeFields(change)
+        _migrateObject(change)
 
         const collection = this.storageManager.collection(change.collection)
         if (change.operation === 'create') {
@@ -281,6 +283,27 @@ export function _filterBadChange({
     }
 
     return { ...change, object }
+}
+
+export function _migrateObject(change: ObjectChange) {
+    const object = change.object
+    const migrate = <T = any>(args: {
+        collection: string
+        field: string
+        value: (prev: T) => any
+    }) => {
+        if (change.collection === args.collection) {
+            object[args.field] = args.value(object)
+        }
+    }
+
+    // `lastEdited` was changed from a nullable to non-null field used in search feature.
+    // All missing data should be set to created time.
+    migrate({
+        collection: 'annotations',
+        field: 'lastEdited',
+        value: prev => prev.createdWhen,
+    })
 }
 
 export function _deserializeChangeFields(change: ObjectChange) {
