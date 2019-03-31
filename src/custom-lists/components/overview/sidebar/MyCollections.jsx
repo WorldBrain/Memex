b9677f8fcef0f5455cab5ebf1fc5bf0cccc10e13
Oliver Sauter <oli@worldbrain.io>
2019-03-31 13:13:00 +0200

    Smaller design changes

--- src/custom-lists/components/overview/sidebar/MyCollections.jsx
+++ src/custom-lists/components/overview/sidebar/MyCollections.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import styles from './Index.css'
 import PropTypes from 'prop-types'
+import ButtonTooltip from 'src/common-ui/components/button-tooltip'
 
 const List = props => (
     <div className={styles.collection} onClick={props.handleRenderCreateList}>
+        <ButtonTooltip
+                    position="bottom"
+                    tooltipText="Drag & drop pages into collections."
+        >
         <span className={styles.myCollection}> My Collections </span>
-        <span className={styles.plus}/>
+        </ButtonTooltip>
+         <ButtonTooltip
+                    position="right"
+                    tooltipText="Add new collection. Add items via Drag & Drop"
+        >
+        	<span className={styles.plus}/>
+        </ButtonTooltip>
     </div>
 )
 
