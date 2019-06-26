f394bcc8de2aca7da300d8f65a15c9dbce95e5c9
Willian Viana <willian.batista.viana@gmail.com>
2019-06-26 15:44:52 -0300

    feat(emoji): Remove overflow scroll from sidebar

--- src/custom-lists/components/overview/sidebar/Index.js
+++ src/custom-lists/components/overview/sidebar/Index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
+
 import cx from 'classnames'
 
 import { actions, selectors } from 'src/custom-lists'
