2b3249204f1eaec8b289a56643dcccdc2936d1eb
Vincent den Boer <vincent@shishkabab.net>
2019-08-19 16:31:41 +0200

    More refactoring work regarding browser API interaction

--- src/direct-linking/background/index.ts
+++ src/direct-linking/background/index.ts
@@ -150,10 +150,10 @@ export default class DirectLinkingBackground {
                 override?: boolean
                 openSidebar?: boolean
             } = {
-            anchor: null,
-            override: false,
-            activeUrl: undefined,
-        },
+                anchor: null,
+                override: false,
+                activeUrl: undefined,
+            },
     ) {
         const [currentTab] = await this.options.browserAPIs.tabs.query({
             active: true,
