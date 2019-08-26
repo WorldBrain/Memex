2e0a361e2a8d0d72e514d07a84a48c0ae78c8392
Vincent den Boer <vincent@shishkabab.net>
2019-08-26 16:34:11 +0200

    Automate inclusion of external modules in Webpack

--- build/external.js
+++ build/external.js
@@ -1,7 +1,22 @@
-export const externalTsModules = [
-    'user-logic',
-    '@worldbrain/storex',
-    '@worldbrain/storex-backend-dexie',
-    '@worldbrain/storex-pattern-modules',
-    '@worldbrain/memex-stemmer',
-]
+const fs = require('fs')
+const path = require('path')
+
+function collectModules(basePath, namespaceDir = null) {
+    const paths = []
+    for (const dirName of fs.readdirSync(
+        path.join(basePath, namespaceDir || ''),
+    )) {
+        if (dirName.startsWith('@')) {
+            paths.push(...collectModules(basePath, dirName))
+        } else if (namespaceDir) {
+            paths.push([namespaceDir, dirName].join('/'))
+        } else {
+            paths.push(dirName)
+        }
+    }
+    return paths
+}
+
+export const externalTsModules = collectModules(
+    path.join(__dirname, '..', 'external'),
+)
