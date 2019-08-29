b2365b1735e707f21982a4e425088972a5e1b3be
Vincent den Boer <vincent@shishkabab.net>
2019-08-29 13:31:20 +0200

    First step towards good background integration test suite

---
+++ src/custom-lists/background/index.tests.ts
@@ -0,0 +1,62 @@
+import expect from 'expect'
+import {
+    backgroundIntegrationTest,
+    BackgroundIntegrationTestSetup,
+    backgroundIntegrationTestSuite,
+} from 'src/tests/integration-tests'
+
+export const INTEGRATION_TESTS = backgroundIntegrationTestSuite(
+    'Custom lists',
+    [
+        backgroundIntegrationTest(
+            'should create a list, edit its title and retrieve it',
+            () => {
+                const customLists = (setup: BackgroundIntegrationTestSetup) =>
+                    setup.backgroundModules.customLists
+                let listId!: number
+                return {
+                    steps: [
+                        {
+                            execute: async ({ setup }) => {
+                                listId = await customLists(
+                                    setup,
+                                ).createCustomList({
+                                    name: 'My Custom List',
+                                })
+                            },
+                        },
+                        {
+                            execute: async ({ setup }) =>
+                                customLists(setup).insertPageToList({
+                                    id: listId,
+                                    url: 'http://www.bla.com/',
+                                }),
+                        },
+                        {
+                            execute: async ({ setup }) =>
+                                customLists(setup).updateList({
+                                    id: listId,
+                                    name: 'Updated List Title',
+                                }),
+                            postCheck: async ({ setup }) => {
+                                expect(
+                                    await customLists(setup).fetchListById({
+                                        id: listId,
+                                    }),
+                                ).toEqual({
+                                    id: expect.any(Number),
+                                    name: 'Updated List Title',
+                                    isDeletable: true,
+                                    isNestable: true,
+                                    createdAt: expect.any(Date),
+                                    pages: ['http://www.bla.com/'],
+                                    active: true,
+                                })
+                            },
+                        },
+                    ],
+                }
+            },
+        ),
+    ],
+)
--- src/tests/background-integration-tests.ts
+++ src/tests/background-integration-tests.ts
@@ -42,8 +42,8 @@ export async function setupBackgroundIntegrationTest(options?: {
                 local: new MemoryLocalStorage(),
             },
             bookmarks: {
-                onCreated: { addListener: () => {} },
-                onRemoved: { addListener: () => {} },
+                onCreated: { addListener: () => { } },
+                onRemoved: { addListener: () => { } },
             },
         } as any,
         tabManager: options && options.tabManager,
@@ -146,7 +146,7 @@ export function registerBackgroundIntegrationTest(
                 } catch (e) {
                     console.error(
                         `Unexpected storage changes in step number ${stepIndex +
-                            1} (counting from 1)`,
+                        1} (counting from 1)`,
                     )
                     throw e
                 }
