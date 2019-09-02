69e2305f6ed33b99d6d0c3bbb92287549056557f
Vincent den Boer <vincent@shishkabab.net>
2019-09-02 12:33:41 +0200

    More refactoring

--- src/custom-lists/background/index.tests.ts
+++ src/custom-lists/background/index.tests.ts
@@ -1,19 +1,26 @@
 import expect from 'expect'
 import {
+    backgroundIntegrationTestSuite,
     backgroundIntegrationTest,
     BackgroundIntegrationTestSetup,
-    backgroundIntegrationTestSuite,
 } from 'src/tests/integration-tests'
+import {
+    StorageDiff,
+    StorageCollectionDiff,
+} from 'src/tests/storage-change-detector'
 
 export const INTEGRATION_TESTS = backgroundIntegrationTestSuite(
     'Custom lists',
     [
         backgroundIntegrationTest(
-            'should create a list, edit its title and retrieve it',
+            'should create a list, edit its title, add an entry to it and retrieve the list and its pages',
             () => {
                 const customLists = (setup: BackgroundIntegrationTestSetup) =>
-                    setup.backgroundModules.customLists
-                let listId!: number
+                    setup.backgroundModules.customLists.remoteFunctions
+                const searchModule = (setup: BackgroundIntegrationTestSetup) =>
+                    setup.backgroundModules.search
+                let listId!: any
+                let listEntry!: any
                 return {
                     steps: [
                         {
@@ -24,20 +31,58 @@ export const INTEGRATION_TESTS = backgroundIntegrationTestSuite(
                                     name: 'My Custom List',
                                 })
                             },
+                            expectedStorageChanges: {
+                                customLists: (): StorageCollectionDiff => ({
+                                    [listId]: {
+                                        type: 'create',
+                                        object: {
+                                            id: listId,
+                                            createdAt: expect.any(Date),
+                                            name: 'My Custom List',
+                                            isDeletable: true,
+                                            isNestable: true,
+                                        },
+                                    },
+                                }),
+                            },
                         },
                         {
                             execute: async ({ setup }) =>
                                 customLists(setup).insertPageToList({
                                     id: listId,
                                     url: 'http://www.bla.com/',
+                                }).object,
+                            expectedStorageChanges: {
+                                pageListEntries: (): StorageCollectionDiff => ({
+                                    [listEntry &&
+                                        `[${listId},"${listEntry.pageUrl}"]`]: {
+                                        type: 'create',
+                                        object: {
+                                            listId,
+                                            createdAt: expect.any(Date),
+                                            fullUrl: 'http://www.bla.com/',
+                                            pageUrl: 'bla.com',
+                                        },
+                                    },
                                 }),
+                            },
                         },
                         {
                             execute: async ({ setup }) =>
-                                customLists(setup).updateList({
+                                customLists(setup).updateListName({
                                     id: listId,
                                     name: 'Updated List Title',
                                 }),
+                            expectedStorageChanges: {
+                                customLists: (): StorageCollectionDiff => ({
+                                    [listId]: {
+                                        type: 'modify',
+                                        updates: {
+                                            name: 'Updated List Title',
+                                        },
+                                    },
+                                }),
+                            },
                             postCheck: async ({ setup }) => {
                                 expect(
                                     await customLists(setup).fetchListById({
@@ -52,6 +97,26 @@ export const INTEGRATION_TESTS = backgroundIntegrationTestSuite(
                                     pages: ['http://www.bla.com/'],
                                     active: true,
                                 })
+
+                                expect(
+                                    await customLists(setup).fetchListPagesById(
+                                        {
+                                            id: listId,
+                                        },
+                                    ),
+                                ).toEqual([
+                                    {
+                                        listId,
+                                        pageUrl: 'bla.com',
+                                        fullUrl: 'http://www.bla.com/',
+                                        createdAt: expect.any(Date),
+                                    },
+                                ])
+
+                                // expect(await setup.backgroundModules.search.remoteFunctions.search.searchPages({
+                                //     contentTypes: { pages: true, notes: false, highlights: false },
+                                //     collections: [listId]
+                                // })).toEqual([])
                             },
                         },
                     ],
