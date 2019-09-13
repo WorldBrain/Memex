dbbc5db9e0d01bd777a476e0a216a54a668385ab
Vincent den Boer <vincent@shishkabab.net>
2019-09-13 12:17:08 +0200

    Add collection version mapping function

---
+++ src/sync/background/storage.test.ts
@@ -0,0 +1,98 @@
+import {
+    StorageModule,
+    StorageModuleCollections,
+} from '@worldbrain/storex-pattern-modules'
+import { mapCollectionVersions } from './storage'
+
+const TEST_STORAGE_COLLECTIONS: StorageModuleCollections = {
+    foo: {
+        history: [
+            {
+                version: new Date('2019-01-01'),
+                fields: {
+                    spam: { type: 'text' },
+                },
+            },
+            {
+                version: new Date('2019-03-03'),
+                fields: {
+                    spam: { type: 'text' },
+                    eggs: { type: 'text' },
+                },
+            },
+        ],
+        version: new Date('2019-05-05'),
+        fields: {
+            spam: { type: 'text' },
+            eggs: { type: 'text' },
+        },
+    },
+}
+
+describe('Collection version mapping', () => {
+    it('should work', () => {
+        expect(
+            mapCollectionVersions({
+                collectionDefinitions: TEST_STORAGE_COLLECTIONS,
+                mappings: [
+                    {
+                        moduleVersion: new Date('2019-01-01'),
+                        applicationVersion: new Date('2019-02-02'),
+                    },
+                    {
+                        moduleVersion: new Date('2019-03-03'),
+                        applicationVersion: new Date('2019-04-04'),
+                    },
+                    {
+                        moduleVersion: new Date('2019-05-05'),
+                        applicationVersion: new Date('2019-06-06'),
+                    },
+                ],
+            }),
+        ).toEqual({
+            foo: {
+                ...TEST_STORAGE_COLLECTIONS.foo,
+                version: new Date('2019-06-06'),
+                history: [
+                    {
+                        ...TEST_STORAGE_COLLECTIONS.foo.history![0],
+                        version: new Date('2019-02-02'),
+                    },
+                    {
+                        ...TEST_STORAGE_COLLECTIONS.foo.history![1],
+                        version: new Date('2019-04-04'),
+                    },
+                ],
+            },
+        })
+    })
+
+    it('should ignore collection history from before the module was used in the application', () => {
+        expect(
+            mapCollectionVersions({
+                collectionDefinitions: TEST_STORAGE_COLLECTIONS,
+                mappings: [
+                    {
+                        moduleVersion: new Date('2019-03-03'),
+                        applicationVersion: new Date('2019-04-04'),
+                    },
+                    {
+                        moduleVersion: new Date('2019-05-05'),
+                        applicationVersion: new Date('2019-06-06'),
+                    },
+                ],
+            }),
+        ).toEqual({
+            foo: {
+                ...TEST_STORAGE_COLLECTIONS.foo,
+                version: new Date('2019-06-06'),
+                history: [
+                    {
+                        ...TEST_STORAGE_COLLECTIONS.foo.history![1],
+                        version: new Date('2019-04-04'),
+                    },
+                ],
+            },
+        })
+    })
+})
