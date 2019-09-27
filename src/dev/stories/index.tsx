905da1ff6125b9bf887f7d219e0c81cdf6c45936
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-09-27 11:17:38 +1000

    Add some storybook stories to demonstrate QRCanvas usage

--- src/dev/stories/index.tsx
+++ src/dev/stories/index.tsx
@@ -2,6 +2,7 @@ import 'babel-polyfill'
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import QRCanvas from 'src/common-ui/components/qr-canvas'
 import ProgressStepContainer from 'src/common-ui/components/progress-step-container'
 import OnboardingTooltip from 'src/overview/onboarding/components/onboarding-tooltip'
 
@@ -25,3 +26,17 @@ storiesOf('Onboarding Tooltip', module).add('Import example', () => (
         onDismiss={() => undefined}
     />
 ))
+
+const longText = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat.
+Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper.
+Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in.
+Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis.
+Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. '
+Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.
+`
+
+storiesOf('QR Code creation', module)
+    .add('HTML canvas short example', () => <QRCanvas toEncode="test" />)
+    .add('HTML canvas long example', () => <QRCanvas toEncode={longText} />)
