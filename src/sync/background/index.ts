f11c4e1ed3dfe1819aff6a83cdb6723988a06199
Vincent den Boer <vincent@shishkabab.net>
2019-10-11 15:44:36 +0200

    Fix how Sync interacts with `memex-storage`

--- src/sync/background/index.ts
+++ src/sync/background/index.ts
@@ -4,21 +4,10 @@ import { ClientSyncLogStorage } from '@worldbrain/storex-sync/lib/client-sync-lo
 import { SharedSyncLog } from '@worldbrain/storex-sync/lib/shared-sync-log'
 import { SyncLoggingMiddleware } from '@worldbrain/storex-sync/lib/logging-middleware'
 
-import {
-    annotationCollectionName,
-    annotationBookmarkCollectionName,
-    annotationListEntryCollectionName,
-} from '@worldbrain/memex-storage/lib/annotations/constants'
-import { tagCollectionName } from '@worldbrain/memex-storage/lib/tags/constants'
-import {
-    visitCollectionName,
-    pageCollectionName,
-    bookmarkCollectionName,
-} from '@worldbrain/memex-storage/lib/pages/constants'
-import {
-    listEntryCollectionName,
-    listCollectionName,
-} from '@worldbrain/memex-storage/lib/lists/constants'
+import { COLLECTION_NAMES as PAGES_COLLECTION_NAMES } from '@worldbrain/memex-storage/lib/pages/constants'
+import { COLLECTION_NAMES as TAGS_COLLECTION_NAMES } from '@worldbrain/memex-storage/lib/tags/constants'
+import { COLLECTION_NAMES as LISTS_COLLECTION_NAMES } from '@worldbrain/memex-storage/lib/lists/constants'
+import { COLLECTION_NAMES as ANNOTATIONS_COLLECTION_NAMES } from '@worldbrain/memex-storage/lib/annotations/constants'
 
 import AuthBackground from 'src/auth/background'
 import { PublicSyncInterface } from './types'
@@ -35,15 +24,15 @@ export default class SyncBackground {
     syncLoggingMiddleware?: SyncLoggingMiddleware
     firstContinuousSyncPromise?: Promise<void>
     readonly syncedCollections: string[] = [
-        bookmarkCollectionName,
-        listCollectionName,
-        listEntryCollectionName,
-        pageCollectionName,
-        visitCollectionName,
-        tagCollectionName,
-        annotationCollectionName,
-        annotationBookmarkCollectionName,
-        annotationListEntryCollectionName,
+        PAGES_COLLECTION_NAMES.bookmark,
+        PAGES_COLLECTION_NAMES.page,
+        PAGES_COLLECTION_NAMES.visit,
+        TAGS_COLLECTION_NAMES.tag,
+        LISTS_COLLECTION_NAMES.list,
+        LISTS_COLLECTION_NAMES.listEntry,
+        ANNOTATIONS_COLLECTION_NAMES.annotation,
+        ANNOTATIONS_COLLECTION_NAMES.listEntry,
+        ANNOTATIONS_COLLECTION_NAMES.bookmark,
     ]
 
     constructor(
