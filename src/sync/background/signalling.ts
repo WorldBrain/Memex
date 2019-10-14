72af9711e2f19a4f84be275d04708b92cbcaef35
Vincent den Boer <vincent@shishkabab.net>
2019-10-14 16:25:51 +0200

    Signalling fix and some debug output

--- src/sync/background/signalling.ts
+++ src/sync/background/signalling.ts
@@ -1,8 +1,11 @@
 import firebase from 'firebase'
 import { SignalTransport } from 'simple-signalling/lib/types'
 import { FirebaseSignalTransport } from 'simple-signalling/lib/firebase'
+import { initializeFirebaseIfNeeded } from 'src/storage/server'
 
 export function createFirebaseSignalTransport(): SignalTransport {
+    initializeFirebaseIfNeeded()
+
     return new FirebaseSignalTransport({
         database: firebase.database() as any,
         collectionName: 'signalling',
