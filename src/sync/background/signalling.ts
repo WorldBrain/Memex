072fc60733bf8e0398a0702e497ed9f73405f626
Vincent den Boer <vincent@shishkabab.net>
2019-10-15 11:40:25 +0200

    Fix Firestore madness

--- src/sync/background/signalling.ts
+++ src/sync/background/signalling.ts
@@ -1,13 +1,10 @@
-import firebase from 'firebase'
 import { SignalTransport } from 'simple-signalling/lib/types'
 import { FirebaseSignalTransport } from 'simple-signalling/lib/firebase'
-import { initializeFirebaseIfNeeded } from 'src/storage/server'
+import { getFirebase } from 'src/storage/server'
 
 export function createFirebaseSignalTransport(): SignalTransport {
-    initializeFirebaseIfNeeded()
-
     return new FirebaseSignalTransport({
-        database: firebase.database() as any,
+        database: getFirebase().database() as any,
         collectionName: 'signalling',
     })
 }
