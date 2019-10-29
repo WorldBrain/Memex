de98cbd2aefbacda0da12cbd100385190df7e405
Vincent den Boer <vincent@shishkabab.net>
2019-10-29 14:33:54 +0100

    Factor out more Sync logic

--- src/sync/background/constants.ts
+++ src/sync/background/constants.ts
@@ -1,14 +1,1 @@
-import {
-    ContinuousSyncSetting,
-    ContinuousSyncSettingValue,
-} from '@worldbrain/storex-sync/lib/integration/continuous-sync'
-
-export const SYNC_STORAGE_AREA_KEYS: {
-    [Key in ContinuousSyncSetting | 'encryptionKey']: string
-} = {
-    continuousSyncEnabled: 'enable-continuous-sync',
-    deviceId: 'device-id',
-    encryptionKey: 'sync-encryption-key',
-}
-
 export const INCREMENTAL_SYNC_FREQUENCY = 1000 * 60 * 60 * 15 // 15 minutes
