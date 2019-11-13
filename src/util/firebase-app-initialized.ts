32af3f5018de3e7306bf31de3ebf805156926495
Charris <c@harrischris.com>
2019-11-13 17:00:10 +0100

    Auth: Attempt to move API keys to env vars with dotenv

--- src/util/firebase-app-initialized.ts
+++ src/util/firebase-app-initialized.ts
@@ -1,30 +1,15 @@
 import * as firebase from 'firebase/app'
 
 const firebaseConfig = {
-    staging: {
-        apiKey: 'AIzaSyCyxWY7qZSWlncB_JDYOSzeTOfRnYhNcS8',
-        authDomain: 'worldbrain-staging.firebaseapp.com',
-        databaseURL: 'https://worldbrain-staging.firebaseio.com',
-        projectId: 'worldbrain-staging',
-        messagingSenderId: '840601505816',
-        appId: '1:840601505816:web:69fbb7a789882e399fb36d',
-    },
-    // TODO: Add production keys
-    production: {
-        apiKey: 'AIzaSyCyxWY7qZSWlncB_JDYOSzeTOfRnYhNcS8',
-        authDomain: 'worldbrain-staging.firebaseapp.com',
-        databaseURL: 'https://worldbrain-staging.firebaseio.com',
-        projectId: 'worldbrain-staging',
-        messagingSenderId: '840601505816',
-        appId: '1:840601505816:web:69fbb7a789882e399fb36d',
-    },
+    apiKey: process.env.FIREBASE_MEMEX_API_KEY,
+    authDomain: process.env.FIREBASE_MEMEX_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_MEMEX_DATABSE_URL,
+    projectId: process.env.FIREBASE_MEMEX_PROJECT_ID,
+    messagingSenderId: process.env.FIREBASE_MEMEX_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_MEMEX_APP_ID,
 }
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(
-        process.env.NODE_ENV === 'production'
-            ? firebaseConfig.production
-            : firebaseConfig.staging,
-    )
+    firebase.initializeApp(firebaseConfig)
 }
 export { firebase }
