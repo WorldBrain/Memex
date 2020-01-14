26f1c8485950c543f7d3b72a3fadf449f88c37a4
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-01-14 12:21:04 +0700

    Write hack to fix on firefox
    - adds an extra dexie schema on FF, filling out the missing one

--- src/search/storage/dexie-schema.ts
+++ src/search/storage/dexie-schema.ts
@@ -1,4 +1,5 @@
 import { DexieSchema } from '@worldbrain/storex-backend-dexie/lib/types'
+import browserIsChrome from 'src/util/check-browser'
 
 /**
  * Takes the generated schema versions, based on the registed collections, and finds the
@@ -10,36 +11,64 @@ import { DexieSchema } from '@worldbrain/storex-backend-dexie/lib/types'
 export default function patchDirectLinksSchema(
     schemaVersions: DexieSchema[],
 ): any[] {
-    const firstAppears = schemaVersions.findIndex(
+    const directLinksFirstAppears = schemaVersions.findIndex(
         ({ schema }) => schema.directLinks != null,
     )
+    const syncFirstAppears = schemaVersions.findIndex(
+        ({ schema }) => schema.clientSyncLogEntry != null,
+    )
 
-    // Return schemas as-is if direct links schema not found (tests)
-    if (firstAppears === -1) {
+    // Return schemas as-is if direct links/sync schema not found (tests)
+    if (directLinksFirstAppears === -1 || syncFirstAppears === -1) {
         return schemaVersions
     }
 
-    const preceding = schemaVersions[firstAppears - 1]
-
-    const patchedSchema: DexieSchema = {
+    const precedingDirectLinks = schemaVersions[directLinksFirstAppears - 1]
+    const patchedDirectLinksSchema: DexieSchema = {
+        ...precedingDirectLinks,
         schema: {
-            ...preceding.schema,
+            ...precedingDirectLinks.schema,
             directLinks: 'url, *pageTitle, *body, createdWhen',
         },
-        dexieSchemaVersion: preceding.dexieSchemaVersion + 1,
-        storexSchemaVersion: preceding.storexSchemaVersion,
     }
 
-    return [
-        ...schemaVersions.slice(0, firstAppears),
+    const schemasBeforeSync = [
+        ...schemaVersions.slice(0, directLinksFirstAppears),
         // Shim the schema with the incorrect indexes, so Dexie knows about its existence
-        patchedSchema,
-        // All subsequent schemas need to be 1 version higher to take the incorrect index schema into account
-        ...schemaVersions
-            .slice(firstAppears)
-            .map(schema => ({
+        ...[
+            patchedDirectLinksSchema,
+            ...schemaVersions.slice(directLinksFirstAppears, syncFirstAppears),
+        ].map(schema => ({
+            ...schema,
+            dexieSchemaVersion: schema.dexieSchemaVersion + 1,
+        })),
+    ]
+
+    if (browserIsChrome()) {
+        return [
+            ...schemasBeforeSync,
+            ...schemaVersions.slice(syncFirstAppears).map(schema => ({
                 ...schema,
                 dexieSchemaVersion: schema.dexieSchemaVersion + 1,
             })),
+        ]
+    }
+
+    const precedingSync = schemaVersions[syncFirstAppears - 1]
+    const missingPreSyncSchema: DexieSchema = {
+        ...precedingSync,
+        schema: precedingSync.schema,
+    }
+
+    return [
+        ...schemasBeforeSync,
+        // Shim the missing pre-sync schema, so Dexie knows about its existence
+        ...[
+            missingPreSyncSchema,
+            ...schemaVersions.slice(syncFirstAppears),
+        ].map(schema => ({
+            ...schema,
+            dexieSchemaVersion: schema.dexieSchemaVersion + 2,
+        })),
     ]
 }
