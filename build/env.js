d1be8276e95823d62f3c174d423264520560daf8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-04-14 13:12:44 +0700

    Revert "Replace dotenv-webpack with standard node dotenv"
    This reverts commit e46c95fc05f9bf3fb56a3f2c63ffb171047bdaba.

--- build/env.js
+++ build/env.js
@@ -1,7 +1,7 @@
-/* eslint no-console: 0 */
 export default ({ mode }) => {
     const env = {
         VERSION: process.env.npm_package_version,
+        PIWIK_SITE_ID: '1',
         DEBUG_ANALYTICS_EVENTS: '',
         NODE_ENV: mode,
         BACKUP_BACKEND: '',
@@ -12,16 +12,20 @@ export default ({ mode }) => {
         BACKUP_BATCH_SIZE: '500',
         BACKUP_START_SCREEN: '',
         BACKUP_TEST_SIZE_ESTIMATION: '',
-        ...process.env,
+        DEV_AUTH_STATE: '',
     }
 
     if (mode === 'development') {
-        if (env.DEV_AUTH_STATE === '' || env.DEV_AUTH_STATE == null) {
+        if (
+            process.env.DEV_AUTH_STATE === '' ||
+            process.env.DEV_AUTH_STATE == null
+        ) {
             console.warn(
                 `AUTH: Firebase auth will use staging credentials. See authentication/readme.md for more auth options.`,
             )
             env.DEV_AUTH_STATE = 'staging'
         } else {
+            env.DEV_AUTH_STATE = process.env.DEV_AUTH_STATE
             console.info(
                 `AUTH: Firebase auth state set to: ${env.DEV_AUTH_STATE}`,
             )
@@ -29,23 +33,20 @@ export default ({ mode }) => {
     }
 
     // Analytics
-    if (mode === 'development' && env.DEV_ANALYTICS !== 'true') {
+    if (mode === 'development' && process.env.DEV_ANALYTICS !== 'true') {
         console.warn(
             `Turing off analytics for extension development, set DEV_ANALYTICS=true if you're hacking on analytics`,
         )
         env.SENTRY_DSN = ''
-        env.COUNTLY_SERVER_URL = 'http://localhost:1234'
-        env.COUNTLY_APP_KEY = ''
-    } else if (mode === 'production' || env.DEV_ANALYTICS === 'true') {
-        if (env.DEV_ANALYTICS === 'true') {
+    } else if (mode === 'production' || process.env.DEV_ANALYTICS === 'true') {
+        if (process.env.DEV_ANALYTICS === 'true') {
             console.warn(
                 `Forcing analytics to be enabled, but BE CAREFUL: this will send events to the production analytics backend`,
             )
         }
         env.SENTRY_DSN =
             'https://205014a0f65e4160a29db2935250b47c@sentry.io/305612'
-        env.COUNTLY_SERVER_URL = env.COUNTLY_SERVER_URL
-        env.COUNTLY_APP_KEY = env.COUNTLY_APP_KEY
+        // env.COUNTLY_APP_KEY = '47678cda223ca2570cb933959c9037613a751283'
     }
 
     return env
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,2 @@
 require('@babel/register')
-require('dotenv').config()
 module.exports = require('./webpack.config.babel')
