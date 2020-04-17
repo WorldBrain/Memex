90c866720c8ca9a4bb33d10e76c7fc081ce87abd
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-04-17 12:39:25 +0700

    Get bookmark via ribbon test working again
    - originally written for older Memex version
    - also bit of a cleanup of names

--- src/tags/tags.integration-test.ts
+++ src/tags/tags.integration-test.ts
@@ -1,13 +1,13 @@
-import * as fs from 'fs'
 import * as webdriver from 'selenium-webdriver'
 import { Options } from 'selenium-webdriver/chrome'
 import 'chromedriver'
 import expect from 'expect'
 import * as mocha from 'mocha'
 
-const basePath = process.env.PWD
-const unpackedExtensionPath = `${basePath}/extension`
-const extUrl = 'chrome-extension://pocnpohonndlmnkogjbmjamponfhmoko/'
+const BASE_PATH = process.env.PWD
+const EXT_PATH_UNPACKED = BASE_PATH + '/extension'
+const EXT_URL = 'chrome-extension://bchcdcdmibkfclblifbckgodmbbdjfff/'
+const OVERVIEW_PATH = '/options.html#/overview'
 
 describe('Selenium Demo Test Suite', function() {
     let driver: webdriver.ThenableWebDriver
@@ -16,7 +16,7 @@ describe('Selenium Demo Test Suite', function() {
     mocha.before(function() {
         // initializing chrome driver
         const options = new Options()
-        options.addArguments('--load-extension=' + unpackedExtensionPath)
+        options.addArguments('--load-extension=' + EXT_PATH_UNPACKED)
         driver = new webdriver.Builder()
             .withCapabilities(webdriver.Capabilities.chrome())
             .setChromeOptions(options)
@@ -28,32 +28,12 @@ describe('Selenium Demo Test Suite', function() {
             .maximize()
     })
 
-    afterEach(function() {
-        let testCaseName: string = this.currentTest.title
-        let testCaseStatus: string = this.currentTest.state
-        if (testCaseStatus === 'failed') {
-            console.log(`Test: ${testCaseName}, Status: Failed!`)
-            // Might be useful to capture a screenshot if test fails:
-            /*
-            driver.takeScreenshot().then((data) => {
-                let screenshotPath = `TestResults/Screenshots/${testCaseName}.png`;
-                console.log(`Saving Screenshot as: ${screenshotPath}`);
-                fs.writeFileSync(screenshotPath, data, 'base64');
-            });*/
-        } else if (testCaseStatus === 'passed') {
-            console.log(`Test: ${testCaseName}, Status: Passed!`)
-        } else {
-            console.log(`Test: ${testCaseName}, Status: Unknown!`)
-        }
-    })
-
     mocha.after(function() {
         driver.quit()
     })
 
-    async function getExtShadowRoot() {
-        let shadowHost
-        shadowHost = await driver.findElement(
+    async function getExtShadowRoot(): Promise<webdriver.WebElement> {
+        const shadowHost = await driver.findElement(
             webdriver.By.id('memex-ribbon-sidebar-container'),
         )
         return driver.executeScript(
@@ -62,22 +42,36 @@ describe('Selenium Demo Test Suite', function() {
         )
     }
 
-    async function waitUntilShadowDomElementLocated(element) {
-        let shadowRoot
-        shadowRoot = await getExtShadowRoot()
-        const result = await shadowRoot
-        return await driver.wait(async () => result.findElement(element), 5000)
+    async function waitUntilShadowDomElementLocated(
+        element,
+    ): Promise<webdriver.WebElement> {
+        const shadowRoot = await getExtShadowRoot()
+        return driver.wait(async () => shadowRoot.findElement(element), 5000)
+    }
+
+    async function triggerRibbonShow(ribbonSidebar: webdriver.WebElement) {
+        const selectorRibbonTrigger =
+            'div > div > div.src-sidebar-overlay-ribbon-components-__ribbon--1eodv'
+
+        const ribbonTrigger = await ribbonSidebar.findElement(
+            webdriver.By.css(selectorRibbonTrigger),
+        )
+
+        await driver
+            .actions({ bridge: true })
+            .move({ x: 0, y: 0, origin: ribbonTrigger })
+            .perform()
     }
 
     // todo: Give HTML elements in question an id so we can find them more robustly
     const selectorSidebarBookmarkButton =
-        'div > div:nth-child(1) > div > div.src-sidebar-overlay-ribbon-components-__pageActions--HxmKI > div.src-common-ui-components-__tooltipContainer--1AXOQ > button'
-    const selectorBookmarksUl =
+        'div > div > div > div > div.src-sidebar-overlay-ribbon-components-__pageActions--HxmKI > div:nth-child(1) > div'
+    const selectorPageResultsUl =
         '#app > div:nth-child(1) > div.src-overview-results-components-__main--dzcJi > ul'
-    const selectorFirstBookmark = `${selectorBookmarksUl} > li:nth-child(1) > div > a > div:nth-child(2) > div > div > span`
+    const selectorFirstPageTitle = `${selectorPageResultsUl} > li:nth-child(1) > div > a > div > div > div > div:nth-child(2)`
 
     it('should bookmark a page from the ribbon, and that bookmark be present in search results for bookmarks', async function() {
-        const url = extUrl + '/options.html#/overview'
+        const url = EXT_URL + OVERVIEW_PATH
         await driver.get(url)
         const currentUrl = await driver.getCurrentUrl()
         expect(currentUrl).toContain(url)
@@ -98,7 +92,11 @@ describe('Selenium Demo Test Suite', function() {
         const ribbonSidebar = await waitUntilShadowDomElementLocated(
             webdriver.By.id('memex-ribbon-sidebar'),
         )
-        await driver.sleep(1000)
+
+        await triggerRibbonShow(ribbonSidebar)
+
+        await driver.sleep(1000) // TODO: Why does this need to be here? (following selector fails if removed)
+
         const button = await ribbonSidebar.findElement(
             webdriver.By.css(selectorSidebarBookmarkButton),
         )
@@ -106,30 +104,29 @@ describe('Selenium Demo Test Suite', function() {
         await button.click()
         await driver.sleep(500)
 
-        // Visit the bookmarks page
-        const bookmarksUrl =
-            extUrl + 'options.html#/overview?showOnlyBookmarks=true'
+        // Do a bookmarks filtered page search
+        const bookmarksUrl = url + '?showOnlyBookmarks=true'
         await driver.get(bookmarksUrl)
         await driver.sleep(500)
 
         // Test that there should be only one bookmarked page
-        const bookmarks = await driver.wait(
+        const pageResults = await driver.wait(
             webdriver.until.elementsLocated(
-                webdriver.By.css(selectorBookmarksUl + '> li'),
+                webdriver.By.css(selectorPageResultsUl + '> li'),
             ),
             5000,
         )
 
-        expect(bookmarks).toHaveLength(1)
+        expect(pageResults).toHaveLength(1)
 
         // Test that the result should be the bookmarked page
-        const bookmark = await driver.wait(
+        const pageTitle = await driver.wait(
             webdriver.until.elementLocated(
-                webdriver.By.css(selectorFirstBookmark),
+                webdriver.By.css(selectorFirstPageTitle),
             ),
             2000,
         )
-        const titleText = await bookmark.getText()
+        const titleText = await pageTitle.getText()
 
         expect(titleText).toEqual('Memex - Wikipedia')
     })
