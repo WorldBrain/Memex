85d70ff2037b37b02025f76ca075a5084f8ed761
Oliver Sauter <oli@worldbrain.io>
2020-04-20 14:45:07 +0200

    adding "beta" flag to sync

--- src/sync/components/device-list/SyncDevicesPane.tsx
+++ src/sync/components/device-list/SyncDevicesPane.tsx
@@ -250,7 +250,7 @@ class SyncDevicesPaneContainer extends React.Component<
             <div>
                 <div className={settingsStyle.section}>
                     <div className={settingsStyle.sectionTitle}>
-                        Sync your mobile phone
+                        <div className={settingsStyle.sectionTitleText}><span>Sync your mobile phone</span><span className={settingsStyle.betaPill}>{' '}Beta</span></div>
                         <span
                             className={styles.labelFree}
                             onClick={this.handleUpgradeNeeded}
@@ -261,6 +261,9 @@ class SyncDevicesPaneContainer extends React.Component<
                     <div className={settingsStyle.infoText}>
                         Use an end2end encrypted connection to keep your devices
                         in sync.
+                    </div>
+                    <div className={settingsStyle.infoText}>
+                        <strong>This feature is in beta status. You may experience bugs. <a href="https://community.worldbrain.io/c/bug-reports" target="_blank">Let us know if you do!</a></strong>
                     </div>
                     <SyncDevicesPane
                         devices={this.state.devices}
