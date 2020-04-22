5a6538d2f8464f1690900271aa58cbd0d1b06c4a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-04-22 13:41:54 +0700

    Split tests into different modules - revise setup
    - you can now use the `makeTestFactory` setup function to create an `it` function which will auto-setup and pass down the WebDriver, and also deal with ensuring it quits after the test is done
    - also renamed these tests from `.integration-test.` to `.browser-test.` to be more descriptive

--- src/tests/selenium/setup.ts
+++ src/tests/selenium/setup.ts
@@ -5,6 +5,14 @@ import expect from 'expect'
 
 import { EXT_PATH_UNPACKED, EXT_OVERVIEW_URL } from './constants'
 
+export interface TestSetupProps {
+    initUrl?: string
+}
+
+export interface TestDependencies {
+    driver: WebDriver
+}
+
 export function setupChromeDriverWithExtension(props: {
     maximizeWindow?: boolean
 }): WebDriver {
@@ -16,22 +24,19 @@ export function setupChromeDriverWithExtension(props: {
         .build()
 
     if (props.maximizeWindow) {
-        driver
-            .manage()
-            .window()
-            .maximize()
+        driver.manage().window().maximize()
     }
 
     return driver
 }
 
 export async function setupTest({
-    driver,
     initUrl = EXT_OVERVIEW_URL,
-}: {
-    driver: WebDriver
-    initUrl?: string
-}) {
+}: TestSetupProps): Promise<TestDependencies> {
+    const driver = await setupChromeDriverWithExtension({
+        maximizeWindow: true,
+    })
+
     await driver.get(initUrl)
     const currentUrl = await driver.getCurrentUrl()
     expect(currentUrl).toContain(initUrl)
@@ -39,4 +44,24 @@ export async function setupTest({
     // The new installation tab often pops up and steals focus
     // Make sure focus is set to the tab we set
     await driver.switchTo().window((await driver.getAllWindowHandles())[0])
+
+    return { driver }
+}
+
+export function makeTestFactory() {
+    type TestFunction = (deps: TestDependencies) => Promise<void>
+
+    return async function (
+        description: string,
+        test: TestFunction,
+        props?: TestSetupProps,
+    ) {
+        it(description, async () => {
+            const deps = await setupTest(props ?? {})
+
+            await test(deps)
+
+            await deps.driver.quit()
+        })
+    }
 }
