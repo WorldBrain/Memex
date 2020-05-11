7c8404f67e4fab74b78ad250f9e26f2582c04410
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-05-11 14:03:35 +0700

    Add optional `props.onClickOutside` to new tag/list pickers
    - if supplied, is run when clicking outside the picker

--- src/tags/ui/TagPicker/index.tsx
+++ src/tags/ui/TagPicker/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import onClickOutside from 'react-onclickoutside'
 import styled, { ThemeProvider } from 'styled-components'
+
 import { StatefulUIElement } from 'src/util/ui-logic'
 import TagPickerLogic, {
     TagPickerDependencies,
@@ -29,6 +31,12 @@ class TagPicker extends StatefulUIElement<
 > {
     constructor(props: TagPickerDependencies) {
         super(props, new TagPickerLogic(props))
+    }
+
+    handleClickOutside = () => {
+        if (this.props.onClickOutside) {
+            this.props.onClickOutside()
+        }
     }
 
     handleSetSearchInputRef = (ref: HTMLInputElement) =>
@@ -185,4 +193,4 @@ const EmptyTagsView = styled.div`
     text-align: center;
 `
 
-export default TagPicker
+export default onClickOutside(TagPicker)
