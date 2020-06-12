a7368df8a587a6ca202922cb3b3c33a5d8fc8833
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-06-12 13:10:36 +0700

    Add storage check + tests for tag validity
    - mirrors new UI checks

--- src/overview/search-bar/index.test.ts
+++ src/overview/search-bar/index.test.ts
@@ -1,4 +1,4 @@
-import { HASH_TAG_PATTERN } from './constants'
+import { VALID_TAG_PATTERN, HASH_TAG_PATTERN } from './constants'
 
 describe('Overview search bar tests', () => {
     it('hashtag regexp should match desired patterns', () => {
@@ -21,6 +21,30 @@ describe('Overview search bar tests', () => {
 
         for (const { pattern, shouldPass } of testCases) {
             expect([pattern, HASH_TAG_PATTERN.test(pattern)]).toEqual([
+                pattern,
+                shouldPass,
+            ])
+        }
+    })
+
+    it('tag validity regexp should match desired patterns', () => {
+        interface TestCase {
+            pattern: string
+            shouldPass: boolean
+        }
+        const testCases: TestCase[] = [
+            { pattern: 'tag-tag', shouldPass: true },
+            { pattern: 'tag.tag', shouldPass: true },
+            { pattern: 'tag_tag', shouldPass: true },
+            { pattern: 'tag_tag.tag-tag', shouldPass: true },
+            { pattern: 'tag', shouldPass: true },
+            { pattern: 'tag-', shouldPass: false },
+            { pattern: 'tag.', shouldPass: false },
+            { pattern: 'tag tag', shouldPass: false },
+        ]
+
+        for (const { pattern, shouldPass } of testCases) {
+            expect([pattern, VALID_TAG_PATTERN.test(pattern)]).toEqual([
                 pattern,
                 shouldPass,
             ])
--- src/tags/background/storage.ts
+++ src/tags/background/storage.ts
@@ -8,6 +8,8 @@ import {
 } from '@worldbrain/memex-storage/lib/tags/constants'
 import { normalizeUrl } from '@worldbrain/memex-url-utils'
 
+import { VALID_TAG_PATTERN } from 'src/overview/search-bar/constants'
+
 export default class TagStorage extends StorageModule {
     static TAGS_COLL = COLLECTION_NAMES.tag
 
@@ -43,6 +45,10 @@ export default class TagStorage extends StorageModule {
     }
 
     async addTag({ name, url }: { name: string; url: string }) {
+        if (!VALID_TAG_PATTERN.test(name)) {
+            throw new Error(`Attempted to create a badly shaped tag: ${name}`)
+        }
+
         url = normalizeUrl(url, {})
         return this.operation('createTag', { name, url })
     }
