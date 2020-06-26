3634f68e003f94fce7feecae8d637fcbe4e416c6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-06-26 16:11:02 +0700

    Refactor AnnotationView to be like AnnotationCreate

--- src/annotations/components/AnnotationView.tsx
+++ src/annotations/components/AnnotationView.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
-import CommentTags from 'src/annotations/components/old/edit/comment-tags'
-import AllModesFooter from './old/edit/all-modes-footer'
+import styled from 'styled-components'
+
+import TextTruncated from 'src/annotations/components/parts/TextTruncated'
+import { AnnotationMode } from 'src/in-page-ui/sidebar/react/types'
+import AnnotationFooter from './AnnotationFooter'
 
 interface Props {
     env: 'inpage' | 'overview'
-    mode: 'default' | 'delete'
+    mode: AnnotationMode
     body?: string
     comment?: string
     tags: string[]
@@ -28,34 +31,121 @@ interface Props {
 }
 
 /* tslint:disable-next-line variable-name */
-const AnnotationView = (props: Props) => (
-    <React.Fragment>
-        {/* Comment and tags to be displayed. */}
-        {(props.comment || (props.tags && props.tags.length !== 0)) && (
-            <CommentTags
-                comment={props.comment}
-                tags={props.tags}
-                isJustComment={!props.body}
-                handleTagClick={props.handleTagClick}
-                getTruncatedTextObject={props.getTruncatedTextObject}
-            />
-        )}
+class AnnotationView extends React.Component<Props> {
+    private bindHandleTagPillClick: (tag: string) => React.MouseEventHandler = (
+        tag,
+    ) => (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        this.props.handleTagClick(tag)
+    }
 
-        {/* Footer. */}
-        <AllModesFooter
-            mode={props.mode}
-            isEdited={props.isEdited}
-            timestamp={props.timestamp}
-            hasBookmark={props.hasBookmark}
-            displayGoToAnnotation={props.body && props.env === 'overview'}
-            handleGoToAnnotation={props.handleGoToAnnotation}
-            handleDeleteAnnotation={props.handleDeleteAnnotation}
-            handleCancelDeletion={props.handleCancelOperation}
-            editIconClickHandler={props.editIconClickHandler}
-            trashIconClickHandler={props.trashIconClickHandler}
-            handleBookmarkToggle={props.handleBookmarkToggle}
-        />
-    </React.Fragment>
-)
+    private renderCommentAndTags() {
+        if (!this.props.comment) {
+            return
+        }
+
+        return (
+            <CommentBox>
+                <TextTruncated text={this.props.comment} {...this.props} />
+                {this.renderTags()}
+            </CommentBox>
+        )
+    }
+
+    private renderTags() {
+        if (!this.props.tags.length) {
+            return
+        }
+
+        return (
+            <TagsContainerStyled>
+                {this.props.tags.map((tag) => (
+                    <TagPillStyled
+                        key={tag}
+                        onClick={this.bindHandleTagPillClick(tag)}
+                    >
+                        {tag}
+                    </TagPillStyled>
+                ))}
+            </TagsContainerStyled>
+        )
+    }
+
+    private renderFooter() {
+        return (
+            <AnnotationFooter
+                {...this.props}
+                displayGoToAnnotation={
+                    this.props.body && this.props.env === 'overview'
+                }
+                handleCancelDeletion={this.props.handleCancelOperation}
+            />
+        )
+    }
+
+    render() {
+        return (
+            <>
+                {this.renderCommentAndTags()}
+                {this.renderFooter()}
+            </>
+        )
+    }
+}
 
 export default AnnotationView
+
+const TagPillStyled = styled.div`
+    background-color: #83c9f4;
+
+    padding: 2px 8px;
+    border-radius: 4px;
+    font-size: 12px;
+    font-weight: 400;
+    height: auto;
+    color: #284150;
+    margin: 2px 4px 2px 0;
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    font-family: 'Poppins', sans-serif;
+`
+
+const TagsContainerStyled = styled.div`
+    height: auto;
+    padding-top: 13px;
+    display: flex;
+    flex-wrap: wrap;
+
+    ${(props: Props) =>
+        !props.comment &&
+        `
+        margin-top: -8px;
+        margin-left: 15px;
+        padding-bottom: 15px;
+    `}
+`
+
+const CommentBox = styled.div`
+    color: rgb(54, 54, 46);
+
+    font-size: 14px;
+    font-weight: 400;
+    overflow: hidden;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+    margin: 0px;
+    padding: 0 15px 10px 15px;
+    line-height: 1.4;
+    text-align: left;
+
+    ${(props: Props) =>
+        !props.body &&
+        `
+        border-top: none;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        padding: 15px 15px 15px;
+    `}
+`
