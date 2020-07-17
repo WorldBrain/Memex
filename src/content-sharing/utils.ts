4bc3eda01ba425927c810a8d06eefd7a1df0571f
Oliver Sauter <oli@worldbrain.io>
2020-07-17 17:34:07 +0200

    make creation of link dependent on environment variable

--- src/content-sharing/utils.ts
+++ src/content-sharing/utils.ts
@@ -1,3 +1,9 @@
 export function getListShareUrl(options: { remoteListId: string }) {
-    return `https://memex.social/c/${options.remoteListId}`
+	console.log(process.env.NODE_ENV)
+
+	if (process.env.NODE_ENV === 'production') {
+    	return `https://memex.social/c/${options.remoteListId}`
+	} else {
+    	return `https://staging.memex.social/c/${options.remoteListId}`
+    }
 }
