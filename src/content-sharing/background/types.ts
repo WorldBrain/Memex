e4c328fd10ecb8c2ad6ae8a297c2376e6a2131bc
Vincent den Boer <vincent@shishkabab.net>
2020-07-24 15:07:45 +0200

    Move content sharing client storage and storage versions to `memex-common`

--- src/content-sharing/background/types.ts
+++ src/content-sharing/background/types.ts
@@ -1,3 +1,5 @@
+export * from '@worldbrain/memex-common/lib/content-sharing/client-storage/types'
+
 export interface ContentSharingInterface {
     shareList(options: { listId: number }): Promise<{ remoteListId: string }>
     shareListEntries(options: { listId: number }): Promise<void>
@@ -6,41 +8,4 @@ export interface ContentSharingInterface {
         localListIds: number[]
     }): Promise<{ [listId: number]: boolean }>
     waitForListSync(options: { localListId: number }): Promise<void>
-}
-
-export type ContentSharingAction =
-    | AddSharedListEntriesAction
-    | RemoveSharedListEntryAction
-    | ChangeSharedListTitleAction
-    | ChangeSharedListDescriptionAction
-export interface AddSharedListEntriesAction {
-    type: 'add-shared-list-entries'
-    localListId: number
-    remoteListId: string
-    data: Array<{
-        createdWhen: number | '$now'
-        entryTitle: string
-        originalUrl: string
-        normalizedUrl: string
-    }>
-}
-export interface RemoveSharedListEntryAction {
-    type: 'remove-shared-list-entry'
-    localListId: number
-    remoteListId: string
-    normalizedUrl: string
-}
-
-export interface ChangeSharedListTitleAction {
-    type: 'change-shared-list-title'
-    localListId: number
-    remoteListId: string
-    newTitle: string
-}
-
-export interface ChangeSharedListDescriptionAction {
-    type: 'change-shared-list-description'
-    localListId: number
-    remoteListId: string
-    newDescription: string
 }
