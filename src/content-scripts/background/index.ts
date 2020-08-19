dac54610177ec7dad439d0d121a11a300f559864
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-08-19 16:55:42 +0700

    Check sidebar enabled on History API change
    - this fixes the recent regression where the ribbon loads on certain sites even when disabled
    - there was no check before as it was assumed this event would only fire on in-page navigations, but that assumption did not hold true with many sites updating their history state on load

--- src/content-scripts/background/index.ts
+++ src/content-scripts/background/index.ts
@@ -2,6 +2,7 @@ import { ContentScriptsInterface } from './types'
 import { makeRemotelyCallable, runInTab } from 'src/util/webextensionRPC'
 import { InPageUIContentScriptRemoteInterface } from 'src/in-page-ui/content_script/types'
 import { Tabs, WebNavigation } from 'webextension-polyfill-ts'
+import { getSidebarState } from 'src/sidebar-overlay/utils'
 
 export class ContentScriptsBackground {
     remoteFunctions: ContentScriptsInterface<'provider'>
@@ -25,14 +26,7 @@ export class ContentScriptsBackground {
         }
 
         this.options.webNavigation.onHistoryStateUpdated.addListener(
-            async ({ tabId }) => {
-                const inPage = runInTab<InPageUIContentScriptRemoteInterface>(
-                    tabId,
-                )
-
-                await inPage.removeHighlights()
-                await inPage.reloadRibbon()
-            },
+            this.handleHistoryStateUpdate,
         )
     }
 
@@ -46,5 +40,19 @@ export class ContentScriptsBackground {
         this.options.injectScriptInTab(tab.id, {
             file: `/content_script_${component}.js`,
         })
+    }
+
+    private handleHistoryStateUpdate = async ({
+        tabId,
+    }: WebNavigation.OnHistoryStateUpdatedDetailsType) => {
+        const isSidebarEnabled = await getSidebarState()
+        if (!isSidebarEnabled) {
+            return
+        }
+
+        const inPage = runInTab<InPageUIContentScriptRemoteInterface>(tabId)
+
+        await inPage.removeHighlights()
+        await inPage.reloadRibbon()
     }
 }
