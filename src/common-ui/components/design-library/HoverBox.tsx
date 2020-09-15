80d7123f325eb99f3caeec06a7fc459c5c72fc06
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-09-15 13:49:37 +0700

    Write relative wrapper support into <HoverBox>
    - a bit of an experiment, as we've needed wrappers around this a few times. Not sure if actually makes sense to do this or just write separate wrappers around

--- src/common-ui/components/design-library/HoverBox.tsx
+++ src/common-ui/components/design-library/HoverBox.tsx
@@ -1,6 +1,37 @@
+import React from 'react'
 import styled from 'styled-components'
 
-export const HoverBox = styled.div`
+export interface Props {
+    top?: string
+    left?: string
+    right?: string
+    bottom?: string
+    withRelativeContainer?: boolean
+}
+
+export class HoverBox extends React.Component<Props> {
+    render() {
+        if (!this.props.withRelativeContainer) {
+            return <HoverBoxDiv>{this.props.children}</HoverBoxDiv>
+        }
+
+        return (
+            <HoverBoxContainer {...this.props}>
+                <HoverBoxDiv>{this.props.children}</HoverBoxDiv>
+            </HoverBoxContainer>
+        )
+    }
+}
+
+export const HoverBoxContainer = styled.div`
+    position: relative;
+    ${(props) => (props.top ? `top: ${props.top}` : '')}
+    ${(props) => (props.left ? `left: ${props.left}` : '')}
+    ${(props) => (props.right ? `right: ${props.right}` : '')}
+    ${(props) => (props.bottom ? `bottom: ${props.bottom}` : '')}
+`
+
+export const HoverBoxDiv = styled.div`
     box-shadow: rgba(15, 15, 15, 0.05) 0px 0px 0px 1px,
         rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px;
     overflow: visible;
