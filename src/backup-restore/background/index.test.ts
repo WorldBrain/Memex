50691c6abfb5a8355d6db76e1129952b2575d497
Vincent den Boer <vincent@youapt.eu>
2020-10-15 13:24:07 +0200

    Update storage and backup tests

--- src/backup-restore/background/index.test.ts
+++ src/backup-restore/background/index.test.ts
@@ -17,6 +17,7 @@ describe('Backup feature', () => {
         expect(backedUp).toEqual({
             pages: { backup: true, watch: true },
             readablePageArchives: { backup: true, watch: true },
+            readwiseAction: { backup: false, watch: false },
             visits: { backup: true, watch: true },
             bookmarks: { backup: true, watch: true },
             favIcons: { backup: true, watch: true },
