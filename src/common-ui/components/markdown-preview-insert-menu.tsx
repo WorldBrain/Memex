a8d4da536174b29902eb48098d279dbc4905b100
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-11-16 13:16:06 +0700

    Add some basic styles to insert menu

--- src/common-ui/components/markdown-preview-insert-menu.tsx
+++ src/common-ui/components/markdown-preview-insert-menu.tsx
@@ -52,7 +52,12 @@ export class MarkdownPreviewAnnotationInsertMenu extends React.Component<
 
     private renderInsertMenu = () => (
         <MenuContainer>
-            <MenuBtn onClick={this.toggleOpenState}>Insert</MenuBtn>
+            <MenuBtn
+                theme={{ isMenuOpen: this.state.isOpen }}
+                onClick={this.toggleOpenState}
+            >
+                Insert
+            </MenuBtn>
             {this.state.isOpen && (
                 <Menu>
                     {this.props.menuItems.map((props, i) => (
@@ -82,10 +87,52 @@ export class MarkdownPreviewAnnotationInsertMenu extends React.Component<
     }
 }
 
-const MenuContainer = styled.div``
-const MenuItem = styled.li``
-const MenuBtn = styled.button``
-const Menu = styled.ul``
+const MenuContainer = styled.div`
+    position: relative;
+    flex: 1;
+`
+
+const MenuItem = styled.li`
+    ${({ theme }) =>
+        theme.isDisabled
+            ? 'color: #97b2b8;'
+            : '&:hover { background: #97b2b8; cursor: pointer; }'}
+    padding: 10px 20px;
+`
+
+const MenuBtn = styled.button`
+    font-weight: ${({ theme }) => (theme.isMenuOpen ? 'bold' : 'normal')};
+    box-sizing: border-box;
+    cursor: pointer;
+    font-size: 14px;
+    border: none;
+    outline: none;
+    padding: 3px 5px;
+    margin: 5px 5px -5px 0;
+    background: transparent;
+    border-radius: 3px;
+
+    &:focus {
+        background-color: grey;
+    }
+
+    &:hover {
+        background-color: #e0e0e0;
+    }
+
+    &:focus {
+        background-color: #79797945;
+    }
+`
+
+const Menu = styled.ul`
+    position: absolute;
+    list-style: none;
+    padding: 10px 0;
+    background: white;
+    border: black 1px solid;
+    border-radius: 5px;
+`
 
 export const annotationMenuItems: MenuItemProps[] = [
     {
