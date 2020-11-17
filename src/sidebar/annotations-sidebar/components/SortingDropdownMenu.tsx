b0375509103ddc3a219e90cd98ea4337473fe55b
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-11-17 12:01:54 +0700

    Test + write sorting functions
    - page position one is not quite right, need to dig in a bit more

--- src/sidebar/annotations-sidebar/components/SortingDropdownMenu.tsx
+++ src/sidebar/annotations-sidebar/components/SortingDropdownMenu.tsx
@@ -2,25 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 
 import * as icons from 'src/common-ui/components/design-library/icons'
-import { Annotation } from 'src/annotations/types'
+import {
+    sortByCreatedTime,
+    sortByPagePosition,
+    AnnotationsSorter,
+} from '../sorting'
 import {
     DropdownMenuBtn,
     MenuItemProps,
 } from 'src/common-ui/components/dropdown-menu-btn'
 
 interface SortingMenuItemProps extends MenuItemProps {
-    sort: (a: Annotation, b: Annotation) => number
+    sort: AnnotationsSorter
 }
 
-const sortingMenuItems: SortingMenuItemProps[] = [
+export const defaultSortingMenuItems: SortingMenuItemProps[] = [
     {
         name: 'Position on Page',
-        sort: (a, b) => {
-            return 1
-        },
+        sort: sortByPagePosition,
     },
-    { name: 'Creation time (1-9)', sort: (a, b) => 1 },
-    { name: 'Creation time (9-1)', sort: (a, b) => 1 },
+    {
+        name: 'Creation time (1-9)',
+        sort: (a, b) => sortByCreatedTime(a, b),
+    },
+    {
+        name: 'Creation time (9-1)',
+        sort: (a, b) => sortByCreatedTime(b, a),
+    },
 ]
 
 interface Props {
@@ -28,7 +36,7 @@ interface Props {
 }
 
 export class SortingDropdownMenuBtn extends React.PureComponent<Props> {
-    static defaultProps: Partial<Props> = { menuItems: sortingMenuItems }
+    static defaultProps: Partial<Props> = { menuItems: defaultSortingMenuItems }
 
     private handleMenuItemClick = ({ name }: SortingMenuItemProps) => {
         this.props.menuItems.sort
---
+++ src/sidebar/annotations-sidebar/sorting.test.data.ts
@@ -0,0 +1,223 @@
+import { Annotation } from 'src/annotations/types'
+
+export const TEST_DATA: Annotation[] = [
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        body: 'n Math Class: Spirals, Fibon',
+        comment: '',
+        selector: {
+            quote: 'n Math Class: Spirals, Fibon',
+            descriptor: {
+                strategy: 'hyp-anchoring',
+                content: [
+                    { type: 'TextPositionSelector', start: 83465, end: 83493 },
+                ],
+            },
+        },
+        createdWhen: new Date('2020-11-17T04:06:03.681Z'),
+        lastEdited: new Date('2020-11-17T04:06:03.681Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605585963681',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle: '🔍 Results',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        comment: '[LinkText](url)',
+        createdWhen: new Date('2020-11-17T02:57:43.240Z'),
+        lastEdited: new Date('2020-11-17T02:57:43.240Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605581863240',
+        tags: [],
+        isBookmarked: false,
+    },
+
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        body: 'More on Angle',
+        comment: '',
+        selector: {
+            quote: 'More on Angle',
+            descriptor: {
+                strategy: 'hyp-anchoring',
+                content: [
+                    { type: 'TextPositionSelector', start: 84145, end: 84158 },
+                ],
+            },
+        },
+        createdWhen: new Date('2020-11-17T04:06:10.016Z'),
+        lastEdited: new Date('2020-11-17T04:06:10.016Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605585970016',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        comment: '[3:18](https://youtu.be/lOIP_Z_-0Hs?t=198)',
+        createdWhen: new Date('2020-11-17T01:51:19.786Z'),
+        lastEdited: new Date('2020-11-17T01:51:19.786Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605577879786',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        comment: '[3:31](https://youtu.be/lOIP_Z_-0Hs?t=211)',
+        createdWhen: new Date('2020-11-17T01:27:42.134Z'),
+        lastEdited: new Date('2020-11-17T01:27:42.134Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=206/#1605576462134',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        comment:
+            "now now what's up\n\n[3:26](https://youtu.be/lOIP_Z_-0Hs?t=206)",
+        createdWhen: new Date('2020-11-17T01:27:31.441Z'),
+        lastEdited: new Date('2020-11-17T01:27:31.441Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=176/#1605576451441',
+        tags: [],
+        isBookmarked: false,
+    },
+
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        body: '4M subscribe',
+        comment: '',
+        selector: {
+            quote: '4M subscribe',
+            descriptor: {
+                strategy: 'hyp-anchoring',
+                content: [
+                    { type: 'TextPositionSelector', start: 84044, end: 84056 },
+                ],
+            },
+        },
+        createdWhen: new Date('2020-11-17T04:06:06.900Z'),
+        lastEdited: new Date('2020-11-17T04:06:06.900Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605585966900',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        comment:
+            '[2:47](https://youtu.be/lOIP_Z_-0Hs?t=167)\n\ntest: [LinkText](url)',
+        createdWhen: new Date('2020-11-17T01:50:55.755Z'),
+        lastEdited: new Date('2020-11-17T01:50:55.755Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605577855754',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        body: '1,805,042 views',
+        comment: '',
+        selector: {
+            quote: '1,805,042 views',
+            descriptor: {
+                strategy: 'hyp-anchoring',
+                content: [
+                    {
+                        type: 'TextPositionSelector',
+                        start: 83503,
+                        end: 83518,
+                    },
+                ],
+            },
+        },
+        createdWhen: new Date('2020-11-17T04:41:49.154Z'),
+        lastEdited: new Date('2020-11-17T04:41:49.154Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605588109153',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        body: 'Comments are turned off',
+        comment: '',
+        selector: {
+            quote: 'Comments are turned off',
+            descriptor: {
+                strategy: 'hyp-anchoring',
+                content: [
+                    {
+                        type: 'TextPositionSelector',
+                        start: 84631,
+                        end: 84654,
+                    },
+                ],
+            },
+        },
+        createdWhen: new Date('2020-11-17T04:41:54.635Z'),
+        lastEdited: new Date('2020-11-17T04:41:54.635Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605588114635',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        body: 'My personal website, which you might like:',
+        comment: '',
+        selector: {
+            quote: 'My personal website, which you might like:',
+            descriptor: {
+                strategy: 'hyp-anchoring',
+                content: [
+                    {
+                        type: 'TextPositionSelector',
+                        start: 86384,
+                        end: 86426,
+                    },
+                ],
+            },
+        },
+        createdWhen: new Date('2020-11-17T04:42:07.811Z'),
+        lastEdited: new Date('2020-11-17T04:42:07.811Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605588127811',
+        tags: [],
+        isBookmarked: false,
+    },
+    {
+        pageTitle:
+            'Doodling in Math Class: Spirals, Fibonacci, and Being a Plant [2 of 3] - YouTube',
+        pageUrl: 'youtube.com/watch?v=lOIP_Z_-0Hs',
+        comment: '[2:56](https://youtu.be/lOIP_Z_-0Hs?t=176)',
+        createdWhen: new Date('2020-11-17T01:26:56.084Z'),
+        lastEdited: new Date('2020-11-17T01:26:56.084Z'),
+        url:
+            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605576416084',
+        tags: [],
+        isBookmarked: false,
+    },
+]
