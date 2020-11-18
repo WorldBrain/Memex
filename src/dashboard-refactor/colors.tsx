913fe06e4864302e1d7c3acbfeace85027e122a6
Sean Coley <seanwcoley@gmail.com>
2020-11-18 17:26:49 +0100

    Add different background color for searchbar on peek

--- src/dashboard-refactor/colors.tsx
+++ src/dashboard-refactor/colors.tsx
@@ -2,8 +2,9 @@ const colors = {
     white: '#FFF',
     onHover: '#BABABA',
     onSelect: '#B6B6B6', // used for sidebar list item in selected state
-    midGrey: '#C4C4C4', // used for Sidebar Toggle hovered/selected state
-    lightGrey: '#E1E1E1', // used for Search Bar
+    midGrey: '#C4C4C4', // used for Sidebar Toggle hovered/selected state and new items count background
+    lightMidGrey: '#DDD',
+    lightGrey: '#E1E1E1', // used for Header Search Bar background
     lighterGrey: '#CCC', // used for divider on white background
     iconDefault: '#3A2F45',
     fonts: {
