1ac38bf7cdb25efdfb6d0e503d07fcb4d48ed435
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-11-18 12:12:34 +0700

    Keep notes on top of page position sort
    - i.e., annots without highlights should be sorted first as per specs

--- src/sidebar/annotations-sidebar/sorting.test.ts
+++ src/sidebar/annotations-sidebar/sorting.test.ts
@@ -34,7 +34,9 @@ describe('Annotation sorting fns', () => {
 
     it('should be able to sort by page position', () => {
         expect(
-            TEST_DATA.sort(sortingFns.sortByPagePosition).map((a) => a.url),
+            TEST_DATA.sort(sortingFns.sortByPagePosition)
+                .slice(TEST_DATA.length - 6) // The last 6 should have highlights and be sorted - the prev ones are outside of the sorting criteria
+                .map((a) => a.url),
         ).toEqual([
             'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605585963681',
             'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605588109153',
@@ -42,12 +44,6 @@ describe('Annotation sorting fns', () => {
             'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605585970016',
             'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605588114635',
             'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605588127811',
-            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605576416084',
-            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=176/#1605576451441',
-            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=206/#1605576462134',
-            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605577855754',
-            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605577879786',
-            'https://www.youtube.com/watch?v=lOIP_Z_-0Hs&feature=youtu.be&t=160/#1605581863240',
         ])
     })
 })
--- src/sidebar/annotations-sidebar/sorting.ts
+++ src/sidebar/annotations-sidebar/sorting.ts
@@ -2,16 +2,15 @@ import { Annotation } from 'src/annotations/types'
 
 export type AnnotationsSorter = (a: Annotation, b: Annotation) => number
 
-// TODO: something is not quite right with this one
 export const sortByPagePosition: AnnotationsSorter = (a, b) => {
     if (!a.selector && !b.selector) {
         return 0
     }
     if (!a.selector) {
-        return 100000
+        return -100000
     }
     if (!b.selector) {
-        return -100000
+        return 100000
     }
 
     const getTextPosSelector = (contentTypes = []): { start: number } =>
