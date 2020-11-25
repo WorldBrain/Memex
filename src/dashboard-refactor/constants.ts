564f4b4a64667cf5cb51275bae62955e88d6130e
Sean Coley <seanwcoley@gmail.com>
2020-11-25 18:28:40 +0100

    Add constants file

---
+++ src/dashboard-refactor/constants.ts
@@ -0,0 +1,12 @@
+export const sizeConstants = {
+    searchBar: {
+        heightPx: 50,
+        widthPx: 650,
+    },
+    listsSidebar: {
+        widthPx: 173,
+    },
+    syncStatusMenu: {
+        WidthPx: 183,
+    },
+}
