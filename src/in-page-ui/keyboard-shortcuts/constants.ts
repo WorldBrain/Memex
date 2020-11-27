23828b84b77b098eabd9d24c73728b2033953362
Oliver Sauter <oli@worldbrain.io>
2020-11-27 23:57:19 +0100

    updated onboarding process first stab

--- src/in-page-ui/keyboard-shortcuts/constants.ts
+++ src/in-page-ui/keyboard-shortcuts/constants.ts
@@ -1,10 +1,10 @@
 export const KEYBOARDSHORTCUTS_STORAGE_NAME = 'memex-keyboardshortcuts'
 
 export const KEYBOARDSHORTCUTS_DEFAULT_STATE = {
-    shortcutsEnabled: false,
+    shortcutsEnabled: true,
     linkShortcut: 'shift+l',
     toggleSidebarShortcut: 'alt+q',
-    toggleHighlightsShortcut: 'alt+d',
+    toggleHighlightsShortcut: 'alt+r',
     createAnnotationShortcut: 'alt+w',
     createHighlightShortcut: 'alt+a',
     createBookmarkShortcut: 'alt+s',
