4dad03b70fa514f61b029337eed2356297110b22
Sean Coley <seanwcoley@gmail.com>
2020-12-14 20:04:15 +0100

    Move types higher in tree

---
+++ src/dashboard-refactor/header/types.ts
@@ -0,0 +1,27 @@
+export type FilterKey = 't' | 'd' | 'c' | '-t' | '-d' | '-c' | 'from' | 'to'
+
+export type SearchQueryParsed = SearchQueryPart[]
+
+export interface SearchQueryPart {
+    type: 'filter' | 'search terms'
+    detail?: SearchFilterDetail | string
+}
+
+export interface SearchFilterDetail {
+    filterType: FilterList
+    filters: string[]
+}
+
+export type FilterList =
+    | 'dateFrom'
+    | 'dateTo'
+    | 'tagsIncluded'
+    | 'tagsExcluded'
+    | 'domainsIncluded'
+    | 'domainsExcluded'
+    | 'listsIncluded'
+    | 'listsExcluded'
+
+export type SearchFilterType = 'date' | 'tag' | 'domain' | 'list'
+
+export type SearchFilterLabel = 'Date' | 'Tags' | 'Domains' | 'Lists'
