99ea0095ac0b1d3381e861b75233c0840bd026a0
Charris <c@harrischris.com>
2021-03-01 17:46:31 +0100

    PDFs - support towards in-line sidebar view

--- src/content-scripts/content_script/global_pdfjs.ts
+++ src/content-scripts/content_script/global_pdfjs.ts
@@ -1,2 +1,12 @@
 import * as Global from './global'
-Global.main({ loadRemotely: false })
+Global.main({ loadRemotely: false }).then((inPageUI) => {
+    inPageUI.events.on('stateChanged', (event) => {
+        const sidebarState = event?.changes?.sidebar
+
+        if (sidebarState === true) {
+            document.body.classList.add('memexSidebarOpen')
+        } else if (sidebarState === false) {
+            document.body.classList.remove('memexSidebarOpen')
+        }
+    })
+})
