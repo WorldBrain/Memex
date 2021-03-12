4402267a77d410d871632a9e9ad2578de4271615
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-12 14:33:21 +0700

    Bring memex-common up-to-date + fix type issues
    - biggest one was the change in follows from annotations to conversations - though it seems to work just massaging the old shared annotation types into the conversation ones

--- src/common-ui/components/design-library/theme.ts
+++ src/common-ui/components/design-library/theme.ts
@@ -15,10 +15,12 @@ export const theme: Theme = {
         primary: '#3a2f45',
         subText: '#aeaeae',
         purple: '#5671cf',
+        lightgrey: 'lightgrey',
         darkgrey: '#545454',
         secondary: '#5cd9a6',
         grey: '#e0e0e0',
         black: '000',
+        lightblack: '#2c2c2c',
         overlay: {
             background: 'rgba(0, 0, 0, 0.1)',
             dialog: 'white',
@@ -28,6 +30,10 @@ export const theme: Theme = {
         primary: '"Poppins", sans-serif',
         secondary: '"Poppins", sans-serif',
     },
+    fontWeights: {
+        normal: 400,
+        bold: 700,
+    },
     hoverBackgrounds: {
         primary: '#e0e0e0',
     },
@@ -35,8 +41,18 @@ export const theme: Theme = {
         default: '3px',
     },
     fontSizes: {
+        header: '20px',
         listTitle: '16px',
         url: '14px',
+        text: '12px',
+        smallText: '8px',
+    },
+    lineHeights: {
+        header: '30px',
+        listTitle: '24px',
+        url: '21px',
+        text: '18px',
+        smallText: '12px',
     },
     zIndices: {
         overlay: 50,
