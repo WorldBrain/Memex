e01b27262ffc176eb772f5dde09f009822e2e349
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-04-02 11:58:54 +0700

    Set up activity follows server storage module

--- src/storage/types.ts
+++ src/storage/types.ts
@@ -4,11 +4,13 @@ import { ContentSharingStorage } from 'src/content-sharing/background/storage'
 import UserStorage from '@worldbrain/memex-common/lib/user-management/storage'
 import ContentConversationStorage from '@worldbrain/memex-common/lib/content-conversations/storage'
 import ActivityStreamsStorage from '@worldbrain/memex-common/lib/activity-streams/storage'
+import ActivityFollowsStorage from '@worldbrain/memex-common/lib/activity-follows/storage'
 
 export interface ServerStorage {
     storageManager: StorageManager
     storageModules: {
         contentConversations: ContentConversationStorage
+        activityFollows: ActivityFollowsStorage
         activityStreams: ActivityStreamsStorage
         contentSharing: ContentSharingStorage
         sharedSyncLog: SharedSyncLogStorage
