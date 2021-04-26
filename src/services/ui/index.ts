c5d2ebc2d2dd5955696d29e512bf22e944d63b1b
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-04-26 14:58:29 +0700

    Fill-out device service mock some more

--- src/services/ui/index.ts
+++ src/services/ui/index.ts
@@ -4,10 +4,22 @@ import OverlayService from '@worldbrain/memex-common/lib/services/overlay'
 
 import type { UIServices } from './types'
 import { copyToClipboard } from 'src/annotations/content_script/utils'
+import { EventEmitter } from 'events'
+
+// TODO: properly implement this service
+class MockDeviceService implements DeviceServiceInterface {
+    events = new EventEmitter()
+    rootSize = {
+        width: 1920,
+        height: 1080,
+    }
+
+    processRootResize() {}
+}
 
 export function createServices(): UIServices {
     return {
-        device: {} as DeviceServiceInterface, // TODO: properly implement this service (currently unused)
+        device: new MockDeviceService(),
         overlay: new OverlayService(),
         logicRegistry: new LogicRegistryService(),
         clipboard: {
