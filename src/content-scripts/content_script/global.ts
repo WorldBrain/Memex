452f9ba58c439061b00db93abd55ebbb701d0f6b
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-06-08 14:32:52 +0700

    Don't give normalized page URL when creating annots
    - the surrounding logic creating annotations was expecting a full URL, when we were actually giving it a normalized one

--- src/content-scripts/content_script/global.ts
+++ src/content-scripts/content_script/global.ts
@@ -112,7 +112,7 @@ export async function main({ loadRemotely } = { loadRemotely: true }) {
         getSelection: () => document.getSelection(),
         getUrlAndTitle: () => ({
             title: getPageTitle(),
-            pageUrl: getNormalizedPageUrl(),
+            pageUrl: getPageUrl(),
         }),
     }
 
