0d5355c940377a84ffc780b5b4a86e8043eb650a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-06-11 10:54:14 +0700

    Update sync/backup status indicator conditions
    - yellow if last sync > 3 days ago
    - red if last sync > 7 days ago

--- src/dashboard-refactor/header/sync-status-menu/util.ts
+++ src/dashboard-refactor/header/sync-status-menu/util.ts
@@ -1,14 +1,29 @@
-import { RootState } from './types'
+import moment from 'moment'
+
+import type { RootState } from './types'
 
 export const deriveStatusIconColor = ({
     syncState,
     backupState,
+    lastSuccessfulSyncDate,
 }: RootState): 'green' | 'red' | 'yellow' => {
-    if (syncState === 'error' || backupState === 'error') {
+    const daysSinceLastSync = moment().diff(
+        moment(lastSuccessfulSyncDate),
+        'days',
+    )
+
+    if (
+        syncState === 'error' ||
+        daysSinceLastSync > 7 ||
+        backupState === 'error'
+    ) {
         return 'red'
     }
 
-    if (syncState === 'disabled' && backupState === 'disabled') {
+    if (
+        (syncState === 'disabled' || daysSinceLastSync > 3) &&
+        backupState === 'disabled'
+    ) {
         return 'yellow'
     }
 
