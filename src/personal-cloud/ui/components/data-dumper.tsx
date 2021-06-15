8452126d0fcb69a0c25cfdb03d5786bb3584ecf6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-06-15 12:31:39 +0700

    Write component for cloud onboarding data dump

---
+++ src/personal-cloud/ui/components/data-dumper.tsx
@@ -0,0 +1,135 @@
+import React from 'react'
+
+import { PrimaryAction } from 'src/common-ui/components/design-library/actions/PrimaryAction'
+import { SecondaryAction } from 'src/common-ui/components/design-library/actions/SecondaryAction'
+import { UITaskState } from '@worldbrain/memex-common/lib/main-ui/types'
+import LoadingIndicator from '@worldbrain/memex-common/lib/common-ui/components/loading-indicator'
+import Icon from '@worldbrain/memex-common/lib/common-ui/components/icon'
+import { Container, BtnBox, Header, Text } from './shared-components'
+
+export interface Props {
+    supportLink?: string
+    backupState: UITaskState
+    onBackupClick: React.MouseEventHandler
+    onContinueClick: React.MouseEventHandler
+    onRetryBackupClick: React.MouseEventHandler
+    onCancelBackupClick: React.MouseEventHandler
+}
+
+export default class DataDumper extends React.PureComponent<Props> {
+    static defaultProps: Pick<Props, 'supportLink'> = {
+        supportLink: 'mailto:support@worldbrain.io',
+    }
+
+    private renderBtns() {
+        const { backupState } = this.props
+        if (backupState === 'pristine') {
+            return (
+                <>
+                    <SecondaryAction
+                        label="Download Backup"
+                        onClick={this.props.onBackupClick}
+                    />
+                    <PrimaryAction
+                        label="Continue Migration"
+                        onClick={this.props.onContinueClick}
+                    />
+                </>
+            )
+        }
+        if (backupState === 'running') {
+            return (
+                <SecondaryAction
+                    label="Cancel"
+                    onClick={this.props.onCancelBackupClick}
+                />
+            )
+        }
+        if (backupState === 'error') {
+            return (
+                <>
+                    <SecondaryAction
+                        label="Retry"
+                        onClick={this.props.onRetryBackupClick}
+                    />
+                    <PrimaryAction
+                        label="Continue Migration"
+                        onClick={this.props.onContinueClick}
+                    />
+                </>
+            )
+        }
+        return (
+            <PrimaryAction
+                label="Continue Migration"
+                onClick={this.props.onContinueClick}
+            />
+        )
+    }
+
+    private renderContent() {
+        const { backupState, supportLink } = this.props
+        if (backupState === 'pristine') {
+            return (
+                <>
+                    <Header>
+                        Clean your data before starting the migration
+                    </Header>
+                    <Text>
+                        You've been using Memex since when we still had the
+                        history full-text search.
+                    </Text>
+                    <Text>
+                        Because of that, there is a lot of unecessary data in
+                        your Memex that must be cleaned out.
+                    </Text>
+                    <br />
+                    <Text>
+                        You have the chance now to create a{' '}
+                        <strong>one-time backup</strong> of that data before
+                        migrating to the Memex cloud.
+                    </Text>
+                </>
+            )
+        }
+        if (backupState === 'running') {
+            return (
+                <>
+                    <LoadingIndicator />
+                    <Header>Data Backup In Progress</Header>
+                    <Text>
+                        This may take a couple of minutes. You can't use Memex
+                        in the meantime.
+                    </Text>
+                </>
+            )
+        }
+        if (backupState === 'error') {
+            return (
+                <>
+                    <Icon icon="alertRound" height="20px" />
+                    <Header>There was an error with your data backup</Header>
+                    <Text>
+                        <a href={supportLink}>Contact support</a> if problem
+                        persists.
+                    </Text>
+                </>
+            )
+        }
+        return (
+            <>
+                <Icon icon="checkRound" height="20px" />
+                <Header>Data Backup Finished</Header>
+            </>
+        )
+    }
+
+    render() {
+        return (
+            <Container>
+                {this.renderContent()}
+                <BtnBox>{this.renderBtns()}</BtnBox>
+            </Container>
+        )
+    }
+}
---
+++ src/personal-cloud/ui/components/shared-components.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+export const BtnBox = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const Header = styled.h1``
+
+export const Text = styled.span``
