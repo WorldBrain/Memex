9ae54d4df717cc2f1f4b9366afbe4c3aa0272069
Oliver Sauter <oli@worldbrain.io>
2021-06-16 19:07:43 +0200

    retriggering the CI

--- src/activity-indicator/background/index.ts
+++ src/activity-indicator/background/index.ts
@@ -4,6 +4,7 @@ import * as Raven from 'src/util/raven'
 import { Services } from 'src/services/types'
 import { makeRemotelyCallable } from 'src/util/webextensionRPC'
 
+
 export type ActivityStatus =
     | 'has-unseen'
     | 'all-seen'
