f748507069593f32aa0b33aeb2c889ec6fbef34d
Vincent den Boer <vincent@youapt.eu>
2021-10-05 17:48:53 +0200

    Use Terser webpack plugin instead of UglifyJS

--- build/minimizers.js
+++ build/minimizers.js
@@ -1,15 +1,24 @@
-import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+// import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
 import CssAssetsPlugin from 'optimize-css-assets-webpack-plugin'
 
 export default () => [
-    new UglifyJsPlugin({
-        cache: true,
+    new TerserPlugin({
+        // cache: true,
         parallel: true,
-        sourceMap: true,
-        uglifyOptions: {
-            output: { ascii_only: true },
+        // sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+            // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
         },
     }),
+    // new UglifyJsPlugin({
+    //     cache: true,
+    //     parallel: true,
+    //     sourceMap: true,
+    //     uglifyOptions: {
+    //         output: { ascii_only: true },
+    //     },
+    // }),
     new CssAssetsPlugin({
         cssProcessorOptions: {
             autoprefixer: { disable: true },
