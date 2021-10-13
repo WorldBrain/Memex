dbf31c2e812495a3f241df2d340a57a76f8fe3ce
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-10-13 15:30:44 +0700

    Allow tooltip to auto-share if shift held on clicks

--- src/in-page-ui/tooltip/content_script/components/index.tsx
+++ src/in-page-ui/tooltip/content_script/components/index.tsx
@@ -2,20 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
-import TooltipContainer from './container'
-import { TooltipInPageUIInterface } from 'src/in-page-ui/tooltip/types'
+import TooltipContainer, { Props } from './container'
 import { theme } from 'src/common-ui/components/design-library/theme'
 
 export function setupUIContainer(
-    target,
-    params: {
-        inPageUI: TooltipInPageUIInterface
-        createAndCopyDirectLink: any
-        openSettings: any
-        destroyTooltip: any
-        createAnnotation: any
-        createHighlight: any
-    },
+    target: Element,
+    params: Omit<Props, 'onInit'>,
 ): Promise<() => void> {
     return new Promise(async (resolve) => {
         ReactDOM.render(
