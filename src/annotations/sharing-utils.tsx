ce78a4fffa8484d9da935926f55c6700e8aad2ea
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-10-14 13:54:51 +0700

    Properly type button tooltip

--- src/annotations/sharing-utils.tsx
+++ src/annotations/sharing-utils.tsx
@@ -1,17 +1,32 @@
 import * as icons from 'src/common-ui/components/design-library/icons'
 import React from 'react'
 
-// TODO: Properly differentiate all 4 cases
 export function getShareButtonData(
     isShared: boolean,
     isBulkShareProtected: boolean,
-): { icon: string; label: any } {
+): { icon: string; label: React.ReactNode } {
     if (isBulkShareProtected && isShared) {
-        return { icon: icons.sharedProtected, label: <span>Note Shared<br/>& Protected</span> }
+        return {
+            icon: icons.sharedProtected,
+            label: (
+                <span>
+                    Note Shared
+                    <br />& Protected
+                </span>
+            ),
+        }
     } else if (isShared && !isBulkShareProtected) {
         return { icon: icons.shared, label: 'Note Shared' }
     } else if (isBulkShareProtected && !isShared) {
-        return { icon: icons.lock, label: <span>Note Private<br/>& Protected</span> }
+        return {
+            icon: icons.lock,
+            label: (
+                <span>
+                    Note Private
+                    <br />& Protected
+                </span>
+            ),
+        }
     }
     return { icon: icons.person, label: 'Share Note' }
 }
