7483860a9d18ea5906d2607cd89f3b68168c8704
Vincent den Boer <vincent@youapt.eu>
2021-11-01 12:15:57 +0100

    Correctly detect PDFs to redirect to viewer based on headers

--- src/pdf/background/index.ts
+++ src/pdf/background/index.ts
@@ -39,25 +39,67 @@ export class PDFBackground {
         }
     }
 
-    listener = (details) => {
-        if (this.shouldOpen && details.url) {
-            let url = this.routeViewer + '?file=' + details.url
-            const i = details.url.indexOf('#')
-            if (i > 0) {
-                url += details.url.slice(i)
-            }
+    doRedirect(requestUrl: string, tabId: number) {
+        const url = this.routeViewer + '?file=' + encodeURIComponent(requestUrl)
+        // console.log('Redirecting ' + details.url + ' to ' + url)
 
-            // console.log('Redirecting ' + details.url + ' to ' + url)
+        // to get around the blocked state of the request, we update the original tab with the account screen.
+        // this is probably a bit glitchy at first, but we may be able to improve on that experience. For now it should be OK.
+        setTimeout(() => {
+            this.tabs.update(tabId, { active: true, url })
+        }, 1)
 
-            // to get around the blocked state of the request, we update the original tab with the account screen.
-            // this is probably a bit glitchy at first, but we may be able to improve on that experience. For now it should be OK.
-            setTimeout(() => {
-                this.tabs.update(details.tabId, { active: true, url })
-            }, 1)
+        return { redirectUrl: url }
+    }
 
-            return { redirectUrl: url }
+    beforeRequestListener = (
+        details: WebRequest.OnBeforeRequestDetailsType,
+    ) => {
+        // only called for local files matching *.pdf
+        if (!this.shouldOpen || !details.url) {
+            return
         }
-        return undefined
+
+        return this.doRedirect(details.url, details.tabId)
+    }
+
+    headersReceivedListener = (
+        details: WebRequest.OnHeadersReceivedDetailsType,
+    ) => {
+        console.log('hr', details)
+        if (!this.shouldOpen || !details.url) {
+            return
+        }
+        if (!this.isPdfRequestForViewer(details)) {
+            return
+        }
+        return this.doRedirect(details.url, details.tabId)
+    }
+
+    isPdfRequestForViewer(details: WebRequest.OnHeadersReceivedDetailsType) {
+        if (details.url.endsWith('.pdf')) {
+            return true
+        }
+        const contentTypeHeader = details.responseHeaders?.find?.(
+            (header) => header.name.toLowerCase() === 'content-type',
+        )
+        if (
+            contentTypeHeader?.value?.toLowerCase?.().split?.(';')?.[0] !==
+            'application/pdf'
+        ) {
+            return false
+        }
+        const contentDispositionHeader = details.responseHeaders?.find?.(
+            (header) => header.name.toLowerCase() === 'content-disposition',
+        )
+        if (
+            contentDispositionHeader?.value
+                ?.toLowerCase?.()
+                .split?.(';')?.[0] === 'attachement'
+        ) {
+            return false
+        }
+        return true
     }
 
     setupRequestInterceptors = async (opts: {
@@ -66,12 +108,20 @@ export class PDFBackground {
         this.webRequest = opts.webRequest
         await this.refreshSetting()
         this.webRequest.onBeforeRequest.addListener(
-            this.listener,
+            this.beforeRequestListener,
             {
                 types: ['main_frame', 'sub_frame'],
-                urls: ['http://*/*.pdf', 'https://*/*.pdf', 'file://*/*.pdf'],
+                urls: ['file://*/*.pdf'],
             },
             ['blocking'],
+        )
+        this.webRequest.onHeadersReceived.addListener(
+            this.headersReceivedListener,
+            {
+                urls: ['<all_urls>'],
+                types: ['main_frame', 'sub_frame'],
+            },
+            ['responseHeaders', 'blocking'],
         )
     }
 
