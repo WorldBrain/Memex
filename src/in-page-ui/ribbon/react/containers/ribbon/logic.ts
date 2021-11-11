33bc9bb9d28875cf600d70346cc99e2493c227ec
Oliver <oli@worldbrain.io>
2021-11-11 02:44:27 +0000

    Add quick view tutorial and keyboard shortcuts to ribbon

--- src/in-page-ui/ribbon/react/containers/ribbon/logic.ts
+++ src/in-page-ui/ribbon/react/containers/ribbon/logic.ts
@@ -37,7 +37,7 @@ export interface RibbonContainerState {
     loadState: TaskState
     isRibbonEnabled: boolean | null
     areExtraButtonsShown: boolean
-
+    areTutorialShown: boolean
     highlights: ValuesOf<componentTypes.RibbonHighlightsProps>
     tooltip: ValuesOf<componentTypes.RibbonTooltipProps>
     // sidebar: ValuesOf<componentTypes.RibbonSidebarProps>
@@ -56,6 +56,7 @@ export type RibbonContainerEvents = UIEvent<
         toggleRibbon: null
         highlightAnnotations: null
         toggleShowExtraButtons: null
+        toggleShowTutorial: null
         hydrateStateFromDB: { url: string }
     } & SubcomponentHandlers<'highlights'> &
         SubcomponentHandlers<'tooltip'> &
@@ -115,6 +116,7 @@ export class RibbonContainerLogic extends UILogic<
             pageUrl: this.dependencies.getPageUrl(),
             loadState: 'pristine',
             areExtraButtonsShown: false,
+            areTutorialShown: false,
             isRibbonEnabled: null,
             highlights: {
                 areHighlightsEnabled: false,
@@ -229,9 +231,27 @@ export class RibbonContainerLogic extends UILogic<
     }) => {
         const mutation: UIMutation<RibbonContainerState> = {
             areExtraButtonsShown: { $set: !previousState.areExtraButtonsShown },
+            areTutorialShown: { $set: false },
         }
 
         if (!previousState.areExtraButtonsShown) {
+            mutation.commentBox = { showCommentBox: { $set: false } }
+            mutation.tagging = { showTagsPicker: { $set: false } }
+            mutation.lists = { showListsPicker: { $set: false } }
+        }
+
+        this.emitMutation(mutation)
+    }
+
+    toggleShowTutorial: EventHandler<'toggleShowTutorial'> = ({
+        previousState,
+    }) => {
+        const mutation: UIMutation<RibbonContainerState> = {
+            areTutorialShown: { $set: !previousState.areTutorialShown },
+            areExtraButtonsShown: { $set: false },
+        }
+
+        if (!previousState.areTutorialShown) {
             mutation.commentBox = { showCommentBox: { $set: false } }
             mutation.tagging = { showTagsPicker: { $set: false } }
             mutation.lists = { showListsPicker: { $set: false } }
@@ -297,6 +317,7 @@ export class RibbonContainerLogic extends UILogic<
                       lists: { showListsPicker: { $set: false } },
                       search: { showSearchBox: { $set: false } },
                       areExtraButtonsShown: { $set: false },
+                      areTutorialShown: { $set: false },
                   }
                 : {}
 
@@ -387,6 +408,7 @@ export class RibbonContainerLogic extends UILogic<
                       lists: { showListsPicker: { $set: false } },
                       search: { showSearchBox: { $set: false } },
                       areExtraButtonsShown: { $set: false },
+                      areTutorialShown: { $set: false },
                   }
                 : {}
 
@@ -487,6 +509,7 @@ export class RibbonContainerLogic extends UILogic<
                       tagging: { showTagsPicker: { $set: false } },
                       search: { showSearchBox: { $set: false } },
                       areExtraButtonsShown: { $set: false },
+                      areTutorialShown: { $set: false },
                   }
                 : {}
 
@@ -504,6 +527,7 @@ export class RibbonContainerLogic extends UILogic<
                       tagging: { showTagsPicker: { $set: false } },
                       lists: { showListsPicker: { $set: false } },
                       areExtraButtonsShown: { $set: false },
+                      areTutorialShown: { $set: false },
                   }
                 : {}
 
