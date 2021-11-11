e93a7cf06ee4cc43b9d73c82942513a1625e8bd6
TheExGenesis <theexgenesis@gmail.com>
2021-11-11 10:49:37 +0000

    refactored timestamp normalizing

--- src/imports/background/utils.test.ts
+++ src/imports/background/utils.test.ts
@@ -1,25 +1,13 @@
 import expect from 'expect'
 
-import { padShortTimestamp, shaveLongTimestamp } from './utils'
+import { normalizeTimestamp } from './utils'
 
-describe('padding of short epoch timestamp tests', () => {
-    it('should throw errors on long timestamps', () => {
-        const timestamps = [
-            1566980564800234,
-            15669805648001566980564800,
-            156698056480015669805648001566980564800,
-        ]
-
-        for (const timestamp of timestamps) {
-            expect(() => padShortTimestamp(timestamp)).toThrow(Error)
-        }
-    })
-
+describe('normalizing timestamp tests', () => {
     it('should leave correct timestamps untouched', () => {
         const timestamps = [1566980564800, 1566980562800, 1566980524800]
 
         for (const timestamp of timestamps) {
-            expect(padShortTimestamp(timestamp)).toEqual(timestamp)
+            expect(normalizeTimestamp(timestamp)).toEqual(timestamp)
         }
     })
 
@@ -28,35 +16,18 @@ describe('padding of short epoch timestamp tests', () => {
         const timestampB = 156698056
         const timestampC = 1566980562
 
-        expect(padShortTimestamp(timestampA)).toEqual(1566980500000)
-        expect(padShortTimestamp(timestampB)).toEqual(1566980560000)
-        expect(padShortTimestamp(timestampC)).toEqual(1566980562000)
-    })
-})
-
-describe('shaving of milisecond timestamp tests', () => {
-    it('should throw errors on long timestamps', () => {
-        const timestamps = [1566980, 156698, 15669]
-
-        for (const timestamp of timestamps) {
-            expect(() => padShortTimestamp(timestamp)).toThrow(Error)
-        }
-    })
-    it('should leave correct timestamps untouched', () => {
-        const timestamps = [1566980564800, 1566980562800, 1566980524800]
-
-        for (const timestamp of timestamps) {
-            expect(shaveLongTimestamp(timestamp)).toEqual(timestamp)
-        }
+        expect(normalizeTimestamp(timestampA)).toEqual(1566980500000)
+        expect(normalizeTimestamp(timestampB)).toEqual(1566980560000)
+        expect(normalizeTimestamp(timestampC)).toEqual(1566980562000)
     })
 
-    it('should pad short timestamps with zeroes', () => {
+    it('should shave long timestamps to expected length', () => {
         const timestampA = 15669805000000
         const timestampB = 156698050000000
         const timestampC = 1566980500000000
 
-        expect(shaveLongTimestamp(timestampA)).toEqual(1566980500000)
-        expect(shaveLongTimestamp(timestampB)).toEqual(1566980500000)
-        expect(shaveLongTimestamp(timestampC)).toEqual(1566980500000)
+        expect(normalizeTimestamp(timestampA)).toEqual(1566980500000)
+        expect(normalizeTimestamp(timestampB)).toEqual(1566980500000)
+        expect(normalizeTimestamp(timestampC)).toEqual(1566980500000)
     })
 })
