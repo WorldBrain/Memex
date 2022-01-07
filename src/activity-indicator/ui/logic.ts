1091f06b7949b182ff4ac5b619bde76b4d542f80
Oliver <oli@worldbrain.io>
2022-01-07 21:06:46 +0100

    Store the activity status in local storage to not hammer the server every time the ribbon is opened

--- src/activity-indicator/ui/logic.ts
+++ src/activity-indicator/ui/logic.ts
@@ -1,5 +1,6 @@
 import { UILogic, UIEventHandler, UIEvent } from 'ui-logic-core'
 import type { ActivityIndicatorInterface } from '../background'
+import { getLocalStorage, setLocalStorage } from 'src/util/storage'
 
 export interface Dependencies {
     activityIndicatorBG: Pick<
@@ -37,10 +38,21 @@ export default class Logic extends UILogic<State, Events> {
     }
 
     init: EventHandler<'init'> = async () => {
-        const activityStatus = await this.dependencies.activityIndicatorBG.checkActivityStatus()
-        this.emitMutation({
-            hasFeedActivity: { $set: activityStatus === 'has-unseen' },
-        })
+        const hasActivityStored = await getLocalStorage('feedactivity')
+        if (hasActivityStored === true) {
+            this.emitMutation({
+                hasFeedActivity: { $set: true },
+            })
+        } else {
+            const activityStatus = await this.dependencies.activityIndicatorBG.checkActivityStatus()
+            await setLocalStorage(
+                'feedactivity',
+                activityStatus === 'has-unseen',
+            )
+            this.emitMutation({
+                hasFeedActivity: { $set: activityStatus === 'has-unseen' },
+            })
+        }
     }
 
     clickFeedEntry: EventHandler<'clickFeedEntry'> = async ({
@@ -49,6 +61,10 @@ export default class Logic extends UILogic<State, Events> {
         this.dependencies.openFeedUrl()
 
         if (previousState.hasFeedActivity) {
+            const hasActivityStored = await setLocalStorage(
+                'feedactivity',
+                false,
+            )
             this.emitMutation({ hasFeedActivity: { $set: false } })
             await this.dependencies.activityIndicatorBG.markActivitiesAsSeen()
         }
