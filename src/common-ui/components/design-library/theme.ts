4a3be40ab4194aa2171e066da2e833050f82edd0
Oliver <oli@worldbrain.io>
2022-02-08 20:40:25 +0100

    update theme files

--- src/common-ui/components/design-library/theme.ts
+++ src/common-ui/components/design-library/theme.ts
@@ -2,4 +2,4 @@ import { MemexTheme } from '@worldbrain/memex-common/lib/common-ui/styles/types'
 import * as icons from 'src/common-ui/components/design-library/icons'
 import { THEME } from '@worldbrain/memex-common/lib/common-ui/styles/theme'
 
-export const theme: MemexTheme = THEME
+export const theme: MemexTheme = THEME({ icons })
