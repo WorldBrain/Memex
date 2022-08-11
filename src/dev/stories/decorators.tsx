de5989f34aec976d6a3b816a2d56a5d923fa4328
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2022-08-11 10:35:05 +0700

    Remove storybook stories + add pluralize
    - storybook hasn't been used in years, and recently removed the deps causing type errors in these files
    - pluralize needed for storex

--- src/dev/stories/decorators.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import React from 'react'
-
-const GlobalStyles = styled.div`
-    font-family: ${(props) => props.theme.fonts.primary};
-`
-export const withGlobalStyles = (storyFn) => (
-    <GlobalStyles>{storyFn()}</GlobalStyles>
-)
