4fad565629ba1892304b7a44434440ff436755a6
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2022-11-18 14:52:38 +0700

    Remove FCM listeners for page activity indicator

--- src/push-messaging/background/index.test.ts
+++ src/push-messaging/background/index.test.ts
@@ -58,260 +58,272 @@ describe('Push messaging client tests', () => {
         expect(hasSyncBeenTriggered).toBe(true)
     })
 
-    it(`should create/delete followedListEntry records when receiving created/deleted sharedListEntry messages`, async () => {
-        const {
-            pushMessagingClient,
-            defaultListId,
-            storageManager,
-        } = await setupTest({
-            createDefaultList: true,
-            createAssocLocalFollowedList: true,
-        })
+    describe.skip('FCM implementation for page activity indicator', () => {
+        it(`should create/delete followedListEntry records when receiving created/deleted sharedListEntry messages`, async () => {
+            const {
+                pushMessagingClient,
+                defaultListId,
+                storageManager,
+            } = await setupTest({
+                createDefaultList: true,
+                createAssocLocalFollowedList: true,
+            })
 
-        const expectedEntryA = {
-            id: expect.any(Number),
-            creator: TEST_USER.id,
-            entryTitle: 'test title a',
-            followedList: defaultListId,
-            hasAnnotations: false,
-            normalizedPageUrl: 'test.com/a',
-            createdWhen: 1,
-            updatedWhen: 1,
-        }
-        const expectedEntryB = {
-            id: expect.any(Number),
-            creator: TEST_USER.id,
-            entryTitle: 'test title b',
-            followedList: defaultListId,
-            hasAnnotations: false,
-            normalizedPageUrl: 'test.com/b',
-            createdWhen: 2,
-            updatedWhen: 2,
-        }
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([])
-
-        await pushMessagingClient.handleIncomingMessage(
-            {
-                type: 'createPageListEntry',
-                creator: TEST_USER.id,
-                sharedList: defaultListId,
-                entryTitle: expectedEntryA.entryTitle,
-                normalizedPageUrl: expectedEntryA.normalizedPageUrl,
-            },
-            { now: expectedEntryA.createdWhen },
-        )
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([expectedEntryA])
-
-        await pushMessagingClient.handleIncomingMessage(
-            {
-                type: 'createPageListEntry',
-                creator: TEST_USER.id,
-                sharedList: defaultListId,
-                entryTitle: expectedEntryB.entryTitle,
-                normalizedPageUrl: expectedEntryB.normalizedPageUrl,
-            },
-            { now: expectedEntryB.createdWhen },
-        )
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([expectedEntryA, expectedEntryB])
-
-        await pushMessagingClient.handleIncomingMessage({
-            type: 'deletePageListEntry',
-            sharedList: defaultListId,
-            normalizedPageUrl: expectedEntryA.normalizedPageUrl,
-        })
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([expectedEntryB])
-
-        await pushMessagingClient.handleIncomingMessage({
-            type: 'deletePageListEntry',
-            sharedList: defaultListId,
-            normalizedPageUrl: expectedEntryB.normalizedPageUrl,
-        })
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([])
-    })
-
-    it(`should toggle 'hasAnnotations' flag on assoc. followedListEntry when receiving first/last sharedListEntry added/removed messages`, async () => {
-        const {
-            pushMessagingClient,
-            storageManager,
-            defaultListId,
-        } = await setupTest({
-            createDefaultList: true,
-            createAssocLocalFollowedList: true,
-        })
-
-        const expectedEntryA = {
-            id: expect.any(Number),
-            creator: TEST_USER.id,
-            entryTitle: 'test title a',
-            followedList: defaultListId,
-            hasAnnotations: false,
-            normalizedPageUrl: 'test.com/a',
-            createdWhen: 1,
-            updatedWhen: 1,
-        }
-
-        // Do create list entry message first, to trigger the creation of a followedListEntry to work with
-        await pushMessagingClient.handleIncomingMessage(
-            {
-                type: 'createPageListEntry',
-                creator: TEST_USER.id,
-                sharedList: defaultListId,
-                entryTitle: expectedEntryA.entryTitle,
-                normalizedPageUrl: expectedEntryA.normalizedPageUrl,
-            },
-            { now: expectedEntryA.createdWhen },
-        )
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([expectedEntryA])
-
-        await pushMessagingClient.handleIncomingMessage(
-            {
-                type: 'createFirstAnnotationListEntry',
-                normalizedPageUrl: expectedEntryA.normalizedPageUrl,
-                sharedList: defaultListId,
-            },
-            { now: 2 },
-        )
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([{ ...expectedEntryA, hasAnnotations: true, updatedWhen: 2 }])
-
-        await pushMessagingClient.handleIncomingMessage(
-            {
-                type: 'deleteLastAnnotationListEntry',
-                normalizedPageUrl: expectedEntryA.normalizedPageUrl,
-                sharedList: defaultListId,
-            },
-            { now: 3 },
-        )
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([
-            { ...expectedEntryA, hasAnnotations: false, updatedWhen: 3 },
-        ])
-
-        await pushMessagingClient.handleIncomingMessage(
-            {
-                type: 'createFirstAnnotationListEntry',
-                normalizedPageUrl: expectedEntryA.normalizedPageUrl,
-                sharedList: defaultListId,
-            },
-            { now: 4 },
-        )
-
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([{ ...expectedEntryA, hasAnnotations: true, updatedWhen: 4 }])
-    })
-
-    it(`should create/delete followedList and assoc. followedListEntry records when receiving sharedList followed/unfollowed messages`, async () => {
-        const {
-            pushMessagingClient,
-            storageManager,
-            defaultListId,
-        } = await setupTest({
-            createDefaultList: true,
-            createAssocLocalFollowedList: false,
-        })
-
-        expect(
-            await storageManager.collection('followedList').findAllObjects({}),
-        ).toEqual([])
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([])
-
-        await pushMessagingClient.handleIncomingMessage({
-            type: 'followList',
-            sharedList: defaultListId,
-        })
-
-        expect(
-            await storageManager.collection('followedList').findAllObjects({}),
-        ).toEqual([
-            {
-                name: defaultListName,
-                creator: TEST_USER.id,
-                sharedList: defaultListId,
-                lastSync: expect.any(Number),
-            },
-        ])
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([
-            {
+            const expectedEntryA = {
+                id: expect.any(Number),
                 creator: TEST_USER.id,
                 entryTitle: 'test title a',
                 followedList: defaultListId,
                 hasAnnotations: false,
                 normalizedPageUrl: 'test.com/a',
-                createdWhen: expect.any(Number),
-                updatedWhen: expect.any(Number),
-            },
-            {
+                createdWhen: 1,
+                updatedWhen: 1,
+            }
+            const expectedEntryB = {
+                id: expect.any(Number),
                 creator: TEST_USER.id,
                 entryTitle: 'test title b',
                 followedList: defaultListId,
                 hasAnnotations: false,
                 normalizedPageUrl: 'test.com/b',
-                createdWhen: expect.any(Number),
-                updatedWhen: expect.any(Number),
-            },
-        ])
+                createdWhen: 2,
+                updatedWhen: 2,
+            }
 
-        await pushMessagingClient.handleIncomingMessage({
-            type: 'unfollowList',
-            sharedList: defaultListId,
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([])
+
+            await pushMessagingClient.handleIncomingMessage(
+                {
+                    type: 'createPageListEntry',
+                    creator: TEST_USER.id,
+                    sharedList: defaultListId,
+                    entryTitle: expectedEntryA.entryTitle,
+                    normalizedPageUrl: expectedEntryA.normalizedPageUrl,
+                },
+                { now: expectedEntryA.createdWhen },
+            )
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([expectedEntryA])
+
+            await pushMessagingClient.handleIncomingMessage(
+                {
+                    type: 'createPageListEntry',
+                    creator: TEST_USER.id,
+                    sharedList: defaultListId,
+                    entryTitle: expectedEntryB.entryTitle,
+                    normalizedPageUrl: expectedEntryB.normalizedPageUrl,
+                },
+                { now: expectedEntryB.createdWhen },
+            )
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([expectedEntryA, expectedEntryB])
+
+            await pushMessagingClient.handleIncomingMessage({
+                type: 'deletePageListEntry',
+                sharedList: defaultListId,
+                normalizedPageUrl: expectedEntryA.normalizedPageUrl,
+            })
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([expectedEntryB])
+
+            await pushMessagingClient.handleIncomingMessage({
+                type: 'deletePageListEntry',
+                sharedList: defaultListId,
+                normalizedPageUrl: expectedEntryB.normalizedPageUrl,
+            })
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([])
         })
 
-        expect(
-            await storageManager.collection('followedList').findAllObjects({}),
-        ).toEqual([])
-        expect(
-            await storageManager
-                .collection('followedListEntry')
-                .findAllObjects({}),
-        ).toEqual([])
+        it(`should toggle 'hasAnnotations' flag on assoc. followedListEntry when receiving first/last sharedListEntry added/removed messages`, async () => {
+            const {
+                pushMessagingClient,
+                storageManager,
+                defaultListId,
+            } = await setupTest({
+                createDefaultList: true,
+                createAssocLocalFollowedList: true,
+            })
+
+            const expectedEntryA = {
+                id: expect.any(Number),
+                creator: TEST_USER.id,
+                entryTitle: 'test title a',
+                followedList: defaultListId,
+                hasAnnotations: false,
+                normalizedPageUrl: 'test.com/a',
+                createdWhen: 1,
+                updatedWhen: 1,
+            }
+
+            // Do create list entry message first, to trigger the creation of a followedListEntry to work with
+            await pushMessagingClient.handleIncomingMessage(
+                {
+                    type: 'createPageListEntry',
+                    creator: TEST_USER.id,
+                    sharedList: defaultListId,
+                    entryTitle: expectedEntryA.entryTitle,
+                    normalizedPageUrl: expectedEntryA.normalizedPageUrl,
+                },
+                { now: expectedEntryA.createdWhen },
+            )
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([expectedEntryA])
+
+            await pushMessagingClient.handleIncomingMessage(
+                {
+                    type: 'createFirstAnnotationListEntry',
+                    normalizedPageUrl: expectedEntryA.normalizedPageUrl,
+                    sharedList: defaultListId,
+                },
+                { now: 2 },
+            )
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([
+                { ...expectedEntryA, hasAnnotations: true, updatedWhen: 2 },
+            ])
+
+            await pushMessagingClient.handleIncomingMessage(
+                {
+                    type: 'deleteLastAnnotationListEntry',
+                    normalizedPageUrl: expectedEntryA.normalizedPageUrl,
+                    sharedList: defaultListId,
+                },
+                { now: 3 },
+            )
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([
+                { ...expectedEntryA, hasAnnotations: false, updatedWhen: 3 },
+            ])
+
+            await pushMessagingClient.handleIncomingMessage(
+                {
+                    type: 'createFirstAnnotationListEntry',
+                    normalizedPageUrl: expectedEntryA.normalizedPageUrl,
+                    sharedList: defaultListId,
+                },
+                { now: 4 },
+            )
+
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([
+                { ...expectedEntryA, hasAnnotations: true, updatedWhen: 4 },
+            ])
+        })
+
+        it(`should create/delete followedList and assoc. followedListEntry records when receiving sharedList followed/unfollowed messages`, async () => {
+            const {
+                pushMessagingClient,
+                storageManager,
+                defaultListId,
+            } = await setupTest({
+                createDefaultList: true,
+                createAssocLocalFollowedList: false,
+            })
+
+            expect(
+                await storageManager
+                    .collection('followedList')
+                    .findAllObjects({}),
+            ).toEqual([])
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([])
+
+            await pushMessagingClient.handleIncomingMessage({
+                type: 'followList',
+                sharedList: defaultListId,
+            })
+
+            expect(
+                await storageManager
+                    .collection('followedList')
+                    .findAllObjects({}),
+            ).toEqual([
+                {
+                    name: defaultListName,
+                    creator: TEST_USER.id,
+                    sharedList: defaultListId,
+                    lastSync: expect.any(Number),
+                },
+            ])
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([
+                {
+                    creator: TEST_USER.id,
+                    entryTitle: 'test title a',
+                    followedList: defaultListId,
+                    hasAnnotations: false,
+                    normalizedPageUrl: 'test.com/a',
+                    createdWhen: expect.any(Number),
+                    updatedWhen: expect.any(Number),
+                },
+                {
+                    creator: TEST_USER.id,
+                    entryTitle: 'test title b',
+                    followedList: defaultListId,
+                    hasAnnotations: false,
+                    normalizedPageUrl: 'test.com/b',
+                    createdWhen: expect.any(Number),
+                    updatedWhen: expect.any(Number),
+                },
+            ])
+
+            await pushMessagingClient.handleIncomingMessage({
+                type: 'unfollowList',
+                sharedList: defaultListId,
+            })
+
+            expect(
+                await storageManager
+                    .collection('followedList')
+                    .findAllObjects({}),
+            ).toEqual([])
+            expect(
+                await storageManager
+                    .collection('followedListEntry')
+                    .findAllObjects({}),
+            ).toEqual([])
+        })
     })
 })
--- src/push-messaging/background/index.ts
+++ src/push-messaging/background/index.ts
@@ -18,43 +18,45 @@ export default class PushMessagingClient {
         const { bgModules } = this.deps
         if (payload.type === 'downloadClientUpdates') {
             bgModules.personalCloud.triggerSyncContinuation()
-        } else if (payload.type === 'createPageListEntry') {
-            await bgModules.pageActivityIndicator.createFollowedListEntry({
-                creator: payload.creator,
-                entryTitle: payload.entryTitle,
-                followedList: payload.sharedList,
-                normalizedPageUrl: payload.normalizedPageUrl,
-                createdWhen: opts?.now,
-                updatedWhen: opts?.now,
-            })
-        } else if (payload.type === 'deletePageListEntry') {
-            await bgModules.pageActivityIndicator.deleteFollowedListEntry({
-                normalizedPageUrl: payload.normalizedPageUrl,
-                followedList: payload.sharedList,
-            })
-        } else if (payload.type === 'createFirstAnnotationListEntry') {
-            await bgModules.pageActivityIndicator.updateFollowedListEntryHasAnnotations(
-                {
-                    normalizedPageUrl: payload.normalizedPageUrl,
-                    followedList: payload.sharedList,
-                    updatedWhen: opts?.now,
-                    hasAnnotations: true,
-                },
-            )
-        } else if (payload.type === 'deleteLastAnnotationListEntry') {
-            await bgModules.pageActivityIndicator.updateFollowedListEntryHasAnnotations(
-                {
-                    normalizedPageUrl: payload.normalizedPageUrl,
-                    followedList: payload.sharedList,
-                    updatedWhen: opts?.now,
-                    hasAnnotations: false,
-                },
-            )
-        } else if (payload.type === 'followList') {
-        } else if (payload.type === 'unfollowList') {
-            await bgModules.pageActivityIndicator.deleteFollowedListAndAllEntries(
-                { sharedList: payload.sharedList },
-            )
         }
+        // This is the setup for the old FCM-based implementation for page activity indicator
+        // } else if (payload.type === 'createPageListEntry') {
+        //     await bgModules.pageActivityIndicator.createFollowedListEntry({
+        //         creator: payload.creator,
+        //         entryTitle: payload.entryTitle,
+        //         followedList: payload.sharedList,
+        //         normalizedPageUrl: payload.normalizedPageUrl,
+        //         createdWhen: opts?.now,
+        //         updatedWhen: opts?.now,
+        //     })
+        // } else if (payload.type === 'deletePageListEntry') {
+        //     await bgModules.pageActivityIndicator.deleteFollowedListEntry({
+        //         normalizedPageUrl: payload.normalizedPageUrl,
+        //         followedList: payload.sharedList,
+        //     })
+        // } else if (payload.type === 'createFirstAnnotationListEntry') {
+        //     await bgModules.pageActivityIndicator.updateFollowedListEntryHasAnnotations(
+        //         {
+        //             normalizedPageUrl: payload.normalizedPageUrl,
+        //             followedList: payload.sharedList,
+        //             updatedWhen: opts?.now,
+        //             hasAnnotations: true,
+        //         },
+        //     )
+        // } else if (payload.type === 'deleteLastAnnotationListEntry') {
+        //     await bgModules.pageActivityIndicator.updateFollowedListEntryHasAnnotations(
+        //         {
+        //             normalizedPageUrl: payload.normalizedPageUrl,
+        //             followedList: payload.sharedList,
+        //             updatedWhen: opts?.now,
+        //             hasAnnotations: false,
+        //         },
+        //     )
+        // } else if (payload.type === 'followList') {
+        // } else if (payload.type === 'unfollowList') {
+        //     await bgModules.pageActivityIndicator.deleteFollowedListAndAllEntries(
+        //         { sharedList: payload.sharedList },
+        //     )
+        // }
     }
 }
