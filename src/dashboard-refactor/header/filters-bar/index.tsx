d8115098d8cfe22650aed02726b8a438c0c240c7
Oliver <oli@worldbrain.io>
2022-12-18 19:57:25 +0100

    Improve behaviour of filter buttons

--- src/dashboard-refactor/header/filters-bar/index.tsx
+++ src/dashboard-refactor/header/filters-bar/index.tsx
@@ -17,6 +17,8 @@ import { Icon } from 'src/dashboard-refactor/styled-components'
 import * as icons from 'src/common-ui/components/design-library/icons'
 import type { SpacePickerDependencies } from 'src/custom-lists/ui/CollectionPicker/logic'
 import { PopoutBox } from '@worldbrain/memex-common/lib/common-ui/components/popout-box'
+import { PrimaryAction } from '@worldbrain/memex-common/lib/common-ui/components/PrimaryAction'
+import { IconKeys } from '@worldbrain/memex-common/lib/common-ui/styles/types'
 
 const windowWidth: number = window.innerWidth
 const searchBarWidthPx: number = sizeConstants.searchBar.widthPx
@@ -54,7 +56,7 @@ export default class FiltersBar extends PureComponent<FiltersBarProps> {
         onToggle: React.MouseEventHandler,
         isShown: boolean,
         isFiltered: boolean,
-        filterIcons: string,
+        filterIcons: IconKeys,
         filterProps:
             | TagPickerDependencies
             | SpacePickerDependencies
@@ -63,16 +65,20 @@ export default class FiltersBar extends PureComponent<FiltersBarProps> {
         componentRef: React.RefObject<HTMLDivElement>,
     ) => (
         <Margin vertical="7px" width="auto">
-            <FilterSelectButton
-                selected={isShown}
+            <PrimaryAction
+                active={isShown || isFiltered}
                 onClick={onToggle}
-                className={`${name}-picker-button`}
-                filterActive={isShown || isFiltered}
-                ref={componentRef}
-            >
-                <Icon path={filterIcons} heightAndWidth="16px" hoverOff />
-                {this.renderFilterInfo(filterProps, name, isFiltered, label)}
-            </FilterSelectButton>
+                innerRef={componentRef}
+                icon={filterIcons}
+                type={'forth'}
+                size={'medium'}
+                label={this.renderFilterInfo(
+                    filterProps,
+                    name,
+                    isFiltered,
+                    label,
+                )}
+            />
         </Margin>
     )
 
