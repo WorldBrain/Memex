a48cfd2a8d4e547d5920042ef53c56785f57ea9f
Oliver <oli@worldbrain.io>
2023-02-25 12:33:51 +0100

    Fix tests and add "beta" button

--- src/sidebar/annotations-sidebar/components/AnnotationsSidebar.tsx
+++ src/sidebar/annotations-sidebar/components/AnnotationsSidebar.tsx
@@ -1137,12 +1137,22 @@ export class AnnotationsSidebar extends React.Component<
                 <SummaryContainer>
                     <SummaryText>{this.props.pageSummary}</SummaryText>
                     <SummaryFooter>
-                        <PrimaryAction
-                            type="tertiary"
-                            size="small"
-                            onClick={() => {}}
-                            label="Report Bug"
-                        />
+                        <RightSideButtons>
+                            <BetaButton>
+                                <BetaButtonInner>BETA</BetaButtonInner>
+                            </BetaButton>
+                            <PrimaryAction
+                                type="tertiary"
+                                size="small"
+                                onClick={() => {
+                                    window.open(
+                                        'https://memex.garden/chatsupport',
+                                        '_blank',
+                                    )
+                                }}
+                                label="Report Bug"
+                            />
+                        </RightSideButtons>
                         <PoweredBy>
                             Powered by
                             <Icon
@@ -1814,6 +1824,41 @@ export class AnnotationsSidebar extends React.Component<
         )
     }
 }
+const RightSideButtons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    grid-gap: 10px;
+`
+
+const BetaButton = styled.div`
+    display: flex;
+    background: linear-gradient(
+        90deg,
+        #d9d9d9 0%,
+        #2e73f8 0.01%,
+        #0a4bca 78.86%,
+        #0041be 100%
+    );
+    border-radius: 50px;
+    height: 24px;
+    width: 50px;
+    align-items: center;
+    justify-content: center;
+`
+
+const BetaButtonInner = styled.div`
+    display: flex;
+    background: ${(props) => props.theme.colors.black};
+    color: #0a4bca;
+    font-size: 12px;
+    letter-spacing: 1px;
+    height: 20px;
+    width: 46px;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50px;
+`
 
 const ErrorContainer = styled.div`
     display: flex;
@@ -1835,9 +1880,9 @@ const SummaryFooter = styled.div`
     width: fill-available;
     display: flex;
     align-items: center;
-    justify-content: flex-end;
+    justify-content: space-between;
     grid-gap: 10px;
-    padding: 0 20px 10px 0px;
+    padding: 0 20px 10px 20px;
 `
 
 const PoweredBy = styled.div`
