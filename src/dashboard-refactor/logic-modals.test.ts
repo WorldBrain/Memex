15f3be9c3b4650c4aa04c8c98871a7ef220c965d
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-04-28 12:53:53 +0700

    Get dashboard modals tests passing again

--- src/dashboard-refactor/logic-modals.test.ts
+++ src/dashboard-refactor/logic-modals.test.ts
@@ -1,3 +1,4 @@
+import { normalizedStateToArray } from '@worldbrain/memex-common/lib/common-ui/utils/normalized-state'
 import { makeSingleDeviceUILogicTestFactory } from 'src/tests/ui-logic-tests'
 import { setupTest } from './logic.test.util'
 
@@ -7,37 +8,35 @@ describe('Dashboard Refactor modals logic', () => {
     it('should be able to set the Share List modal visibility state', async ({
         device,
     }) => {
-        const { searchResults } = await setupTest(device, {
-            withAuth: true,
+        await device.backgroundModules.customLists.createCustomList({
+            name: 'test a',
         })
-        const listId = 123
+        const { searchResults, annotationsCache } = await setupTest(device, {
+            withAuth: true,
+            runInitLogic: true,
+        })
+
+        const listData = normalizedStateToArray(annotationsCache.lists)[0]
+
         searchResults.processMutation({
-            listsSidebar: {
-                showMoreMenuListId: { $set: listId },
-                listData: {
-                    [listId]: {
-                        $set: {
-                            id: listId,
-                            name: 'test',
-                        },
-                    },
-                },
-            },
+            listsSidebar: { showMoreMenuListId: { $set: listData.unifiedId } },
         })
 
         expect(searchResults.state.modals.shareListId).toBeUndefined()
         expect(searchResults.state.listsSidebar.showMoreMenuListId).toEqual(
-            listId,
+            listData.unifiedId,
         )
         expect(searchResults.state.listsSidebar.listShareLoadingState).toEqual(
             'pristine',
         )
 
         await searchResults.processEvent('setShareListId', {
-            listId,
+            listId: listData.unifiedId,
         })
 
-        expect(searchResults.state.modals.shareListId).toEqual(listId)
+        expect(searchResults.state.modals.shareListId).toEqual(
+            listData.unifiedId,
+        )
         expect(
             searchResults.state.listsSidebar.showMoreMenuListId,
         ).toBeUndefined()
@@ -48,52 +47,6 @@ describe('Dashboard Refactor modals logic', () => {
         await searchResults.processEvent('setShareListId', {})
 
         expect(searchResults.state.modals.shareListId).toBeUndefined()
-    })
-
-    it('should be able to set the Share List modal state, detecting that the list has already been shared', async ({
-        device,
-    }) => {
-        const listId = 123
-        const remoteListId = 'test'
-        device.backgroundModules.contentSharing.remoteFunctions.getRemoteListId = async () =>
-            remoteListId
-
-        const { searchResults } = await setupTest(device, {
-            withAuth: true,
-        })
-
-        searchResults.processMutation({
-            listsSidebar: {
-                listData: {
-                    [listId]: {
-                        $set: {
-                            id: listId,
-                            name: 'test',
-                        },
-                    },
-                },
-            },
-        })
-
-        expect(searchResults.state.modals.shareListId).toBeUndefined()
-        expect(searchResults.state.listsSidebar.listShareLoadingState).toEqual(
-            'pristine',
-        )
-        expect(
-            searchResults.state.listsSidebar.listData[listId].remoteId,
-        ).toBeUndefined()
-
-        await searchResults.processEvent('setShareListId', {
-            listId,
-        })
-
-        expect(searchResults.state.modals.shareListId).toEqual(listId)
-        expect(searchResults.state.listsSidebar.listShareLoadingState).toEqual(
-            'success',
-        )
-        expect(
-            searchResults.state.listsSidebar.listData[listId].remoteId,
-        ).toEqual(remoteListId)
     })
 
     it('should be able to set the Login modal visibility', async ({
