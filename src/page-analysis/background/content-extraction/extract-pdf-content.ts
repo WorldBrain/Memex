278717bb8b6631236cdc2b44fa0f5f6c3e451ddd
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-06-08 12:57:49 +0700

    Update old PDF fetch+content extraction logic
    - Will need to re-use this with the page link creations for PDF, to be able to get PDF data remotely

--- src/page-analysis/background/content-extraction/extract-pdf-content.ts
+++ src/page-analysis/background/content-extraction/extract-pdf-content.ts
@@ -1,49 +1,29 @@
-import browser from 'webextension-polyfill'
 import * as PDFJS from 'pdfjs-dist'
-import { loadBlob } from 'src/imports/background/utils'
-import { extractDataFromPDFDocument } from 'src/pdf/util'
+import type { TypedArray } from 'pdfjs-dist/types/display/api'
 import type { RawPdfPageContent } from '@worldbrain/memex-common/lib/page-indexing/content-extraction/types'
+import { extractDataFromPDFDocument } from 'src/pdf/util'
+import type { ExtractedPDFData } from './types'
 
 // NOTE: This is the old mv2 logic where PDF data got fetched then processed in the BG.
 //  It moved to happen in the content script as PDFJS's getDocument no longer works in BG Service Worker
 
-// Run PDF.js to extract text from each page and read document metadata.
-async function extractContent(
-    pdfData: ArrayBuffer,
-    rawContent: RawPdfPageContent,
-) {
-    if (process.env.NODE_ENV !== 'test') {
-        // Point PDF.js to its worker code, a static file in the extension.
-        PDFJS.GlobalWorkerOptions.workerSrc = browser.runtime.getURL(
-            '/build/pdf.worker.js',
-        )
-    }
-
-    // Load PDF document into PDF.js
-    // @ts-ignore
-    const pdf = await PDFJS.getDocument(pdfData).promise
-    return extractDataFromPDFDocument(pdf, rawContent.title)
-}
+// NOTE: old extension workerSrc was gotten via: browser.runtime.getURL('/build/pdf.worker.js')
 
 // Given a PDF as blob or URL, return a promise of its text and metadata.
-export default async function extractPdfContent(
-    rawContent: RawPdfPageContent,
-    options?: { fetch?: typeof fetch },
-) {
-    // TODO: If the PDF is open in a Memex PDF Reader, we should be able to save the content from that tab
-    // instead of re-fetching it.
-    const blob = await (options.fetch
-        ? (await options.fetch(rawContent.url)).blob()
-        : loadBlob<Blob>({
-              url: rawContent.url,
-              timeout: 5000,
-              responseType: 'blob',
-          }))
-
-    if (!blob) {
-        throw new Error(`Could not load PDF from: ${rawContent.url}`)
+export default async function fetchPdfContent(
+    { url, title }: Pick<RawPdfPageContent, 'url' | 'title'>,
+    opts: {
+        fetch: typeof fetch
+        pdfJSWorkerSrc?: string
+    },
+): Promise<ExtractedPDFData> {
+    const response = await opts.fetch(url)
+    if (!response.ok) {
+        throw new Error(
+            `PDF fetch failed with HTTP status ${response.status}: ${response.statusText}`,
+        )
     }
-
+    const blob = await response.blob()
     const pdfData = await new Promise<ArrayBuffer>(function (resolve, reject) {
         const fileReader = new FileReader()
         fileReader.onload = async (event) => {
@@ -53,5 +33,10 @@ export default async function extractPdfContent(
         fileReader.readAsArrayBuffer(blob)
     })
 
-    return extractContent(pdfData, rawContent)
+    if (opts.pdfJSWorkerSrc) {
+        PDFJS.GlobalWorkerOptions.workerSrc = opts.pdfJSWorkerSrc
+    }
+
+    const pdf = await PDFJS.getDocument(pdfData as TypedArray).promise
+    return extractDataFromPDFDocument(pdf, title)
 }
