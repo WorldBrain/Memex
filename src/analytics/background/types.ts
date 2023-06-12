3154e642d6070ea384bae832a0af67d65f8ca113
Vincent den Boer <vincent@youapt.eu>
2023-06-12 16:20:13 +0200

    Expose analytics function to content script

--- src/analytics/background/types.ts
+++ src/analytics/background/types.ts
@@ -1,7 +1,9 @@
+import { ClientAnalyticsEvent } from '@worldbrain/memex-common/lib/analytics/types'
 import { Analytics } from '../types'
 import { updateLastActive } from '../utils'
 
 export interface AnalyticsInterface {
     trackEvent: Analytics['trackEvent']
+    trackBqEvent(event: ClientAnalyticsEvent): Promise<void>
     updateLastActive: typeof updateLastActive
 }
