760d7d46e13ca5b10a31d67e56345d36dc60b33b
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-06-22 16:14:16 +0700

    Don't init raven-js on FF
    - Traced a bug with the annotation create/edit editor not working on FF back to this

--- src/util/check-browser.ts
+++ src/util/check-browser.ts
@@ -2,6 +2,9 @@ import browser from 'webextension-polyfill'
 
 export type BrowserName = 'chrome' | 'firefox' | 'brave'
 
+/**
+ * NOTE: Does not work from the content-script.
+ */
 function checkBrowser(): BrowserName {
     // `runtime.getBrowserInfo` is only available on FF web ext API
     if (typeof browser.runtime.getBrowserInfo !== 'undefined') {
