7004c6582e7d966dd8387052e78117beaa264b0e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-08-29 12:26:06 +0700

    Write tests for OG tags rendered for different link types

--- src/social-previews/cloud-functions.test.ts
+++ src/social-previews/cloud-functions.test.ts
@@ -1,6 +1,11 @@
 import { setupBackgroundIntegrationTest } from 'src/tests/background-integration-tests'
 import { renderTemplate } from '@worldbrain/memex-common/lib/firebase-backend/social-previews/html-template'
 import { SocialPreviewHTMLRenderer } from '@worldbrain/memex-common/lib/firebase-backend/social-previews/html-renderer'
+import {
+    LIST_DESCRIPTION_FALLBACK,
+    PAGE_DESCRIPTION_FALLBACK,
+    PAGE_TITLE_FALLBACK,
+} from '@worldbrain/memex-common/lib/firebase-backend/social-previews/constants'
 import type {
     SharedAnnotation,
     SharedList,
@@ -13,7 +18,12 @@ import {
     generateListPreviewUrl,
     generatePagePreviewUrl,
 } from '@worldbrain/memex-common/lib/firebase-backend/social-previews/utils'
-import { createPageLinkListTitle } from 'src/content-sharing/utils'
+import {
+    createPageLinkListTitle,
+    getListShareUrl,
+    getNoteShareUrl,
+    getSinglePageShareUrl,
+} from 'src/content-sharing/utils'
 
 const TEST_BUNDLES_STR = 'test-1.js test-2.js test-1.css'
 const TEST_BUNDLES_ARR = TEST_BUNDLES_STR.split(' ')
@@ -231,5 +241,205 @@ describe('Social previews tests', () => {
         expect(
             await htmlRenderer.renderPagePreview({ listId, listEntryId }),
         ).toEqual(renderTemplate({ bundleSrcs: TEST_BUNDLES_ARR }))
+    })
+
+    it('should render HTML correct OG tags for annotation', async () => {
+        const { storageManager, htmlRenderer } = await setupTest({})
+
+        const now = Date.now()
+        const sharedPageInfoData: SharedPageInfo & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 1,
+            creator: 1,
+            originalUrl: 'https://test.com/test',
+            normalizedUrl: 'test.com/test',
+            fullTitle: 'Test page title',
+            createdWhen: now,
+            updatedWhen: now,
+        }
+        const sharedAnnotationData: SharedAnnotation & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 1,
+            creator: 1,
+            normalizedPageUrl: sharedPageInfoData.normalizedUrl,
+            comment: 'test comment',
+            createdWhen: now,
+            updatedWhen: now,
+            uploadedWhen: now,
+        }
+
+        await storageManager
+            .collection('sharedPageInfo')
+            .createObject(sharedPageInfoData)
+        await storageManager
+            .collection('sharedAnnotation')
+            .createObject(sharedAnnotationData)
+
+        const ogImgSrc = generateAnnotationPreviewUrl({
+            pageInfo: sharedPageInfoData,
+            annotation: sharedAnnotationData,
+        })
+
+        const renderedHtmlA = await htmlRenderer.renderAnnotationPreview({
+            annotationId: sharedAnnotationData.id,
+        })
+
+        // prettier-ignore
+        {
+        expect(renderedHtmlA.includes(`<meta property="twitter:image" content="${ogImgSrc}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="twitter:title" content="${sharedPageInfoData.fullTitle}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:image" content="${ogImgSrc}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:title" content="${sharedPageInfoData.fullTitle}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:description" content="${sharedPageInfoData.originalUrl}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:url" content="${getNoteShareUrl({ remoteAnnotationId: sharedAnnotationData.id.toString() })}" />`)).toBe(true)
+        }
+    })
+
+    it('should render HTML correct OG tags for a list', async () => {
+        const { storageManager, htmlRenderer } = await setupTest({})
+
+        const now = Date.now()
+        const sharedListDataA: SharedList & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 1,
+            creator: 1,
+            title: 'my test list A',
+            description: 'great list',
+            createdWhen: now,
+            updatedWhen: now,
+        }
+        const sharedListDataB: SharedList & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 2,
+            creator: 1,
+            title: 'my test list B',
+            description: undefined, // NOTE: No description
+            createdWhen: now,
+            updatedWhen: now,
+        }
+
+        for (const sharedList of [sharedListDataA, sharedListDataB]) {
+            await storageManager
+                .collection('sharedList')
+                .createObject(sharedList)
+        }
+
+        const ogImgSrcA = generateListPreviewUrl({ list: sharedListDataA })
+        const ogImgSrcB = generateListPreviewUrl({ list: sharedListDataB })
+        const renderedHtmlA = await htmlRenderer.renderListPreview({
+            listId: sharedListDataA.id,
+        })
+        const renderedHtmlB = await htmlRenderer.renderListPreview({
+            listId: sharedListDataB.id,
+        })
+
+        // prettier-ignore
+        {
+        expect(renderedHtmlA.includes(`<meta property="twitter:image" content="${ogImgSrcA}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="twitter:title" content="${sharedListDataA.title}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:image" content="${ogImgSrcA}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:title" content="${sharedListDataA.title}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:description" content="${sharedListDataA.description}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:url" content="${getListShareUrl({ remoteListId: sharedListDataA.id.toString() })}" />`)).toBe(true)
+
+        expect(renderedHtmlB.includes(`<meta property="twitter:image" content="${ogImgSrcB}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="twitter:title" content="${sharedListDataB.title}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:image" content="${ogImgSrcB}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:title" content="${sharedListDataB.title}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:description" content="${LIST_DESCRIPTION_FALLBACK}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:url" content="${getListShareUrl({ remoteListId: sharedListDataB.id.toString() })}" />`)).toBe(true)
+        }
+    })
+
+    it('should render HTML correct OG tags for a page', async () => {
+        const { storageManager, htmlRenderer } = await setupTest({})
+
+        const now = Date.now()
+        const sharedListData: SharedList & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 1,
+            creator: 1,
+            title: createPageLinkListTitle(new Date(now)),
+            type: 'page-link',
+            createdWhen: now,
+            updatedWhen: now,
+        }
+        const sharedListEntryDataA: SharedListEntry & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 1,
+            creator: 1,
+            normalizedUrl: 'test.com/test',
+            originalUrl: 'https://test.com/test',
+            entryTitle: 'test page title',
+            createdWhen: now,
+            updatedWhen: now,
+        }
+        const sharedListEntryDataB: SharedListEntry & {
+            id: AutoPk
+            creator: AutoPk
+        } = {
+            id: 2,
+            creator: 1,
+            normalizedUrl: 'test.com/test2',
+            originalUrl: 'https://test.com/test2',
+            entryTitle: undefined, // NOTE: No page title
+            createdWhen: now,
+            updatedWhen: now,
+        }
+
+        await storageManager
+            .collection('sharedList')
+            .createObject(sharedListData)
+        await storageManager
+            .collection('sharedListEntry')
+            .createObject(sharedListEntryDataA)
+        await storageManager
+            .collection('sharedListEntry')
+            .createObject(sharedListEntryDataB)
+
+        const ogImgSrcA = generatePagePreviewUrl({
+            listEntry: sharedListEntryDataA,
+        })
+        const ogImgSrcB = generatePagePreviewUrl({
+            listEntry: sharedListEntryDataB,
+        })
+
+        const renderedHtmlA = await htmlRenderer.renderPagePreview({
+            listId: sharedListData.id,
+            listEntryId: sharedListEntryDataA.id,
+        })
+        const renderedHtmlB = await htmlRenderer.renderPagePreview({
+            listId: sharedListData.id,
+            listEntryId: sharedListEntryDataB.id,
+        })
+
+        // prettier-ignore
+        {
+        expect(renderedHtmlA.includes(`<meta property="twitter:image" content="${ogImgSrcA}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="twitter:title" content="${sharedListEntryDataA.entryTitle}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:image" content="${ogImgSrcA}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:title" content="${sharedListEntryDataA.entryTitle}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:description" content="${PAGE_DESCRIPTION_FALLBACK}" />`)).toBe(true)
+        expect(renderedHtmlA.includes(`<meta property="og:url" content="${getSinglePageShareUrl({ remoteListId: sharedListData.id, remoteListEntryId: sharedListEntryDataA.id })}" />`)).toBe(true)
+
+        expect(renderedHtmlB.includes(`<meta property="twitter:image" content="${ogImgSrcB}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="twitter:title" content="${PAGE_TITLE_FALLBACK}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:image" content="${ogImgSrcB}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:title" content="${PAGE_TITLE_FALLBACK}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:description" content="${PAGE_DESCRIPTION_FALLBACK}" />`)).toBe(true)
+        expect(renderedHtmlB.includes(`<meta property="og:url" content="${getSinglePageShareUrl({ remoteListId: sharedListData.id, remoteListEntryId: sharedListEntryDataB.id })}" />`)).toBe(true)
+        }
     })
 })
