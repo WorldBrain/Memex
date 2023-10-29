a156bcf5e62f40fecb8da8b8837670403e06e007
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2023-10-29 14:19:43 +0700

    Remove sortFn from tree util fns
    - This was meant to afford us ordering the children according to their order field values, though we can just do that in `getChildren`

--- src/custom-lists/tree-utils.ts
+++ src/custom-lists/tree-utils.ts
@@ -1,35 +1,37 @@
 interface TreeTraverseArgs<T, CBReturn> {
     root: T
     strategy?: 'dfs' | 'bfs'
-    cb: (node: T) => CBReturn
-    sortFn?: (a: T, b: T) => number
+    cb: (node: T, index: number) => CBReturn
     getChildren: (node: T) => T[]
 }
 
+/**
+ * Assumes no cycles.
+ */
 export function forEachTree<T>(args: TreeTraverseArgs<T, void>): void {
     mapTree(args)
 }
 
+/**
+ * Assumes no cycles.
+ */
 export function mapTree<T, Return>({
     cb,
     root,
-    sortFn,
     getChildren,
     strategy = 'bfs',
 }: TreeTraverseArgs<T, Return>): Return[] {
     const returnVal: Return[] = []
 
     const pendingNodes = [root]
+    let i = 0
     while (pendingNodes.length) {
         const currentNode =
             strategy === 'bfs' ? pendingNodes.shift() : pendingNodes.pop()
 
-        returnVal.push(cb(currentNode))
+        returnVal.push(cb(currentNode, i++))
 
         const children = getChildren(currentNode)
-        if (sortFn) {
-            children.sort(sortFn)
-        }
         pendingNodes.push(...children)
     }
 
