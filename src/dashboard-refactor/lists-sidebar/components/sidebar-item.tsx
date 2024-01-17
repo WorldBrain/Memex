ceaca1a2c400f6f79a45e27eb537bd8ddddec9d4
Oliver Sauter <oli@worldbrain.io>
2024-01-17 18:48:16 +0100

    fix type issues

--- src/dashboard-refactor/lists-sidebar/components/sidebar-item.tsx
+++ src/dashboard-refactor/lists-sidebar/components/sidebar-item.tsx
@@ -22,6 +22,7 @@ export interface Props {
     areAnyMenusDisplayed?: boolean
     forceRightSidePermanentDisplay?: boolean
     spaceSidebarWidth: string
+    sidebarItemRef?: (el: any) => void
 }
 
 export interface State {
