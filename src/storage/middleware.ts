ec9ac999b20265b72bd22dac9c544e80dc5d0877
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-01-19 15:25:55 +0700

    Place list tree middleware before change watch middleware
    - These are interdependent and never realized the ordering was a problem until properly manually testing now

--- src/storage/middleware.ts
+++ src/storage/middleware.ts
@@ -112,8 +112,8 @@ export function setStorageMiddleware(
             createOperationLoggingMiddleware({
                 shouldLog: () => shouldLogStorageOperations,
             }),
-            changeWatchMiddleware,
             listTreeMiddleware,
+            changeWatchMiddleware,
         ]),
     )
 
