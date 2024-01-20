17c2818e5909165611f4620369c6f5e464bdcdc9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-01-20 16:18:51 +0700

    Update to use new shared list tree op watcher

--- src/storage/middleware.ts
+++ src/storage/middleware.ts
@@ -2,19 +2,15 @@ import type StorageManager from '@worldbrain/storex'
 import type { StorageMiddleware } from '@worldbrain/storex/lib/types/middleware'
 import type { StorexHubBackground } from 'src/storex-hub/background'
 import type ContentSharingBackground from 'src/content-sharing/background'
-import type {
-    RawStorageOperationWatcher,
-    StorageOperationEvent,
-} from '@worldbrain/storex-middleware-change-watcher/lib/types'
+import type { StorageOperationEvent } from '@worldbrain/storex-middleware-change-watcher/lib/types'
 import type { PersonalCloudBackground } from 'src/personal-cloud/background'
 import type CustomListBackground from 'src/custom-lists/background'
 import { WATCHED_COLLECTIONS } from './constants'
 import { ChangeWatchMiddleware } from '@worldbrain/storex-middleware-change-watcher'
 import {
     ListTreeMiddleware,
-    LIST_TREE_OPERATION_ALIASES,
+    initListTreeOperationWatchers,
 } from '@worldbrain/memex-common/lib/content-sharing/storage/list-tree-middleware'
-import { PersonalCloudUpdateType } from '@worldbrain/memex-common/lib/personal-cloud/backend/types'
 
 export function setStorageMiddleware(
     storageManager: StorageManager,
@@ -55,46 +51,12 @@ export function setStorageMiddleware(
             options.contentSharing.performDeleteListAndAllAssociatedData(args),
     })
 
-    // Set up custom operation watchers for list tree ops to set them up differently for cloud sync
-    const listTreeOperationWatchers: {
-        [name: string]: RawStorageOperationWatcher
-    } = {
-        [LIST_TREE_OPERATION_ALIASES.moveTree]: async ([
-            opName,
-            collection,
-            args,
-            { skipSync } = { skipSync: false },
-        ]) => {
-            if (skipSync) {
-                return { shouldExecuteNextMiddleware: false }
-            }
-            await options.personalCloud?.handleListTreeStorageChange({
-                type: PersonalCloudUpdateType.ListTreeMove,
-                parentLocalListId: args.newParentListId,
-                rootNodeLocalListId: args.localListId,
-            })
-            return { shouldExecuteNextMiddleware: true }
-        },
-        [LIST_TREE_OPERATION_ALIASES.deleteTree]: async ([
-            opName,
-            collection,
-            args,
-            { skipSync } = { skipSync: false },
-        ]) => {
-            if (skipSync) {
-                return { shouldExecuteNextMiddleware: false }
-            }
-            await options.personalCloud?.handleListTreeStorageChange({
-                type: PersonalCloudUpdateType.ListTreeDelete,
-                rootNodeLocalListId: args.localListId,
-            })
-            return { shouldExecuteNextMiddleware: true }
-        },
-    }
-
     const changeWatchMiddleware = new ChangeWatchMiddleware({
         storageManager,
-        rawOperationWatchers: listTreeOperationWatchers,
+        rawOperationWatchers: initListTreeOperationWatchers({
+            handleListTreeStorageChange: (update) =>
+                options.personalCloud?.handleListTreeStorageChange(update),
+        }),
         shouldWatchCollection: (collection) =>
             watchedCollections.has(collection),
         postprocessOperation: async (event) => {
