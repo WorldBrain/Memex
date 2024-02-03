8475192aae84d9bfceeaaa1dc7cb98c590b9fc7f
Oliver <oli@worldbrain.io>
2024-02-03 17:02:31 +0100

    Fix tooltips for gpt and summary functionality

--- src/sidebar/annotations-sidebar/components/AnnotationsSidebar.tsx
+++ src/sidebar/annotations-sidebar/components/AnnotationsSidebar.tsx
@@ -2154,6 +2154,9 @@ export class AnnotationsSidebar extends React.Component<
                                                 : 'summarize'
                                         }
                                         keepSelectedState
+                                        getRootElement={
+                                            this.props.getRootElement
+                                        }
                                     />
                                 )}
                                 <DropdownMenuBtnSmall
@@ -2186,6 +2189,7 @@ export class AnnotationsSidebar extends React.Component<
                                     }}
                                     initSelectedItem={'gpt-3.5-turbo-1106'}
                                     keepSelectedState
+                                    getRootElement={this.props.getRootElement}
                                 />
 
                                 {!this.props.fullPageUrl.includes(
