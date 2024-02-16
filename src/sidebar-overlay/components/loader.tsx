b0a5a1565676584f4a1b9eb87bba007809f43427
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-02-16 15:09:26 +0700

    Remove ancient sidebar-overlay UIs
    - Haven't been used in many years. Getting in the way

--- src/sidebar-overlay/components/loader.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-
-const styles = require('./loader.css')
-
-/* tslint:disable-next-line variable-name */
-const Loader = () => <div className={'loader'}>Loading...</div>
-
-export default Loader
