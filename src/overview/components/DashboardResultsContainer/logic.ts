2522d9dfc7a98ec1e6c4dacf76548b076bf3cff2
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-02-26 19:37:58 +0700

    Remove unneeded dashboard->in-page-ui dep
    - Looks like we set this up but have never used this

--- src/overview/components/DashboardResultsContainer/logic.ts
+++ src/overview/components/DashboardResultsContainer/logic.ts
@@ -1,28 +1,14 @@
 import { UILogic } from 'ui-logic-core'
-import {
+import type {
     DashboardResultsDependencies,
     DashboardResultsEvent,
     DashboardResultsState,
 } from 'src/overview/components/DashboardResultsContainer/types'
-import { EventEmitter } from 'events'
-import TypedEventEmitter from 'typed-emitter'
-import { SharedInPageUIEvents } from 'src/in-page-ui/shared-state/types'
-import { SharedInPageUIState } from 'src/in-page-ui/shared-state/shared-in-page-ui-state'
 
 export default class DashboardResultsLogic extends UILogic<
     DashboardResultsState,
     DashboardResultsEvent
 > {
-    overviewUIEvents = new EventEmitter() as TypedEventEmitter<
-        SharedInPageUIEvents
-    >
-
-    dashboardUI = new SharedInPageUIState({
-        loadComponent: (c) => null,
-        unloadComponent: (c) => null,
-        getNormalizedPageUrl: () => '',
-    })
-
     constructor(private dependencies: DashboardResultsDependencies) {
         super()
     }
@@ -33,30 +19,4 @@ export default class DashboardResultsLogic extends UILogic<
             readerUrl: null,
         }
     }
-
-    getDashboardUILogic = () => {}
-
-    handleReaderViewClick = ({ event }) => {
-        this.emitMutation({
-            readerShow: { $set: true },
-            readerUrl: { $set: event },
-        })
-        // testing
-        this.overviewUIEvents.emit('stateChanged', {
-            changes: { sidebar: true },
-            newState: null,
-        })
-    }
-    handleToggleAnnotationsSidebar = ({
-        event: { pageUrl, pageTitle },
-        prevState,
-    }) => {
-        this.dashboardUI.showSidebar()
-    }
-
-    handleReaderClose = ({ event }) =>
-        this.emitMutation({
-            readerShow: { $set: false },
-            readerUrl: { $set: null },
-        })
 }
