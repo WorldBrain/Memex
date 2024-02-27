ec2c6ae71a0d303beb3b7fe148e6512f915c5a03
Oliver Sauter <oli@worldbrain.io>
2024-02-27 21:28:15 +0100

    fix up type errors

--- src/annotations/components/AnnotationFooter.tsx
+++ src/annotations/components/AnnotationFooter.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export interface AnnotationFooterEventProps {
     onDeleteConfirm: React.MouseEventHandler
     onDeleteCancel: React.MouseEventHandler
-    onDeleteIconClick: React.MouseEventHandler
+    onDeleteIconClick: (instaDelete?: boolean) => void
     onEditIconClick: React.MouseEventHandler
     onEditHighlightIconClick: React.MouseEventHandler
     onShareClick: React.MouseEventHandler
