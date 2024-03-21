f2a54790fc5990d3619655a9fa97d492645f1867
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-03-21 11:07:01 +0700

    Move back to NPM turndown (again!)
    - Realized I could just pass in a Document obj to turndown instead of a string, thus didn't need to supply my own HTMLParser implementation

--- src/background-script/html-to-markdown.ts
+++ src/background-script/html-to-markdown.ts
@@ -1,4 +1,4 @@
-import TurndownService from 'turndown/src/turndown'
+import TurndownService from 'turndown'
 import { parseHTML } from 'linkedom/worker'
 
 // TOOD: Refactor this into a service which can instantiated then passed down into BG modules
@@ -9,19 +9,14 @@ export function htmlToMarkdown(
     if (!html?.length) {
         return ''
     }
+    const { document: doc } = parseHTML(html)
 
     const turndownService = new TurndownService({
         headingStyle: 'atx',
         hr: '---',
         codeBlockStyle: 'fenced',
-        htmlParser: {
-            parseFromString: (html: string) => {
-                const { document } = parseHTML(html)
-                return document
-            },
-        },
     })
     applyCustomRules?.(turndownService)
-    const markdown = turndownService.turndown(html)
+    const markdown = turndownService.turndown(doc)
     return markdown
 }
