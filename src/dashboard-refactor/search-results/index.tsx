734ecc9ad5d860baaf9ea559cf2adedfe39f8cac
Oliver Sauter <oli@worldbrain.io>
2024-04-26 10:39:23 +0200

    fix bug with searchterms being empty

--- src/dashboard-refactor/search-results/index.tsx
+++ src/dashboard-refactor/search-results/index.tsx
@@ -761,9 +761,8 @@ export default class SearchResultsContainer extends React.Component<
                     searchType={this.props.searchType}
                     isInFocus={page.isInFocus}
                     uploadedPdfLinkLoadState={page.uploadedPdfLinkLoadState}
-                    searchQuery={this.props.searchQuery}
                     searchTerms={this.props.searchQuery
-                        .split(' ')
+                        ?.split(' ')
                         .filter((term) => term.trim() !== '')}
                     renderSpacePicker={() => (
                         <CollectionPicker
