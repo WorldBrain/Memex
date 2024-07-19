e95a6c13eaf5c42812612971ee67f21ccde0e8f9
Oliver <oli@worldbrain.io>
2024-07-19 17:14:56 +0200

    function to try and extract audio data

--- src/in-page-ui/background/index.ts
+++ src/in-page-ui/background/index.ts
@@ -8,6 +8,8 @@ import { OVERVIEW_URL } from 'src/constants'
 import { checkStripePlan } from '@worldbrain/memex-common/lib/subscriptions/storage'
 
 import { blobToDataURL } from 'src/util/blob-utils'
+import { pipeline, env } from '@xenova/transformers'
+// import * as ort from 'onnxruntime-web'
 
 export const CONTEXT_MENU_ID_PREFIX = '@memexContextMenu:'
 export const CONTEXT_MENU_HIGHLIGHT_ID =
@@ -31,6 +33,7 @@ export class InPageUIBackground {
             showSidebar: bindMethod(this, 'showSidebar'),
             openDashboard: bindMethod(this, 'openDashboard'),
             getCurrentTabURL: bindMethod(this, 'getCurrentTabURL'),
+            transcribeAudioUrl: bindMethod(this, 'transcribeAudioUrl'),
             checkStripePlan: bindMethod(this, 'checkStripePlan'),
             updateContextMenuEntries: bindMethod(
                 this,
@@ -115,6 +118,38 @@ export class InPageUIBackground {
     async checkStripePlan(email: string) {
         await checkStripePlan(email, this.options.browserAPIs)
     }
+    async transcribeAudioUrl(url2: string) {
+        console.log('url', url2)
+
+        // Create a video element
+        const video = document.createElement('video')
+        video.src = url2
+        video.crossOrigin = 'anonymous' // Ensure CORS is handled
+
+        // Initialize the audio context
+        const audioContext = new window.AudioContext()
+        const mediaSource = audioContext.createMediaElementSource(video)
+        const analyser = audioContext.createAnalyser()
+        mediaSource.connect(analyser)
+        analyser.connect(audioContext.destination)
+
+        // Play the video to start processing the audio
+        video.play()
+
+        // Function to get sound data
+        function getSoundData() {
+            const sample = new Float32Array(analyser.frequencyBinCount)
+            analyser.getFloatFrequencyData(sample)
+            return sample
+        }
+
+        // Example usage: log sound data after a delay
+        setTimeout(() => {
+            const soundData = getSoundData()
+            console.log('Sound Data:', soundData)
+        }, 5000) // Adjust the delay as needed
+    }
+
     async getCurrentTabURL() {
         const tabs = await this.options.tabsAPI.query({
             active: true,
@@ -153,4 +188,35 @@ export class InPageUIBackground {
         runInTab<InPageUIContentScriptRemoteInterface>(
             tabId,
         ).analyseImageAsWithAI(imageData)
+}
+
+class PipelineSingleton {
+    static task = 'text-classification'
+    static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english'
+    // static task: any = 'automatic-speech-recognition' //'text-classification';
+    // static model_name_or_path = 'onnx-community/whisper-base_timestamped' //'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
+    static quantized = true
+    static instance = null
+    static model_buffer = null
+    static tokenizer = null
+
+    static initializeEnv() {
+        env.allowLocalModels = false //this is a must and if it's true by default for the first time, wrong data is cached to keep failing after this line is added, until the cache is cleared in browser!
+        // Due to a bug in onnxruntime-web, we must disable multithreading for now.
+        // See https://github.com/microsoft/onnxruntime/issues/14445 for more information.
+        env.backends.onnx.wasm.numThreads = 1
+    }
+
+    static async getInstance(progress_callback = null) {
+        if (this.instance === null) {
+            this.initializeEnv()
+            console.log('pipeline')
+            this.instance = pipeline(this.task, this.model_name_or_path, {
+                quantized: this.quantized,
+                progress_callback /*more options: https://huggingface.co/docs/transformers.js/api/utils/hub#module_utils/hub..PretrainedOptions*/,
+            })
+        }
+
+        return this.instance
+    }
 }
--- src/in-page-ui/background/types.ts
+++ src/in-page-ui/background/types.ts
@@ -4,6 +4,7 @@ export interface InPageUIInterface<Role extends RemoteFunctionRole> {
     showSidebar: RemoteFunction<Role, {}>
     openDashboard: RemoteFunction<Role, void>
     getCurrentTabURL: () => Promise<string>
+    transcribeAudioUrl: (url: string) => Promise<string>
     checkStripePlan: (email: string) => Promise<void>
     updateContextMenuEntries: RemoteFunction<Role, void>
 }
